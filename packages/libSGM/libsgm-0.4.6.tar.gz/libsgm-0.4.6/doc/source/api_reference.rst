bea58dcb4571c1e988c85b5035839acf348124f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:03:26 +0000

    Add package libSGM/0.4.6/libsgm-0.4.6.tar.gz

---
+++ packages/libSGM/libsgm-0.4.6.tar.gz/libsgm-0.4.6/doc/source/api_reference.rst
@@ -0,0 +1,10 @@
+Api Reference
+===============
+
+
+
+.. toctree::
+
+    api_reference/sgm_wrapper
+    api_reference/library_root
+    api_reference/libsgm_python/index
---
+++ packages/libSGM/libsgm-0.4.6.tar.gz/libsgm-0.4.6/setup.py
@@ -0,0 +1,66 @@
+# type:ignore
+# pylint: disable=ungrouped-imports
+#!/usr/bin/env python
+# coding: utf8
+#
+# Copyright (c) 2024 Centre National d'Etudes Spatiales (CNES).
+#
+# This file is part of LIBSGM
+#
+#     https://github.com/CNES/Pandora_libsgm
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""
+This module contains the required libraries and softwares allowing to execute the software,
+and setup elements to configure and identify the software.
+"""
+
+import os
+import shutil
+from codecs import open as copen
+import numpy
+from setuptools import Extension, setup
+
+SCR_DIR = "src/libSGM"
+
+sources = [SCR_DIR + "/lib/sgm.cpp", SCR_DIR + "/sgm_wrapper.pyx"]
+
+ext_1 = Extension(
+    "libSGM.sgm_wrapper",
+    sources,
+    language="c++",
+    library_dirs=[],
+    libraries=[],
+    include_dirs=[numpy.get_include(), SCR_DIR + "/lib/"],
+    extra_compile_args=["-O3", "-fopenmp", "-std=c++11"],
+    extra_link_args=["-lgomp"],
+)
+
+extensions = [ext_1]
+
+os.environ["CC"] = shutil.which("gcc")
+os.environ["CXX"] = shutil.which("g++")
+
+
+def readme():
+    with copen("README.md", "r", "utf-8") as fstream:
+        return fstream.read()
+
+
+setup(
+    use_scm_version=True,
+    long_description=readme(),
+    zip_safe=False,
+    ext_modules=extensions,
+)
