724bebd4c5bf392ab62047feda717180ca3a4815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:03:08 +0000

    Add package astmain/0.0.89/astmain-0.0.89-py2.py3-none-any.whl

---
+++ packages/astmain/astmain-0.0.89-py2.py3-none-any.whl/astmain/__init__.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+from fs import *
+# 数据转化
+from to import *
+from desco import *
+
+
+# 操作系统指令
+# from ..cmd_exe import run
+
+
+def test():
+    print("test            :", 15160)
---
+++ packages/astmain/astmain-0.0.89-py2.py3-none-any.whl/astmain/desco/__init__.py
@@ -0,0 +1,8 @@
+# from ..fs import write_add
+# import ..fs as aaa
+
+# import importlib
+# importlib.import_module("requests")
+
+from .try_error import try_error
+from .. import fs as fffsss
