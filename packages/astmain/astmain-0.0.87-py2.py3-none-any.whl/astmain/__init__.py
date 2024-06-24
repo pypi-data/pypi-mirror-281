6b6294ee88c852e1a2fb42836346296bc0095922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:03:10 +0000

    Add package astmain/0.0.87/astmain-0.0.87-py2.py3-none-any.whl

---
+++ packages/astmain/astmain-0.0.87-py2.py3-none-any.whl/astmain/__init__.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+# 文件操作
+# from .fs import delete
+# from .fs import read
+# from .fs import write
+# from .fs import write
+# from .fs import cwd
+
+
+from ..fs import *
+
+# 数据转化
+from ..to import num
+
+# 操作系统指令
+# from ..cmd_exe import run
+
+
+def test():
+    print("test            :", 15160)
