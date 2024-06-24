73cd0eefe6d78cd0295c6ba0c687f17a87d1d500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:02:58 +0000

    Add package geolet-reloaded/0.0.1.post3/geolet-reloaded-0.0.1.post3.tar.gz

---
+++ packages/geolet-reloaded/geolet-reloaded-0.0.1.post3.tar.gz/geolet-reloaded-0.0.1.post3/geoletrld/partitioners/NoPartitioner.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+import numpy as np
+
+from geoletrld.partitioners.PartitionerInterface import PartitionerInterface
+from geoletrld.utils.Trajectories import Trajectories
+from geoletrld.utils.Trajectory import Trajectory
+
+
+class NoPartitioner(PartitionerInterface):
+    def transform(self, X: Trajectories[Any, Trajectory]) -> Trajectories[Any, Trajectory]:
+        return_val = Trajectories()
+        for i, (k, v) in enumerate(X.items()):
+            return_val[f"{i}_{k}"] = v.copy()
+
+        return return_val
