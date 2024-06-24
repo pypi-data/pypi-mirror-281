184eaa18fa456f6b849bab78a1b577293520fc40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:03:25 +0000

    Add package SatyrScheduler/0.0.1/satyrscheduler-0.0.1.tar.gz

---
+++ packages/SatyrScheduler/satyrscheduler-0.0.1.tar.gz/satyrscheduler-0.0.1/src/SatyrScheduler/GetData/GetData.py
@@ -0,0 +1,23 @@
+#Python specific packages
+import os
+
+#pip imported packages
+
+
+#Self created dependencies
+import SongData
+import SketchData
+
+
+def GetData(lstCSV):
+	list_Objects = []
+	for csvFile in lstCSV:
+		if(os.path.splitext(csvFile) == "song.csv"):
+			myClass = SongData(csvFile)
+		if(os.path.splitext(csvFile) == "sketch.csv"):
+			myClass = SketchData(csvFile)
+		list_Objects.append(myClass)
+	if not list_Objects:
+		print("ERROR:\n\tNo .csv files was detected was empty!\n\tMake sure to have a song.csv or/and song.csv file withing your main program.")
+		raise RuntimeError("list of valid .csv file is empty")
+	return list_Objects
