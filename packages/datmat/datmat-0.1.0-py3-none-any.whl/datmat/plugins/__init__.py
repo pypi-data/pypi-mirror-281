1c4959e886a983464e1b740dae0857ef7bf57f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-24 14:02:54 +0000

    Add package datmat/0.1.0/datmat-0.1.0-py3-none-any.whl

---
+++ packages/datmat/datmat-0.1.0-py3-none-any.whl/datmat/plugins/__init__.py
@@ -0,0 +1,6 @@
+from .commaseperatedvaluefile import CommaSeperatedValueFile
+from .filesystem import FileSystem
+from .httpplugin import HTTPPlugin
+from .xnatstorage import XNATStorage
+from .null import Null
+from .eucaim import EUCaimDirectory
