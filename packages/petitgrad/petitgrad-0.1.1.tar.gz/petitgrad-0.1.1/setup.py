7fed8646baa2d4d048320d9c38c8e32ef9267367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:02:55 +0000

    Add package petitgrad/0.1.1/petitgrad-0.1.1.tar.gz

---
+++ packages/petitgrad/petitgrad-0.1.1.tar.gz/petitgrad-0.1.1/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="petitgrad",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+    ],
+    tests_require=[
+        "unittest",
+    ],
+    author="Burak Ç.",
+    author_email="bcivitcioglu@gmail.com",
+    description="A tiny or petit autograd engine with matrix operations, based on Andrej Karpathy's micrograd.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/bcivitcioglu/petitgrad",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
