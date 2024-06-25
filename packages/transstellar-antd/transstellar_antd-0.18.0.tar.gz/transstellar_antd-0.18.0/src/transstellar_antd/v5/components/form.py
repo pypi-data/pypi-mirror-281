66dfb761efc67e55965e93584a12db45aca49b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:00 +0000

    Add package transstellar-antd/0.18.0/transstellar_antd-0.18.0.tar.gz

---
+++ packages/transstellar-antd/transstellar_antd-0.18.0.tar.gz/transstellar_antd-0.18.0/src/transstellar_antd/v5/components/form.py
@@ -0,0 +1,17 @@
+from ...components import Form as BaseForm
+from .checkbox import Checkbox
+from .form_item import FormItem
+from .input import Input
+from .select import Select
+from .switch import Switch
+from .text_area import TextArea
+
+
+class Form(BaseForm):
+    INPUT_CLASS = Input
+    TEXT_AREA_CLASS = TextArea
+    SELECT_CLASS = Select
+    SWITCH_CLASS = Switch
+    CHECKBOX_CLASS = Checkbox
+
+    pass
