9bd0caa5a718aa755fbf9f06691d1b42df6973a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:02:56 +0000

    Add package morebuiltins/0.0.7/morebuiltins-0.0.7-py3-none-any.whl

---
+++ packages/morebuiltins/morebuiltins-0.0.7-py3-none-any.whl/morebuiltins/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.7"
+__all__ = [
+    "morebuiltins.utils",
+    "morebuiltins.date",
+    "morebuiltins.functools",
+    "morebuiltins.ipc",
+    "morebuiltins.request",
+    "morebuiltins.download_python",
+]
