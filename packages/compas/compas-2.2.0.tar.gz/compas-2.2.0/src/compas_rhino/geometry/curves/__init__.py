f54f91f78a6ef9de8dead62bfabfd2ef2cfee7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:50 +0000

    Add package compas/2.2.0/compas-2.2.0.tar.gz

---
+++ packages/compas/compas-2.2.0.tar.gz/compas-2.2.0/src/compas_rhino/geometry/curves/__init__.py
@@ -0,0 +1,43 @@
+from .curve import RhinoCurve
+from .nurbs import RhinoNurbsCurve
+
+from compas.plugins import plugin
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_curve(cls, *args, **kwargs):
+    curve = object.__new__(RhinoCurve)
+    curve.__init__(*args, **kwargs)
+    return curve
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_nurbscurve(cls, *args, **kwargs):
+    curve = object.__new__(RhinoNurbsCurve)
+    curve.__init__(*args, **kwargs)
+    return curve
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_nurbscurve_from_native(cls, *args, **kwargs):
+    return RhinoNurbsCurve.from_rhino(*args, **kwargs)
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_nurbscurve_from_parameters(cls, *args, **kwargs):
+    return RhinoNurbsCurve.from_parameters(*args, **kwargs)
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_nurbscurve_from_points(cls, *args, **kwargs):
+    return RhinoNurbsCurve.from_points(*args, **kwargs)
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_nurbscurve_from_interpolation(cls, *args, **kwargs):
+    return RhinoNurbsCurve.from_interpolation(*args, **kwargs)
+
+
+@plugin(category="factories", requires=["Rhino"])
+def new_nurbscurve_from_step(cls, *args, **kwargs):
+    return RhinoNurbsCurve.from_step(*args, **kwargs)
