502f31af2024bda04bd532c2b57a97362fb56473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:00 +0000

    Add package folktexts/0.0.13/folktexts-0.0.13-py3-none-any.whl

---
+++ packages/folktexts/folktexts-0.0.13-py3-none-any.whl/folktexts/__init__.py
@@ -0,0 +1,5 @@
+from ._version import __version__, __version_info__
+from .task import TaskMetadata
+from .benchmark import BenchmarkConfig, CalibrationBenchmark
+from .classifier import LLMClassifier
+from .acs import ACSDataset, ACSTaskMetadata
