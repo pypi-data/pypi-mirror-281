6960e5e4e32a40c50c1d84fb98aae88e15a278b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:00 +0000

    Add package collibra-assessments-100/1.0.0/collibra_assessments_100-1.0.0-py3-none-any.whl

---
+++ packages/collibra-assessments-100/collibra_assessments_100-1.0.0-py3-none-any.whl/collibra_assessments/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_assessments.api.assessments_api import AssessmentsApi
+from collibra_assessments.api.templates_api import TemplatesApi
