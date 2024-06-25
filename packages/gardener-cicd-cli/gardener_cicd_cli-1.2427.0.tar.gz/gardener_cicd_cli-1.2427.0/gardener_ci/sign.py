8eb815c3da0ee4e4e14f5a4b586eba23ba6274c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:15 +0000

    Add package gardener-cicd-cli/1.2427.0/gardener_cicd_cli-1.2427.0.tar.gz

---
+++ packages/gardener-cicd-cli/gardener_cicd_cli-1.2427.0.tar.gz/gardener_cicd_cli-1.2427.0/gardener_ci/sign.py
@@ -0,0 +1,37 @@
+import signingserver
+
+
+def sign(
+    filepath: str,
+    signing_server_url: str,
+    signing_server_client_cert: str,
+    signing_server_client_cert_key: str,
+    output: str='signature',
+    signing_server_certificate_ca: str=None,
+    signing_algorithm: str='rsassa-pkcs1-v1_5',
+    tls_validation: bool=True,
+):
+    with open(filepath, 'rb') as f:
+        payload_bytes = f.read()
+
+    signingserver_client = signingserver.SigningserverClient(
+        cfg=signingserver.SigningserverClientCfg(
+            base_url=signing_server_url,
+            client_certificate=signing_server_client_cert,
+            client_certificate_key=signing_server_client_cert_key,
+            server_certificate_ca=signing_server_certificate_ca,
+            validate_tls_certificate=tls_validation,
+        ),
+    )
+
+    signature = signingserver_client.sign(
+        content=payload_bytes,
+        signing_algorithm=signing_algorithm,
+    )
+
+    if output == 'signature':
+        print(signature.signature.replace('\n', ''), end='')
+    elif output == 'raw':
+        print(signature.raw)
+    elif output == 'certificate':
+        print(signature.certificate)
