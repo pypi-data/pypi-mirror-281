459767d77508857b3e7603b5e3578b83d9cddc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:02:50 +0000

    Add package ahmpy/0.1/ahmpy-0.1.tar.gz

---
+++ packages/ahmpy/ahmpy-0.1.tar.gz/ahmpy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ahmpy',
+    version='0.1',
+    author='nameingishard',
+    author_email='2569674866@qq.com',
+    description='tools for ahm company',
+    packages=find_packages(),
+
+)
