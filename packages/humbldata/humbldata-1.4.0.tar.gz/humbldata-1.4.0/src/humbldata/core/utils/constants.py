e2ff237281ef4fbb543cc7396fd2b1e863ebcd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 06:04:27 +0000

    Add package humbldata/1.4.0/humbldata-1.4.0.tar.gz

---
+++ packages/humbldata/humbldata-1.4.0.tar.gz/humbldata-1.4.0/src/humbldata/core/utils/constants.py
@@ -0,0 +1,23 @@
+"""A module to contain all project-wide constants."""
+
+from typing import Literal
+
+OBB_EQUITY_PRICE_QUOTE_PROVIDERS = Literal[
+    "cboe", "fmp", "intrinio", "tmx", "tradier", "yfinance"
+]
+
+OBB_EQUITY_PROFILE_PROVIDERS = Literal[
+    "finviz", "fmp", "intrinio", "tmx", "yfinance"
+]
+
+OBB_EQUITY_PRICE_HISTORICAL_PROVIDERS = Literal[
+    "alpha_vantage",
+    "cboe",
+    "fmp",
+    "intrinio",
+    "polygon",
+    "tiingo",
+    "tmx",
+    "tradier",
+    "yfinance",
+]
---
+++ packages/humbldata/humbldata-1.4.0.tar.gz/humbldata-1.4.0/src/humbldata/core/utils/descriptions.py
@@ -0,0 +1,31 @@
+"""Common descriptions for model fields."""
+
+QUERY_DESCRIPTIONS = {
+    "symbol": "Symbol to get data for.",
+    "start_date": "Start date of the data, in YYYY-MM-DD format.",
+    "end_date": "End date of the data, in YYYY-MM-DD format.",
+    "interval": "Time interval of the data to return.",
+    "period": "Time period of the data to return.",
+    "date": "A specific date to get data for.",
+    "limit": "The number of data entries to return.",
+    "countries": "The country or countries to get data.",
+    "units": "The unit of measurement for the data.",
+    "frequency": "The frequency of the data.",
+    "symbol_list_warning": "Lists of symbols are not allowed for this function. "
+    "Multiple symbols will be ignored in favour of the first symbol.",
+    "provider": "The provider of the data.",
+    "user_role": "The role of a user in supabase.",
+}
+
+DATA_DESCRIPTIONS = {
+    "symbol": "Symbol representing the entity requested in the data.",
+    "cik": "Central Index Key (CIK) for the requested entity.",
+    "date": "The date of the data as a Date object, the returned data's original dtype",
+    "open": "The open price.",
+    "high": "The high price.",
+    "low": "The low price.",
+    "close": "The close price.",
+    "volume": "The trading volume.",
+    "adj_close": "The adjusted close price.",
+    "vwap": "Volume Weighted Average Price over the period.",
+}
