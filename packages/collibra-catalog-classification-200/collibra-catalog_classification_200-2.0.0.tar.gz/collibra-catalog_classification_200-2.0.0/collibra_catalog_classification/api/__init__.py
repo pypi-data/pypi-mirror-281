63b3dea0d33e7cf5efa5b92d7a0492bb52c00d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:00 +0000

    Add package collibra-catalog-classification-200/2.0.0/collibra-catalog_classification_200-2.0.0.tar.gz

---
+++ packages/collibra-catalog-classification-200/collibra-catalog_classification_200-2.0.0.tar.gz/collibra-catalog_classification_200-2.0.0/collibra_catalog_classification/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_catalog_classification.api.classification_data_category_api import ClassificationDataCategoryApi
+from collibra_catalog_classification.api.classification_process_api import ClassificationProcessApi
