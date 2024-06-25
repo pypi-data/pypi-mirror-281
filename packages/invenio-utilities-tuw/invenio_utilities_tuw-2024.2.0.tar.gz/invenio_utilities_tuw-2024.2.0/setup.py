ab92ea1edfb47033ffa99b95caa237cbf4c3fb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 06:03:50 +0000

    Add package invenio-utilities-tuw/2024.2.0/invenio_utilities_tuw-2024.2.0.tar.gz

---
+++ packages/invenio-utilities-tuw/invenio_utilities_tuw-2024.2.0.tar.gz/invenio_utilities_tuw-2024.2.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020-2022 TU Wien.
+#
+# Invenio-Utilities-TUW is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Some utilities for InvenioRDM."""
+
+from setuptools import setup
+
+setup()
