ec481320e1a73d6fdd6e15dc07ba3fa2e268e74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:04 +0000

    Add package baxiapi/1.4.1/baxiapi-1.4.1.tar.gz

---
+++ packages/baxiapi/baxiapi-1.4.1.tar.gz/baxiapi-1.4.1/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='baxiapi',
+    version='1.4.1',
+    packages=["baxiapi"],
+    install_requires=["requests"],
+    author="Red_Wolf2467",
+    author_email="support@pyropixle.com",
+    description="Allows you to interact with our Baxi API."
+)
