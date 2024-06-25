9ec33d791095fc9afc50c72201a90456609424d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:50 +0000

    Add package ganquan/0.3/ganquan-0.3.tar.gz

---
+++ packages/ganquan/ganquan-0.3.tar.gz/ganquan-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ganquan',
+    version='0.3',
+    description="this is a tesk package",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+)
