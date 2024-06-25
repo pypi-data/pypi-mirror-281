0b8444ef250ddbb7c01c2f3ec2e6d55ec37eb185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:00 +0000

    Add package collibra-catalog-cloud-ingestions-100/1.0.0/collibra_catalog_cloud_ingestions_100-1.0.0-py3-none-any.whl

---
+++ packages/collibra-catalog-cloud-ingestions-100/collibra_catalog_cloud_ingestions_100-1.0.0-py3-none-any.whl/collibra_catalog_cloud_ingestions/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_catalog_cloud_ingestions.api.adls_api import ADLSApi
+from collibra_catalog_cloud_ingestions.api.gcs_api import GCSApi
+from collibra_catalog_cloud_ingestions.api.generic_api import GENERICApi
+from collibra_catalog_cloud_ingestions.api.powerbi_api import POWERBIApi
+from collibra_catalog_cloud_ingestions.api.s3_api import S3Api
+from collibra_catalog_cloud_ingestions.api.tableau_api import TABLEAUApi
