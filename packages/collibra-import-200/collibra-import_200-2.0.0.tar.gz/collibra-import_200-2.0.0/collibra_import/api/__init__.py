ce01bc1324289d51adbe8c08c8c76e5c088e1f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:49 +0000

    Add package collibra-import-200/2.0.0/collibra-import_200-2.0.0.tar.gz

---
+++ packages/collibra-import-200/collibra-import_200-2.0.0.tar.gz/collibra-import_200-2.0.0/collibra_import/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_import.api.import_api import ImportApi
+from collibra_import.api.import_results_api import ImportResultsApi
