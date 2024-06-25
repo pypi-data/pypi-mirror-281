c95f5ba28188ac47fead36ba00e76693f9dfcfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:04:04 +0000

    Add package cd-directory-manager/0.5.0/cd_directory_manager-0.5.0.tar.gz

---
+++ packages/cd-directory-manager/cd_directory_manager-0.5.0.tar.gz/cd_directory_manager-0.5.0/cd_directory_manager/__init__.py
@@ -0,0 +1,4 @@
+from cd_directory_manager.directory_manager import DirectoryManager
+from cd_directory_manager.file_manager import FileManager
+from cd_directory_manager.path_manager import PathManager
+from cd_directory_manager.zipper import ZipManager
