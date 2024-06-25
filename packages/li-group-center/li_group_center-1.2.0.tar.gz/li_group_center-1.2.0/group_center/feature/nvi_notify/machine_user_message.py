077636904786ced25b4629501b6614daca3d43b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:27 +0000

    Add package li-group-center/1.2.0/li_group_center-1.2.0.tar.gz

---
+++ packages/li-group-center/li_group_center-1.2.0.tar.gz/li_group_center-1.2.0/group_center/feature/nvi_notify/machine_user_message.py
@@ -0,0 +1,16 @@
+from group_center.feature.nvi_notify import notify_api
+
+
+def machine_user_message_via_local_nvi_notify(
+        user_name: str,
+        content: str,
+):
+    data_dict: dict = {
+        "userName": user_name,
+        "content": content,
+    }
+
+    notify_api.send_to_nvi_notify(
+        dict_data=data_dict,
+        target="/machine_user_message"
+    )
---
+++ packages/li-group-center/li_group_center-1.2.0.tar.gz/li_group_center-1.2.0/test/test_machine_msg_via_nvinotify.py
@@ -0,0 +1,7 @@
+from group_center.feature.nvi_notify.machine_user_message import machine_user_message_via_local_nvi_notify
+
+if __name__ == '__main__':
+    machine_user_message_via_local_nvi_notify(
+        user_name="konghaomin",
+        content="test1",
+    )
