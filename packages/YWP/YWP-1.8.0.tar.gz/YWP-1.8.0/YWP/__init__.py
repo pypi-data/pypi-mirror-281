9fbacc41a27fbdda0945c43f7727cc9a40f8ec66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:10 +0000

    Add package YWP/1.8.0/YWP-1.8.0.tar.gz

---
+++ packages/YWP/YWP-1.8.0.tar.gz/YWP-1.8.0/YWP/__init__.py
@@ -0,0 +1,62 @@
+r"""
+Created By Your Wanted Products (YWP)
+
+Email: pbstzidr@ywp.freewebhostmost.com
+
+Phone Number: +201096730619
+
+WhatsApp Number: +201096730619
+
+website: https://ywp.freewebhostmost.com
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+# First
+from .play_audio import play_audio
+from .stop_recording import stop_recording
+from .create_file import create_file
+from .open_file import open_file
+from .open_website import open_website
+from .shutdown import shutdown
+from .record_audio import record_audio
+from .transcribe_audio import transcribe_audio
+from .text_to_speech import text_to_speech
+from .play_sound import play_sound
+# Second
+from .restart import restart
+from .log_off import log_off
+from .hibernate import hibernate
+from .play_audio_online import play_audio_online
+from .token_information import token_information
+# Third
+from .basic_flask import route_flask
+from .basic_flask import run
+from .train import AI
+# Fourth
+from .basic_video_creator import basic_video_creator
+# Fiveth
+from .encrypt_decrypt import endecrypt
+# Sixth
