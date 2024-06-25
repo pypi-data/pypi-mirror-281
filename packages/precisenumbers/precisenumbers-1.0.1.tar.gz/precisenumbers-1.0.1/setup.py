8d9a22c56e456e796e5d0e72455bf5fce707ab2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:16 +0000

    Add package precisenumbers/1.0.1/precisenumbers-1.0.1.tar.gz

---
+++ packages/precisenumbers/precisenumbers-1.0.1.tar.gz/precisenumbers-1.0.1/setup.py
@@ -0,0 +1,13 @@
+# full credit to: https://github.com/jessica-writes-code, https://github.com/sbromberger
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1' 
+DESCRIPTION = 'For when you want your numbers to be precise'
+
+setup(
+        name="precisenumbers", 
+        version=VERSION,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        url='https://github.com/ricklentz/precisenumbers'
+)
