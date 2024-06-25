dff5fbaa4dfe83008b1b0e4e68cbda3c7abdefe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 07:03:13 +0000

    Add package uk_address_matcher/0.0.1.dev4/uk_address_matcher-0.0.1.dev4.tar.gz

---
+++ packages/uk_address_matcher/uk_address_matcher-0.0.1.dev4.tar.gz/uk_address_matcher-0.0.1.dev4/uk_address_matcher/splink_model.py
@@ -0,0 +1,50 @@
+import importlib.resources as pkg_resources
+import json
+from typing import List
+
+from duckdb import DuckDBPyConnection, DuckDBPyRelation
+from splink.duckdb.linker import DuckDBLinker
+
+
+def get_pretrained_linker(
+    dfs: List[DuckDBPyRelation],
+    *,
+    con: DuckDBPyConnection,
+    precomputed_numeric_tf_table: DuckDBPyRelation = None,
+    additional_columns_to_retain: List[str] = None,
+):
+    # Load the settings file
+    with pkg_resources.path(
+        "uk_address_matcher.data", "splink_model.json"
+    ) as settings_path:
+        settings_as_dict = json.load(open(settings_path))
+
+    if additional_columns_to_retain is not None:
+        settings_as_dict["additional_columns_to_retain"] = additional_columns_to_retain
+    # Convert DuckDBPyRelations to pandas DataFrames
+    dfs_pd = [d.df() for d in dfs]
+
+    # Initialize the linker
+    linker = DuckDBLinker(dfs_pd, settings_dict=settings_as_dict, connection=con)
+
+    # Load the default term frequency table if none is provided
+    if precomputed_numeric_tf_table is None:
+        with pkg_resources.path(
+            "uk_address_matcher.data", "numeric_token_frequencies.parquet"
+        ) as default_tf_path:
+            precomputed_numeric_tf_table = con.read_parquet(str(default_tf_path))
+
+    if precomputed_numeric_tf_table is not None:
+        for i in range(1, 4):
+            df_sql = f"""
+                select
+                    numeric_token as numeric_token_{i},
+                    tf_numeric_token as tf_numeric_token_{i}
+                from precomputed_numeric_tf_table"""
+
+            df = con.sql(df_sql).df()
+            linker.register_term_frequency_lookup(
+                df, f"numeric_token_{i}", overwrite=True
+            )
+
+    return linker
