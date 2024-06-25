26a9fe3e4c912ac03705a3286be57a6c94952799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:02 +0000

    Add package fairbalance/0.1.15/fairbalance-0.1.15-py3-none-any.whl

---
+++ packages/fairbalance/fairbalance-0.1.15-py3-none-any.whl/fairbalance/processors/__init__.py
@@ -0,0 +1,11 @@
+"""
+The :mod:`fairbalance.processor` module includes different processors to use in mitigation strategies.
+"""
+
+from ._processors import RandomOverSamplerProcessor, RandomUnderSamplerProcessor, SMOTENCProcessor
+
+__all__ = [
+    "RandomOverSamplerProcessor",
+    "RandomUnderSamplerProcessor",
+    "SMOTENCProcessor"
+]
