226f82e545989337e320b50bbf7c1352cad9b955
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:54 +0000

    Add package storage-local/0.1.46/storage_local-0.1.46-py3-none-any.whl

---
+++ packages/storage-local/storage_local-0.1.46-py3-none-any.whl/storage_local/aws_s3_storage_local/StorageInterface.py
@@ -0,0 +1,24 @@
+# Abstract Base Classes (abc)
+from abc import ABC, abstractmethod
+from logger_local.MetaLogger import ABCMetaLogger
+
+
+# Interface for all storage services
+
+
+class StorageInterface(ABC, metaclass=ABCMetaLogger):
+    """
+    Method uploads a file from local_path to the cloud in remoth_path, and names the file filname
+    """
+
+    @abstractmethod
+    def upload_file(self, *, local_file_path: str, remote_path: str, url: str = None) -> int or None:
+        pass
+
+    @abstractmethod
+    def download_file(self, *, remote_path: str, local_path: str) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, *, remote_path: str, filename: str) -> None:
+        pass
