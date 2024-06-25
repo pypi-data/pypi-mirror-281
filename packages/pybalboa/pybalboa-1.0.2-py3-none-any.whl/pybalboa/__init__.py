d811a1988bf0aab7ceb6a8a45c5d325f03776317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:23 +0000

    Add package pybalboa/1.0.2/pybalboa-1.0.2-py3-none-any.whl

---
+++ packages/pybalboa/pybalboa-1.0.2-py3-none-any.whl/pybalboa/__init__.py
@@ -0,0 +1,10 @@
+"""Balboa spa module."""
+
+__version__ = "1.0.2"
+
+
+from .client import SpaClient
+from .control import EVENT_UPDATE, SpaControl
+from .exceptions import SpaConnectionError
+
+__all__ = ["SpaClient", "SpaControl", "SpaConnectionError", "EVENT_UPDATE"]
