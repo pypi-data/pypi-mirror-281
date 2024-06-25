4a5326493e960c647fe377017914326f93ebcf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:04 +0000

    Add package stack-gen/0.0.13/stack_gen-0.0.13.tar.gz

---
+++ packages/stack-gen/stack_gen-0.0.13.tar.gz/stack_gen-0.0.13/stack_gen/main.py
@@ -0,0 +1,35 @@
+import click
+import os
+
+
+@click.command()
+@click.option(
+    "repository",
+    "-r",
+    prompt="""Welcome on stack-gen !\nRepository name containing the template""",
+    help="Name of the repository containing the cookicutter template",
+)
+@click.option(
+    "--user",
+    "-u",
+    prompt="Github user owning the repository",
+    help="Github user on which stack generator searches for template repository",
+)
+@click.option("--access_token", "-gac", help="GITHUB access token")
+def main(repository, user, access_token):
+    os.system(f"git clone git@github.com:{user}/{repository}.git --quiet")
+    os.system(
+        f"python3 -m pip install -r {repository}/requirements.txt --quiet",
+    )
+    if access_token:
+        with open(
+            f"{repository}/app/{{{{cookiecutter.project_slug}}}}/.env", "a+"
+        ) as file:
+            file.write(f'\nGITHUB_ACCESS_TOKEN="{access_token}"')
+
+    os.system(f"cookiecutter {repository}/app")
+    os.system(f"rm -rf {repository}")
+
+
+if __name__ == "__main__":
+    main()
