2d2add76c7681cc457e8041425e8bcf2332fb7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:11 +0000

    Add package grrproc/1.0.1/grrproc-1.0.1-py3-none-any.whl

---
+++ packages/grrproc/grrproc-1.0.1-py3-none-any.whl/grrproc/__about__.py
@@ -0,0 +1,17 @@
+"""
+A package of python routines to apply graph theory to r-process nucleosynthesis.
+"""
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__title__ = "grrproc"
+__summary__ = "Python project to follow r-process nucleosynthesis graphically."
+__version__ = "1.0.1"
+__author__ = "Clemson University"
+__copyright__ = "Clemson University, 2024"
