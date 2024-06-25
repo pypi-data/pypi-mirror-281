14334e51cc7cde2a8a1c611d5de7eb634ee2e4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:31 +0000

    Add package tap-pipedream/0.2.2/tap_pipedream-0.2.2.tar.gz

---
+++ packages/tap-pipedream/tap_pipedream-0.2.2.tar.gz/tap_pipedream-0.2.2/tap_pipedream/client.py
@@ -0,0 +1,59 @@
+"""REST client handling, including PipedreamStream base class."""
+
+from __future__ import annotations
+
+from typing import Any
+
+from singer_sdk import RESTStream
+from singer_sdk.authenticators import BearerTokenAuthenticator
+
+
+class PipedreamStream(RESTStream[str]):
+    """Pipedream stream class."""
+
+    url_base = "https://api.pipedream.com/v1"
+    records_jsonpath = "$.data[*]"
+    next_page_token_jsonpath = "$.page_info.end_cursor"  # noqa: S105
+
+    page_size = 100
+
+    @property
+    def authenticator(self) -> BearerTokenAuthenticator:
+        """Get an authenticator object.
+
+        Returns:
+            The authenticator instance for this REST stream.
+        """
+        token: str = self.config["token"]
+        return BearerTokenAuthenticator.create_for_stream(
+            self,
+            token=token,
+        )
+
+    @property
+    def http_headers(self) -> dict[str, str]:
+        """Return the http headers needed.
+
+        Returns:
+            A dictionary of HTTP headers.
+        """
+        return {"User-Agent": f"{self.tap_name}/{self._tap.plugin_version}"}
+
+    def get_url_params(
+        self,
+        context: dict[str, Any] | None,  # noqa: ARG002
+        next_page_token: str | None,
+    ) -> dict[str, Any]:
+        """Get URL query parameters.
+
+        Args:
+            context: Stream sync context.
+            next_page_token: Next offset.
+
+        Returns:
+            Mapping of URL query parameters.
+        """
+        return {
+            "limit": self.page_size,
+            "after": next_page_token,
+        }
