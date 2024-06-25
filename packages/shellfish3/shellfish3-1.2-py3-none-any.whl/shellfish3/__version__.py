ab43c0f7df5cdb9b17c37b61af54d7deaffa50d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:04 +0000

    Add package shellfish3/1.2/shellfish3-1.2-py3-none-any.whl

---
+++ packages/shellfish3/shellfish3-1.2-py3-none-any.whl/shellfish3/__version__.py
@@ -0,0 +1,14 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = "shellfish3"
+__description__ = 'Custom fork of the requests library for extended API calls'
+__url__ = 'https://github.com/appsecgroup/shellfish3'
+__version__ = "1.2"
+__build__ = ""
+__author__ = ""
+__author_email__ = ""
+__license__ = ""
+__copyright__ = ""
+__cake__ = "\u2728 \U0001f370 \u2728"
