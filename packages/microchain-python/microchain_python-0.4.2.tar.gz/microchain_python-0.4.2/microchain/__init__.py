1f08e75eacd586b26c4b109735ffd2f70851bd07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:04:02 +0000

    Add package microchain-python/0.4.2/microchain_python-0.4.2.tar.gz

---
+++ packages/microchain-python/microchain_python-0.4.2.tar.gz/microchain_python-0.4.2/microchain/__init__.py
@@ -0,0 +1,8 @@
+from microchain.models.generators import OpenAITextGenerator, OpenAIChatGenerator
+from microchain.models.templates import HFChatTemplate, VicunaTemplate
+from microchain.models.llm import LLM
+
+from microchain.engine.function import Function, FunctionResult
+from microchain.engine.engine import Engine
+
+from microchain.engine.agent import Agent
