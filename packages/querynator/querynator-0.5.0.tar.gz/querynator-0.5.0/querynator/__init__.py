c7253df4b17278b23539e44cef6d5c4531b02800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:56 +0000

    Add package querynator/0.5.0/querynator-0.5.0.tar.gz

---
+++ packages/querynator/querynator-0.5.0.tar.gz/querynator-0.5.0/querynator/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for querynator."""
+
+__author__ = """Susanne Jodoin & Mark Polster"""
+__email__ = "susanne.jodoin@qbic.uni-tuebingen.de | mark.polster@uni-tuebingen.de"
+__version__ = "0.5.0"
