1138d0db30a3c3ec206df375e554175e9c6f1aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:00 +0000

    Add package pygdsdesign/0.16.0/pygdsdesign-0.16.0.tar.gz

---
+++ packages/pygdsdesign/pygdsdesign-0.16.0.tar.gz/pygdsdesign-0.16.0/pygdsdesign/__init__.py
@@ -0,0 +1,11 @@
+from .polygonSet import PolygonSet
+from .polygons import *
+from .operation import *
+from .library import GdsLibrary
+from .transmission_lines.cpw import CPW
+from .transmission_lines.microstrip import MicroStrip
+from .functions import *
+from .shapes import *
+from .materials_color import colors
+
+
