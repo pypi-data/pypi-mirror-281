b23ce6c332eb71cf3a09dfe3c37423eced70bd92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:04:06 +0000

    Add package ebsfm-schedule/0.0.1/ebsfm-schedule-0.0.1.tar.gz

---
+++ packages/ebsfm-schedule/ebsfm-schedule-0.0.1.tar.gz/ebsfm-schedule-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="ebsfm-schedule",
+    version="0.0.1",
+    license='MIT',
+    author="cheddars",
+    author_email="nezahrish@gmail.com",
+    description="Crwaling EBS FM, EBS 외국어전문 방송 편성표",
+    long_description=open('README.md').read(),
+    url="https://github.com/cheddars/pypi-ebsfm-schedule",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
