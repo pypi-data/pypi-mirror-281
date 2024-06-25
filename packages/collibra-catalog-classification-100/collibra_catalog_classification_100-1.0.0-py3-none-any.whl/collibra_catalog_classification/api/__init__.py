dc32ef0ec3b504ab5b187afa78e3668ef7cb542c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:00 +0000

    Add package collibra-catalog-classification-100/1.0.0/collibra_catalog_classification_100-1.0.0-py3-none-any.whl

---
+++ packages/collibra-catalog-classification-100/collibra_catalog_classification_100-1.0.0-py3-none-any.whl/collibra_catalog_classification/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_catalog_classification.api.data_classification_api import DataClassificationApi
