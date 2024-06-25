88193d0ac00072a3d8afe763d93b684cfe5f5831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:19 +0000

    Add package oaios/0.3.0/oaios-0.3.0-py3-none-any.whl

---
+++ packages/oaios/oaios-0.3.0-py3-none-any.whl/oaios/__init__.py
@@ -0,0 +1,7 @@
+""" Our Asynchronously Interchangeable Objects Server
+
+
+
+"""
+
+__version__ = '0.3.0'
---
+++ packages/oaios/oaios-0.3.0-py3-none-any.whl/oaios/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for oaios project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings                                                    # type: ignore
+from django.conf.urls.i18n import i18n_patterns                                     # type: ignore
+from django.conf.urls.static import static                                          # type: ignore
+from django.contrib import admin                                                    # type: ignore
+from django.urls import include, path                                               # type: ignore
+from django.views.i18n import JavaScriptCatalog                                     # type: ignore
+
+urlpatterns = i18n_patterns(
+    path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
+    path('admin/', admin.site.urls),
+    path('filer/', include('filer.urls')),
+    path('', include('cms.urls')),
+)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
