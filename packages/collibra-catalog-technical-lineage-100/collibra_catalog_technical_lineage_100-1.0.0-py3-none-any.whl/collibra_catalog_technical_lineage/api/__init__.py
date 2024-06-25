2373b9f16a569d218a50acaec7ec26938607495e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:44 +0000

    Add package collibra-catalog-technical-lineage-100/1.0.0/collibra_catalog_technical_lineage_100-1.0.0-py3-none-any.whl

---
+++ packages/collibra-catalog-technical-lineage-100/collibra_catalog_technical_lineage_100-1.0.0-py3-none-any.whl/collibra_catalog_technical_lineage/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_catalog_technical_lineage.api.technical_lineage_api import TechnicalLineageApi
