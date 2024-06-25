ab7b587c990573f776fa06e8e1464a25b0d3387d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:10 +0000

    Add package byte-harmony/0.1.0/byte_harmony-0.1.0.tar.gz

---
+++ packages/byte-harmony/byte_harmony-0.1.0.tar.gz/byte_harmony-0.1.0/byte_harmony/__init__.py
@@ -0,0 +1,6 @@
+# export AWS_ACCESS_KEY_ID=AKIA4YQKVLRVBEIAD32N
+# export AWS_SECRET_ACCESS_KEY=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
+# export AWS_DEFAULT_REGION=us-east-1
+
+def main():
+    return "test"
