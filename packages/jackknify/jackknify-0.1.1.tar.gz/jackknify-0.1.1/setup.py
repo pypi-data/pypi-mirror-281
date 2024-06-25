237f4f4d9a3a0d11db897a529d9a149a582705fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:00 +0000

    Add package jackknify/0.1.1/jackknify-0.1.1.tar.gz

---
+++ packages/jackknify/jackknify-0.1.1.tar.gz/jackknify-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="jackknify", 
+    version="0.1.1",
+    author="Joshiwa van Marrewijk",
+    author_email="joshiwa01@gmail.com",
+    description="Jackknifing interferometric datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Joshiwavm/jackknify", 
+    packages=find_packages(),
+    install_requires = [
+        'astropy',
+        'casatasks',
+        'casatools',
+        'jupyterlab',
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'tqdm'
+    ]
+    )
