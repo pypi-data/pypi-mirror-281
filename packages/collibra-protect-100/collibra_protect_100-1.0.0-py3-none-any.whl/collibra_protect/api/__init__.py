35e881db841fe2d2d4c06b1882493b03b9a882cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:49 +0000

    Add package collibra-protect-100/1.0.0/collibra_protect_100-1.0.0-py3-none-any.whl

---
+++ packages/collibra-protect-100/collibra_protect_100-1.0.0-py3-none-any.whl/collibra_protect/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from collibra_protect.api.custom_maskings_api import CustomMaskingsApi
+from collibra_protect.api.data_sources_api import DataSourcesApi
+from collibra_protect.api.groups_api import GroupsApi
+from collibra_protect.api.prescriptive_paths_api import PrescriptivePathsApi
