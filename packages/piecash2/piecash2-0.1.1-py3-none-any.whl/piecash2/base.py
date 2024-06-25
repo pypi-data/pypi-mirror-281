a38492f724346fabb880ee2a67c69b8e8ef82d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:15 +0000

    Add package piecash2/0.1.1/piecash2-0.1.1-py3-none-any.whl

---
+++ packages/piecash2/piecash2-0.1.1-py3-none-any.whl/piecash2/base.py
@@ -0,0 +1,17 @@
+"""
+piecash2 base module.
+
+This is the principal module of the piecash2 project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "piecash2"
