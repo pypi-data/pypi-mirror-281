8e7632b3a90de016c31201228243640b54d3cc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:18 +0000

    Add package AdvanceCalculatorProject/0.0.2/advancecalculatorproject-0.0.2.tar.gz

---
+++ packages/AdvanceCalculatorProject/advancecalculatorproject-0.0.2.tar.gz/advancecalculatorproject-0.0.2/AdvanceCalculator/methods/check_inputs.py
@@ -0,0 +1,73 @@
+from sympy import * 
+
+
+class CheckMethods:
+
+    def __init__(self, the_string):
+
+        self.number = input(the_string)
+
+    def _check_int(self):
+
+        while self.number.isdigit() != True: 
+
+            self.number = input('Please enter positiv integer : ')
+
+        return int(self.number)
+        
+    def _check_float(self):
+
+        while True:
+            try:
+                self.number = float(self.number)
+                return(self.number)
+            except ValueError:
+                self.number = input('Please input a number :')
+
+    def _check_zero(self):
+        if self.check_int() == 0:
+            print ('Error : Division by zero not possible ')
+            return False
+        else:
+            return True
+
+    def _check_eval(self, box):
+
+        valid_operations = ["0","1","2","3","4","5","6","7","8","9","+","-","*","/",".","%"]
+
+        for item in box:
+            if item in valid_operations:
+                no_E = True
+            else :
+                print("\nError")
+                print('-----------------------------------------------')
+                print(" \n To exit or break. Type 'exit' or 'break' ")
+                print('\n-----------------------------------------------')
+                no_E = False
+                break 
+
+        if no_E :
+            return eval(box)
+        else:
+            return False
+    
+    def _check_sympy_equation (self):
+
+
+        print('-------------Checking the sympyfication ----------\n ')
+
+                    
+        try :
+
+            fct = sympify(self.number)
+
+        except :
+
+            print('You enter an invalid syntax. : type -help for list of function')
+
+            return False
+        
+        return fct
+    
+
+
---
+++ packages/AdvanceCalculatorProject/advancecalculatorproject-0.0.2.tar.gz/advancecalculatorproject-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Simple advance calculator '
+LONG_DESCRIPTION = 'A package that allows to perfoming simple calculus integrals and derivative, ploting any type of graph as well as solving simple cuadratic equations with the help of Numpy and Simpy packages'
+
+# Setting up
+setup(
+    name="AdvanceCalculatorProject",
+    version=VERSION,
+    author="Napain (Simon Torrealba)",
+    author_email="storrealba24@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['simpy','numpy'],
+    keywords=['python', 'calculus', 'ploting', 'solver', 'calculator']
+
+)
