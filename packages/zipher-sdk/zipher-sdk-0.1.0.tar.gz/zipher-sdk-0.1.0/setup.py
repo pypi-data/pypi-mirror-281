abf0e78000abd3a73c929bbb2c8db4aba52d22f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:03:24 +0000

    Add package zipher-sdk/0.1.0/zipher-sdk-0.1.0.tar.gz

---
+++ packages/zipher-sdk/zipher-sdk-0.1.0.tar.gz/zipher-sdk-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zipher-sdk',
+    version='0.1.0',
+    description='Python SDK for accessing the Zipher API',
+    author='Zipher Inc.',
+    author_email='yoav@zipher.cloud',
+    packages=find_packages(),
+    install_requires=[
+        "pydantic==2.1.1",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
