7e2f4cdb8f58dafc2295b2ff75d1aed42eeee06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 07:03:28 +0000

    Add package ampari/0.1.3/ampari-0.1.3.tar.gz

---
+++ packages/ampari/ampari-0.1.3.tar.gz/ampari-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ampari",
+    version="0.1.3",
+    install_requires=[
+        # dependencies in here
+        #                 
+    ],
+    extras_require={
+        "dev": ["twine>=5.1.0"]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+)
