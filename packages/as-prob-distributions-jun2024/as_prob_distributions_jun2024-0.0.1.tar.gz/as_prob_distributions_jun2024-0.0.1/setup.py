b83bf32b2cacf958dfa4b2bd2c636a894e007afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:59 +0000

    Add package as-prob-distributions-jun2024/0.0.1/as_prob_distributions_jun2024-0.0.1.tar.gz

---
+++ packages/as-prob-distributions-jun2024/as_prob_distributions_jun2024-0.0.1.tar.gz/as_prob_distributions_jun2024-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="as_prob_distributions_jun2024",
+      version="0.0.1",
+      description="Gaussian and Binomial distribution",
+      packages=['as_prob_distributions_jun2024'],
+      zip_safe=False
+      )
