9d05ffae2b89362993ec0fd028b95b7593c57613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 06:03:50 +0000

    Add package compositio/0.0.1/compositio-0.0.1.tar.gz

---
+++ packages/compositio/compositio-0.0.1.tar.gz/compositio-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'the idea of assembling parts into a unified structure'
+
+setup(
+        name="compositio", 
+        version=VERSION,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        url='https://github.com/ricklentz/compositio'
+)
