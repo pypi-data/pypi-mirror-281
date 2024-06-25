906623176308725b9a9d4be44a1333bd10544c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:36 +0000

    Add package versifier/0.1.1/versifier-0.1.1.tar.gz

---
+++ packages/versifier/versifier-0.1.1.tar.gz/versifier-0.1.1/versifier/config.py
@@ -0,0 +1,30 @@
+import sys
+from pathlib import Path
+from typing import List
+
+import tomli
+
+
+def get_private_packages_from_pyproject(path: str = "pyproject.toml") -> List[str]:
+    with open(path) as f:
+        config = tomli.loads(f.read())
+
+    try:
+        return config["tool"]["versifier"]["private_packages"]  # type: ignore
+    except KeyError:
+        return []
+
+
+def get_site_packages_path() -> str:
+    for p in sys.path:
+        if p.endswith("site-packages"):
+            return p
+
+    return ""
+
+
+def get_available_packages(path: str) -> List[str]:
+    root = Path(path)
+    path_set = set(str(i.parent) for i in root.glob("*/*.py"))
+
+    return list(path_set)
