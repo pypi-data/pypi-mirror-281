6be9277fb430cf612128d8063bddbd1555103c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:04:03 +0000

    Add package compas-xr/0.9.4/compas_xr-0.9.4.tar.gz

---
+++ packages/compas-xr/compas_xr-0.9.4.tar.gz/compas_xr-0.9.4/CHANGELOG.md
@@ -0,0 +1,37 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.9.4] 2024-06-24
+
+### Changed
+
+* Fixed missing dependency for GH.
+
+## [0.9.3] 2024-06-24
+
+### Added
+
+* Add GH components to release.
+
+
+## [0.9.2] 2024-06-24
+
+### Added
+
+### Changed
+
+### Removed
+
+
+## [0.9.1] 2024-06-24
+
+* Zenodo DOI generation.
+
+## [0.9.0] 2024-06-24
+
+* Initial release.
+
---
+++ packages/compas-xr/compas_xr-0.9.4.tar.gz/compas_xr-0.9.4/src/compas_xr/ghpython/components/Cx_AppSettings/code.py
@@ -0,0 +1,27 @@
+"""
+Application Settings.
+
+COMPAS XR v0.9.4
+"""
+
+from ghpythonlib.componentbase import executingcomponent as component
+
+from compas_xr.ghpython.app_settings import AppSettings
+from compas_xr.project import ProjectManager
+
+
+class ApplicationSettingsComponent(component):
+    def RunScript(self, config_filepath, project_name, storage_folder, z_to_y_remap, write):
+        if not (config_filepath):
+            self.Message = "Missing Config"
+
+        elif not (project_name):
+            self.Message = "Missing Settings Data"
+
+        else:
+            app_settings = AppSettings(project_name, storage_folder, z_to_y_remap)
+            pm = ProjectManager(config_filepath)
+            self.Message = None
+
+            if write:
+                pm.application_settings_writer(app_settings.project_name, app_settings.storage_folder, app_settings.z_to_y_remap)
