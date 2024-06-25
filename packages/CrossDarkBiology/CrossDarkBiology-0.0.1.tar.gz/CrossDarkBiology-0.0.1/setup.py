f8b3eedcd606e3c09a835f201db5467801ec3de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:02:55 +0000

    Add package CrossDarkBiology/0.0.1/CrossDarkBiology-0.0.1.tar.gz

---
+++ packages/CrossDarkBiology/CrossDarkBiology-0.0.1.tar.gz/CrossDarkBiology-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CrossDarkBiology",
+    version="0.0.1",
+    author="CrossDark",
+    author_email="liuhanbo333@icloud.com",
+    description="CrossDark's biology tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CrossDark/CellSpeedMeasure",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'moviepy',
+        'av',
+        'ultralytics',
+        'pymysql'
+    ],
+    entry_points={
+        'console_scripts': [
+            'douyin_image=douyin_image:main'
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
