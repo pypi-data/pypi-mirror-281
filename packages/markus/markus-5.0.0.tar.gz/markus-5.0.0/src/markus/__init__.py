0db24d1eb3b20bcb7cd858fd747f6c02385d205f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 06:04:01 +0000

    Add package markus/5.0.0/markus-5.0.0.tar.gz

---
+++ packages/markus/markus-5.0.0.tar.gz/markus-5.0.0/src/markus/__init__.py
@@ -0,0 +1,26 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from importlib.metadata import (
+    version as importlib_version,
+    PackageNotFoundError,
+)
+
+from markus.main import configure, get_metrics  # noqa
+
+try:
+    __version__ = importlib_version("markus")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+
+INCR = "incr"
+GAUGE = "gauge"
+TIMING = "timing"
+HISTOGRAM = "histogram"
+
+__all__ = ["configure", "get_metrics", "INCR", "GAUGE", "TIMING", "HISTOGRAM"]
+
+# x.y.z or x.y.z.dev0 -- semver
+__version__ = "4.2.0"
