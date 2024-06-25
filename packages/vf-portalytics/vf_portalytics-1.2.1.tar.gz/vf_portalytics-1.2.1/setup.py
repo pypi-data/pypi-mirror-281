edcd245f09c15018c168830610e15edac04dccd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:59 +0000

    Add package vf-portalytics/1.2.1/vf_portalytics-1.2.1.tar.gz

---
+++ packages/vf-portalytics/vf_portalytics-1.2.1.tar.gz/vf_portalytics-1.2.1/setup.py
@@ -0,0 +1,13 @@
+########################################################################
+#       File based on https://github.com/Blosc/bcolz
+########################################################################
+#
+# Created: June 11, 2017
+#       Author:  Carst Vaartjes - cvaartjes@visualfabriq.com
+#
+########################################################################
+import setuptools
+setuptools.setup(
+    setup_requires=['pbr', 'pytest-runner'],
+    pbr=True
+)
