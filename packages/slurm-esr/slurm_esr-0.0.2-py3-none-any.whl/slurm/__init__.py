ecca6eb526b59e78aa225c883218dd604d9c02f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 07:03:29 +0000

    Add package slurm-esr/0.0.2/slurm_esr-0.0.2-py3-none-any.whl

---
+++ packages/slurm-esr/slurm_esr-0.0.2-py3-none-any.whl/slurm/__init__.py
@@ -0,0 +1,9 @@
+HEAD_INFO = """#!/bin/bash
+#SBATCH --job-name {job_name}
+#SBATCH --partition {partition}
+#SBATCH --nice {nice_value}
+#SBATCH --output {workdir}/log/{job_name}.%A.%a.out # STDOUT
+#SBATCH --error {workdir}/log/{job_name}.%A.%a.log # STDERR
+"""
+
+PRIORITY = {"default": 0, "high": -100, "low": 100}
