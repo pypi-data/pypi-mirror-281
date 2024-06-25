99aa0f626765cad98cfe96b43cdccff0859337ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:09 +0000

    Add package chess-notation/0.1.17/chess_notation-0.1.17.tar.gz

---
+++ packages/chess-notation/chess_notation-0.1.17.tar.gz/chess_notation-0.1.17/src/chess_notation/__init__.pyi
@@ -0,0 +1,15 @@
+from .language import Language, LANGUAGES, translate
+from .styles import Styles, PawnCapture, PieceCapture, CapturedPiece, Castle, Check, Mate, style, \
+  UNIQ_PIECE_CAPTURES, UNIQ_PAWN_CAPTURES
+from .represent import representations, KingEffectStyles, MotionStyles, all_representations
+from .notation import Notation, random_notation, styled, uniq_random_notation
+from .vocabulary import legal_sans
+from . import language, styles, represent
+
+__all__ = [
+  'Language', 'LANGUAGES', 'Notation', 'random_notation', 'style', 'translate',
+  'UNIQ_PIECE_CAPTURES', 'UNIQ_PAWN_CAPTURES', 'uniq_random_notation',
+  'Styles', 'PawnCapture', 'PieceCapture', 'CapturedPiece', 'Castle', 'Check', 'Mate',
+  'representations', 'KingEffectStyles', 'MotionStyles', 'all_representations',
+  'language', 'styles', 'represent', 'styled', 'legal_sans',
+]
