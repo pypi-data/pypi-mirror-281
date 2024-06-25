a78a99b18babefdec3d74cc9062c1904d380e6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 04:03:41 +0000

    Add package lauterbach-trace32-rcl/1.0.10/lauterbach_trace32_rcl-1.0.10-py3-none-any.whl

---
+++ packages/lauterbach-trace32-rcl/lauterbach_trace32_rcl-1.0.10-py3-none-any.whl/lauterbach/trace32/rcl/__init__.py
@@ -0,0 +1,67 @@
+from ._rc._address import Address, AddressService
+from ._rc._breakpoint import Breakpoint, BreakpointService
+from ._rc._command import CommandError, CommandService
+from ._rc._error import *
+from ._rc._functions import FunctionError, FunctionService
+from ._rc._memory import MemoryService
+from ._rc._practice import PracticeError, PracticeMacro, PracticeService
+from ._rc._register import Register, RegisterService
+from ._rc._symbol import Symbol, SymbolService
+from ._rc._variable import Variable, VariableError, VariableService
+
+# We don't export bare functions in __all__ to not pollute the caller's
+# namespace in case they use star imports.
+#
+# This applies to: autoconnect, connect, init
+#
+# To use these bare functions either import them like so (recommended):
+#     import lauterbach.trace32.rcl as t32
+#     def main():
+#         t32.init()
+# Or:
+#     from lauterbach.trace32.rcl import init
+#     def main():
+#         init()
+# Using star imports (from ... import *) will not provide these!
+from .rcl import Debugger
+from .rcl import autoconnect as autoconnect  # noqa
+from .rcl import connect as connect  # noqa
+from .rcl import hexversion as hexversion  # noqa
+from .rcl import init as init  # noqa
+from .rcl import VERSION as VERSION  # noqa
+
+__all__ = [
+    # RCL
+    "Debugger",
+    # Address
+    "Address",
+    "AddressService",
+    # Breakpoint
+    "Breakpoint",
+    "BreakpointService",
+    # Command
+    "CommandError",
+    "CommandService",
+    # Error
+    "BaseError",
+    "InternalError",
+    # Function
+    "FunctionError",
+    "FunctionService",
+    # Memory
+    "MemoryService",
+    # Practice
+    "PracticeError",
+    "PracticeMacro",
+    "PracticeService",
+    # Register
+    "Register",
+    "RegisterService",
+    # Symbol
+    "Symbol",
+    "SymbolService",
+    # Variable
+    "Variable",
+    "VariableError",
+    "VariableService",
+]
