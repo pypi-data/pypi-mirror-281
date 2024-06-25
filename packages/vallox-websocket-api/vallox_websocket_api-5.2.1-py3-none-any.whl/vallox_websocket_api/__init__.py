2ed4d44f9ea67eb4330fffa08f98c5c35a0bb588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:12 +0000

    Add package vallox-websocket-api/5.2.1/vallox_websocket_api-5.2.1-py3-none-any.whl

---
+++ packages/vallox-websocket-api/vallox_websocket_api-5.2.1-py3-none-any.whl/vallox_websocket_api/__init__.py
@@ -0,0 +1,23 @@
+from .client import Client, MetricValue
+from .exceptions import (
+    ValloxApiException,
+    ValloxException,
+    ValloxInvalidInputException,
+    ValloxWebsocketException,
+)
+from .vallox import Alarm, MetricData, Profile, Vallox
+
+__all__ = [
+    "Alarm",
+    "Client",
+    "MetricData",
+    "MetricValue",
+    "Profile",
+    "Vallox",
+    "ValloxException",
+    "ValloxInvalidInputException",
+    "ValloxApiException",
+    "ValloxWebsocketException",
+]
+
+__version__ = "5.2.1"
