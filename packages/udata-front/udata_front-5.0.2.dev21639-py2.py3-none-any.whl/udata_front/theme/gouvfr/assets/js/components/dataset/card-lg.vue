d2b3dd7b68a7c9de21265ad07b6d8f451b55e510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:11 +0000

    Add package udata-front/5.0.2.dev21639/udata_front-5.0.2.dev21639-py2.py3-none-any.whl

---
+++ packages/udata-front/udata_front-5.0.2.dev21639-py2.py3-none-any.whl/udata_front/theme/gouvfr/assets/js/components/dataset/card-lg.vue
@@ -0,0 +1,14 @@
+<template>
+  <DatasetCard :dataset :style :show-metrics :dataset-url="dataset.page" />
+</template>
+
+<script setup lang="ts">
+import { DatasetCard, type Dataset, type DatasetV2 } from '@etalab/data.gouv.fr-components';
+import type { StyleValue } from "vue";
+
+defineProps<{
+  dataset: Dataset | DatasetV2,
+  style?: StyleValue,
+  showMetrics?: boolean,
+}>();
+</script>
