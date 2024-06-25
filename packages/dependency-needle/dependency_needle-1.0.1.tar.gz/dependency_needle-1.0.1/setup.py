dff93f2ecc4b18f3acbe91f7f644fb91dfe7befe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 03:03:09 +0000

    Add package dependency-needle/1.0.1/dependency_needle-1.0.1.tar.gz

---
+++ packages/dependency-needle/dependency_needle-1.0.1.tar.gz/dependency_needle-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dependency_needle",
+    version="1.0.1",
+    description="Dependency injection container",
+    author="Your Name",
+    author_email="24torky@gmail.com",
+    packages=["dependency_needle"],
+    install_requires=[]
+)
