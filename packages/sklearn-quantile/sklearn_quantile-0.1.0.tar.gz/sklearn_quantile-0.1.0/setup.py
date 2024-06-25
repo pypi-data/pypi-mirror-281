b21b933b250add0de403e0b0506c97d97c80aab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 05:04:08 +0000

    Add package sklearn-quantile/0.1.0/sklearn_quantile-0.1.0.tar.gz

---
+++ packages/sklearn-quantile/sklearn_quantile-0.1.0.tar.gz/sklearn_quantile-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+
+extensions = [
+    Extension("sklearn_quantile.utils.weighted_quantile",
+              ["sklearn_quantile/utils/weighted_quantile.pyx"]),
+    Extension("sklearn_quantile.ensemble.quantile",
+              ["sklearn_quantile/ensemble/quantile.pyx"]),
+    Extension("sklearn_quantile.ensemble.maximum",
+              ["sklearn_quantile/ensemble/maximum.pyx"])
+]
+
+setup(
+    packages=find_packages(),
+    ext_modules=cythonize(extensions, annotate=True, language_level="3"),
+    include_dirs=[numpy.get_include()]
+)
