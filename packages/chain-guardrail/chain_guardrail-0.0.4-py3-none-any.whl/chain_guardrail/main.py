47f7a3857c088b30921e687487f806d13314cc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-25 06:04:23 +0000

    Add package chain-guardrail/0.0.4/chain_guardrail-0.0.4-py3-none-any.whl

---
+++ packages/chain-guardrail/chain_guardrail-0.0.4-py3-none-any.whl/chain_guardrail/main.py
@@ -0,0 +1,15 @@
+from langchain_core.messages import AIMessage
+
+from chain_guardrail.pii.presidio.parser import parse_text
+
+
+class ChainValidator:
+    def __init__(self):
+        self.anonymizer_dict = {}
+
+    def static_validator(self, ai_message: AIMessage) -> str:
+        """Parse the AI message."""
+        filtered_text, anonymizer_dict = parse_text(ai_message.content)
+        self.anonymizer_dict = anonymizer_dict
+        print(f'Anonymizer dict:\n {anonymizer_dict}')
+        return filtered_text
