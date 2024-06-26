c26a0289c29cc12e5eee339618e66e4afc0ddc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:20 +0000

    Add package dotenvx/0.0.0/dotenvx-0.0.0.tar.gz

---
+++ packages/dotenvx/dotenvx-0.0.0.tar.gz/dotenvx-0.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dotenvx",
+    version="0.0.0",
+    description="An enhanced dotenv library for Python",
+    long_description="This package provides extended functionalities for dotenv management in Python applications.",
+    author="Your Name",
+    author_email="bas@nijho.lt",
+    url="https://github.com/basnijholt/dotenvx",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
