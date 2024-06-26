8df12ecbd0a6283b920e4be58e6834727199cf5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:04:43 +0000

    Add package GPT-SoVITS-Infer/0.2.7/gpt_sovits_infer-0.2.7.tar.gz

---
+++ packages/GPT-SoVITS-Infer/gpt_sovits_infer-0.2.7.tar.gz/gpt_sovits_infer-0.2.7/src/gpt_sovits/infer/__init__.py
@@ -0,0 +1,9 @@
+from gpt_sovits.infer.inference import GPTSoVITSInference
+from gpt_sovits.infer.inference_pool import GPTSoVITSInferencePool
+from gpt_sovits.infer.interface import GPTSoVITSInterfaceSimple
+
+__all__ = [
+    "GPTSoVITSInference",
+    "GPTSoVITSInferencePool",
+    "GPTSoVITSInterfaceSimple",
+]
