6db2ae8301f436797f5d1de93d8781a139ca573a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:30 +0000

    Add package tgcnn-act-graph/0.3/tgcnn_act_graph-0.3-py3-none-any.whl

---
+++ packages/tgcnn-act-graph/tgcnn_act_graph-0.3-py3-none-any.whl/test_graphs/test_calculations.py
@@ -0,0 +1,169 @@
+import pytest # noqa: F401
+import numpy as np
+import pandas as pd
+import os
+print(os.getcwd())
+
+from tgcnn_act_graph import utils, max_act_diff, figures, calculations  # noqa: E402
+
+test_pats = np.array([[[[0, 0, 0], 
+                          [0, 0, 3], 
+                          [0, 0, 0]], [[0, 0, 0],
+                                       [0, 0, 0],
+                                       [0, 4, 4]], [[0, 0, 0], 
+                                                    [0, 2, 0], 
+                                                    [0, 2, 0]], [[0, 0, 0], 
+                                                                 [5, 0, 5], 
+                                                                 [0, 0, 0]]],                        
+                        [[[1, 0, 0], 
+                          [1, 0, 0], 
+                          [1, 0, 0]], [[0, 0, 0],
+                                       [0, 8, 0],
+                                       [0, 0, 0]], [[0, 11, 0], 
+                                                    [0, 0, 0], 
+                                                    [0, 11, 0]], [[0, 0, 30], 
+                                                                 [0, 0, 30], 
+                                                                 [0, 0, 30]]],                       
+                          
+                          [[[0, 0, 9], 
+                          [0, 0, 0], 
+                          [0, 0, 0]], [[0, 0, 0],
+                                       [7, 0, 0],
+                                       [7, 0, 0]], [[0, 0, 0], 
+                                                    [0, 0, 2], 
+                                                    [0, 0, 0]], [[0, 0, 0], 
+                                                                 [1, 0, 0], 
+                                                                 [0, 0, 0]]],                          
+                          [[[1, 1, 1], 
+                          [0, 0, 0], 
+                          [0, 0, 0]], [[0, 0, 1],
+                                       [0, 0, 1],
+                                       [0, 0, 0]], [[0, 0, 0], 
+                                                    [0, 1, 0], 
+                                                    [0, 1, 0]], [[0, 2, 0], 
+                                                                 [0, 0, 0], 
+                                                                 [0, 2, 0]]]]
+                        
+                        )
+
+test_filts = np.array([[[[0, 0, 1], 
+                    [0, 1, 0], 
+                    [0, 0, 1]], [[1, 1, 0], 
+                                 [0, 0, 0], 
+                                 [0, 0, 0]]], 
+                   [[[0, 1, 0], 
+                    [0, 1, 0], 
+                    [0, 0, 1]], [[1, 0, 0], 
+                                 [0, 0, 0], 
+                                 [0, 0, 0]]]])
+
+
+test_pat = np.array([[[0, 0, 0], 
+                        [0, 0, 3], 
+                        [0, 0, 0]], [[0, 0, 0],
+                                    [0, 0, 0],
+                                    [0, 4, 4]], [[0, 0, 0], 
+                                                [0, 2, 0], 
+                                                [0, 2, 0]], [[0, 0, 0], 
+                                                                [5, 0, 5], 
+                                                                [0, 0, 0]]])
+
+test_filt = np.array([[[0, 0, 1], 
+                        [0, 1, 0], 
+                        [0, 0, 1]], [[1, 1, 0], 
+                                        [0, 0, 0], 
+                                        [0, 0, 0]]])
+
+labels = [0, 1, 1, 0] #hip_replacement labels
+def test_y_coord_list_gen():
+    """Test the lists generated for the y coordinates of the nodes
+    are generating as expected.
+    """
+    auto_seq = utils.generate_pos_sequence(10)
+
+    test_ans1 = auto_seq[0]
+    true_ans1 = [0]
+
+    assert test_ans1 == true_ans1, "The two lists should be equal"
+
+    test_ans2 = auto_seq[4]
+    true_ans2 = [-2, -1, 0, 1, 2]
+
+    assert test_ans2 == true_ans2, "The two lists should be equal"
+
+    test_ans3 = auto_seq[7]
+    true_ans3 = [-4, -3, -2, -1, 1, 2, 3, 4]
+
+    assert test_ans3 == true_ans3, "The two lists should be equal"
+
+
+def test_pos_dict():
+    """ Test that the y coordinate dictionaries have the same keys and values.
+    The dictionaries may not match exactly as the y coordinate is dependent on the order of the
+    DataFrame.
+    """
+    # How pos dictionary should look:
+    pos_true = {
+        '1_v0': (0, 0),
+        '2_v1': (1, 0),
+        '1_v2': (2, 1),
+        '2_v2': (2, -1),
+        '1_v3': (3, 0),
+        '0_v4': (4, 1),
+        '2_v4': (4, -1)
+    }
+
+    # Code chunk to get y position dictionary
+    mean_activation_df = max_act_diff.max_act_diff_calc('Hip Replacement', test_pats, test_filts, labels, verbose=False, show_plot=False)
+    max_act_filt = utils.get_max_act_filt(mean_activation_df, test_filts)
+    act_graph = calculations.get_act_graph_array(test_pat, max_act_filt)
+    edges_df = utils.create_edges_df(test_pat, act_graph)
+    pos_df = utils.create_position_df(edges_df)
+    pos_list = utils.generate_pos_sequence(pos_df['max_codes_per_visit'].max())
+    pos_df = utils.map_y_coord_to_node(pos_df, pos_list)
+    pos_test = utils.create_pos_dict(pos_df)
+
+
+    pos_true_keys = set(pos_true.keys())
+    pos_test_keys = set(pos_test.keys())
+
+    pos_true_values = set(pos_true.values())
+    pos_test_values = set(pos_test.values())
+
+    assert pos_true_keys == pos_test_keys, "The keys of dictionaries of pos_true and pos_test should be equal"
+    assert pos_true_values == pos_test_values, "The values of dictionaries of pos_true and pos_test should be equal"
+
+
+def test_edge_weight():
+    """Test that the weight of the activated edge in the test patient is calculated correctly.
+    """
+
+    true_weight1 = 2.0
+    
+    mean_activation_df = max_act_diff.max_act_diff_calc('Hip Replacement', test_pats, test_filts, labels, verbose=False, show_plot=False)
+    max_act_filt = utils.get_max_act_filt(mean_activation_df, test_filts)
+    act_graph = calculations.get_act_graph_array(test_pat, max_act_filt)
+    edges_df = utils.create_edges_df(test_pat, act_graph)
+
+    test_weight1 = edges_df.loc[edges_df['activated']==1, 'weight'].astype(float).values
+
+    assert true_weight1 == test_weight1, "The weight of this edge should be 2."
+
+def test_extract_visit_num():
+    """Test the extracted visit number is the visit number and not the node number.
+    """
+    string = "5_v2"
+    true_visit_num = 2
+    false_visit_num = 5
+    test_visit_num = utils.extract_visit_number(string)
+
+    assert true_visit_num == test_visit_num, "True visit number should equal test visit number"
+    assert false_visit_num != test_visit_num, "True visit number should equal test visit number not EHR code number"
+
+# TODO: Test that the repeat_array_fractional function is repeating correctly
+
+
+# def test_repeat_array_fractional():
+#     """Test to make sure filter is repeating correctly.
+#     """
+    
---
+++ packages/tgcnn-act-graph/tgcnn_act_graph-0.3-py3-none-any.whl/tgcnn_act_graph/utils.py
@@ -0,0 +1,174 @@
+import numpy as np
+import pandas as pd
+
+
+def select_patient(input_tensors:np.array, num:int):
+    """Select a single patient. Choose an integer as the patient number.
+
+    Args:
+        input_tensors (np.array): 4D numpy array representing patients EHR graphs.
+        num (int): patient number to choose.
+
+    Returns:
+        np.array: 3D array representing an individuals EHR graph.
+    """
+    return input_tensors[num]
+
+def extract_visit_number(s:str):
+    """Extract the visit number from a string
+
+    Args:
+        s (str): string to split
+
+    Returns:
+        int: visit number
+    """
+    return int(s.split('_')[1][1:])
+
+def get_max_act_filt(mean_activation_df:pd.DataFrame, filters:np.array):
+    """Using the dataframe of acitvation difference select the filter with the largest difference.
+
+    Args:
+        mean_activation_df (pd.DataFrame): dataframe with difference in activation between positive and negative class 
+        for each filter.
+        filters (np.array): 4D array with 3D filters.
+
+    Returns:
+        np.array: 3D array representing one filter.
+    """
+
+    max_idx = mean_activation_df['Difference'].idxmax()
+    max_act_filt_num = mean_activation_df.loc[max_idx, 'Filter']
+    max_act_filt =  filters[max_act_filt_num-1] # minus 1 as we don't have a filter called 0
+    print(f"Filter {max_act_filt_num} had the largest difference in activation.")
+    return max_act_filt
+
+
+
+def create_edges_df(patient_graph:np.array, act_graph:np.array):
+    """Create a DataFrame of the edges of the patient graph, including the start and end nodes
+    whether the edge is activated (more than 0), the 'weight' of the activation, and the time 
+    between visits.
+
+    Args:
+        patient_graph (np.array): 3D numpy array showing the patients health codes over time.
+        act_graph (np.array): 3D numpy array showing graph activation.
+
+    Returns:
+        DataFrame: with columns start_node, end_node, activated, weight, time_between.
+    """
+    num_edges = np.count_nonzero(patient_graph)
+    edges_df = pd.DataFrame(columns=['start_node', 'end_node', 'activated', 'weight', 'time_between'], index=range(num_edges))
+    num_nodes = patient_graph.shape[1]
+    timesteps = patient_graph.shape[0]
+
+    row_num = 0
+    for t in range(timesteps):
+        for i in range(num_nodes):
+            for j in range(num_nodes):
+                if patient_graph[t, i, j] != 0:
+                    if t == 0:
+                        start_node_v = 0
+                        end_node_v = 1
+                    else:
+                        start_node_v = t
+                        end_node_v = t+1
+                    
+                    edges_df.at[row_num, 'end_node'] = f'{j}_v{end_node_v}' #[row num, col name]
+                    edges_df.at[row_num, 'weight'] = act_graph[t, i, j]
+                    edges_df.at[row_num, 'time_between'] = patient_graph[t, i, j] #add a column for the edge label too (time between from original graph)
+                    edges_df.at[row_num, 'start_node'] = f'{i}_v{start_node_v}' #[row num, col name]
+                    
+                    row_num += 1
+
+
+    # activated_graph col is more than 0, the activated column = 1, otherwise it is 0
+    edges_df['activated'] = edges_df['weight'].apply(lambda x: 0 if x == 0 else 1)  
+    # change 0 weights to 0.5 so we can still see them on the graph figure
+    edges_df['weight'] = edges_df['weight'].apply(lambda x: 0.5 if x == 0 else x)
+    return edges_df
+
+
+def create_position_df(edges_df:np.array):
+    """Create a DataFrame to find the number of nodes per visit.
+
+    Args:
+        edges_df (pd.DataFrame): DataFrame with columns start_node, end_node, activated, weight, time_between.
+
+    Returns:
+        pd.DataFrame: with columns showing node name (node), visit number (x), node number in visits (cumulative count),
+        and maximum codes per visit.
+    """
+    pos_df = edges_df[['start_node', 'end_node']].stack().drop_duplicates().reset_index(drop=True)
+    pos_df = pos_df.to_frame(name='node')
+    pos_df['x'] = pos_df['node'].apply(extract_visit_number)
+    pos_df['cumulative_count'] = pos_df.groupby('x').cumcount()
+    pos_df['max_codes_per_visit'] = pos_df.groupby('x')['cumulative_count'].transform('max') + 1
+
+    return pos_df
+
+
+def generate_pos_sequence(x:int):
+    """Generate a list of lists to get y coordinate positions for the nodes
+     based on the number of events recorded per visit.
+
+    Args:
+        x (int): maximum number of nodes in any one visit
+
+    Returns:
+        List: List of lists of y coordinates index mapping to the max nodes for
+               each visit.
+    """
+    sequence = []
+    for i in range(x):
+        if i % 2 == 0:  # Even index, include zero
+            sequence.append(list(range(-i // 2, i // 2 + 1)))
+        else:  # Odd index, exclude zero
+            sublist = list(range(-(i // 2 + 1), i // 2 + 2))
+            sublist.remove(0)
+            sequence.append(sublist)
+    return sequence
+
+
+def get_pos_y_value_per_node(row, pos_list:list):
+    """Get the y position for each node. Use max_codes_per_visit column to select the sublist 
+    and the cumulative_count to get the position from the sublist.
+
+    Args:
+        row (pd.Series): row of the DataFrame
+
+    Returns:
+        int: y coordinate position 
+    """
+    cum_count = row['cumulative_count']
+    max_codes = row['max_codes_per_visit']
+    return pos_list[max_codes - 1][cum_count]
+
+def map_y_coord_to_node(pos_df:pd.DataFrame, pos_list:list):
+    """Map the y coordinates to the relevant node and correct row.
+
+    Args:
+        pos_df (pd.DataFrame): columns with node name and x coordinate.
+        pos_list (list of lists): List of lists of y coordinates index mapping to the max nodes for
+               each visit.
+
+    Returns:
+        pd.DataFrame: dataframe with x and y coordinates for node plotting.
+    """
+    pos_df['y'] = pos_df.apply(lambda row: get_pos_y_value_per_node(row, pos_list), axis=1)
+    return pos_df
+
+def create_pos_dict(pos_df:pd.DataFrame):
+    """Make a dictionary with the node name as the key and the x and y coordinates 
+    as a tuple value.
+
+    Args:
+        pos_df (pd.DataFrame): dataframe with columns for the node name, x coordinates,
+        and y coordinates.
+
+    Returns:
+        dict: dictionary of node: (x,y)
+    """
+    # the visit number is x and the y value is the number of nodes with the same visit number
+    pos = pos_df.set_index('node')[['x', 'y']].apply(tuple, axis=1).to_dict()
+    return pos
