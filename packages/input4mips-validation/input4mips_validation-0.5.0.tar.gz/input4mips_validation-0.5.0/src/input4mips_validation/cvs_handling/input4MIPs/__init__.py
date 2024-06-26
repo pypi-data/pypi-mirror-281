041f1b4cd6282a3162262c3f2889bd91878126f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:23 +0000

    Add package input4mips-validation/0.5.0/input4mips_validation-0.5.0.tar.gz

---
+++ packages/input4mips-validation/input4mips_validation-0.5.0.tar.gz/input4mips_validation-0.5.0/src/input4mips_validation/cvs_handling/input4MIPs/__init__.py
@@ -0,0 +1,28 @@
+"""
+input4MIPs CVs handling
+"""
+from __future__ import annotations
+
+from input4mips_validation.cvs_handling.input4MIPs.activity_id import (
+    ACTIVITY_ID_FILENAME,
+    ActivityIDEntries,
+    ActivityIDEntry,
+    ActivityIDValues,
+)
+from input4mips_validation.cvs_handling.input4MIPs.source_id import (
+    SOURCE_ID_FILENAME,
+    SourceIDEntries,
+    SourceIDEntry,
+    SourceIDValues,
+)
+
+__all__ = [
+    "ACTIVITY_ID_FILENAME",
+    "ActivityIDEntries",
+    "ActivityIDEntry",
+    "ActivityIDValues",
+    "SOURCE_ID_FILENAME",
+    "SourceIDEntry",
+    "SourceIDEntries",
+    "SourceIDValues",
+]
