043dccfb12d734f565a0f9ec0c8d715503f1c4a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:05:13 +0000

    Add package notifications-android-tv/1.2.1/notifications_android_tv-1.2.1.tar.gz

---
+++ packages/notifications-android-tv/notifications_android_tv-1.2.1.tar.gz/notifications_android_tv-1.2.1/notifications_android_tv/exceptions.py
@@ -0,0 +1,21 @@
+"""Exceptions raised by the library."""
+
+
+class NotificationException(Exception):
+    """Base class for all exceptions raised by the library."""
+
+
+class ConnectError(NotificationException):
+    """Exception raised for connection error."""
+
+
+class InvalidResponse(NotificationException):
+    """Exception raised for invalid response."""
+
+
+class InvalidImage(NotificationException):
+    """Exception raised for invalid image."""
+
+
+class InvalidImageData(NotificationException):
+    """Exception raised for invalid image data is provided."""
