5581ccae519658b1f5ad94dc2e8241da6c0abc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:30 +0000

    Add package faststream/0.5.14/faststream-0.5.14.tar.gz

---
+++ packages/faststream/faststream-0.5.14.tar.gz/faststream-0.5.14/faststream/cli/supervisors/multiprocess.py
@@ -0,0 +1,40 @@
+from typing import TYPE_CHECKING, Any, List, Tuple
+
+from faststream.cli.supervisors.basereload import BaseReload
+from faststream.log import logger
+
+if TYPE_CHECKING:
+    from multiprocessing.context import SpawnProcess
+
+    from faststream.types import DecoratedCallable
+
+
+class Multiprocess(BaseReload):
+    """A class to represent a multiprocess."""
+
+    def __init__(
+        self,
+        target: "DecoratedCallable",
+        args: Tuple[Any, ...],
+        workers: int,
+    ) -> None:
+        super().__init__(target, args, None)
+
+        self.workers = workers
+        self.processes: List[SpawnProcess] = []
+
+    def startup(self) -> None:
+        logger.info(f"Started parent process [{self.pid}]")
+
+        for _ in range(self.workers):
+            process = self._start_process()
+            logger.info(f"Started child process [{process.pid}]")
+            self.processes.append(process)
+
+    def shutdown(self) -> None:
+        for process in self.processes:
+            process.terminate()
+            logger.info(f"Stopping child process [{process.pid}]")
+            process.join()
+
+        logger.info(f"Stopping parent process [{self.pid}]")
