3a6c5e7b5e700859301fa2db6ac25b7d5b8da767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:28 +0000

    Add package kfish/99.0.202406251112/kfish-99.0.202406251112.tar.gz

---
+++ packages/kfish/kfish-99.0.202406251112.tar.gz/kfish-99.0.202406251112/kvirt/cluster/microshift/scripts/03_microshift.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ -d /root/manifests ] ; then
+ mkdir -p /etc/microshift/manifests
+ cp /root/manifests/*y*ml /etc/microshift/manifests
+fi
+
+dnf -y install microshift {{ 'microshift-olm' if olm|default(False) else '' }}
+BASEDOMAIN={{ "$(hostname)" if sslip else cluster + '.' + domain }}
+sed -i "s@#baseDomain: .*@baseDomain: $BASEDOMAIN@" /etc/microshift/config.yaml.default
+sed -i "s@#subjectAltNames:.*@subjectAltNames: \[\"api.$BASEDOMAIN\"\]@" /etc/microshift/config.yaml.default
+mv /etc/microshift/config.yaml.default /etc/microshift/config.yaml
+systemctl enable --now microshift
