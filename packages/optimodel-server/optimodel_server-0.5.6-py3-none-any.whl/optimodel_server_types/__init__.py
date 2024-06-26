b6ca55174e1c92890150483cd65eb29266fecd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 14:04:33 +0000

    Add package optimodel-server/0.5.6/optimodel_server-0.5.6-py3-none-any.whl

---
+++ packages/optimodel-server/optimodel_server-0.5.6-py3-none-any.whl/optimodel_server_types/__init__.py
@@ -0,0 +1,50 @@
+import enum
+import json
+from pydantic import BaseModel
+
+
+class ModelTypes(enum.Enum):
+    llama_3_8b_instruct = "llama_3_8b_instruct"
+    llama_3_8b_chat = "llama_3_8b_chat"
+    llama_3_70b_instruct = "llama_3_70b_instruct"
+    gpt_4 = "gpt_4"
+    gpt_3_5_turbo = "gpt_3_5_turbo"
+
+
+class SpeedPriority(enum.Enum):
+    low = "low"
+    high = "high"
+
+
+class ModelMessage(BaseModel):
+    role: str
+    content: str
+
+
+class TogetherAICredentials(BaseModel):
+    togetherApiKey: str
+
+
+class OpenAICredentials(BaseModel):
+    openAiKey: str
+
+
+class AWSBedrockCredentials(BaseModel):
+    awsAccessKeyId: str
+    awsSecretKey: str
+
+
+class QueryBody(BaseModel):
+    messages: list[ModelMessage]
+    modelToUse: str
+    speedPriority: SpeedPriority | None = None
+    temperature: float = 0.2
+    maxGenLen: int = 1024
+
+    """
+    If we are running in SAAS mode, we'll expect each request to bring their
+    own credentials
+    """
+    credentials: (
+        list[TogetherAICredentials | OpenAICredentials | AWSBedrockCredentials] | None
+    ) = None
