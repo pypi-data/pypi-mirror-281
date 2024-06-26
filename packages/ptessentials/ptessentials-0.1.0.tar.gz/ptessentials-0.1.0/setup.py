1ad3752c8aef80a12e88d170c6b1ce0806a60a58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:04:40 +0000

    Add package ptessentials/0.1.0/ptessentials-0.1.0.tar.gz

---
+++ packages/ptessentials/ptessentials-0.1.0.tar.gz/ptessentials-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ptessentials',
+    version='0.1.0',
+    description='A PixelTycoons Dev Best Friend.',
+    url='https://github.com/funkaclau',
+    author='funkaclau',
+    packages=find_packages(),
+    install_requires=[
+        "waxnftdispatcher==0.3.5",
+        "waxtion==0.1.4",
+        "waxfetcher"
+    ],
+    include_package_data=True
+)
