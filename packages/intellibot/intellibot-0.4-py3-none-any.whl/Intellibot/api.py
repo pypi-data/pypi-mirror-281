eb236c045ce17004acc0659b0cbac778817c01e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:32 +0000

    Add package intellibot/0.4/intellibot-0.4-py3-none-any.whl

---
+++ packages/intellibot/intellibot-0.4-py3-none-any.whl/Intellibot/api.py
@@ -0,0 +1,115 @@
+import requests
+import json
+import os
+
+BASE_URL = "https://intellihack-backend-rcbmvyttca-uc.a.run.app"
+BASE_AGENT_URL = "https://intellihack-research-agent-rcbmvyttca-uc.a.run.app"
+CREDENTIALS_FILE = "credentials.json"
+
+class IntelliBotAPI:
+    def __init__(self):
+        self.access_token = None
+        self.user_id = None
+        self.username = None
+        self.load_credentials()
+
+    def load_credentials(self):
+        if os.path.exists(CREDENTIALS_FILE):
+            with open(CREDENTIALS_FILE, 'r') as file:
+                credentials = json.load(file)
+                self.access_token = credentials.get('access_token')
+                self.user_id = credentials.get('user_id')
+                self.username = credentials.get('username')
+
+    def save_credentials(self, credentials):
+        with open(CREDENTIALS_FILE, 'w') as file:
+            json.dump(credentials, file)
+
+    def connect(self, username, password):
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        data = {"username": username, "password": password}
+        response = requests.post(BASE_URL + "/login", data=data, headers=headers)
+        if response.status_code == 200:
+            auth_data = response.json()
+            self.access_token = auth_data["access_token"]
+            self.user_id = auth_data["id"]
+            self.username = auth_data["email"]
+            self.save_credentials({
+                "access_token": self.access_token,
+                "user_id": self.user_id,
+                "username": self.username,
+            })
+        return response
+
+    def get_active_user_details(self):
+        if not self.access_token or not self.user_id:
+            raise Exception("User not authenticated. Please connect first.")
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        response = requests.get(BASE_URL + f"/user/{self.user_id}", headers=headers)
+        return response
+
+    def chat(self, message, history=None):
+        if not self.access_token or not self.user_id or not self.username:
+            raise Exception("User not authenticated. Please connect first.")
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        json_data = {
+            "user_id": self.user_id,
+            "username": self.username,
+            "message": message,
+            "history": history if history else "",
+        }
+        return requests.post(BASE_URL + "/query/chat", json=json_data, headers=headers)
+
+    def initialize_crew(self, config_data):
+        if not self.access_token or not self.user_id:
+            raise Exception("User not authenticated. Please connect first.")
+
+        config_data['user_id'] = self.user_id
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+
+        response = requests.post(
+            BASE_AGENT_URL + f"/agentConfiguration/",
+            json=config_data,
+            headers=headers,
+        )
+        return response
+
+    def get_active_projects(self):
+        if not self.access_token or not self.user_id:
+            raise Exception("User not authenticated. Please connect first.")
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        response = requests.get(
+            f"{BASE_AGENT_URL}/agentConfiguration/projects/{self.user_id}",
+            headers=headers,
+        )
+        return response
+
+    def execute_project(self, project_name):
+        if not self.access_token:
+            raise Exception("User not authenticated. Please connect first.")
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.access_token}",
+        }
+        response = requests.get(
+            f"{BASE_AGENT_URL}/agentConfiguration/execute/{project_name}",
+            headers=headers,
+        )
+        return response
