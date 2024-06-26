a3ec4dba821f3a61f2a9b5df345a630a619a980f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:04:13 +0000

    Add package odiff/0.1.6/odiff-0.1.6-py3-none-any.whl

---
+++ packages/odiff/odiff-0.1.6-py3-none-any.whl/odiff/__init__.py
@@ -0,0 +1,17 @@
+from odiff.odiff import (
+    odiff,
+    diff_dicts,
+    diff_lists,
+    diff_values,
+)
+
+from odiff.options import Config, OutputType
+
+__all__ = [
+    "odiff",
+    "diff_dicts",
+    "diff_lists",
+    "diff_values",
+    "Config",
+    "OutputType",
+]
