3fc72646331225bd0deddff69bc00b4b954bdd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:04:48 +0000

    Add package givemehelp/0.70/givemehelp-0.70.tar.gz

---
+++ packages/givemehelp/givemehelp-0.70.tar.gz/givemehelp-0.70/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='givemehelp',
+    version='0.70',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'boto3',
+        'google-generativeai',
+        'spacy'
+    ], 
+    entry_points={
+        "console_scripts":[
+            "givemehelp = givemehelp:retreiveSecretKey",
+        ],
+    },
+)
