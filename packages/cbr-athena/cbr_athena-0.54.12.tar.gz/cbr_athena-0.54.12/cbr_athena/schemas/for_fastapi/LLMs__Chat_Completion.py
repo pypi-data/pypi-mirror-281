26bd6f7b0177ba924b8577963dc67a82378944e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 03:05:38 +0000

    Add package cbr-athena/0.54.12/cbr_athena-0.54.12.tar.gz

---
+++ packages/cbr-athena/cbr_athena-0.54.12.tar.gz/cbr_athena-0.54.12/cbr_athena/schemas/for_fastapi/LLMs__Chat_Completion.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from cbr_athena.schemas.for_fastapi.GPT_Prompt_With_System_And_History import GPT_Prompt_With_System_And_History
+
+
+class LLMs__Chat_Completion(GPT_Prompt_With_System_And_History):
+    llm_platform: Optional[str] = None
+    llm_provider: Optional[str] = None
+    llm_model   : Optional[str] = None
