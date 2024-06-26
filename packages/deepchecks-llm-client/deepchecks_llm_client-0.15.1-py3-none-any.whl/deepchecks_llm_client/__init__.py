2d6efb9b6c2245b2dfc71cf083f80f1a533caa06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:42 +0000

    Add package deepchecks-llm-client/0.15.1/deepchecks_llm_client-0.15.1-py3-none-any.whl

---
+++ packages/deepchecks-llm-client/deepchecks_llm_client-0.15.1-py3-none-any.whl/deepchecks_llm_client/__init__.py
@@ -0,0 +1,11 @@
+# pylint: disable=used-relative-import,cyclic-import
+
+import dunamai as _dunamai
+
+from . import api, client, data_types, utils
+from .sdk.langchain.callbacks.deepchecks_callback_handler import DeepchecksCallbackHandler
+
+__version__ = _dunamai.get_version("deepchecks_llm_client",
+                                   third_choice=_dunamai.Version.from_any_vcs).serialize(style=_dunamai.Style.Pep440)
+
+
