eaadd8e38439bbb73e713f5faec30109d13cae5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:05:21 +0000

    Add package react-frontend/20240625164333/react_frontend-20240625164333-py3-none-any.whl

---
+++ packages/react-frontend/react_frontend-20240625164333-py3-none-any.whl/react_frontend/entrypoint.js
@@ -0,0 +1,30 @@
+
+try {
+    var parentStyleSheets = parent.document.styleSheets;
+    var cssString = "";
+    for (var i = 0, count = parentStyleSheets.length; i < count; ++i) {
+        if (parentStyleSheets[i].cssRules) {
+            var cssRules = parentStyleSheets[i].cssRules;
+            for (var j = 0, countJ = cssRules.length; j < countJ; ++j)
+                cssString += cssRules[j].cssText;
+        }
+        else
+            cssString += parentStyleSheets[i].cssText;  // IE8 and earlier
+    }
+    var style = document.createElement("style");
+    style.type = "text/css";
+    try {
+        style.innerHTML = cssString;
+    }
+    catch (ex) {
+        style.styleSheet.cssText = cssString;  // IE8 and earlier
+    }
+    document.getElementsByTagName("head")[0].appendChild(style);
+    
+    new Function("import('/reactfiles/frontend/main.b6d10dd0.js')")();
+} catch (err) {
+    var el = document.createElement('script');
+    el.src = '/reactfiles/frontend/main.b6d10dd0.js';
+    el.type = 'module';
+    document.body.appendChild(el);
+}
