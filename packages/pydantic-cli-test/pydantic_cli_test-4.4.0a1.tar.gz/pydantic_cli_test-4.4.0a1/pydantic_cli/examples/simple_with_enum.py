b5547d6c9f1047b354b1b6bd9614652e55193433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:16 +0000

    Add package pydantic-cli-test/4.4.0a1/pydantic_cli_test-4.4.0a1.tar.gz

---
+++ packages/pydantic-cli-test/pydantic_cli_test-4.4.0a1.tar.gz/pydantic_cli_test-4.4.0a1/pydantic_cli/examples/simple_with_enum.py
@@ -0,0 +1,42 @@
+from enum import Enum, auto
+from typing import Set
+
+from pydantic_cli._compat import PYDANTIC_V2
+if PYDANTIC_V2:
+    from pydantic.v1 import BaseModel
+else:
+    from pydantic import BaseModel
+
+from pydantic_cli import run_and_exit
+
+
+class Mode(str, Enum):
+    """Note that if you use `auto`, you must specify the enum
+    using the int value (as a string).
+    """
+
+    alpha = auto()
+    beta = auto()
+
+
+class State(str, Enum):
+    """Note, this is case sensitive when providing it from the commandline"""
+
+    RUNNING = "RUNNING"
+    FAILED = "FAILED"
+    SUCCESSFUL = "SUCCESSFUL"
+
+
+class Options(BaseModel):
+    states: Set[State]
+    mode: Mode
+    max_records: int = 100
+
+
+def example_runner(opts: Options) -> int:
+    print(f"Mock example running with {opts}")
+    return 0
+
+
+if __name__ == "__main__":
+    run_and_exit(Options, example_runner, description=__doc__, version="0.1.0")
