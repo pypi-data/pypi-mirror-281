550a66476372ddf7563fca9935d2dce1ab7d59a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:04:57 +0000

    Add package queshy/0.1.0/queshy-0.1.0.tar.gz

---
+++ packages/queshy/queshy-0.1.0.tar.gz/queshy-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='queshy',
+version='0.1.0',
+author='queshy',
+author_email='lukemarshall511@gmail.com',
+description='A test package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
