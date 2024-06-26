5440781ea363370369083e886961083163eee421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 04:04:14 +0000

    Add package patchwork-cli/0.0.19/patchwork_cli-0.0.19-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.19-py3-none-any.whl/patchwork/steps/CallLLM/typed.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing_extensions import Iterable, NotRequired, TypedDict
+
+
+class CallLLMInputs(TypedDict):
+    prompt_file: NotRequired[str]
+    prompts: NotRequired[Iterable[dict]]
+    model: NotRequired[str]
+    allow_truncated: NotRequired[bool]
+    model_args: NotRequired[str]
+    client_args: NotRequired[str]
+    openai_api_key: NotRequired[str]
+    patched_api_key: NotRequired[str]
+    google_api_key: NotRequired[str]
+
+
+class CallLLMOutputs(TypedDict):
+    new_code: Iterable[dict]
+    openai_responses: list[str]
