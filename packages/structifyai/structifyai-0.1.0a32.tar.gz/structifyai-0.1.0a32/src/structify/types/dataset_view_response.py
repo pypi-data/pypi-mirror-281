087c1e06e901b5c62fac8d90cc9cc62ac240f90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 03:04:40 +0000

    Add package structifyai/0.1.0a32/structifyai-0.1.0a32.tar.gz

---
+++ packages/structifyai/structifyai-0.1.0a32.tar.gz/structifyai-0.1.0a32/src/structify/types/dataset_view_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+
+from .entity import Entity
+
+__all__ = ["DatasetViewResponse"]
+
+DatasetViewResponse = List[Entity]
