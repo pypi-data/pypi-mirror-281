c0f1f6f22b677b11116021891cd0e69c5c388184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:38 +0000

    Add package sc3dg/0.0.29/sc3dg-0.0.29.tar.gz

---
+++ packages/sc3dg/sc3dg-0.0.29.tar.gz/sc3dg-0.0.29/sc3dg/commands/make_index.py
@@ -0,0 +1,16 @@
+# Description: This script is used to make index for bowtie2 or bwa
+import click
+import time
+from utils.download import make_index
+
+@click.command()
+@click.option('-g', '--genome', type=click.Choice(['hg38', 'hg19', 'mm10', 'mm9']), required=True, help='Choose from hg38, hg19, mm10, mm9')
+@click.option('-a', '--aligner', type=click.Choice(['bowtie2', 'bwa']), required=True, help='Choose from bowtie2, bwa')
+@click.option('-p', '--path', required=True, help='Path to save index')
+def make_index(genome, aligner, path):
+    make_index(path, genome, aligner)
+
+if __name__ == '__main__':
+    start_time = time.time()
+    main()
+    print('=============time spent:', time.time() - start_time, 'seconds===================')
