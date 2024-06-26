472d5601e149531f3416d358957604394f0237ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:03:27 +0000

    Add package structuretoolkit/0.0.26/structuretoolkit-0.0.26-py3-none-any.whl

---
+++ packages/structuretoolkit/structuretoolkit-0.0.26-py3-none-any.whl/structuretoolkit/common/pyscal.py
@@ -0,0 +1,22 @@
+from ase.atoms import Atoms
+
+
+def ase_to_pyscal(structure: Atoms):
+    """
+    Converts atoms to ase atoms and than to a pyscal system.
+    Also adds the pyscal publication.
+
+    Args:
+        structure (ase.atoms.Atoms): Structure to convert.
+
+    Returns:
+        Pyscal system: See the pyscal documentation.
+    """
+    import pyscal.core as pc
+
+    sys = pc.System()
+    sys.read_inputfile(
+        filename=structure,
+        format="ase",
+    )
+    return sys
