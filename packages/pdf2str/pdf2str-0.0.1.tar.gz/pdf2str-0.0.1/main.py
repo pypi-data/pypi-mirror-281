2ab93b75d951761f1941b4fa690d63c1b9c55a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 04:04:09 +0000

    Add package pdf2str/0.0.1/pdf2str-0.0.1.tar.gz

---
+++ packages/pdf2str/pdf2str-0.0.1.tar.gz/pdf2str-0.0.1/main.py
@@ -0,0 +1,7 @@
+from pdftostring.src.pdf2str.functions import *
+
+#texto = pdf_to_str(r'C:\Users\bruno\Desktop\Scripts\9. LLMs\7. ChatGPT passa na OAB\5. Provas anteriores\35 a 40\Provas\2023-40.pdf', 2, 2, progress=False)
+
+#print(texto[:100])
+
+pdf_to_txt(r'C:\Users\bruno\Desktop\Scripts\9. LLMs\7. ChatGPT passa na OAB\5. Provas anteriores\35 a 40\Provas\2023-40.pdf')
