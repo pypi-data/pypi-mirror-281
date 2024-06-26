20e92f07866be4a37710a7552ec44a0aab4f0ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 03:05:45 +0000

    Add package lyynew/1.1/lyynew-1.1.tar.gz

---
+++ packages/lyynew/lyynew-1.1.tar.gz/lyynew-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lyynew',
+    version='1.1',
+    author='yy',
+    author_email='',
+    description='lyy new data for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+    ],
+)
