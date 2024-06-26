2a1a4faa6f55df360307bfb2acff5c19ebdeabf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:04:54 +0000

    Add package candore/1.3.0/candore-1.3.0.tar.gz

---
+++ packages/candore/candore-1.3.0.tar.gz/candore-1.3.0/candore/config.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from pathlib import PurePath
+
+from dynaconf import Dynaconf
+from dynaconf.validator import Validator
+
+CURRENT_DIRECTORY = Path().resolve()
+
+
+def candore_settings(option_settings_file=None, option_components_file=None):
+    settings_file = (
+        PurePath(option_settings_file)
+        if option_settings_file
+        else PurePath(CURRENT_DIRECTORY, "settings.yaml")
+    )
+    components_file = (
+        PurePath(option_components_file)
+        if option_components_file
+        else PurePath(CURRENT_DIRECTORY, "components.yaml")
+    )
+    # Initialize and Configure Settings
+    settings = Dynaconf(
+        core_loaders=["YAML"],
+        envvar_prefix="CANDORE",
+        settings_files=[settings_file, components_file],
+        preload=["conf/*.yaml"],
+        envless_mode=True,
+        lowercase_read=True,
+    )
+    validate_settings(settings)
+    return settings
+
+
+def validate_settings(settings):
+    provider_settings = [
+        f"candore.{setting_key}" for setting_key in settings.to_dict().get("CANDORE")
+    ]
+    settings.validators.register(Validator(*provider_settings, ne=None))
+    try:
+        settings.validators.validate()
+    except Exception as ecc:
+        raise ecc
