c37a2896ab5c34c90ca020514068103bc21f4ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:05:11 +0000

    Add package rag-helper/0.0.6/rag_helper-0.0.6.tar.gz

---
+++ packages/rag-helper/rag_helper-0.0.6.tar.gz/rag_helper-0.0.6/rag_helper/models.py
@@ -0,0 +1,18 @@
+from lancedb.pydantic import LanceModel, Vector
+from lancedb.embeddings import get_registry
+from enum import Enum
+
+class MacroModel(LanceModel):
+    query: str
+    selected_chunk_ids: list[str]
+    vector: Vector(1024) # type: ignore
+
+
+class QueryItem(LanceModel):
+    query: str
+    selected_chunk_ids: list[str]
+
+class FormatType(Enum):
+    sentence_transformers = "sentence-transformers"
+    onnx = "onnx"
+
