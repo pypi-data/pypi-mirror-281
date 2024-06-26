46d0c8c56dd383ee6e7aae3ff4a49082151be3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:04:51 +0000

    Add package geometry98765/0.1/geometry98765-0.1.tar.gz

---
+++ packages/geometry98765/geometry98765-0.1.tar.gz/geometry98765-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geometry98765',
+    version='0.1',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='abcd',
+    packages=find_packages(),
+)
+
+
+
+
+# pypi-AgEIcHlwaS5vcmcCJGNmNzQyNjRkLTYwZGEtNGQ5OS1hZGQ5LWQyMWRmOWMyMDFmZAACKlszLCIwNDVhMTU0ZS1lNDJhLTQ2OGMtOTA0MC1lMTk3NWY2YjRhMjIiXQAABiCWN6J4vsj5hoPLjW0k7ppTtDqCQuHx3VL-SYGYYQQyaQ
+# pypi-AgEIcHlwaS5vcmcCJGU1ODU3M2M3LTVhYjgtNGRjNy05YzBhLWFjODRiZWRlZDI5NAACKlszLCIwNDVhMTU0ZS1lNDJhLTQ2OGMtOTA0MC1lMTk3NWY2YjRhMjIiXQAABiByYbDSCOsDwvEF5wfpwDdsfyN5SNo5LrHxM0AJgF908A
