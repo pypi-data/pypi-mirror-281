3ed3b596906e9642c23f15fc64db78388552041b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:24 +0000

    Add package itk-ringartifact/2.0.1/itk_ringartifact-2.0.1-cp311-abi3-win_amd64.whl

---
+++ packages/itk-ringartifact/itk_ringartifact-2.0.1-cp311-abi3-win_amd64.whl/itk/Configuration/RingArtifactConfig.py
@@ -0,0 +1,9 @@
+depends = ('ITKPyBase', 'ITKFFT', 'ITKCommon', )
+templates = (  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterIF2', True, 'itk::Image< float,2 >'),
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterID2', True, 'itk::Image< double,2 >'),
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterIF3', True, 'itk::Image< float,3 >'),
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterID3', True, 'itk::Image< double,3 >'),
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterIF4', True, 'itk::Image< float,4 >'),
+  ('FourierStripeArtifactImageFilter', 'itk::FourierStripeArtifactImageFilter', 'itkFourierStripeArtifactImageFilterID4', True, 'itk::Image< double,4 >'),
+)
+factories = ()
---
+++ packages/itk-ringartifact/itk_ringartifact-2.0.1-cp311-abi3-win_amd64.whl/itk/Configuration/RingArtifact_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('fourier_stripe_artifact_image_filter', )
