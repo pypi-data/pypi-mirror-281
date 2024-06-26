fc44d7ca7f5cab85270f54319e79d910be09da2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 03:05:48 +0000

    Add package HinetPy/0.9.0/hinetpy-0.9.0.tar.gz

---
+++ packages/HinetPy/hinetpy-0.9.0.tar.gz/hinetpy-0.9.0/HinetPy/__init__.py
@@ -0,0 +1,28 @@
+# noqa: N999
+"""
+HinetPy
+=======
+
+HinetPy is a Python package to request and process seismic waveform data from the NIED
+Hi-net website.
+
+Basis usage:
+
+>>> from HinetPy import Client, win32
+>>> from datetime import datetime
+>>> # You need to provide your Hi-net username and password here!
+>>> client = Client("username", "password")
+>>> starttime = datetime(2010, 1, 1, 0, 0)
+>>> data, ctable = client.get_continuous_waveform("0101", starttime, 20)
+>>> win32.extract_sac(data, ctable)
+>>> win32.extract_sacpz(ctable)
+"""
+
+from importlib.metadata import version
+
+from .client import Client
+from .header import NETWORK
+
+__all__ = ["Client", "NETWORK", "win32"]
+# Get semantic version through setuptools-scm
+__version__ = f'v{version("HinetPy")}'  # e.g. v0.1.2.dev3+g0ab3cd78
