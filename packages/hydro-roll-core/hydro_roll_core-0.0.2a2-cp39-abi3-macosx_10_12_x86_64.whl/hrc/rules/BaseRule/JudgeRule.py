a9f9cd7ea4c23c44c67abb3274aa9f7dbff212a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:51 +0000

    Add package hydro-roll-core/0.0.2a2/hydro_roll_core-0.0.2a2-cp39-abi3-macosx_10_12_x86_64.whl

---
+++ packages/hydro-roll-core/hydro_roll_core-0.0.2a2-cp39-abi3-macosx_10_12_x86_64.whl/hrc/rules/BaseRule/JudgeRule.py
@@ -0,0 +1,13 @@
+import dataclasses
+from dataclasses import dataclass
+from typing import Literal, Optional, Union
+
+@dataclass
+class Custom(object):
+    ...
+
+class Attribute(Custom):
+    ...
+
+class Skill(Custom):
+    ...
