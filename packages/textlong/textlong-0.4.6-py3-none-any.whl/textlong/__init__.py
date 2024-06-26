6b5d0a75a5aa65eca0a234d9a53c1802d5460a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:03:56 +0000

    Add package textlong/0.4.6/textlong-0.4.6-py3-none-any.whl

---
+++ packages/textlong/textlong-0.4.6-py3-none-any.whl/textlong/__init__.py
@@ -0,0 +1,10 @@
+from .writing import write, idea, outline, more_outline, from_outline
+from .project import Project, list_projects, init_project, is_project_existing
+
+__all__ = [
+  "idea",
+  "outline",
+  "more_outline",
+  "from_outline",
+  "Project",
+]
