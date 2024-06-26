22d1327b5fbcc80062421e6fcf2a7132340685ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:04:18 +0000

    Add package pydex0/1.2.0/pydex0-1.2.0.tar.gz

---
+++ packages/pydex0/pydex0-1.2.0.tar.gz/pydex0-1.2.0/pydex/containers.py
@@ -0,0 +1,339 @@
+from __future__ import annotations
+
+import io
+import zipfile
+import pathlib
+import json
+
+from collections.abc import Generator
+
+from pydex.dalvik.executable import DexPool, DexFile
+
+
+class Container:
+    """
+    A class that represents a container file(zip, apk, xapk, apks, etc.) which
+    contains either apk files or dex files.
+    """
+
+    def __init__(self, path: str):
+        self.path = pathlib.Path(path)
+
+
+class InMemoryContainer:
+    """
+    A class that represents an in-memory container which contains either apk
+    files or dex files.
+    """
+
+    def __init__(self, data: bytes):
+        self.data = data
+
+
+class DexContainer(Container):
+    """
+    A class that represents a container file(zip, apk, jar) which
+    contains dex files.
+    """
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+    def enumerate_dex_files(self) -> Generator[str, None, None]:
+        """
+        Enumerate the dex files in the container file.
+        """
+
+        raise NotImplementedError()
+
+    def get_dex_data(self, dex_file: str) -> bytes:
+        """
+        Get the data of the dex file.
+        """
+
+        raise NotImplementedError()
+
+    def fetch_dex_files(self) -> DexPool:
+        """
+        Lazily load the dex files in the container file.
+        """
+
+        return DexPool([DexFile(self.get_dex_data(dex_file)) for dex_file in self.enumerate_dex_files()])
+
+
+class InMemoryDexContainer(InMemoryContainer):
+    """
+    A class that represents a container file(zip, apk, jar) which
+    contains dex files.
+    """
+
+    def __init__(self, data: bytes):
+        super().__init__(data)
+
+    def enumerate_dex_files(self) -> Generator[str, None, None]:
+        """
+        Enumerate the dex files in the container file.
+        """
+
+        raise NotImplementedError()
+
+    def get_dex_data(self, dex_file: str) -> bytes:
+        """
+        Get the data of the dex file.
+        """
+
+        raise NotImplementedError()
+
+    def fetch_dex_files(self) -> DexPool:
+        """
+        Lazily load the dex files in the container file.
+        """
+
+        return DexPool([DexFile(self.get_dex_data(dex_file)) for dex_file in self.enumerate_dex_files()])
+
+
+class InMemoryZipContainer(InMemoryDexContainer):
+    """
+    A class that represents an in-memory zip file which contains one or more
+    dex files.
+
+    Parameters:
+        - data: bytes: The zip file data.
+        - root_only: bool: If True, only the root files in the archive will be
+            considered.
+    """
+
+    def __init__(self, data: bytes, root_only: bool = False):
+        self.root_only = root_only
+
+        super().__init__(data)
+
+    def enumerate_dex_files(self) -> Generator[str, None, None]:
+        with zipfile.ZipFile(io.BytesIO(self.data), "r") as zip_file:
+            for file in zip_file.namelist():
+                file: pathlib.Path = pathlib.Path(file)
+
+                if self.root_only:
+                    if file.parent.name != "":
+                        continue
+
+                    if file.suffix == ".dex":
+                        yield file.name
+
+                    continue
+
+                if file.suffix == ".dex":
+                    yield file.name
+
+    def get_dex_data(self, dex_file: str) -> bytes:
+        with zipfile.ZipFile(io.BytesIO(self.data), "r") as zip_file:
+            with zip_file.open(dex_file, "r") as file:
+                return file.read()
+
+
+class ZipContainer(DexContainer):
+    """
+    A class that represents a zip file which contains one or more dex files.
+
+    Parameters:
+        - path: str: The path to the zip file.
+        - root_only: bool: If True, only the root files in the archive will be
+            considered.
+    """
+
+    def __init__(self, path: str, root_only: bool = False):
+        self.root_only = root_only
+        self.internal_container = InMemoryZipContainer(pathlib.Path(path).read_bytes())
+
+        super().__init__(path)
+
+    def enumerate_dex_files(self) -> Generator[str, None, None]:
+        return self.internal_container.enumerate_dex_files()
+
+    def get_dex_data(self, dex_file: str) -> bytes:
+        return self.internal_container.get_dex_data(dex_file)
+
+
+class InMemoryMultiAPKContainer(InMemoryContainer):
+    """
+    A class that represents an in-memory apk container which contains one or
+    more apk files.
+
+    Parameters:
+        - data: bytes: The apk container file data.
+    """
+
+    def __init__(self, data: bytes):
+        super().__init__(data)
+
+    def get_base_apk(self) -> bytes:
+        """
+        Get the base apk file as a bytes object.
+        """
+
+        raise NotImplementedError()
+
+    def fetch_dex_files(self, root_only: bool = False) -> DexPool:
+        """
+        Lazily load the dex files in the base apk file.
+
+        Parameters:
+            - root_only: bool: If True, only the root files in the archive will
+                be considered.
+        """
+
+        zip_container = InMemoryZipContainer(self.get_base_apk(), root_only)
+
+        return zip_container.fetch_dex_files()
+
+
+class MultiAPKContainer(Container):
+    """
+    A class that represents a container file which contains one or more apk
+    files.
+
+    Parameters:
+        - path: str: The path to the apk container file.
+    """
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+    def get_base_apk(self) -> bytes:
+        """
+        Get the base apk file as a bytes object.
+        """
+
+        raise NotImplementedError()
+
+    def fetch_dex_files(self, root_only: bool = False) -> DexPool:
+        """
+        Lazily load the dex files in the base apk file.
+
+        Parameters:
+            - root_only: bool: If True, only the root files in the archive will
+                be considered.
+        """
+
+        zip_container = InMemoryZipContainer(self.get_base_apk(), root_only)
+
+        return zip_container.fetch_dex_files()
+
+
+class InMemoryXAPKContainer(InMemoryMultiAPKContainer):
+    """
+    A class that represents an in-memory xapk container file which contains one
+    or more apk files.
+
+    Parameters:
+        - data: bytes: The xapk container file data.
+    """
+
+    def __init__(self, data: bytes):
+        super().__init__(data)
+
+    def get_base_apk(self) -> bytes:
+        with zipfile.ZipFile(io.BytesIO(self.data), "r") as zip_file:
+            for file in zip_file.namelist():
+                file: pathlib.Path = pathlib.Path(file)
+
+                if file.name != "manifest.json":
+                    continue
+
+                with zip_file.open(str(file), "r") as manifest_file:
+                    manifest = json.load(manifest_file)
+
+                    for split_apk in manifest["split_apks"]:
+                        if split_apk["id"] != "base":
+                            continue
+
+                        with zip_file.open(split_apk["file"], "r") as base_apk_file:
+                            return base_apk_file.read()
+                    else:
+                        raise ValueError("Base apk not found in manifest file.")
+            else:
+                raise FileNotFoundError("Manifest file not found in xapk file.")
+
+
+class XAPKContainer(MultiAPKContainer):
+    """
+    A class that represents a xapk file which contains one or more apk files.
+    These conatiner files contain various apk files, whose purpose is noted in
+    the `manifest.json` file.
+
+    Parameters:
+        - path: str: The path to the xapk file.
+    """
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+    def get_base_apk(self) -> bytes:
+        return InMemoryXAPKContainer(pathlib.Path(self.path).read_bytes()).get_base_apk()
+
+
+class InMemoryAPKSContainer(InMemoryMultiAPKContainer):
+    """
+    A class that represents an in-memory apks container file which contains one
+    or more apk files.
+
+    Parameters:
+        - data: bytes: The apks container file data.
+    """
+
+    def __init__(self, data: bytes):
+        super().__init__(data)
+
+    def get_base_apk(self) -> bytes:
+        with zipfile.ZipFile(io.BytesIO(self.data), "r") as zip_file:
+            for file in zip_file.namelist():
+                file: pathlib.Path = pathlib.Path(file)
+
+                if file.name != "base.apk":
+                    continue
+
+                with zip_file.open(str(file), "r") as base_apk_file:
+                    return base_apk_file.read()
+            else:
+                raise FileNotFoundError("Base apk not found in apks file.")
+
+
+class APKSContainer(MultiAPKContainer):
+    """
+    A class that represents an apks file which contains one or more apk files.
+    This is also known as a split-apk.
+
+    Parameters:
+        - path: str: The path to the apks file.
+    """
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+    def get_base_apk(self) -> bytes:
+        return InMemoryAPKSContainer(pathlib.Path(self.path).read_bytes()).get_base_apk()
+
+
+class JarContainer(ZipContainer):
+    """
+    A class that represents a jar file which contains one or more dex files.
+
+    Parameters:
+        - path: str: The path to the jar file.
+    """
+
+    def __init__(self, path: str):
+        super().__init__(path)
+
+
+class APKContainer(ZipContainer):
+    """
+    A class that represents an apk file which contains one or more dex files.
+    Only the root files in the archive will be considered.
+
+    Parameters:
+        - path: str: The path to the apk file.
+    """
+
+    def __init__(self, path: str):
+        super().__init__(path, root_only=True)
---
+++ packages/pydex0/pydex0-1.2.0.tar.gz/pydex0-1.2.0/pydex/dalvik/executable.py
@@ -0,0 +1,281 @@
+from __future__ import annotations
+
+import struct
+import typing
+import zlib
+
+from dataclasses import dataclass
+
+from datastream import DeserializingStream, ByteOrder
+
+from pydex.dalvik.models import DalvikHeader, DalvikHeaderItem, LazyDalvikString, DalvikStringID, DalvikStringItem
+
+
+@dataclass
+class DexPool:
+    """
+    A class that holds a pool of dex files.
+    """
+
+    dex_files: list[DexFile]
+
+
+class DexFile:
+    def __init__(self, data: bytes):
+        self.data = data
+        self.stream = DeserializingStream(data, ByteOrder.LITTLE_ENDIAN)
+
+        self.header: DalvikHeaderItem = typing.cast(DalvikHeaderItem, None)
+        self.strings: list[LazyDalvikString] = []
+
+    @classmethod
+    def from_path(cls, path: str) -> DexFile:
+        """
+        Create a DexFile object from a file path.
+        """
+
+        with open(path, "rb") as f:
+            return cls(f.read())
+
+    def parse_dex(self) -> typing.Self:
+        """
+        Parse the dex file.
+        """
+
+        # reset the stream in case it was read from before
+        self.stream.seek(0)
+
+        # get the byte order of the dex file
+        # it is at 8 + 4 + 20 + 4 + 4 = 40
+        endian_tag = int.from_bytes(self.stream.seekpeek(40, 4))
+
+        if endian_tag == 0x12345678:
+            self.stream.byteorder = ByteOrder.BIG_ENDIAN
+        elif endian_tag == 0x78563412:
+            self.stream.byteorder = ByteOrder.LITTLE_ENDIAN
+        else:
+            raise ValueError("Invalid endian tag")
+
+        # parse the header
+        self.header = self.parse_header()
+
+        # collect all the dalvik string items as LazyDalvikStrings
+        self.strings = self.parse_strings()
+
+        return self
+
+    def parse_header(self) -> DalvikHeaderItem:
+        """
+        Parse the header of the dex file.
+        """
+
+        clonestream = self.stream.clone()
+
+        # get the magic bytes
+        magic = clonestream.read(8)
+
+        # the magic bytes are not constant, as they contain a version number
+        # dex\0x0Axxx\x00, where xxx is the version number(zero padded).
+        if magic[:4] != b"dex\x0A":
+            raise ValueError("Invalid magic bytes")
+
+        if magic[7] != 0:
+            raise ValueError("Invalid magic bytes")
+
+        # get the checksum
+        checksum = clonestream.read_uint32()
+
+        # verify the checksum. the checksum applies to the entire file except
+        # the magic bytes and this field, so 8 bytes + 4 bytes = 12 bytes to
+        # skip.
+        if checksum != zlib.adler32(self.data[12:]):
+            raise ValueError(f"Invalid checksum: {checksum}")
+
+        # get the sha-1 signature bytes(fingerprint)
+        signature = clonestream.read(20)
+
+        # get the file size
+        file_size = clonestream.read_uint32()
+
+        # get the header size
+        header_size = clonestream.read_uint32()
+
+        # according to the spec, this should always be == 0x70
+        if header_size != 0x70:
+            raise ValueError(f"Invalid header size: {header_size}")
+
+        # get the endian tag
+        endian_tag = clonestream.read_uint32()
+
+        # get the link size
+        link_size = clonestream.read_uint32()
+
+        # get the link offset
+        link_off = clonestream.read_uint32()
+
+        # get the map offset
+        map_off = clonestream.read_uint32()
+
+        # get the string ids size
+        string_ids_size = clonestream.read_uint32()
+
+        # get the string ids offset
+        string_ids_off = clonestream.read_uint32()
+
+        # get the type ids size
+        type_ids_size = clonestream.read_uint32()
+
+        # according to the spec, this should always be <= 0xFFFF
+        if type_ids_size >= 0xFFFF:
+            raise ValueError(f"Invalid type ids size: {type_ids_size}")
+
+        # get the type ids offset
+        type_ids_off = clonestream.read_uint32()
+
+        # get the proto ids size
+        proto_ids_size = clonestream.read_uint32()
+
+        # according to the spec, this should always be <= 0xFFFF
+        if proto_ids_size >= 0xFFFF:
+            raise ValueError(f"Invalid proto ids size: {proto_ids_size}")
+
+        # get the proto ids offset
+        proto_ids_off = clonestream.read_uint32()
+
+        # get the field ids size
+        field_ids_size = clonestream.read_uint32()
+
+        # get the field ids offset
+        field_ids_off = clonestream.read_uint32()
+
+        # get the method ids size
+        method_ids_size = clonestream.read_uint32()
+
+        # get the method ids offset
+        method_ids_off = clonestream.read_uint32()
+
+        # get the class defs size
+        class_defs_size = clonestream.read_uint32()
+
+        # get the class defs offset
+        class_defs_off = clonestream.read_uint32()
+
+        # get the data size
+        data_size = clonestream.read_uint32()
+
+        # this field should be divisible by sizeof(uint)
+        if data_size % struct.calcsize("I") != 0:
+            raise ValueError(f"Invalid data size: {data_size}")
+
+        # get the data offset
+        data_off = clonestream.read_uint32()
+
+        return DalvikHeaderItem.from_raw_item(
+            DalvikHeader(
+                offset=0,
+                size=header_size,
+                data=self.data[:0x70],
+                magic=magic,
+                checksum=checksum,
+                signature=signature,
+                file_size=file_size,
+                header_size=header_size,
+                endian_tag=endian_tag,
+                link_size=link_size,
+                link_off=link_off,
+                map_off=map_off,
+                string_ids_size=string_ids_size,
+                string_ids_off=string_ids_off,
+                type_ids_size=type_ids_size,
+                type_ids_off=type_ids_off,
+                proto_ids_size=proto_ids_size,
+                proto_ids_off=proto_ids_off,
+                field_ids_size=field_ids_size,
+                field_ids_off=field_ids_off,
+                method_ids_size=method_ids_size,
+                method_ids_off=method_ids_off,
+                class_defs_size=class_defs_size,
+                class_defs_off=class_defs_off,
+                data_size=data_size,
+                data_off=data_off,
+            )
+        )
+
+    def parse_strings(self) -> list[LazyDalvikString]:
+        """
+        Collect all the dalvik string items.
+
+        Returns: The dalvik string items as LazyDalvikStrings.
+        """
+
+        lazy_strings = []
+        clonestream = self.stream.clone()
+
+        for i in range(self.header.raw_item.string_ids_size):
+            clonestream.seek(self.header.raw_item.string_ids_off + i * 4)
+            string_id_off = clonestream.tell()
+
+            string_data_off = clonestream.read_uint32()
+            clonestream.seek(string_data_off)
+
+            lazy_strings.append(
+                LazyDalvikString(
+                    DalvikStringID(
+                        offset=string_id_off,
+                        size=4,
+                        data=self.data[string_id_off : string_id_off + 4],
+                        string_data_off=string_data_off,
+                        id_number=i,
+                    )
+                )
+            )
+
+        return lazy_strings
+
+    def load_all_strings(self) -> list[DalvikStringItem]:
+        """
+        Load all the dalvik string items.
+
+        Returns: All the dalvik string items for this dex file.
+        """
+
+        strings = []
+
+        for lazy_string in self.strings:
+            strings.append(lazy_string.load(self.stream))
+
+        return strings
+
+    def get_string_by_id(self, string_id: int) -> LazyDalvikString:
+        """
+        Get a dalvik string by its id. The difference between this, and
+        `dex.strings[id]` is that this method does not require all the strings
+        to be collected from the dex file. This method is useful when you only
+        need a single string from the dex file and don't want to load the
+        entire dex file with `parse_dex`.
+
+        Args:
+            string_id: The id of the string to get. IDs are 0-indexed, and are
+                   assigned in the order they appear in the dex file.
+
+        Returns: A `LazyDalvikString` object.
+        """
+
+        if len(self.strings) > 0:
+            return self.strings[string_id]
+
+        clonestream = self.stream.clone()
+        clonestream.seek(self.header.raw_item.string_ids_off + string_id * 4)
+
+        string_id_off = clonestream.tell()
+        string_data_off = clonestream.read_uint32()
+
+        return LazyDalvikString(
+            DalvikStringID(
+                offset=string_id_off,
+                size=4,
+                data=self.data[string_id_off : string_id_off + 4],
+                string_data_off=string_data_off,
+                id_number=string_id,
+            )
+        )
