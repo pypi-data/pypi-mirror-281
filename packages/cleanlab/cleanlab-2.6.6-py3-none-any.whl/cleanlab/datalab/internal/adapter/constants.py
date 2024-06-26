101253e7c2a209ac3977879f827c53a351b5e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 14:05:03 +0000

    Add package cleanlab/2.6.6/cleanlab-2.6.6-py3-none-any.whl

---
+++ packages/cleanlab/cleanlab-2.6.6-py3-none-any.whl/cleanlab/datalab/internal/adapter/constants.py
@@ -0,0 +1,13 @@
+DEFAULT_CLEANVISION_ISSUES = {
+    "dark": {},
+    "light": {},
+    "low_information": {
+        "threshold": 0.15,
+    },
+    "odd_aspect_ratio": {},
+    "odd_size": {},
+    "grayscale": {},
+    "blurry": {},
+}  # list of CleanVision issue types considered by default in Datalab may differ from CleanVision package default
+
+IMAGELAB_ISSUES_MAX_PREVALENCE = 0.1
