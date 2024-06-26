0a442163956624950586b052d7ffd2c59af2d124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:03:54 +0000

    Add package annot-attrs/0.0.10/annot_attrs-0.0.10.tar.gz

---
+++ packages/annot-attrs/annot_attrs-0.0.10.tar.gz/annot_attrs-0.0.10/annot_attrs/__init__.py
@@ -0,0 +1,36 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .main import (
+    # BASE
+    AnnotAttrs,
+    AnnotsNested,
+    AnnotsClsKeysAsValues,
+
+    # AUX
+    AnnotsClsKeysAsValues_Meta,
+
+    # TYPES
+
+    # EXX
+    Exx_AttrNotExist,
+)
+
+
+# =====================================================================================================================
