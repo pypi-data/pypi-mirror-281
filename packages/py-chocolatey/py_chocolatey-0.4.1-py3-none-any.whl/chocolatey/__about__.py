a4b438dea4d84f83c35ae47dd6daee087d648b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:05:15 +0000

    Add package py-chocolatey/0.4.1/py_chocolatey-0.4.1-py3-none-any.whl

---
+++ packages/py-chocolatey/py_chocolatey-0.4.1-py3-none-any.whl/chocolatey/__about__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/license/zlib
+
+__import__("pkg_about").about("py-chocolatey")
+__copyright__ = f"Copyright (c) 2022-2024 {__author__}"  # noqa
