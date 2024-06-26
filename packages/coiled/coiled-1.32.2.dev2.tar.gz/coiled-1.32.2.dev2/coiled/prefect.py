01d76a826a269b1e7dabff3759a549cb1016990e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:04:18 +0000

    Add package coiled/1.32.2.dev2/coiled-1.32.2.dev2.tar.gz

---
+++ packages/coiled/coiled-1.32.2.dev2.tar.gz/coiled-1.32.2.dev2/coiled/prefect.py
@@ -0,0 +1,41 @@
+try:
+    import prefect  # type: ignore # noqa
+except ImportError as e:
+    raise ImportError("The `coiled.prefect` module requires `prefect` to be installed.") from e
+
+from typing import Optional
+
+import dask.config
+from prefect.blocks.core import Block  # type: ignore
+
+try:
+    # Prefect docs say this is required for pydantic>=2
+    # https://docs.prefect.io/latest/concepts/blocks/#secret-fields
+    from pydantic.v1 import Field, SecretStr
+except ImportError:
+    from pydantic import Field, SecretStr
+
+
+class Credentials(Block):
+    _block_type_name = "Coiled Credentials"
+    _logo_url = "https://blog.coiled.io/_static/logo.svg"
+    _documentation_url = "https://docs.coiled.io/user_guide/labs/prefect.html"
+    _code_example = """
+    ```python
+    from coiled.prefect import Credentials
+
+    Credentials.load("BLOCK_NAME").login()
+    ```
+    """
+
+    token: SecretStr = Field(default=..., description="Coiled API token.")
+    account: Optional[str] = Field(default=None, description="Deprecated, use ``workspace``.")
+    workspace: Optional[str] = Field(default=None, description="Coiled workspace to use.")
+
+    def login(self):
+        workspace = self.workspace or self.account
+        dask.config.set({
+            "coiled.token": self.token.get_secret_value(),
+            "coiled.account": workspace,
+            "coiled.workspace": workspace,
+        })
