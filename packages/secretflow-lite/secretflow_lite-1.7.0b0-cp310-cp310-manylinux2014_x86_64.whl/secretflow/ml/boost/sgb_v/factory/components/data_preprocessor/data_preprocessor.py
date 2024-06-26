a7a37821925bb04c5bbf78fee3adf94f42d4581e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:04:18 +0000

    Add package secretflow-lite/1.7.0b0/secretflow_lite-1.7.0b0-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/secretflow-lite/secretflow_lite-1.7.0b0-cp310-cp310-manylinux2014_x86_64.whl/secretflow/ml/boost/sgb_v/factory/components/data_preprocessor/data_preprocessor.py
@@ -0,0 +1,53 @@
+# Copyright 2023 Ant Group Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Tuple, Union
+
+from secretflow.data import FedNdarray
+from secretflow.device import PYUObject
+from secretflow.ml.boost.core.data_preprocess import validate, validate_sample_weight
+
+from ..component import Component
+
+
+class DataPreprocessor(Component):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def show_params(self):
+        return
+
+    def set_params(self, _):
+        return
+
+    def get_params(self, _):
+        return
+
+    def set_devices(self, _):
+        return
+
+    def set_actors(self, _):
+        return
+
+    def del_actors(self):
+        return
+
+    def validate(
+        self, dataset, label, sample_weight=None
+    ) -> Tuple[
+        FedNdarray, Tuple[int, int], PYUObject, Tuple[int, int], Union[None, PYUObject]
+    ]:
+        x, x_shape, y, y_shape = validate(dataset, label)
+        w = validate_sample_weight(sample_weight, y_shape=y_shape)
+        return x, x_shape, y, y_shape, w
