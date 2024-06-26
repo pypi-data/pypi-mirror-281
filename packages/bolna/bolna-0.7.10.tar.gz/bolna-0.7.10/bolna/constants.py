e82d45875763654ea06e3e81aab061f68b75bf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:04:50 +0000

    Add package bolna/0.7.10/bolna-0.7.10.tar.gz

---
+++ packages/bolna/bolna-0.7.10.tar.gz/bolna-0.7.10/bolna/constants.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timezone
+PREPROCESS_DIR = 'agent_data'
+
+HIGH_LEVEL_ASSISTANT_ANALYTICS_DATA = {
+        "extraction_details":{}, 
+        "cost_details": {
+            "average_transcriber_cost_per_conversation": 0, 
+            "average_llm_cost_per_conversation": 0,
+            "average_synthesizer_cost_per_conversation": 1.0
+        },
+        "historical_spread": {
+            "number_of_conversations_in_past_5_days": [], 
+            "cost_past_5_days": [],
+            "average_duration_past_5_days": []
+        },
+        "conversation_details": { 
+            "total_conversations": 0,
+            "finished_conversations": 0, 
+            "rejected_conversations": 0
+        },
+        "execution_details": {
+            "total_conversations": 0, 
+            "total_cost": 0,
+            "average_duration_of_conversation": 0
+        },
+        "last_updated_at": datetime.now(timezone.utc).isoformat()
+    }
+
+ACCIDENTAL_INTERRUPTION_PHRASES = [
+    "stop", "quit", "bye", "wait", "no", "wrong", "incorrect", "hold", "pause", "break",
+    "cease", "halt", "silence", "enough", "excuse", "hold on", "hang on", "cut it", 
+    "that's enough", "shush", "listen", "excuse me", "hold up", "not now", "stop there", "stop speaking"
+]
+
+PRE_FUNCTIONAL_CALL_MESSAGE = "Just give me a moment, I'll do that and will be back with you"
