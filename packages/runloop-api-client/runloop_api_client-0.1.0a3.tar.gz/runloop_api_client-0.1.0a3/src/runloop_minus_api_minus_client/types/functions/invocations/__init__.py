67817887792972804b86b96c91c4df3328eee2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:20 +0000

    Add package runloop-api-client/0.1.0a3/runloop_api_client-0.1.0a3.tar.gz

---
+++ packages/runloop-api-client/runloop_api_client-0.1.0a3.tar.gz/runloop_api_client-0.1.0a3/src/runloop_minus_api_minus_client/types/functions/invocations/__init__.py
@@ -0,0 +1,5 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .invocation_span_list_view import InvocationSpanListView as InvocationSpanListView
