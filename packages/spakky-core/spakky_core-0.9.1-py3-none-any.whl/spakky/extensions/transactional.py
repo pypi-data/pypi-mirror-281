191336534eae6debf9dc7a39aa3519ec8eea2669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:04:58 +0000

    Add package spakky-core/0.9.1/spakky_core-0.9.1-py3-none-any.whl

---
+++ packages/spakky-core/spakky_core-0.9.1-py3-none-any.whl/spakky/extensions/transactional.py
@@ -0,0 +1,41 @@
+from typing import Any
+from logging import Logger
+from dataclasses import dataclass
+
+from spakky.aop.advice import Around
+from spakky.aop.advisor import IAsyncAdvisor
+from spakky.aop.aspect import AsyncAspect
+from spakky.aop.order import Order
+from spakky.bean.autowired import autowired
+from spakky.core.annotation import FunctionAnnotation
+from spakky.core.types import AsyncFunc
+from spakky.domain.ports.persistency.transaction import AbstractAsyncTranasction
+
+
+@dataclass
+class AsyncTransactional(FunctionAnnotation): ...
+
+
+@Order(0)
+@AsyncAspect()
+class AsyncTransactionalAdvisor(IAsyncAdvisor):
+    __transacntion: AbstractAsyncTranasction
+    __logger: Logger
+
+    @autowired
+    def __init__(self, transaction: AbstractAsyncTranasction, logger: Logger) -> None:
+        super().__init__()
+        self.__transacntion = transaction
+        self.__logger = logger
+
+    @Around(AsyncTransactional.contains)
+    async def around_async(self, joinpoint: AsyncFunc, *args: Any, **kwargs: Any) -> Any:
+        self.__logger.info(f"[{type(self).__name__}] BEGIN TRANSACTION")
+        try:
+            async with self.__transacntion:
+                result = await joinpoint(*args, **kwargs)
+        except:
+            self.__logger.info(f"[{type(self).__name__}] ROLLBACK")
+            raise
+        self.__logger.info(f"[{type(self).__name__}] COMMIT")
+        return result
