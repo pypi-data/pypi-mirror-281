2c997f2e7b7428bbb4032d3f3c95e1f63505e835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 14:04:13 +0000

    Add package goldenergy/0.0.2/goldenergy-0.0.2.tar.gz

---
+++ packages/goldenergy/goldenergy-0.0.2.tar.gz/goldenergy-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="goldenergy",
+    version="0.0.2",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    python_requires='>=3',
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src'),
+    install_requires=[
+        'python-dateutil',
+        'aiohttp'
+    ]
+)
