208a9199bae3f60e896087cb43bb627ce30e6221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 14:04:49 +0000

    Add package JoshAmesDistributions/0.1/JoshAmesDistributions-0.1.tar.gz

---
+++ packages/JoshAmesDistributions/JoshAmesDistributions-0.1.tar.gz/JoshAmesDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='JoshAmesDistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['JoshAmesDistributions'],
+      zip_safe=False)
