fb8e6cd322fb8e7ac71ef5555df67f087fd61a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:03:34 +0000

    Add package prediction-market-agent-tooling/0.39.0/prediction_market_agent_tooling-0.39.0-py3-none-any.whl

---
+++ packages/prediction-market-agent-tooling/prediction_market_agent_tooling-0.39.0-py3-none-any.whl/prediction_market_agent_tooling/markets/data_models.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from enum import Enum
+from typing import TypeAlias
+
+from pydantic import BaseModel
+
+from prediction_market_agent_tooling.gtypes import OutcomeStr
+
+
+class Currency(str, Enum):
+    xDai = "xDai"
+    Mana = "Mana"
+    USDC = "USDC"
+
+
+class Resolution(str, Enum):
+    YES = "YES"
+    NO = "NO"
+    CANCEL = "CANCEL"
+    MKT = "MKT"
+
+
+class TokenAmount(BaseModel):
+    amount: float
+    currency: Currency
+
+
+BetAmount: TypeAlias = TokenAmount
+ProfitAmount: TypeAlias = TokenAmount
+
+
+class Bet(BaseModel):
+    amount: BetAmount
+    outcome: bool
+    created_time: datetime
+    market_question: str
+
+
+class ResolvedBet(Bet):
+    market_outcome: bool
+    resolved_time: datetime
+    profit: ProfitAmount
+
+    @property
+    def is_correct(self) -> bool:
+        return self.outcome == self.market_outcome
+
+
+class Position(BaseModel):
+    market_id: str
+    amounts: dict[OutcomeStr, TokenAmount]
+
+    def __str__(self) -> str:
+        amounts_str = ", ".join(
+            f"{amount.amount} '{outcome}' tokens"
+            for outcome, amount in self.amounts.items()
+        )
+        return f"Position for market id {self.market_id}: {amounts_str}"
