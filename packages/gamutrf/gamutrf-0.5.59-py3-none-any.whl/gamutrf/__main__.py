efd0cca2344e67a2cc115e4ff33291d6198af50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:16 +0000

    Add package gamutrf/0.5.59/gamutrf-0.5.59-py3-none-any.whl

---
+++ packages/gamutrf/gamutrf-0.5.59-py3-none-any.whl/gamutrf/__main__.py
@@ -0,0 +1,38 @@
+"""Main entrypoint for GamutRF"""
+
+from gamutrf.compress_dirs import main as compress_dirs_main
+from gamutrf.offline import main as offline_main
+from gamutrf.scan import main as scan_main
+from gamutrf.sigfinder import main as sigfinder_main
+from gamutrf.specgram import main as specgram_main
+from gamutrf.worker import main as worker_main
+
+
+def compress_dirs():
+    """Entrypoint for compress_dirs"""
+    compress_dirs_main()
+
+
+def offline():
+    """Entrypoint for offline"""
+    offline_main()
+
+
+def scan():
+    """Entrypoint for scan"""
+    scan_main()
+
+
+def sigfinder():
+    """Entrypoint for sigfinder"""
+    sigfinder_main()
+
+
+def specgram():
+    """Entrypoint for specgram"""
+    specgram_main()
+
+
+def worker():
+    """Entrypoint for worker"""
+    worker_main()
