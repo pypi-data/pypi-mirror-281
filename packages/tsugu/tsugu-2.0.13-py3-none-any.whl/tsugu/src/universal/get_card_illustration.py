ddedccf47c6e1165d6f0627e92ac4803c9eea2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 04:04:07 +0000

    Add package tsugu/2.0.13/tsugu-2.0.13-py3-none-any.whl

---
+++ packages/tsugu/tsugu-2.0.13-py3-none-any.whl/tsugu/src/universal/get_card_illustration.py
@@ -0,0 +1,36 @@
+from ...utils import get_user, text_response, User, server_id_2_server_name, server_name_2_server_id
+import tsugu_api
+from arclet.alconna import Alconna, Option, Subcommand, Args, CommandMeta, Empty, Namespace, namespace, command_manager
+import tsugu_api_async
+
+
+alc = Alconna(
+        ["查卡面", "查插画"],
+        Args["cardId", int],
+        meta=CommandMeta(
+            compact=True,
+            description="查卡面",
+            usage='根据卡片ID查询卡片插画',
+            example='查卡面 1399 :返回1399号卡牌的插画'
+        )
+    )
+
+
+def handler(message: str, user_id: str, platform: str, channel_id: str):
+    res = alc.parse(message)
+
+    if res.matched:
+        return tsugu_api.get_card_illustration(res.cardId)
+
+    return res
+
+
+async def handler_async(message: str, user_id: str, platform: str, channel_id: str):
+    res = alc.parse(message)
+
+    if res.matched:
+        return await tsugu_api_async.get_card_illustration(res.cardId)
+
+    return res
+
+
