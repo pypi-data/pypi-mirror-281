9bdcd516579e88b76ca2b3860c7b679140b7197e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:04:38 +0000

    Add package remotivelabs-broker/0.1.26/remotivelabs_broker-0.1.26-py3-none-any.whl

---
+++ packages/remotivelabs-broker/remotivelabs_broker-0.1.26-py3-none-any.whl/remotivelabs/broker/__init__.py
@@ -0,0 +1,37 @@
+"""
+remotiveLabs Python API for remotiveBroker.
+See `version` below.
+
+In addition to return codes, this package uses logging to convey operational
+status. Logging is done to the name space "com.remotivelabs.broker".
+
+As a user, enable basic logging in your application with:
+
+```python
+logging.basicConfig()
+```
+
+Disable logging for this package:
+
+```python
+logging.getLogger("com.remotivelabs.broker").propagate = False
+```
+
+Use sub module: `remotivelabs.broker.sync`.
+"""
+
+# SPDX-FileCopyrightText: 2022-present remotiveLabs <support@remotivelabs.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+
+from .__about__ import __version__
+
+log: logging.Logger = logging.getLogger("com.remotivelabs.broker")
+"""Package logging interface"""
+
+log.addHandler(logging.NullHandler())
+
+version: str = __version__
+"""Library version"""
