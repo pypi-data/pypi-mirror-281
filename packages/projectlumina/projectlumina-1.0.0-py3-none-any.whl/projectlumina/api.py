d3e6a05e68a35fb47a0659d78162f2cfb784e408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:04:11 +0000

    Add package projectlumina/1.0.0/projectlumina-1.0.0-py3-none-any.whl

---
+++ packages/projectlumina/projectlumina-1.0.0-py3-none-any.whl/projectlumina/api.py
@@ -0,0 +1,39 @@
+import requests
+
+class Api:
+    @staticmethod
+    def fetch(endpoint):
+        url = f"https://projectlumina.xyz/shortcut/{endpoint}"
+        response = requests.get(url)
+        
+        ext = endpoint.split('.')[-1].lower()
+
+        if response.status_code != 200:
+            raise ValueError(f"Failed to fetch {endpoint}: Status code {response.status_code}")
+        
+        if ext == 'json':
+            return response.json()
+        elif ext == 'txt':
+            return response.text
+        else:
+            return response.content
+
+    @property
+    def about(self):
+        return self.fetch('about.txt')
+
+    @property
+    def connections(self):
+        return self.fetch('connections.json')
+
+    @property
+    def options(self):
+        return self.fetch('options.json')
+
+    @property
+    def packs(self):
+        return self.fetch('packs.json')
+
+    @property
+    def releases(self):
+        return self.fetch('releases.json')
