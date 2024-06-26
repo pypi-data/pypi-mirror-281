8353c964f0cfebe48560cda62d59ba16546905c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:04:08 +0000

    Add package cognite-neat/0.82.3/cognite_neat-0.82.3.tar.gz

---
+++ packages/cognite-neat/cognite_neat-0.82.3.tar.gz/cognite_neat-0.82.3/cognite/neat/graph/extractors/__init__.py
@@ -0,0 +1,37 @@
+from ._classic_cdf._assets import AssetsExtractor
+from ._classic_cdf._events import EventsExtractor
+from ._classic_cdf._files import FilesExtractor
+from ._classic_cdf._labels import LabelsExtractor
+from ._classic_cdf._relationships import RelationshipsExtractor
+from ._classic_cdf._sequences import SequencesExtractor
+from ._classic_cdf._timeseries import TimeSeriesExtractor
+from ._dexpi import DexpiExtractor
+from ._mock_graph_generator import MockGraphGenerator
+from ._rdf_file import RdfFileExtractor
+
+__all__ = [
+    "AssetsExtractor",
+    "MockGraphGenerator",
+    "RelationshipsExtractor",
+    "TimeSeriesExtractor",
+    "SequencesExtractor",
+    "EventsExtractor",
+    "FilesExtractor",
+    "LabelsExtractor",
+    "RdfFileExtractor",
+    "DexpiExtractor",
+]
+
+
+TripleExtractors = (
+    AssetsExtractor
+    | MockGraphGenerator
+    | RelationshipsExtractor
+    | TimeSeriesExtractor
+    | SequencesExtractor
+    | EventsExtractor
+    | FilesExtractor
+    | LabelsExtractor
+    | RdfFileExtractor
+    | DexpiExtractor
+)
