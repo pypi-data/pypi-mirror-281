71b759ba4fa4c8004e73d13aa5ba7ea3194bdfcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:05:01 +0000

    Add package PostPie/1.1.1/postpie-1.1.1.tar.gz

---
+++ packages/PostPie/postpie-1.1.1.tar.gz/postpie-1.1.1/tests/test.py
@@ -0,0 +1,27 @@
+# test.py
+# Copyright (C) 2024 the PostPie developers
+# (See DEVELOPERS FILE)
+
+import PostPie
+
+
+def test(**kwargs):
+
+    columns = " ".join(kwargs.keys())
+    values = ' '.join([f" '{values}'" for values in kwargs.values()])
+
+    columns = columns.split()
+    values = values.split()
+
+    sqlCommand = ""
+
+    for i in range(len(columns)):
+        sqlCommand += columns[i] + " = " + values[i] + " , "
+
+    sqlCommand = sqlCommand.split()
+    sqlCommand[-1] = ''
+    sqlCommand = ' '.join(sqlCommand)
+
+    print(sqlCommand)
+    
+test(name="Brandon", age=12) 
