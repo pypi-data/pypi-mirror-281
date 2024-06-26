2d4468d4098253c29eb4f5ac6f0ae7fd1a3b627c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 03:04:53 +0000

    Add package kuro.py/0.3.0/kuro_py-0.3.0-py3-none-any.whl

---
+++ packages/kuro.py/kuro_py-0.3.0-py3-none-any.whl/kuro/constants.py
@@ -0,0 +1,25 @@
+"""Constants for the lib."""
+
+import typing
+
+from kuro import types
+
+__all__ = ["GEETEST_LANGS"]
+
+
+GEETEST_LANGS: typing.Mapping[types.Lang, str] = {
+    types.Lang.CHINESE_SIMPLIFIED: "zh-Hans",
+    types.Lang.CHINESE_TRADITIONAL: "zh-Hant",
+    types.Lang.ENGLISH: "en",
+    types.Lang.JAPANESE: "ja",
+    types.Lang.KOREAN: "ko",
+    types.Lang.FRENCH: "fr",
+    types.Lang.GERMAN: "de",
+    types.Lang.SPANISH: "es",
+}
+"""Geetest languages."""
+
+WUTHERY_CDN: str = "https://nogatekeep.wuthery.com"
+"""Wuthery CDN URL."""
+
+AKI_GAME_RESOURCES: str = "https://aki-gm-resources-back.aki-game.net"
