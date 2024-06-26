549770fcee43befca1c14a32a0c0c4812f22dbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:04:13 +0000

    Add package devkit-math/1.0/devkit_math-1.0.tar.gz

---
+++ packages/devkit-math/devkit_math-1.0.tar.gz/devkit_math-1.0/math/math.py
@@ -0,0 +1,17 @@
+def gcd(x, y):
+    if type(x) != type(int()) or type(y) != type(int()):
+        return 0
+    return gcd(y, x % y) if y else x
+def lcm(x, y):
+    if type(x) != type(int()) or type(y) != type(int()):
+        return 0
+    return x * y / gcd(x, y)
+def prime(x):
+    if type(x) != type(int()):
+        return False
+    i = 2
+    while i * i <= x:
+        if x % i == 0:
+            return False
+        i += 1
+    return x > 1
