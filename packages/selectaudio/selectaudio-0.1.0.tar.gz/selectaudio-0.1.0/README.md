8b456d3c5f857f64ca8c0ca323808cc56349f7d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:04:38 +0000

    Add package selectaudio/0.1.0/selectaudio-0.1.0.tar.gz

---
+++ packages/selectaudio/selectaudio-0.1.0.tar.gz/selectaudio-0.1.0/README.md
@@ -0,0 +1,5 @@
+# selectaudio
+
+## License
+
+This project is licensed under the GPL-3 license.
---
+++ packages/selectaudio/selectaudio-0.1.0.tar.gz/selectaudio-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="selectaudio",
+    version="0.1.0",
+    description="A tool to set PulseAudio source/sink using an interactive menu",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="4thel00z",
+    author_email="4thel00z@gmail.com",
+    url="https://github.com/4thel00z/selectaudio",
+    packages=find_packages(),
+    install_requires=[
+        "pulsectl",
+        "prompt_toolkit",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sa=selectaudio.__main__.py:main",
+            "selectaudio=selectaudio.__main__.py:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
+
