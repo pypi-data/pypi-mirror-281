2023fa9b32f82de64a9e3753c6db5c17d9dc442f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:10 +0000

    Add package patchwork-cli/0.0.22/patchwork_cli-0.0.22.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.22.tar.gz/patchwork_cli-0.0.22/patchwork/steps/CommitChanges/typed.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing_extensions import NotRequired, TypedDict
+
+
+class CommitChangesInputs(TypedDict):
+    modified_code_files: list[str]
+    disable_branch: NotRequired[bool]
+    force_branch_creation: NotRequired[bool]
+    branch_prefix: NotRequired[str]
+    branch_suffix: NotRequired[str]
+
+
+class CommitChangesOutputs(TypedDict):
+    base_branch: str
+    target_branch: str
