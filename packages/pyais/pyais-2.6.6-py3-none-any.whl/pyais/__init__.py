5478b7b32dc0b359d923feb521cc9b5fbdd492b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:05:00 +0000

    Add package pyais/2.6.6/pyais-2.6.6-py3-none-any.whl

---
+++ packages/pyais/pyais-2.6.6-py3-none-any.whl/pyais/__init__.py
@@ -0,0 +1,25 @@
+from pyais.messages import NMEAMessage, ANY_MESSAGE, AISSentence
+from pyais.stream import TCPConnection, FileReaderStream, IterMessages, Stream
+from pyais.encode import encode_dict, encode_msg, ais_to_nmea_0183
+from pyais.decode import decode
+from pyais.tracker import AISTracker, AISTrack
+
+__license__ = 'MIT'
+__version__ = '2.6.6'
+__author__ = 'Leon Morten Richter'
+
+__all__ = (
+    'encode_dict',
+    'encode_msg',
+    'ais_to_nmea_0183',
+    'NMEAMessage',
+    'AISSentence',
+    'ANY_MESSAGE',
+    'TCPConnection',
+    'IterMessages',
+    'FileReaderStream',
+    'Stream',
+    'decode',
+    'AISTracker',
+    'AISTrack',
+)
