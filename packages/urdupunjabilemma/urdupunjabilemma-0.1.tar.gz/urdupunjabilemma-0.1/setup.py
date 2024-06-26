7419bc4930fc8f19fa85dd16ef27ccacfc23efe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:05:22 +0000

    Add package urdupunjabilemma/0.1/urdupunjabilemma-0.1.tar.gz

---
+++ packages/urdupunjabilemma/urdupunjabilemma-0.1.tar.gz/urdupunjabilemma-0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='urdupunjabilemma',
+ version='0.1',
+ author="Muhammad Umar, Malaika Basharat",
+ author_email="umarwaris450@gmail.com, malaikabdev@gmail.com",
+ description="This is Bilingual Lemmatizer for Urdu and Punjabi. This will generate lemma of the given word for both Urdu and Shahmukhi Punjabi.",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ]
+)
