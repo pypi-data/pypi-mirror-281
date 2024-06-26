7bf0224f4819677108b07300cffc9ad9bf9e3dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:26 +0000

    Add package dlio-profiler-py/0.0.7/dlio_profiler_py-0.0.7.tar.gz

---
+++ packages/dlio-profiler-py/dlio_profiler_py-0.0.7.tar.gz/dlio_profiler_py-0.0.7/test/c/test.c
@@ -0,0 +1,44 @@
+//
+// Created by hariharan on 8/8/22.
+//
+
+#include <dlio_profiler/dlio_profiler.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void foo() {
+  DLIO_PROFILER_C_FUNCTION_START();
+  DLIO_PROFILER_C_FUNCTION_UPDATE_INT("key", 0);
+  DLIO_PROFILER_C_FUNCTION_UPDATE_STR("key", "0");
+  sleep(1);
+  DLIO_PROFILER_C_REGION_START(CUSTOM);
+  DLIO_PROFILER_C_REGION_UPDATE_INT(CUSTOM, "key", 0);
+  DLIO_PROFILER_C_REGION_UPDATE_STR(CUSTOM, "key", "0");
+  sleep(1);
+  DLIO_PROFILER_C_REGION_START(CUSTOM_BLOCK);
+  sleep(1);
+  DLIO_PROFILER_C_REGION_END(CUSTOM_BLOCK);
+  DLIO_PROFILER_C_REGION_END(CUSTOM);
+  DLIO_PROFILER_C_FUNCTION_END();
+}
+
+int main(int argc, char *argv[]) {
+  int init = 0;
+  if (argc > 2) {
+    if (strcmp(argv[2], "1") == 0) {
+      DLIO_PROFILER_C_INIT(NULL, NULL, NULL);
+      init = 1;
+    }
+  }
+  char filename[1024];
+  sprintf(filename, "%s/demofile_c.txt", argv[1]);
+  foo();
+  FILE *fh = fopen(filename, "w+");
+  fwrite("hello", sizeof("hello"), 1, fh);
+  fclose(fh);
+  if (init) {
+    DLIO_PROFILER_C_FINI();
+  }
+  return 0;
+}
