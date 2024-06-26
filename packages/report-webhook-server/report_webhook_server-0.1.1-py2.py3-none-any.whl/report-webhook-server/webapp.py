f2196a4123f39ac39940d62c7c58f51e5d50e221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:03:38 +0000

    Add package report-webhook-server/0.1.1/report_webhook_server-0.1.1-py2.py3-none-any.whl

---
+++ packages/report-webhook-server/report_webhook_server-0.1.1-py2.py3-none-any.whl/report-webhook-server/webapp.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import os
+import time
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPBasicAuth
+import jwt
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = set(['pdf', 'csv', 'gz'])
+UPLOAD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Downloads'))
+
+# initialization
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1000 * 1000  # 500 MB
+app.config['CORS_HEADER'] = 'application/json'
+# extensions
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+def allowedFile(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/upload', methods=['POST'])
+@auth.login_required
+def fileUpload():
+    if request.method == 'POST':
+        file = request.files.getlist('files')
+        filename = ""
+        print(request.files, "....")
+        for f in file:
+            print(f.filename)
+            filename = secure_filename(f.filename)
+            print(allowedFile(filename))
+            if allowedFile(filename):
+                f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                return jsonify({'message': 'File type not allowed'}), 400
+        return jsonify({"name": filename, "status": "success"})
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def generate_auth_token(self, expires_in=600):
+        return jwt.encode(
+            {'id': self.id, 'exp': time.time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_auth_token(token):
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'],
+                              algorithms=['HS256'])
+        except:
+            return
+        return User.query.get(data['id'])
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username': user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(600)
+    return jsonify({'token': token.decode('ascii'), 'duration': 600})
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        if not os.path.exists('db.sqlite'):
+            db.create_all()
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.mkdir(UPLOAD_FOLDER)
+        app.run(debug=True)
