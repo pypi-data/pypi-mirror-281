bef7fd2a41642e4aa96f55802a6113121af0064f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:08 +0000

    Add package koenigsberg/0.1.10/koenigsberg-0.1.10-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/koenigsberg/koenigsberg-0.1.10-cp38-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/koenigsberg/examples/__init__.py
@@ -0,0 +1,23 @@
+from functools import cache
+
+from palmerpenguins import load_penguins
+
+
+@cache
+def cached_penguins():
+    return load_penguins()
+
+
+class Example:
+    def __init__(self, name, load):
+        self.con = None
+        self.load = load
+        self.name = name
+
+    def fetch(self, table_name=None):
+        if self.con is None:
+            self.con = ibis.duckdb.connect()
+        return self.con.register(self.load(), table_name=table_name or self.name)
+
+
+penguins = Example("penguins", cached_penguins)
