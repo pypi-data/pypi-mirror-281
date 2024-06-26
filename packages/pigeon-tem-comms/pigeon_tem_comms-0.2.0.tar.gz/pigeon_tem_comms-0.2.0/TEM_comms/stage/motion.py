932e90e4f12c4d02c1087b8623463e11740a90db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 11:04:40 +0000

    Add package pigeon-tem-comms/0.2.0/pigeon_tem_comms-0.2.0.tar.gz

---
+++ packages/pigeon-tem-comms/pigeon_tem_comms-0.2.0.tar.gz/pigeon_tem_comms-0.2.0/TEM_comms/stage/motion.py
@@ -0,0 +1,12 @@
+from pigeon import BaseMessage
+
+class Command(BaseMessage):
+    x: int | None = None
+    y: int | None = None
+    calibrate: bool = False
+
+class Status(BaseMessage):
+    x: int
+    y: int
+    in_motion: bool
+    error: str = ""
