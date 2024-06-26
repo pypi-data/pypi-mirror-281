f433220dda39ff9e25c4a6252ec10e3a0d449dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:04:27 +0000

    Add package ptessentials/0.1.3/ptessentials-0.1.3.tar.gz

---
+++ packages/ptessentials/ptessentials-0.1.3.tar.gz/ptessentials-0.1.3/ptessentials/waxtion/__init__.py
@@ -0,0 +1,1 @@
+from .waxtion import Waxtion, WaxMainnet, WaxTestnet
---
+++ packages/ptessentials/ptessentials-0.1.3.tar.gz/ptessentials-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ptessentials',
+    version='0.1.3',
+    description='A PixelTycoons Dev Best Friend.',
+    url='https://github.com/funkaclau',
+    author='funkaclau',
+    packages=find_packages(),
+    install_requires=[
+        "waxnftdispatcher==0.3.5",
+        "waxtion>=0.1.5",
+        "waxfetcher",
+        "pyntelope",
+        "aanft"
+    ],
+    include_package_data=True
+)
