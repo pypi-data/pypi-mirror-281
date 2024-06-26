e8fc754c80c5caf8e1c3c9379db9bd939d34e3ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:03:53 +0000

    Add package motliere/1.1.5/motliere-1.1.5.tar.gz

---
+++ packages/motliere/motliere-1.1.5.tar.gz/motliere-1.1.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+import urllib.request
+
+# Function to download data files
+def download_data_files():
+        urls = [
+            'https://raw.githubusercontent.com/unimorph/fra/master/fra.derivations',
+            'https://raw.githubusercontent.com/unimorph/fra/master/fra.segmentations',
+        ]
+        data_dir = os.path.join(os.path.dirname(__file__), 'motliere', 'data')
+        os.makedirs(data_dir, exist_ok=True)
+        for url in urls:
+            file_name = os.path.basename(url)
+            file_path = os.path.join(data_dir, file_name)
+            print(f'Downloading {url} to {file_path}')
+            urllib.request.urlretrieve(url, file_path)
+
+# Check if data files exist, if not, download them
+if not os.path.exists(os.path.join(os.path.dirname(__file__), 'motliere', 'data')):
+    download_data_files()
+
+setup(
+    name='motliere',
+    version='1.1.5',
+    description='A morphological and linguistically motivated segmentation tool for French !',
+    authors=['Nicolas', 'Rémi'],
+    authors_emails=['E239002K@etu.univ-nantes.fr',
+                 'E23B509C@etu.univ-nantes.fr'],
+    license='gnu',
+    packages=['motliere'],
+    urls=['https://github.com/MorphSeg/Motliere/','https://gitlab.com/morphseg/motliere',],
+    install_requires=[
+        'numpy < 2.0.0',
+        'pandas',
+        'spacy',
+    ],
+    package_data={'motliere': ['data/*',]},
+)
