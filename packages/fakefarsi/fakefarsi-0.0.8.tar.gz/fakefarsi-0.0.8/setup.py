979dc4bf6392f180a54c026abbca806455a6ad40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 11:04:25 +0000

    Add package fakefarsi/0.0.8/fakefarsi-0.0.8.tar.gz

---
+++ packages/fakefarsi/fakefarsi-0.0.8.tar.gz/fakefarsi-0.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="fakefarsi",
+    version="0.0.8",
+    author="Mahan Rahmani",
+    author_email="mahanrahmani777@gmail.com",
+    description="A package for generating fake Iranian personal information",
+    long_description=readme,
+    url="https://github.com/mhnrhmni/FakeFarsi/",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Mit License",
+        "Operating System :: OS Independent",
+    ],
+)
