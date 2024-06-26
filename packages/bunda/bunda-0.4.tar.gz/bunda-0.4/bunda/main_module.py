55bd5de049ffac0bbdc6ef537b11992c9b0ba6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:04:11 +0000

    Add package bunda/0.4/bunda-0.4.tar.gz

---
+++ packages/bunda/bunda-0.4.tar.gz/bunda-0.4/bunda/main_module.py
@@ -0,0 +1,9 @@
+def print_6():
+    with open('./p6.txt', 'r') as file:
+        content = file.read()
+    print(content)
+
+def print_7():
+    with open('./p7.txt', 'r') as file:
+        content = file.read()
+    print(content)
