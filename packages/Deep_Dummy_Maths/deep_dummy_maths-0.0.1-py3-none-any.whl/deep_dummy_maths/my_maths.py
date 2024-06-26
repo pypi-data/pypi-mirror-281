775bc7492ef76f5d63cc19b97c793cbde60ba596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:04:44 +0000

    Add package Deep_Dummy_Maths/0.0.1/deep_dummy_maths-0.0.1-py3-none-any.whl

---
+++ packages/Deep_Dummy_Maths/deep_dummy_maths-0.0.1-py3-none-any.whl/deep_dummy_maths/my_maths.py
@@ -0,0 +1,18 @@
+def my_sum(a:int, b:int)->int:
+    print("Deep dummy maths 🚀")
+    return a+b
+
+
+def my_diff(a: int, b: int) -> int:
+    print("Deep dummy maths 🚀")
+    return a-b
+
+
+def my_pro(a: int, b: int) -> int:
+    print("Deep dummy maths 🚀")
+    return a*b
+
+
+def my_div(a: int, b: int) -> float:
+    print("Deep dummy maths 🚀")
+    return a/b
