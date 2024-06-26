b1656eb982b15e77acd1cf4826933a23dc1ea9cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:05:13 +0000

    Add package brainunit/0.0.1.post20240625/brainunit-0.0.1.post20240625-py2.py3-none-any.whl

---
+++ packages/brainunit/brainunit-0.0.1.post20240625-py2.py3-none-any.whl/brainunit/math/__init__.py
@@ -0,0 +1,81 @@
+# Copyright 2024 BDP Ecosystem Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from ._numpy_array_creation import *
+from ._numpy_array_creation import __all__ as _compat_array_creation_all
+from ._numpy_array_manipulation import *
+from ._numpy_array_manipulation import __all__ as _compat_array_manipulation_all
+from ._numpy_accept_unitless import *
+from ._numpy_accept_unitless import __all__ as _compat_funcs_accept_unitless_all
+from ._numpy_bit_operation import *
+from ._numpy_bit_operation import __all__ as _compat_funcs_bit_operation_all
+from ._numpy_change_unit import *
+from ._numpy_change_unit import __all__ as _compat_funcs_change_unit_all
+from ._numpy_indexing import *
+from ._numpy_indexing import __all__ as _compat_funcs_indexing_all
+from ._numpy_keep_unit import *
+from ._numpy_keep_unit import __all__ as _compat_funcs_keep_unit_all
+from ._numpy_logic import *
+from ._numpy_logic import __all__ as _compat_funcs_logic_all
+from ._numpy_match_unit import *
+from ._numpy_match_unit import __all__ as _compat_funcs_match_unit_all
+from ._numpy_remove_unit import *
+from ._numpy_remove_unit import __all__ as _compat_funcs_remove_unit_all
+from ._numpy_window_funs import *
+from ._numpy_window_funs import __all__ as _compat_funcs_window_all
+from ._numpy_get_attribute import *
+from ._numpy_get_attribute import __all__ as _compat_get_attribute_all
+from ._numpy_linear_algebra import *
+from ._numpy_linear_algebra import __all__ as _compat_linear_algebra_all
+from ._numpy_misc import *
+from ._numpy_misc import __all__ as _compat_misc_all
+from ._einops import *
+from ._einops import __all__ as _einops_all
+from ._others import *
+from ._others import __all__ as _other_all
+
+__all__ = (_compat_array_creation_all +
+           _compat_array_manipulation_all +
+           _compat_funcs_change_unit_all +
+           _compat_funcs_keep_unit_all +
+           _compat_funcs_accept_unitless_all +
+           _compat_funcs_match_unit_all +
+           _compat_funcs_remove_unit_all +
+           _compat_get_attribute_all +
+           _compat_funcs_bit_operation_all +
+           _compat_funcs_logic_all +
+           _compat_funcs_indexing_all +
+           _compat_funcs_window_all +
+           _compat_linear_algebra_all +
+           _compat_misc_all + _other_all +
+           _other_all +
+           _einops_all)
+
+del (_compat_array_creation_all,
+     _compat_array_manipulation_all,
+     _compat_funcs_change_unit_all,
+     _compat_funcs_keep_unit_all,
+     _compat_funcs_accept_unitless_all,
+     _compat_funcs_match_unit_all,
+     _compat_funcs_remove_unit_all,
+     _compat_get_attribute_all,
+     _compat_funcs_bit_operation_all,
+     _compat_funcs_logic_all,
+     _compat_funcs_indexing_all,
+     _compat_funcs_window_all,
+     _compat_linear_algebra_all,
+     _compat_misc_all,
+     _other_all,
+     _einops_all)
