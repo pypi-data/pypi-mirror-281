6074a2784e0138d875ab6324091f5aeed6833642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 11:04:12 +0000

    Add package pyqrack/1.29.5/pyqrack-1.29.5.tar.gz

---
+++ packages/pyqrack/pyqrack-1.29.5.tar.gz/pyqrack-1.29.5/setup.py
@@ -0,0 +1,57 @@
+# Adapted from https://github.com/Qiskit/qiskit-aer/blob/master/setup.py
+
+import os
+from setuptools import setup
+
+
+VERSION = "1.29.5"
+
+# Read long description from README.
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')
+with open(README_PATH) as readme_file:
+    README = readme_file.read()
+
+# This is technically a module included in the language standard by PEP,
+# but some systems might opt not to implement any particular PEP.
+requirements = [
+    "pathlib"
+]
+
+setup(
+    name='pyqrack',
+    version=VERSION,
+    packages=['pyqrack', 'pyqrack.qrack_system', 'pyqrack.util'],
+    description="pyqrack - Pure Python vm6502q/qrack Wrapper",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url="https://github.com/vm6502q/pyqrack",
+    author="Daniel Strano",
+    author_email="dan@unitary.fund",
+    license="MIT",
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+    ],
+    keywords="pyqrack qrack simulator quantum gpu",
+    install_requires=requirements,
+    setup_requires=[],
+    extras_require={
+        "dev": [
+            "pytest>=7.3.1",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
