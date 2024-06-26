adcf8eac2aff2610cb2e970606ce85cdd8567d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:10 +0000

    Add package rhkpy/1.3.9/rhkpy-1.3.9-py3-none-any.whl

---
+++ packages/rhkpy/rhkpy-1.3.9-py3-none-any.whl/rhkpy/__init__.py
@@ -0,0 +1,27 @@
+from .version import __version__
+from .rhkpy_loader import *
+from .rhkpy_process import *
+
+import hvplot.xarray
+import holoviews as hv
+from holoviews import dim, opts
+
+import logging
+from bokeh.util import logconfig
+# Suppress Bokeh's warnings
+logconfig.basicConfig(level=logging.ERROR)
+
+import warnings
+# suppress holoviews warnings
+warnings.filterwarnings('ignore', category = UserWarning, module = 'holoviews.plotting.bokeh.plot')
+
+# select bokeh
+hvplot.extension('bokeh')
+
+# global plotting options
+from holoviews import opts
+opts.defaults(
+    opts.Image(
+        aspect = 'equal'
+        )
+    ) # can't set default colormap with this
