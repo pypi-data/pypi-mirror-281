f49416fd12b70a59be8085aec0620e0651d831ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:41 +0000

    Add package wiseman-library/1.0.0/wiseman_library-1.0.0-py3-none-any.whl

---
+++ packages/wiseman-library/wiseman_library-1.0.0-py3-none-any.whl/wiseman_library/Recognizer.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+def recognize_from_google(voice="voice.wav", lang='en'):
+    engine = sr.Recognizer()
+    with sr.AudioFile(voice) as source:
+        voice_data = engine.record(source)
+        try:
+            text = engine.recognize_google(voice_data, language=lang) 
+            return text
+        except sr.UnknownValueError as e:
+            return str(e)
+        except sr.RequestError as e:
+            return str(e)
+        
+def recognize(voice="voice.wav", lang='en'):
+    return recognize_from_google(voice)
