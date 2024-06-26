0c5218f50db167613a82c8c4f9144809118f3353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:09 +0000

    Add package mediacatch/0.0.1/mediacatch-0.0.1-py3-none-any.whl

---
+++ packages/mediacatch/mediacatch-0.0.1-py3-none-any.whl/mediacatch/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+import os
+
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    format='[%(asctime)s][%(levelname)s][%(name)s] %(message)s',
+    datefmt='%d/%m/%Y-%H:%M:%S',
+    handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO,
+)
+
+load_dotenv()
+mediacatch_api_key = os.environ['MEDIACATCH-API-KEY']
+
+__version__ = '0.0.1'
