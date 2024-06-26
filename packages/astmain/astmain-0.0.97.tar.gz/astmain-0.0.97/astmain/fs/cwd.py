515cf5f49f235a6f619a479b4d4925c1e487d3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 04:04:06 +0000

    Add package astmain/0.0.97/astmain-0.0.97.tar.gz

---
+++ packages/astmain/astmain-0.0.97.tar.gz/astmain-0.0.97/astmain/fs/cwd.py
@@ -0,0 +1,9 @@
+import os
+def cwd(my_path=""):
+    if (my_path):
+
+        current_dir = os.getcwd()
+        return os.path.join(current_dir,my_path)
+    else:
+        return os.getcwd()
+
---
+++ packages/astmain/astmain-0.0.97.tar.gz/astmain-0.0.97/astmain/fs/log_all.py
@@ -0,0 +1,27 @@
+import datetime
+
+from write_add import write_add
+
+
+# level 默认就好   "error"
+# info  必填参数
+# path  默认参数   r"C:\log_all.txt"
+
+
+def log_all(level="error", info="默认内容1", path=r"C:\log_all.txt"):
+    """ EXPLAIN : 日志追加记录函数
+        PARAMS  :
+        level     默认就好   "error"
+        info      必填参数
+        path      默认参数   r"C:\log_all.txt"
+
+        RETURN  : null
+        EXAMPLE :  log_all(info="我的错误", path=r"C:\log_all.txt")
+    """
+
+    content = level + "|||" + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "|||" + info
+    write_add(path, content)
+
+
+if __name__ == '__main__':
+    log_all(info="我的错误", path=r"C:\log_all.txt")
