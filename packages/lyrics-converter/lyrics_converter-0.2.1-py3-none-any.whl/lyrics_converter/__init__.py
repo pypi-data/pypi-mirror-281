f26611a0a380ac5a1c0bdfe7865f8b3f684e5a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:53 +0000

    Add package lyrics-converter/0.2.1/lyrics_converter-0.2.1-py3-none-any.whl

---
+++ packages/lyrics-converter/lyrics_converter-0.2.1-py3-none-any.whl/lyrics_converter/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024-present Andrew Beveridge <andrew@beveridge.uk>
+#
+# SPDX-License-Identifier: MIT
+from .converter import LyricsConverter
+
+__all__ = ["LyricsConverter"]
