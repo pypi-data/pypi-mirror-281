95f870f716ed58ea17e1f8746c5abc4992c17f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:05:16 +0000

    Add package py-chocolatey/0.4.1/py_chocolatey-0.4.1.zip

---
+++ packages/py-chocolatey/py_chocolatey-0.4.1.zip/py_chocolatey-0.4.1/src/chocolatey/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022 Adam Karpierz
+# Licensed under the zlib/libpng License
+# https://opensource.org/license/zlib
+
+from .__about__ import * ; del __about__  # noqa
+
+from ._chocolatey     import * ; del _chocolatey  # noqa
+from ._chocolatey_cmd import * ; del _chocolatey_cmd  # noqa
