a92c90273a41f9c202488e14148afd037030d4c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:10 +0000

    Add package openassetio/1.0.0b2.post2/openassetio-1.0.0b2.post2-cp311-cp311-win_amd64.whl

---
+++ packages/openassetio/openassetio-1.0.0b2.post2-cp311-cp311-win_amd64.whl/openassetio/_openassetio/constants.pyi
@@ -0,0 +1,8 @@
+from __future__ import annotations
+__all__ = ['kField_EntityReferencesMatchPrefix', 'kField_Icon', 'kField_SmallIcon', 'kInfoKey_EntityReferencesMatchPrefix', 'kInfoKey_Icon', 'kInfoKey_SmallIcon']
+kField_EntityReferencesMatchPrefix: str = 'entityReferencesMatchPrefix'
+kField_Icon: str = 'icon'
+kField_SmallIcon: str = 'smallIcon'
+kInfoKey_EntityReferencesMatchPrefix: str = 'entityReferencesMatchPrefix'
+kInfoKey_Icon: str = 'icon'
+kInfoKey_SmallIcon: str = 'smallIcon'
