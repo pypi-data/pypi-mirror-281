c2879012d0778552c1df1048e2770e5443051cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 17:05:27 +0000

    Add package mpsqd/0.1.0/mpsqd-0.1.0-py3-none-any.whl

---
+++ packages/mpsqd/mpsqd-0.1.0-py3-none-any.whl/mpsqd/utils/mctdh2python.py
@@ -0,0 +1,95 @@
+import sys
+import re
+import numpy as np
+from ..models.construct_tds import write_file_define
+AU2EV = 27.2113845
+def get_all(fr):
+  data = fr.readlines()
+  fr.close()
+  data_re = []
+  for item in data:
+    data_re.append(item.strip().replace(' ', '').lower())
+#everything after "#" will be ignored
+    if(re.search('#', item)):
+      begin = re.search('#', item).span()[0]
+      item = item[:begin]
+  return data_re
+
+def mctdh2python(input_name,output_name):
+  data_re = get_all(open(input_name))
+  parameters = {}
+  e0_0 = {}
+  omega_0 = {}
+  coef1_0 = {}
+  coef2_0 = {}
+  try:
+    for k in data_re:
+      if '=' in k:
+        key, valunit = k.split("=")[0:2]
+        value, unit = valunit.split(",")[0:2]
+        param = {}
+        if unit == 'ev':
+          param = float(value) / AU2EV
+        else:
+          param = float(value)
+        parameters[key] = param
+    for k in data_re:
+      if re.search(u'\|\ds\d+&\d+\|\d+q\^2', k):
+        result = re.search(u'(\w+).*?s(\d+).*?(\d+)\|(\d+).*', k)
+        key, istate1, istate2, imode = result.groups()
+        coef2_0[(int(istate1)-1, int(istate2)-1, int(imode)-2, int(imode)-2)] = parameters[key]
+      elif re.search(u'\|\ds\d+&\d+\|\d+q\|\d+q', k):
+        result = re.search(u'.*\*(\w+).*?s(\d+).*?(\d+)\|(\d+).*\|(\d+).*', k)
+        key, istate1, istate2, imode1, imode2 = result.groups()
+        coef2_0[(int(istate1)-1, int(istate2)-1, int(imode1)-2, int(imode2)-2)] = parameters[key]
+      elif re.search(u'\|\ds\d+&\d+\|\d+q', k):
+        result = re.search(u'(\w+).*?s(\d+).*?(\d+)\|(\d+).*', k)
+        key, istate1, istate2, imode = result.groups()
+        coef1_0[(int(istate1)-1, int(istate2)-1, int(imode)-2)] = parameters[key]
+      elif re.search(u'\|\d+ke', k):
+        result = re.search(u'.*\*(\w+)\|(\d+)ke', k)
+        key, imode = result.groups()
+        omega_0[int(imode)-2] = parameters[key]
+      elif re.search(u'\d+&\d+', k):
+        result = re.search(u'(\w+).*\|\d+s(\d+)&(\d+)', k)
+        key, istate1, istate2 = result.groups()
+        if '-' in k:
+          e0_0[(int(istate1)-1, int(istate2)-1)] = -1*parameters[key]
+        else:
+          e0_0[(int(istate1)-1, int(istate2)-1)] = parameters[key]
+  except(ValueError,KeyError):
+    print("Wrong value: "+k)
+    sys.exit()
+  statelist = [x[0] for x in e0_0.keys()] + [x[1] for x in e0_0.keys()] + [x[0] for x in coef1_0.keys()] + [x[1] for x in coef1_0.keys()] + [x[0] for x in coef2_0.keys()] + [x[1] for x in coef2_0.keys()]
+  modelist = [x for x in omega_0.keys()]
+  nstate = max(statelist)+1
+  nmode = max(modelist)+1
+  params = {}
+  params["ndvr"] = nstate
+  params["nlevel"] = nmode
+  e0 = np.zeros((nstate, nstate), dtype=np.float64)
+  omega = np.zeros(nmode, dtype=np.float64)
+  coef1 = np.zeros((nstate, nstate, nmode), dtype=np.float64)
+  coef2 = np.zeros((nstate, nstate, nmode, nmode), dtype=np.float64)
+  for key in e0_0.keys():
+    istate1,istate2 = key
+    e0[istate1, istate2] = e0_0[key]
+    e0[istate2, istate1] = e0_0[key]
+  for key in omega_0.keys():
+    omega[key] = omega_0[key]
+  for key in coef1_0.keys():
+    istate1,istate2,imode = key
+    coef1[istate1, istate2, imode] = coef1_0[key]
+    coef1[istate2, istate1, imode] = coef1_0[key]
+  for key in coef2_0.keys():
+    istate1,istate2,imode1,imode2 = key
+    coef2[istate1, istate2, imode1, imode2] = coef2_0[key]
+    coef2[istate2, istate1, imode1, imode2] = coef2_0[key]
+    coef2[istate1, istate2, imode2, imode1] = coef2_0[key]
+    coef2[istate2, istate1, imode2, imode1] = coef2_0[key]
+  params['e0'] = e0
+  params['omega'] = omega
+  params['coef1'] = coef1
+  params['coef2'] = coef2
+  write_file_define(params,output_name)
+  return
