33b247b87c34c6457045518d1b4149fad5662ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:28 +0000

    Add package hack4u-adraul/0.10.5/hack4u_adraul-0.10.5-py3-none-any.whl

---
+++ packages/hack4u-adraul/hack4u_adraul-0.10.5-py3-none-any.whl/hack4u/courses.py
@@ -0,0 +1,26 @@
+class Course:
+    def __init__(self,name,duration,link):
+        self.name = name
+        self.duration = duration
+        self.link = link
+    
+    def __repr__(self) -> str:
+        return f"course name: {self.name}\nduration: {self.duration}\nlink: {self.link}"
+
+courses =[
+    Course("Introduccion a Linux",15,"link1"),
+    Course("Personalizacion de Linux",3,"link2"),
+    Course("Introduccion al Hacking",53,"link3"),
+]
+
+def list_courses():
+    for c in courses:
+        print(c)
+
+def search_by_name(name):
+    for c in courses:
+        if c.name == name:
+            return c
+    return None 
+
+search_by_name("Personalizacion de Linux")
