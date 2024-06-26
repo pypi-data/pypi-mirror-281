9acb056c5759f011aebf8f2d1d1fb4a908f1d90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:04:02 +0000

    Add package ry/0.0.8/ry-0.0.8-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/ry/ry-0.0.8-cp312-cp312-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/ry/__init__.py
@@ -0,0 +1,13 @@
+from ry import _ry
+from ry._ry import *
+
+__version__ = _ry.__version__
+__doc__ = _ry.__doc__
+__build_profile__ = _ry.__build_profile__
+__build_timestamp__ = _ry.__build_timestamp__
+if hasattr(_ry, "__all__"):
+    __all__ = _ry.__all__
+
+# ry start
+# TODO: auto generate stuff here...
+# ry end
