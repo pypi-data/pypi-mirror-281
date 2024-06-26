be4c44af5e2fe863c181afab92584e45d2b7b2da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:46 +0000

    Add package glean-client-api/1.0.0/glean-client-api-1.0.0.tar.gz

---
+++ packages/glean-client-api/glean-client-api-1.0.0.tar.gz/glean-client-api-1.0.0/glean_client_api/api/__init__.py
@@ -0,0 +1,24 @@
+# flake8: noqa
+
+# import apis into api package
+from glean_client_api.api.activity_api import ActivityApi
+from glean_client_api.api.announcements_api import AnnouncementsApi
+from glean_client_api.api.answers_api import AnswersApi
+from glean_client_api.api.authentication_api import AuthenticationApi
+from glean_client_api.api.calendar_api import CalendarApi
+from glean_client_api.api.chat_api import ChatApi
+from glean_client_api.api.collections_api import CollectionsApi
+from glean_client_api.api.displayable_lists_api import DisplayableListsApi
+from glean_client_api.api.documents_api import DocumentsApi
+from glean_client_api.api.entities_api import EntitiesApi
+from glean_client_api.api.images_api import ImagesApi
+from glean_client_api.api.insights_api import InsightsApi
+from glean_client_api.api.messages_api import MessagesApi
+from glean_client_api.api.pins_api import PinsApi
+from glean_client_api.api.search_api import SearchApi
+from glean_client_api.api.shortcuts_api import ShortcutsApi
+from glean_client_api.api.summarize_api import SummarizeApi
+from glean_client_api.api.tools_api import ToolsApi
+from glean_client_api.api.user_api import UserApi
+from glean_client_api.api.verification_api import VerificationApi
+
