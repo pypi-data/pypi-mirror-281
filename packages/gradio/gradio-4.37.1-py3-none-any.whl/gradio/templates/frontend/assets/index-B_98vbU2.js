5f4a69fe0895f7b0721462b73bb3c9e82b287b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:48 +0000

    Add package gradio/4.37.1/gradio-4.37.1-py3-none-any.whl

---
+++ packages/gradio/gradio-4.37.1-py3-none-any.whl/gradio/templates/frontend/assets/index-B_98vbU2.js
@@ -0,0 +1,2 @@
+import{L as e,S as m,S as p}from"./Index-B0pzGNvZ.js";import{T as i}from"./Blocks-CbMTi72h.js";import"./index-BBXRYz6K.js";import"./svelte/svelte.js";import"./Button-B9ZzokJD.js";export{e as Loader,m as StatusTracker,i as Toast,p as default};
+//# sourceMappingURL=index-B_98vbU2.js.map
---
+++ packages/gradio/gradio-4.37.1-py3-none-any.whl/gradio/templates/frontend/gradio.js
@@ -0,0 +1,1 @@
+import("./assets/index-BBXRYz6K.js");
