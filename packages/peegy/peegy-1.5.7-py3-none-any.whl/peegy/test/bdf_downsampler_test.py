0cc0ba07b6068b37eea42715d055fb073533189e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:04:14 +0000

    Add package peegy/1.5.7/peegy-1.5.7-py3-none-any.whl

---
+++ packages/peegy/peegy-1.5.7-py3-none-any.whl/peegy/test/bdf_downsampler_test.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from peegy.io.eeg.reader import eeg_reader
+from peegy.io.edf_bdf_writer import bdf_resampler
+from peegy.io.external_tools.file_tools import get_files_and_meta_files
+from peegy.io.external_tools.aep_gui.dataReadingTools import get_files_and_meta_data
+import numpy as np
+from peegy.processing.events.event_tools import get_event_times
+import os
+from pathlib import Path
+import matplotlib
+import astropy.units as u
+if 'Qt5Agg' in matplotlib.rcsetup.all_backends:
+    matplotlib.use('Qt5Agg')
+
+
+_path = Path(os.path.abspath(os.path.dirname(__file__)))
+data_folder = _path.parent.parent.absolute().joinpath("test_data/")
+data_folder = r'/home/jundurraga/Documents/Measurements/ITD-FR-Pilots/JU/'
+data_files_meta = get_files_and_meta_files(data_folder, file_types=['bdf'])
+
+for _, _file in data_files_meta.iterrows():
+    output_file_path = _path.parent.parent.absolute().joinpath('split_test')
+    _file_path = _file.data_links.data_file
+    bdf_resampler(input_file_name=_file_path,
+                  # output_path=output_file_path,
+                  new_fs=1024 * u.Hz,
+                  trash_original=True)
+
+data_files_meta = get_files_and_meta_data(data_folder)
+_first_file = data_files_meta.iloc[0].data_links.data_file
+data4 = eeg_reader(file_name=_first_file)
+data_ds, events_ds, _, _ = data4.get_data(channels_idx=np.array([0]))
+et_ds = get_event_times(event_channel=events_ds, fs=data4.fs)
+et_ds.summary()
+fig1 = plt.figure(figsize=(7, 6), dpi=100)
+t_ds = np.arange(data_ds.shape[0]) / data4.fs
+ax = fig1.add_subplot(1, 1, 1)
+ax.plot(t_ds, data_ds[:, 0])
+ax.plot(t_ds, events_ds)
+plt.show()
