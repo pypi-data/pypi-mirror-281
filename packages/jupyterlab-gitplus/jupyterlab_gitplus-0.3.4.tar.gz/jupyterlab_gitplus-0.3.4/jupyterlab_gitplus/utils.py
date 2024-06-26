17fe51821d9072ac71a657cec0cfc99885a55722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:03:29 +0000

    Add package jupyterlab-gitplus/0.3.4/jupyterlab_gitplus-0.3.4.tar.gz

---
+++ packages/jupyterlab-gitplus/jupyterlab_gitplus-0.3.4.tar.gz/jupyterlab_gitplus-0.3.4/jupyterlab_gitplus/utils.py
@@ -0,0 +1,51 @@
+
+import re
+import os
+import stat
+import logging
+logger = logging.getLogger(__name__)
+
+GITHUB_REMOTE_URL_REGEX = re.compile(r"github\.com[:\/](.*?)\/(.*?)\.git")
+
+
+def get_owner_login_and_repo_name(repo):
+    owner_login, repo_name = '', ''
+    remote_url = repo.remotes.origin.url
+
+    if not remote_url.endswith('.git'):
+        remote_url += '.git'
+
+    match = GITHUB_REMOTE_URL_REGEX.search(remote_url)
+
+    if match:
+        owner_login = match.group(1)
+        repo_name = match.group(2)
+        logger.info(f"For git repo {remote_url}, found {owner_login}/{repo_name}")
+    else:
+        logger.error(f"Unable to find owner/repo name for repo {remote_url}")
+
+    return owner_login, repo_name
+
+
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+
+    Copied from: https://stackoverflow.com/a/2656405/10674324
+    """
+    if exc_info[0].__name__ == 'FileNotFoundError':
+        # folder does not exist, no need to delete
+        pass
+    elif not os.access(path, os.W_OK):
+        # Is the error an access error ?
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
---
+++ packages/jupyterlab-gitplus/jupyterlab_gitplus-0.3.4.tar.gz/jupyterlab_gitplus-0.3.4/setup.py
@@ -0,0 +1,73 @@
+import re
+import setuptools
+from pathlib import Path
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('jupyterlab_gitplus/__init__.py').read(),
+    re.M
+    ).group(1)
+
+
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+HERE = Path(__file__).parent.resolve()
+
+# The name of the project
+name = "jupyterlab_gitplus"
+
+
+lab_path = (HERE / name.replace("-", "_") / "labextension")
+labext_name = "@reviewnb/jupyterlab_gitplus"
+
+# Representative files that should exist after a successful build
+ensured_targets = [
+    str(lab_path / "package.json"),
+    str(lab_path / "static/style.js")
+]
+
+data_files_spec = [
+    ("share/jupyter/labextensions/%s" % labext_name, str(lab_path), "**"),
+    ("share/jupyter/labextensions/%s" % labext_name, str(HERE), "install.json"),
+    ("etc/jupyter/jupyter_server_config.d", "jupyter-config/server-config", "jupyterlab_gitplus.json"),
+    # For backward compatibility with notebook server
+    ("etc/jupyter/jupyter_notebook_config.d", "jupyter-config/nb-config", "jupyterlab_gitplus.json"),
+]
+
+setup_args = dict(
+    name = "jupyterlab_gitplus",
+    packages = ["jupyterlab_gitplus"],
+    python_requires='>=3',
+    version = version,
+    description = "JupyterLab extension to create GitHub pull requests",
+    long_description = long_descr,
+    long_description_content_type="text/markdown",
+    author = "Amit Rathi",
+    author_email = "amit@reviewnb.com",
+    url = "https://github.com/ReviewNB/jupyterlab-gitplus",
+    keywords=['github', 'jupyter', 'notebook', 'pull request', 'version control', 'git'],
+    include_package_data=True,
+    platforms="Linux, Mac OS X, Windows",
+    install_requires=[
+        'jupyterlab',
+        'gitpython',
+        'requests',
+        'urllib3',
+        'jupyter_server>=1.6,<3'
+    ]
+)
+
+
+from jupyter_packaging import (
+    wrap_installers,
+    npm_builder,
+    get_data_files
+)
+
+post_develop = npm_builder(build_cmd="install:extension", source_dir="src", build_dir=lab_path)
+setup_args['cmdclass'] = wrap_installers(post_develop=post_develop, ensured_targets=ensured_targets)
+setup_args['data_files'] = get_data_files(data_files_spec)
+
+if __name__ == "__main__":
+    setuptools.setup(**setup_args)
