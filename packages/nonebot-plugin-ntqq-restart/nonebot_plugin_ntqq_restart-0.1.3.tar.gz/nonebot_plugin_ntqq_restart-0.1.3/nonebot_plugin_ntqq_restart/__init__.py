cb5b5fed2ec3c127d5123f2ebcec4cf752d347f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:03:58 +0000

    Add package nonebot-plugin-ntqq-restart/0.1.3/nonebot_plugin_ntqq_restart-0.1.3.tar.gz

---
+++ packages/nonebot-plugin-ntqq-restart/nonebot_plugin_ntqq_restart-0.1.3.tar.gz/nonebot_plugin_ntqq_restart-0.1.3/nonebot_plugin_ntqq_restart/__init__.py
@@ -0,0 +1,21 @@
+import os
+
+from nonebot import require
+from nonebot.plugin import PluginMetadata
+require("nonebot_plugin_apscheduler")
+
+from .config import Config
+# 强制Windows系统插件生效
+if os.name == "nt":
+    from .__main__ import *
+else:
+    logger.warning("此插件仅能在Windows系统上使用, 已自动禁用插件!")
+
+__plugin_meta__ = PluginMetadata(
+    name="NTQQ自动登录/断连重启",
+    description="一个基于WinAPI的简易NTQQ重启插件",
+    usage=".env填写NTQQ路径后加载插件即可自动运行",
+    type="application",
+    config=Config,
+    extra={},
+)
