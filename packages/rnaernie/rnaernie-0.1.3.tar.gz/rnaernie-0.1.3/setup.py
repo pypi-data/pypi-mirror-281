a823ca12a846d471ae778d5aa83e4a092eb59734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:22 +0000

    Add package rnaernie/0.1.3/rnaernie-0.1.3.tar.gz

---
+++ packages/rnaernie/rnaernie-0.1.3.tar.gz/rnaernie-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rnaernie',
+    version='0.1.3',
+    packages=find_packages(exclude=['ss_token/']),
+    install_requires=[
+        # List your package dependencies here
+        # 'somepackage>=1.0',
+    ],
+    include_package_data=True,
+    description='RNAErnie2',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/CatIIIIIIII/RNAErnie2',  # Replace with your repo URL
+    author='WANG Ning',
+    author_email='wangning.roci@gmail.com',
+    license='MIT',  # Choose your license
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
