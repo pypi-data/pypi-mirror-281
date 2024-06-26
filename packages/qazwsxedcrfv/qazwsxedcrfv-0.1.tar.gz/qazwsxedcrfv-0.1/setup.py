b97b288aa47b63aa34e1278a285f2eb58f6f1411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:04:48 +0000

    Add package qazwsxedcrfv/0.1/qazwsxedcrfv-0.1.tar.gz

---
+++ packages/qazwsxedcrfv/qazwsxedcrfv-0.1.tar.gz/qazwsxedcrfv-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qazwsxedcrfv',
+    version='0.1',
+    description='A simple Python library for calculating the area of geometric shapes.',
+    author='Yuvashree',
+    packages=find_packages(),
+    # Specify the 'bdist_wheel' command to build wheel distributions
+    setup_requires=['wheel'],
+)
