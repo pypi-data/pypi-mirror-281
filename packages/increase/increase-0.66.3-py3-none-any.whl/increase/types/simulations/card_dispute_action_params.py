9ab691e17b60e242db4f3d6fa6c894f4181f991d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 14:04:10 +0000

    Add package increase/0.66.3/increase-0.66.3-py3-none-any.whl

---
+++ packages/increase/increase-0.66.3-py3-none-any.whl/increase/types/simulations/card_dispute_action_params.py
@@ -0,0 +1,20 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["CardDisputeActionParams"]
+
+
+class CardDisputeActionParams(TypedDict, total=False):
+    status: Required[Literal["accepted", "rejected"]]
+    """The status to move the dispute to.
+
+    - `accepted` - The Card Dispute has been accepted and your funds have been
+      returned.
+    - `rejected` - The Card Dispute has been rejected.
+    """
+
+    explanation: str
+    """Why the dispute was rejected. Not required for accepting disputes."""
