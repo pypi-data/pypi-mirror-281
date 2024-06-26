b2024248aa950d3849f480fdae22c4484df70e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:30 +0000

    Add package Heb/5.1.0/Heb-5.1.0.tar.gz

---
+++ packages/Heb/Heb-5.1.0.tar.gz/Heb-5.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Heb',
+    version='5.1.0',
+    packages=find_packages(),
+    install_requires=[
+      
+    ],description="Clean your Directory with just one command",
+    # entry_points={
+    #     'console_scripts': [
+    #         'clean = LeanDesk.cli:main',
+    #     ],
+    # },
+)
