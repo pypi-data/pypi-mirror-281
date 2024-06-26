919cae943c0473b764efc3425fcdf52537737af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:11 +0000

    Add package astmain/0.0.108/astmain-0.0.108.tar.gz

---
+++ packages/astmain/astmain-0.0.108.tar.gz/astmain-0.0.108/astmain/test222.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+import os, io, sys, asyncio, requests, arrow, json, time
+# import astmain as __
+
+
+# coding=utf-8
+import os, io, sys, asyncio, requests, arrow, json, time
+# import astmain as __
+import astmain_py777.astmain as __
+from astmain_py777.astmain import print
+__.print_hook = True
+print = __.print
+
+print("111                 :", 111)
+print("111                 :", 111)
+print("111                 :", 222)
