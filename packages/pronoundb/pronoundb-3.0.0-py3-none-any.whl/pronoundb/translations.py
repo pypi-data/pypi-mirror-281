c7079bfdf6619adc1b014227732e6348dd371d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 07:05:09 +0000

    Add package pronoundb/3.0.0/pronoundb-3.0.0-py3-none-any.whl

---
+++ packages/pronoundb/pronoundb-3.0.0-py3-none-any.whl/pronoundb/translations.py
@@ -0,0 +1,23 @@
+english_pronouns: dict[str, list[str]] = {
+    "unspecified": [],
+    "he": ["he", "him"],
+    "she": ["she", "her"],
+    "it": ["it", "its"],
+    "they": ["they", "them"],
+    "any": ["any"],
+    "other": ["other"],
+    "ask": ["ask"],
+    "avoid": ["use name"],
+}
+
+german_pronouns: dict[str, list[str]] = {
+    "unspecified": [],
+    "he": ["Er", "Ihn"],
+    "she": ["Sie", "Ihr"],
+    "it": ["Es", "Seine"],
+    "they": ["They", "Them"],
+    "any": ["Jede"],
+    "other": ["Anderes"],
+    "ask": ["Frag"],
+    "avoid": ["Nutz Name"],
+}
