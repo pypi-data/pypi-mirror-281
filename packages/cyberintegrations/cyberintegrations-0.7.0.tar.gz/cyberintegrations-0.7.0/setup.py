98d01fa689ded555735626e229998ce6406b8a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 06:04:20 +0000

    Add package cyberintegrations/0.7.0/cyberintegrations-0.7.0.tar.gz

---
+++ packages/cyberintegrations/cyberintegrations-0.7.0.tar.gz/cyberintegrations-0.7.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='cyberintegrations',
+    version="0.7.0",
+    description='Package provides pollers',
+    python_requires='>=3.6',
+    install_requires=['requests>=2.25.1', 'dataclasses', 'urllib3'],
+    packages=['cyberintegrations'],
+    author='Сyberintegrations',
+    author_email='cyberintegrationsdev@gmail.com',
+    license='MIT',
+    # long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+)
