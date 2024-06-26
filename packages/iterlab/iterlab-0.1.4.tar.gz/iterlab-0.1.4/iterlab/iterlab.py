5176c56276e2699035bf7530c207e0b01d3839b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:05:06 +0000

    Add package iterlab/0.1.4/iterlab-0.1.4.tar.gz

---
+++ packages/iterlab/iterlab-0.1.4.tar.gz/iterlab-0.1.4/iterlab/iterlab.py
@@ -0,0 +1,165 @@
+import re
+
+
+
+def natural_sort(array, inplace=False):
+    '''
+    Description
+    ----------
+    Sorts list via natural sorting as opposed to lexicographical sorting. For example, consider a
+    list of file names starting with numbers ['101 Dalmatians.xlsx', '3 Blind Mice.xlsx']. Sorting
+    using the default method will yield the same list because the first character "1" < "3". Conversely,
+    natural sorting will intuitively place the name beginning with "3" before "101" since "3" < "101".
+
+    Parameters
+    ----------
+    array :  list
+        list to sort
+    inplace : bool
+        if True, sorting will be done inplace
+
+    Returns
+    ----------
+    out : list | None
+        list if inplace is False otherwise None
+    '''
+
+    def alphanumeric_key(element):
+        ''' converts string into a list of string and number chunks (e.g. 'z23a' -> ['z', 23, 'a']) '''
+
+        def try_int(x):
+            try:
+                return int(x)
+            except:
+                return x
+
+        out = list(map(try_int, re.split('([0-9]+)', str(element))))
+        return out
+
+    if inplace:
+        array.sort(key=alphanumeric_key)
+    else:
+        return sorted(array, key=alphanumeric_key)
+
+
+
+def iter_get(array, index=0, default=None):
+    ''' extends dictionary's .get() to other iterables such as lists '''
+    if array is None:
+        return default
+    try:
+        return array[index]
+    except IndexError:
+        return default
+
+
+
+def to_iter(x):
+    ''' converts variable to a list '''
+    return x if isinstance(x, (list, tuple)) else [x]
+
+
+
+def delimit_iter(x, func=None, delimiter=', ', quotes='single', encase=True):
+    '''
+    Description
+    ----------
+    Delimits an iterable
+
+    Parameters
+    ----------
+    x : iterable
+        iterable to delimit
+    func : type | func | None
+        formats each element before it is delimited.
+    delimiter : str
+        delimiter
+    quotes : str | None
+        • 'single' ➜ encase each element in single quotes
+        • 'double' ➜ encase each element in double quotes
+        • None ➜ each element is not encased
+    encase : bool
+        if True, the final output is encased in parenthesis
+
+    Returns
+    ----------
+    out : str
+        delimited iterable
+    '''
+    if func is not None:
+        x = list(map(func, x))
+
+    x =  [f'{z}' for z in x]
+
+    if quotes is not None:
+        q = {'single': "'", 'double': '"'}[quotes]
+        x =  [z.join([q] * 2) for z in x]
+
+    x = delimiter.join(x)
+    return f'({x})' if encase else x
+
+
+
+def lower_iter(iterable):
+    return [x.lower() for x in iterable]
+
+
+
+def text_to_iter(text, transform=str, delimiter='\n'):
+    '''
+    Description
+    ----------
+    Converts a list of items in text format to a Python list.
+
+    Parameters
+    ----------
+    text : str
+        Delimited string
+    transform : func
+        Function applied to each item as list is constructed
+    delimiter : str
+        text delimiter
+
+    Returns
+    ----------
+    out : list
+        list if items in text file
+    '''
+    return [transform(x.strip()) for x in text.split(delimiter) if x.strip()]
+
+
+
+def iter_window(x, left=0, right=0, strict=False, step=False, include_index=False):
+    '''
+    iterates over a window of values in a list
+
+    Parameters
+    ----------
+    x : list | other iterable
+        list to iterate over
+    left : int
+        how many indices to the left of the current index to include in each returned slice
+    right : int
+        how many indices to the right of the current index to include in each returned slice
+    strict : bool
+        if True, only complete slices will be returned. (e.g. x=[1,2,3,4,5] and left=2 the first
+        slice returned will be [1,2,3] at index 2. Index 0 and 1 would be partial slices and not returned.
+    step : bool
+        if True, the right-most index of a slice will define the boundary line for the next slice so that
+        no values repeat. (e.g. x=[1,2,3,4,5] and right=2 will return slices [1,2,3] and [4,5])
+    include_index : bool
+        if True, the index of the slice is also returned
+    '''
+    n, start = len(x) - 1, -1
+    for i,v in enumerate(x):
+        a, b = max(0, i - left), min(n, i + right)
+        #print('i =', i, 'v =', v, 'start again =', start, 'a =', a, 'b =', b)
+        if step:
+            if left > 0 and i == n: a = max(start, 0)
+            if a < start: continue
+        subset = x[ a : b + 1 ]
+        #print('subset =',subset,a,b+1)
+        if strict and len(subset) < left + right + 1: continue
+        start = b + 1
+        #color.print(str(subset),'o')
+        yield (i, subset) if include_index else subset
