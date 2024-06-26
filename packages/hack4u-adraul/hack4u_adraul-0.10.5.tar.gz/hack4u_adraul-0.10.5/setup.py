25b115bcbdd447e8e792b65d5df6a935efa16f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:28 +0000

    Add package hack4u-adraul/0.10.5/hack4u_adraul-0.10.5.tar.gz

---
+++ packages/hack4u-adraul/hack4u_adraul-0.10.5.tar.gz/hack4u_adraul-0.10.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+with open("README.md","r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hack4u-adraul",
+    version="0.10.5",
+    packages=find_packages(),
+    install_requires=[],
+    author="raul leon",
+    description="ejemplo de proyecto cursos Hack4u",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://hack4u.io"
+)
