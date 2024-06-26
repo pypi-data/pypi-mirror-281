829bf2e2c7995b8d1b01848c576430508a6c9064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 09:03:59 +0000

    Add package md2tgmd/0.2.4/md2tgmd-0.2.4.tar.gz

---
+++ packages/md2tgmd/md2tgmd-0.2.4.tar.gz/md2tgmd-0.2.4/setup.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+setup(
+    name="md2tgmd",
+    version="0.2.4",
+    description="md2tgmd is a Markdown to Telegram-specific-markdown converter.",
+    long_description=Path("README.md").open(encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=["md2tgmd"],
+    install_requires=Path.open(Path("requirements.txt"), encoding="utf-8").read().splitlines(),
+)
