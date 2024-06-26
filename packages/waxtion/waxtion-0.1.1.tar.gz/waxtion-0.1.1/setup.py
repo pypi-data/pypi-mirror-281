3a0bb774dc087020e3da7b0907485e2460819a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:04:53 +0000

    Add package waxtion/0.1.1/waxtion-0.1.1.tar.gz

---
+++ packages/waxtion/waxtion-0.1.1.tar.gz/waxtion-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='waxtion',
+    version='0.1.1',
+    description='A transaction signer and table fetcher on wax made easier.',
+    author='funkaclau',
+    packages=find_packages(),
+    install_requires=[
+        "pyntelope"
+    ],
+)
