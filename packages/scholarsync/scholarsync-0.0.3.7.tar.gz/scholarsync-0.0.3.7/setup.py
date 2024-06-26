f92cac81b5329d7f928a3e4f433bbcbcfe0d097d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 15:05:05 +0000

    Add package scholarsync/0.0.3.7/scholarsync-0.0.3.7.tar.gz

---
+++ packages/scholarsync/scholarsync-0.0.3.7.tar.gz/scholarsync-0.0.3.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+setup(
+    name='scholarsync',
+    version='0.0.3.7',
+    packages=find_packages(),
+    author='Amit',
+    author_email='idonthaveemail@mail.com',
+    description='The Literature mining and processing utility',
+    long_description=open('README.md').read(),
+    package_data={'scholarsync': ['data/*']},
+  # include_package_data=True,  # Include all package data
+
+    install_requires=[
+        'biopython',
+        'requests',
+        'beautifulsoup4',
+        'PyPDF2',
+	'numpy',
+	'plotly',
+	'lxml',
+	'spacy>=3.7.5',  # Adjust version as necessary
+        'beautifulsoup4',  # bs4 is part of beautifulsoup4 package
+        'pandas',
+	'scikit-learn',
+	'torch>=2.3.0',  # Adjust version as necessary
+        'transformers',
+	
+    ],
+    entry_points={
+        'console_scripts': [
+            'scholarsync = scholarsync.aio:main',
+        ],
+    },
+)
