f525ffda7419f6c378d87b301cb934461420c424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 16:04:11 +0000

    Add package jose-hello-world/0.0.1.1/jose_hello_world-0.0.1.1-py3-none-any.whl

---
+++ packages/jose-hello-world/jose_hello_world-0.0.1.1-py3-none-any.whl/jose_hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Jose Pereira"
+__copyright__ = "Copyright 2024-Bosch Rexroth"
+
+
+def add_numbers(a, b):
+    return a + b
+
+def quicktext():
+    print('Hello world!!')
+
+if __name__ == "__main__":
+    print("Hello World!")
