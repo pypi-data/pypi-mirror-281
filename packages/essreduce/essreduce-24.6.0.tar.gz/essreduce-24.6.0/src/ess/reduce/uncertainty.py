d7e011ca3d24ee30ad6ebaa684b96bc4fee43d94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:03:58 +0000

    Add package essreduce/24.6.0/essreduce-24.6.0.tar.gz

---
+++ packages/essreduce/essreduce-24.6.0.tar.gz/essreduce-24.6.0/src/ess/reduce/uncertainty.py
@@ -0,0 +1,152 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+"""Tools for handling statistical uncertainties.
+
+This module provides tools for handling statistical uncertainties in the context of
+data reduction. Handling variances during broadcast operations is not handled
+correctly by Scipp because correlations are not tracked.
+See https://doi.org/10.3233/JNR-220049 for context.
+
+This module provides two ways of handling variances during broadcast operations:
+- `drop_variances_if_broadcast`: Drop variances if the data is broadcasted.
+- `broadcast_with_upper_bound_variances`: Compute an upper bound for the variances.
+"""
+
+from enum import Enum
+from typing import Dict, TypeVar, Union, overload
+
+import numpy as np
+import scipp as sc
+from scipp.core.concepts import irreducible_mask
+
+T = TypeVar("T", bound=Union[sc.Variable, sc.DataArray])
+
+
+UncertaintyBroadcastMode = Enum(
+    'UncertaintyBroadcastMode', ['drop', 'upper_bound', 'fail']
+)
+"""
+Mode for broadcasting uncertainties.
+
+- `drop`: Drop variances if the data is broadcasted.
+- `upper_bound`: Compute an upper bound for the variances.
+- `fail`: Do not broadcast, simply return the input data.
+
+See https://doi.org/10.3233/JNR-220049 for context.
+"""
+
+
+@overload
+def broadcast_with_upper_bound_variances(
+    data: sc.Variable, prototype: sc.DataArray | sc.Variable
+) -> sc.Variable:
+    pass
+
+
+@overload
+def broadcast_with_upper_bound_variances(
+    data: sc.DataArray, prototype: sc.DataArray | sc.Variable
+) -> sc.DataArray:
+    pass
+
+
+def broadcast_with_upper_bound_variances(
+    data: Union[sc.Variable, sc.DataArray], prototype: sc.DataArray | sc.Variable
+) -> Union[sc.Variable, sc.DataArray]:
+    """
+    Compute an upper bound for the variances of the broadcasted data.
+
+    The variances of the broadcasted data are computed by scaling the variances of the
+    input data by the volume of the new subspace. The volume of the new subspace is
+    computed as the product of the sizes of the new dimensions.
+
+    Parameters
+    ----------
+    data:
+        The data to broadcast.
+    prototype:
+        Defines the new sizes (dims and shape). If present, masks are used to exclude
+        masked values from the variance computation.
+
+    Returns
+    -------
+    :
+        The data with the variances scaled by the volume of the new subspace.
+    """
+    if _no_variance_broadcast(data, prototype.sizes):
+        return data
+    sizes = prototype.sizes
+    mask = sc.scalar(False)
+    if isinstance(prototype, sc.DataArray):
+        if (irred := irreducible_mask(prototype, dim=sizes)) is not None:
+            for dim in data.dims:
+                if dim in irred.dims:
+                    irred = irred.all(dim)
+            mask = irred
+    size = (~mask).sum().value
+    for dim, dim_size in sizes.items():
+        if dim not in data.dims and dim not in mask.dims:
+            size *= dim_size
+    data = data.copy()
+    data.variances *= size
+    sizes = {**sizes, **data.sizes}
+    data = data.broadcast(sizes=sizes).copy()
+    if mask is not None:
+        # The masked values are not counted in the variance, so we set them to infinity.
+        data.variances[mask.broadcast(sizes=sizes).values] = np.inf
+    return data
+
+
+@overload
+def drop_variances_if_broadcast(
+    data: sc.Variable, prototype: sc.DataArray | sc.Variable
+) -> sc.Variable:
+    pass
+
+
+@overload
+def drop_variances_if_broadcast(
+    data: sc.DataArray, prototype: sc.DataArray | sc.Variable
+) -> sc.DataArray:
+    pass
+
+
+def drop_variances_if_broadcast(
+    data: Union[sc.Variable, sc.DataArray], prototype: sc.DataArray | sc.Variable
+) -> Union[sc.Variable, sc.DataArray]:
+    """
+    Drop variances if the data is broadcasted.
+
+    Parameters
+    ----------
+    data:
+        The data to broadcast.
+    prototype:
+        Defines the new sizes (dims and shape).
+
+    Returns
+    -------
+    :
+        The data without variances if the data is broadcasted.
+    """
+    if _no_variance_broadcast(data, prototype.sizes):
+        return data
+    return sc.values(data)
+
+
+def _no_variance_broadcast(
+    data: Union[sc.Variable, sc.DataArray], sizes: Dict[str, int]
+) -> bool:
+    return (data.variances is None) or all(
+        data.sizes.get(dim) == size for dim, size in sizes.items()
+    )
+
+
+# TODO: For now, we only have broadcasters for dense data. Event-data broadcasters will
+# be added at a later stage, as we currently only have one which is valid for SANS.
+
+broadcasters = {
+    UncertaintyBroadcastMode.drop: drop_variances_if_broadcast,
+    UncertaintyBroadcastMode.upper_bound: broadcast_with_upper_bound_variances,
+    UncertaintyBroadcastMode.fail: lambda x, _: x,
+}
---
+++ packages/essreduce/essreduce-24.6.0.tar.gz/essreduce-24.6.0/tests/uncertainty_test.py
@@ -0,0 +1,165 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+
+import numpy as np
+import pytest
+import scipp as sc
+from ess.reduce import uncertainty as unc
+from scipp.testing import assert_identical
+
+
+def test_fail_mode_always_returns_original():
+    broadcaster = unc.broadcasters[unc.UncertaintyBroadcastMode.fail]
+    var = sc.ones(dims=['x', 'y'], shape=[2, 3], with_variances=True)
+    assert broadcaster(var, sc.zeros(sizes={'x': 2})) is var
+    assert broadcaster(var, sc.zeros(sizes={'y': 3})) is var
+    assert broadcaster(var, sc.zeros(sizes={'x': 2, 'y': 3})) is var
+    assert broadcaster(var, sc.zeros(sizes={'z': 5})) is var
+
+
+@pytest.mark.parametrize(
+    'mode',
+    [unc.UncertaintyBroadcastMode.drop, unc.UncertaintyBroadcastMode.upper_bound],
+)
+def test_broadcaster_returns_original_if_no_new_dims(mode):
+    broadcaster = unc.broadcasters[mode]
+    var = sc.ones(dims=['x', 'y'], shape=[2, 3], with_variances=True)
+    assert broadcaster(var, sc.zeros(sizes={'x': 2})) is var
+    assert broadcaster(var, sc.zeros(sizes={'y': 3})) is var
+    assert broadcaster(var, sc.zeros(sizes={'x': 2, 'y': 3})) is var
+
+
+@pytest.mark.parametrize(
+    'mode',
+    [unc.UncertaintyBroadcastMode.drop, unc.UncertaintyBroadcastMode.upper_bound],
+)
+def test_broadcaster_original_if_no_variances(mode):
+    broadcaster = unc.broadcasters[mode]
+    var = sc.ones(dims=['x'], shape=[2], with_variances=False)
+    assert broadcaster(var, sc.zeros(sizes={'y': 3})) is var
+
+
+def test_broadcast_scales_variances_by_new_subspace_volume():
+    x = sc.linspace('x', 0.0, 1.0, 2)
+    y = sc.linspace('y', 0.0, 2.0, 3)
+    values = x * y
+    var = values.copy()
+    var.variances = var.values
+    expected = sc.ones(dims=['z'], shape=[1]) * values
+    expected.variances = 1 * expected.values
+    assert_identical(
+        unc.broadcast_with_upper_bound_variances(var, sc.zeros(sizes={'z': 1})),
+        expected,
+    )
+    expected = sc.ones(dims=['z'], shape=[2]) * values
+    expected.variances = 2 * expected.values
+    assert_identical(
+        unc.broadcast_with_upper_bound_variances(var, sc.zeros(sizes={'z': 2})),
+        expected,
+    )
+    expected = sc.ones(dims=['z'], shape=[2]) * values
+    expected.variances = 2 * expected.values
+    assert_identical(
+        unc.broadcast_with_upper_bound_variances(var, sc.zeros(sizes={'y': 3, 'z': 2})),
+        expected.transpose(['y', 'z', 'x']),
+    )
+
+
+def test_drop_mode_drops_if_new_dims():
+    broadcaster = unc.broadcasters[unc.UncertaintyBroadcastMode.drop]
+    var = sc.ones(dims=['x', 'y'], shape=[2, 3], with_variances=True)
+    assert_identical(broadcaster(var, sc.zeros(sizes={'z': 1})), sc.values(var))
+
+
+def test_broadcast_with_mask_along_single_broadcast_dim_does_not_count_masked():
+    x = sc.ones(dims=['x'], shape=[2], with_variances=True)
+    y = sc.DataArray(
+        sc.ones(dims=['y'], shape=[3]),
+        masks={'y': sc.array(dims=['y'], values=[False, True, False])},
+    )
+    xy = unc.broadcast_with_upper_bound_variances(x, prototype=y)
+    expected = sc.ones(dims=['y', 'x'], shape=[3, 2], with_variances=True)
+    expected.variances *= 2
+    expected['y', 1].variances = [np.inf, np.inf]
+    assert_identical(xy, expected)
+
+
+def test_broadcast_with_mask_along_existing_dim_has_no_effect():
+    x = sc.ones(dims=['x'], shape=[2], with_variances=True)
+    y = sc.DataArray(
+        sc.ones(dims=['y'], shape=[3]),
+        masks={'x': sc.array(dims=['x'], values=[True, False])},
+    )
+    xy = unc.broadcast_with_upper_bound_variances(x, prototype=y)
+    expected = sc.ones(dims=['y', 'x'], shape=[3, 2], with_variances=True)
+    expected.variances *= 3
+    assert_identical(xy, expected)
+
+
+def test_broadcast_2d_with_mask_along_single_broadcast_dim_does_not_count_masked():
+    x = sc.ones(dims=['x'], shape=[2], with_variances=True)
+    y = sc.DataArray(
+        sc.ones(dims=['z', 'y'], shape=[2, 3]),
+        masks={'y': sc.array(dims=['y'], values=[False, True, False])},
+    )
+    xy = unc.broadcast_with_upper_bound_variances(x, prototype=y)
+    expected = sc.ones(dims=['z', 'y', 'x'], shape=[2, 3, 2], with_variances=True)
+    expected.variances *= 4
+    expected['y', 1].variances = [[np.inf, np.inf], [np.inf, np.inf]]
+    assert_identical(xy, expected)
+
+
+def test_broadcast_into_orthogonal_2d_mask_does_not_count_masked():
+    x = sc.ones(dims=['x'], shape=[2], with_variances=True)
+    y = sc.DataArray(
+        sc.ones(dims=['z', 'y'], shape=[2, 3]),
+        masks={
+            'zy': sc.array(
+                dims=['z', 'y'], values=[[False, False, False], [False, True, False]]
+            )
+        },
+    )
+    xy = unc.broadcast_with_upper_bound_variances(x, prototype=y)
+    expected = sc.ones(dims=['z', 'y', 'x'], shape=[2, 3, 2], with_variances=True)
+    expected.variances *= 5
+    expected['z', 1]['y', 1].variances = [np.inf, np.inf]
+    assert_identical(xy, expected)
+
+
+def test_broadcast_into_nonorthogonal_2d_mask_ignores_mask():
+    x = sc.ones(dims=['x'], shape=[2], with_variances=True)
+    # There is no simple way of handling this, so we just ignore the mask.
+    # This is ok since it will simply mean the upper bound is larger than strictly
+    # necessary.
+    y = sc.DataArray(
+        sc.ones(dims=['x', 'y'], shape=[2, 3]),
+        masks={
+            'xy': sc.array(
+                dims=['x', 'y'], values=[[False, False, False], [False, True, False]]
+            )
+        },
+    )
+    xy = unc.broadcast_with_upper_bound_variances(x, prototype=y)
+    expected = sc.ones(dims=['x', 'y'], shape=[2, 3], with_variances=True)
+    expected.variances *= 3
+    assert_identical(xy, expected)
+
+
+def test_broadcast_into_nonorthogonal_2d_mask_reducible_mask_counts_masked():
+    x = sc.ones(dims=['x'], shape=[2], with_variances=True)
+    # There is no simple way of handling this, so we just ignore the mask.
+    # This is ok since it will simply mean the upper bound is larger than strictly
+    # necessary.
+    y = sc.DataArray(
+        sc.ones(dims=['x', 'y'], shape=[2, 3]),
+        masks={
+            'xy': sc.array(
+                dims=['x', 'y'], values=[[False, True, False], [False, True, False]]
+            )
+        },
+    )
+    xy = unc.broadcast_with_upper_bound_variances(x, prototype=y)
+    expected = sc.ones(dims=['x', 'y'], shape=[2, 3], with_variances=True)
+    expected.variances *= 2
+    expected['y', 1].variances = [np.inf, np.inf]
+    assert_identical(xy, expected)
