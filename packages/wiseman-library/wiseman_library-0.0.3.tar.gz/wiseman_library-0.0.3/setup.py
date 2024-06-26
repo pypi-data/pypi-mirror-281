9021258a1ed68e632f7f1563a23bf944b73fef9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 08:03:58 +0000

    Add package wiseman-library/0.0.3/wiseman_library-0.0.3.tar.gz

---
+++ packages/wiseman-library/wiseman_library-0.0.3.tar.gz/wiseman_library-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='wiseman_library',
+    version='0.0.3',
+    author='adem öcel',
+    author_email='ademocel83@gmail.com',
+    description='wiseman bundles the voice assistant infrastructure into a python library',
+    packages=find_packages(),
+    url='https://github.com/adem-ocel/wiseman-library',
+    license='MIT license'
+)
