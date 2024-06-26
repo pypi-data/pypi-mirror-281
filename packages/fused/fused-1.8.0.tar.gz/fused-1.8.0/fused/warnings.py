38fb5cd973b886e56947bb2842465fc2048ce402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-26 05:03:59 +0000

    Add package fused/1.8.0/fused-1.8.0.tar.gz

---
+++ packages/fused/fused-1.8.0.tar.gz/fused-1.8.0/fused/warnings.py
@@ -0,0 +1,50 @@
+class FusedWarning(Warning):
+    pass
+
+
+class FusedDeprecationWarning(FusedWarning):
+    pass
+
+
+class FusedInProgressWarning(FusedWarning):
+    pass
+
+
+class FusedNonProductionWarning(FusedWarning):
+    pass
+
+
+class FusedIPythonWarning(FusedWarning):
+    pass
+
+
+class FusedUdfWarning(FusedWarning):
+    pass
+
+
+class FusedDefaultWarning(FusedWarning):
+    pass
+
+
+class FusedPythonVersionWarning(FusedWarning):
+    pass
+
+
+class FusedTypeWarning(FusedWarning):
+    pass
+
+
+class FusedPathWarning(FusedWarning):
+    pass
+
+
+class FusedReservedWarning(FusedWarning):
+    pass
+
+
+class FusedIgnoredWarning(FusedWarning):
+    pass
+
+
+class FusedRefreshingWarning(FusedWarning):
+    pass
