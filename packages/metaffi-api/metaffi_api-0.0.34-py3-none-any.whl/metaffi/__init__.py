7a3b98eac14c1926a1bfd24d40f939497d96247c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 16:05:12 +0000

    Add package metaffi-api/0.0.34/metaffi_api-0.0.34-py3-none-any.whl

---
+++ packages/metaffi-api/metaffi_api-0.0.34-py3-none-any.whl/metaffi/__init__.py
@@ -0,0 +1,16 @@
+"""Python MetaFFI API"""
+
+__version__ = "0.0.34"
+
+__all__ = ['metaffi', 'metaffi_types', 'metaffi_runtime', 'metaffi_module', 'MetaFFIHandle', 'metaffi_types', 'xllr_wrapper', 'pycdts_converter', 'metaffi_type_info', 'MetaFFITypes', 'MetaFFIEntity']
+
+import metaffi
+from . import metaffi_types
+from . import metaffi_runtime
+from . import metaffi_module
+from .metaffihandle import MetaFFIHandle
+from . import xllr_wrapper
+from . import pycdts_converter
+from .metaffi_types import metaffi_type_info
+from .metaffi_types import MetaFFITypes
+from .metaffi_module import MetaFFIEntity
