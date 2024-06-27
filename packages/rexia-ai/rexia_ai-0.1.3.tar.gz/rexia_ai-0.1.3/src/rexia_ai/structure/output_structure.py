c2abeb5e11e57230308f4610a7b64a1739e45105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:13 +0000

    Add package rexia-ai/0.1.3/rexia_ai-0.1.3.tar.gz

---
+++ packages/rexia-ai/rexia_ai-0.1.3.tar.gz/rexia_ai-0.1.3/src/rexia_ai/structure/output_structure.py
@@ -0,0 +1,59 @@
+"""LLM output structure for ReXia.AI."""
+
+
+class LLMOutput:
+    """
+    LLM output structure for ReXiaAI.
+
+    This class provides static methods to get the output structure for different types of responses.
+    """
+
+    @staticmethod
+    def get_output_structure() -> str:
+        """
+        Get the output structure with examples.
+
+        Returns:
+            A string representing the output structure with examples.
+        """
+        return (
+            "{\n"
+            '    "question": "string enclosed in double quotes",\n'
+            '    "plan": "list of strings enclosed in double quotes",\n'
+            '    "answer": "a string enclosed in double quotes",\n'
+            '    "confidence_score": "float between 0 and 100 percent, eg. 95.0, 10.1, 100.0",\n'
+            '    "chain_of_reasoning": ["list of strings encased in double quotes"]\n'
+            '    "tool_calls": ["list of tool calls. Leave blank unless you are a tool calling agent."]\n'
+            "}"
+        )
+
+    @staticmethod
+    def get_json_scehma():
+        """
+        Get the output schema.
+
+        Returns:
+            A string representing the output schema.
+        """
+
+        schema = {
+            "type": "object",
+            "properties": {
+                "question": {"type": "string"},
+                "plan": {"type": "array", "items": {"type": "string"}},
+                "answer": {"type": "string"},
+                "confidence_score": {"type": "integer"},
+                "chain_of_reasoning": {"type": "array", "items": {"type": "string"}},
+                "tool_calls": {"type": "array", "items": {"type": "string"}},
+            },
+            "required": [
+                "question",
+                "answer",
+                "confidence_score",
+                "chain_of_reasoning",
+                "plan",
+                "tool_calls",
+            ],
+        }
+
+        return schema
---
+++ packages/rexia-ai/rexia_ai-0.1.3.tar.gz/rexia_ai-0.1.3/src/rexia_ai/workflows/reflect_workflow.py
@@ -0,0 +1,97 @@
+"""Reflect workflow for ReXia.AI."""
+
+from typing import Any
+from ..base import BaseWorkflow, BaseMemory
+from ..common import CollaborationChannel, TaskStatus
+from ..agents import Component
+from ..agents.workers import PlanWorker, FinaliseWorker, Worker, ToolWorker
+
+
+class ReflectWorkflow(BaseWorkflow):
+    """
+    ReflectWorkflow Class for ReXia AI, refactored for better design principles.
+
+    Attributes:
+        llm: The language model used by the workflow.
+        task: The task that the workflow is designed to perform.
+        verbose: A flag used for enabling verbose mode.
+        memory: The memory component of the workflow.
+        channel: The collaboration channel for the workflow.
+        plan: The plan component of the workflow.
+        tool: The tool component of the workflow.
+        work: The work component of the workflow.
+        finalise: The finalise component of the workflow.
+    """
+
+    llm: Any
+    task: str
+    verbose: bool
+    memory: BaseMemory
+    channel: CollaborationChannel
+    plan: Component
+    tool: Component
+    work: Component
+    finalise: Component
+
+    def __init__(
+        self,
+        llm: Any,
+        task: str,
+        memory: BaseMemory,
+        verbose: bool = False,
+        max_attempts: int = 3,
+    ) -> None:
+        super().__init__(llm, task, verbose)
+        self.channel = CollaborationChannel(task)
+        self.memory = memory
+        self.plan = Component(
+            "plan",
+            self.channel,
+            PlanWorker(model=llm, verbose=verbose, max_attempts=max_attempts),
+            memory=self.memory,
+        )
+        self.tool = Component(
+            "tool",
+            self.channel,
+            ToolWorker(model=llm, verbose=verbose, max_attempts=max_attempts),
+            memory=self.memory,
+        )
+        self.work = Component(
+            "work",
+            self.channel,
+            Worker(model=llm, verbose=verbose, max_attempts=max_attempts),
+            memory=self.memory,
+        )
+        self.finalise = Component(
+            "finalise",
+            self.channel,
+            FinaliseWorker(model=llm, verbose=verbose, max_attempts=max_attempts),
+            memory=self.memory,
+        )
+
+    def _run_task(self):
+        """Run the agent process."""
+        try:
+            print(f"ReXia.AI is working on the task: {self.channel.task}")
+
+            self.channel.status = TaskStatus.WORKING
+            self.plan.run()
+
+            if self.llm.tools:
+                self.tool.run()
+            self.work.run()
+
+            self.finalise.run()
+
+            self.channel.status = TaskStatus.COMPLETED
+
+            # Add the final message to the memory
+            self.memory.add_message(self.channel.messages[-1])
+
+            print(f"ReXia.AI has completed the task: {self.channel.task}")
+        except Exception as e:
+            print(f"An error occurred while running the task: {e}")
+
+    def run(self):
+        """Run the agent."""
+        self._run_task()
