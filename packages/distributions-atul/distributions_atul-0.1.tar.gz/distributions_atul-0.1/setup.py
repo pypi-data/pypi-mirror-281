0c7180ed78fd49295bf0e5b903cdcb728f877767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:06:57 +0000

    Add package distributions-atul/0.1/distributions_atul-0.1.tar.gz

---
+++ packages/distributions-atul/distributions_atul-0.1.tar.gz/distributions_atul-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_atul',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_atul'],
+      zip_safe=False)
