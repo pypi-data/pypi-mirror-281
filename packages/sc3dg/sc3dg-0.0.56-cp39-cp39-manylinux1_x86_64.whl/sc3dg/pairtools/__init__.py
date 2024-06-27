c088c291300c22ecdc5b4e4445b17e5ca1a2ad0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:38 +0000

    Add package sc3dg/0.0.56/sc3dg-0.0.56-cp39-cp39-manylinux1_x86_64.whl

---
+++ packages/sc3dg/sc3dg-0.0.56-cp39-cp39-manylinux1_x86_64.whl/sc3dg/pairtools/__init__.py
@@ -0,0 +1,15 @@
+"""
+pairtools
+~~~~~~~~~
+
+CLI tools to process mapped Hi-C data
+
+:copyright: (c) 2017-2022 Open2C
+:author: Open2C
+:license: MIT
+
+"""
+
+__version__ = "1.0.2"
+
+# from . import lib
