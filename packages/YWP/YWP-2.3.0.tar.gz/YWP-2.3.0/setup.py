45a65eaeffd9a1fafbd7c85fb2056869babbe8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 07:04:38 +0000

    Add package YWP/2.3.0/YWP-2.3.0.tar.gz

---
+++ packages/YWP/YWP-2.3.0.tar.gz/YWP-2.3.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YWP',
+    version='2.3.0',
+    packages=find_packages(),
+    install_requires=[
+        "dill",
+        "flask",
+        "flask-cors",
+        "gtts",
+        "joblib",
+        "moviepy",
+        "nltk",
+        "pyaudio",
+        "pygame",
+        "selenium",
+        "setuptools",
+        "sounddevice",
+        "SpeechRecognition",
+        "tensorflow",
+        "tflearn",
+        "twine",
+        "wheel",
+        "pycryptodome",
+        "vosk",
+        "tqdm",
+        "pyttsx3",
+        "requests",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    description='This is a library to simplify the Python language for beginners while adding some features that are not found in other libraries',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Wanted Products (YWP)',
+    author_email='pbstzidr@ywp.freewebhostmost.com',
+    entry_points={
+        'console_scripts': [
+            'YWP.install_packages=YWP:install_system_packages',
+        ],
+    },
+)
