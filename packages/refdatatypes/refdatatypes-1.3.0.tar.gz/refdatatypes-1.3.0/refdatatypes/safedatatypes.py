aade8a211f803d09ff338bddc88403aba79469ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 06:04:49 +0000

    Add package refdatatypes/1.3.0/refdatatypes-1.3.0.tar.gz

---
+++ packages/refdatatypes/refdatatypes-1.3.0.tar.gz/refdatatypes-1.3.0/refdatatypes/safedatatypes.py
@@ -0,0 +1,63 @@
+from typing import Union
+
+
+def safe_int(value: Union[str, int, None], default_value: Union[int, None] = 0) -> Union[int, None]:
+    try:
+        return int(value)
+    except (TypeError, ValueError):
+        return None if default_value is None else int(default_value)
+
+
+def safe_float(value: Union[str, float, None], default_value: Union[float, None] = 0.0) -> Union[float, None]:
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return None if default_value is None else float(default_value)
+
+
+def safe_bool(value: Union[str, bool, None], default_value: bool = False) -> bool:
+    try:
+        return bool(value)
+    except (TypeError, ValueError):
+        return bool(default_value)
+
+
+class SafeDict(dict):
+    def __init__(self, init_data: dict = None, default_value=None, autoset: bool = False):
+        self._default_value = default_value
+        self._autoset = autoset
+        if init_data:
+            self._transform(init_data)
+
+    def __getitem__(self, item: str):
+        try:
+            return super().__getitem__(item)
+        except KeyError:
+            if self._autoset:
+                self.__setitem__(item, self._default_value)
+            return self._default_value
+
+    def get(self, key: str, default_value=None, if_none=None):
+        value = super().get(key, default_value)
+        if value is None:
+            return if_none
+        return value
+
+    def _transform(self, init_data: dict):
+        for key, value in init_data.items():
+            if isinstance(value, dict):
+                self[key] = SafeDict(value)
+            else:
+                self[key] = value
+
+class SafeList(list):
+    def __init__(self, init_data: list = None, default_value=None):
+        self._default_value = default_value
+        if init_data:
+            self.extend(init_data)
+
+    def __getitem__(self, item: str):
+        try:
+            return super().__getitem__(item)
+        except IndexError:
+            return self._default_value
