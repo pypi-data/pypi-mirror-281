85f987cec82702ce3c57fb7da5572c1f9cc4c38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 16:04:57 +0000

    Add package fastapi-build/0.2.3/fastapi_build-0.2.3-py3-none-any.whl

---
+++ packages/fastapi-build/fastapi_build-0.2.3-py3-none-any.whl/fastapi_build/config/dev.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 --
+# @Time : 2024/5/15 18:25
+# @Author : PinBar
+# @File : dev.py
+
+
+ES_HOST = "http://127.0.0.1:10039"
+ES_PASSWORD = "datagrand"
+ES_USER = "elastic"
+ES_AUTH = 1
