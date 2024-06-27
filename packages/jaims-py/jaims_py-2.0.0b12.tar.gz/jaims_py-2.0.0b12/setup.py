f13daa84c3fa09ba38e4c4cc6d6df385e928d51e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:47 +0000

    Add package jaims-py/2.0.0b12/jaims_py-2.0.0b12.tar.gz

---
+++ packages/jaims-py/jaims_py-2.0.0b12.tar.gz/jaims_py-2.0.0b12/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+# Read the main requirements
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+# Read OpenAI specific requirements
+with open("requirements-openai.txt") as f:
+    requirements_openai = f.read().splitlines()
+
+# Read Google Cloud AI specific requirements
+with open("requirements-googleai.txt") as f:
+    requirements_google_ai = f.read().splitlines()
+
+with open("requirements-mistral.txt") as f:
+    requirements_mistral = f.read().splitlines()
+
+setup(
+    name="jaims-py",
+    version="2.0.0-beta.12",
+    packages=find_packages(),
+    description="A Python package for creating LLM powered, agentic, platform agnostic software.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Marco Musella",
+    url="https://github.com/dev-mush/jaims-py",
+    license="MIT",
+    keywords="An extensible library to create LLM Agents and LLM based applications.",
+    install_requires=requirements,
+    extras_require={
+        "openai": requirements_openai,
+        "google": requirements_google_ai,
+        "mistral": requirements_mistral,
+    },
+)
