249d5dcc3d04a30ae2bd4cd1e41d93a3faceaf40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 06:04:50 +0000

    Add package auto-click-auto/0.1.5/auto_click_auto-0.1.5-py3-none-any.whl

---
+++ packages/auto-click-auto/auto_click_auto-0.1.5-py3-none-any.whl/auto_click_auto/__init__.py
@@ -0,0 +1,6 @@
+from .core import (
+    enable_click_shell_completion as enable_click_shell_completion,
+)
+from .core import (
+    enable_click_shell_completion_option as enable_click_shell_completion_option,
+)
