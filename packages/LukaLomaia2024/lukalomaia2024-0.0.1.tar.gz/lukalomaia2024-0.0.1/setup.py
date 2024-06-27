e93e20abb351b7126da05ab36f65c52912f5ffa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:57 +0000

    Add package LukaLomaia2024/0.0.1/lukalomaia2024-0.0.1.tar.gz

---
+++ packages/LukaLomaia2024/lukalomaia2024-0.0.1.tar.gz/lukalomaia2024-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='LukaLomaia2024',
+    version='0.0.1',
+    description='simple Calculator',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    author='Code For bTUf',
+    author_email='pron9812@Gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.txt'],
+    },
+    install_requires=[],
+)
