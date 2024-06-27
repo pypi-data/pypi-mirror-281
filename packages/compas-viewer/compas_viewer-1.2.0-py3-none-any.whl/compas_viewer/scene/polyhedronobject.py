4e4ea0e4945cb9e784d7b216b99231dc313201e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:57 +0000

    Add package compas-viewer/1.2.0/compas_viewer-1.2.0-py3-none-any.whl

---
+++ packages/compas-viewer/compas_viewer-1.2.0-py3-none-any.whl/compas_viewer/scene/polyhedronobject.py
@@ -0,0 +1,29 @@
+from typing import Optional
+
+from compas.datastructures import Mesh
+from compas.geometry import Line
+from compas.geometry import Point
+from compas.geometry import Polyhedron
+from compas.scene import GeometryObject
+
+from .geometryobject import GeometryObject as ViewerGeometryObject
+
+
+class PolyhedronObject(ViewerGeometryObject, GeometryObject):
+    """Viewer scene object for displaying COMPAS Polyhedron geometry."""
+
+    geometry: Polyhedron
+
+    @property
+    def points(self) -> Optional[list[Point]]:
+        return self.geometry.points
+
+    @property
+    def lines(self) -> Optional[list[Line]]:
+        return self.geometry.lines
+
+    @property
+    def viewmesh(self):
+        mesh = self.geometry.to_mesh()
+        vertices, faces = mesh.to_vertices_and_faces(triangulated=True)
+        return Mesh.from_vertices_and_faces(vertices, faces)
