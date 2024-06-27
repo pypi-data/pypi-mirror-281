6603ec10bec5867f23fec36f882c75dc9d2a90d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 14:05:05 +0000

    Add package ppRank/1.1/ppRank-1.1.tar.gz

---
+++ packages/ppRank/ppRank-1.1.tar.gz/ppRank-1.1/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.1'
+DESCRIPTION = 'Bi-objective Lexicographical Classification and Par10 Ranking'
+LONG_DESCRIPTION = ('A package that allows building a matrix with the Bi-objective Lexicographical Classification '
+                    'of different algorithms.')
+
+# Setting up
+setup(
+    name="ppRank",
+    version=VERSION,
+    author="Tiago Costa Soares, Pedro Augusto Mendes, Iago Augusto de Carvalho",
+    author_email="tiagocsoares22@gmail.com, pedroaugusto.mendes035@gmail.com, iago.carvalho@unifal-mg.edu.brZ",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['csvkit'],
+    keywords=['classification', 'bi-objetive', 'lexicographical', 'ranking', "csv", "bilex", "par10"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
