a46232bc64d9d6216efda17fdb2f547cacae974f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 07:03:52 +0000

    Add package pyerualjetwork/2.6.5/pyerualjetwork-2.6.5.tar.gz

---
+++ packages/pyerualjetwork/pyerualjetwork-2.6.5.tar.gz/pyerualjetwork-2.6.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.6.5",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Visual improvements, new option added for val paramater in fit function: 'final'. Removed: val_count parameter.",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
