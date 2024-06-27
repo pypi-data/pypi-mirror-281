ba61c200bfbf30c94c37d062f34e120f89cf43c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:47 +0000

    Add package curvesimilarities/0.3.0a1/curvesimilarities-0.3.0a1.tar.gz

---
+++ packages/curvesimilarities/curvesimilarities-0.3.0a1.tar.gz/curvesimilarities-0.3.0a1/tests/test_dtw.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+
+from curvesimilarities import dtw
+
+
+def test_dtw_dtype():
+    assert type(dtw([[0, 0], [1, 0]], [[0, 1], [1, 1]])) is float
+
+
+def test_dtw_degenerate():
+
+    def check(P, Q):
+        assert dtw(P, Q) == np.sum(cdist(P, Q))
+
+    check([[0, 0]], [[0, 1]])
+    check([[0, 0], [1, 0]], [[0, 1]])
+    check([[0, 0]], [[0, 1], [1, 1]])
