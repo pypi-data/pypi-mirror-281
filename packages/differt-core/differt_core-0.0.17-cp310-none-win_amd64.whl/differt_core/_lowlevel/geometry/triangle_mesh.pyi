35db23217e5d75264c7c08bf0fb66f1b8bb889a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:36 +0000

    Add package differt-core/0.0.17/differt_core-0.0.17-cp310-none-win_amd64.whl

---
+++ packages/differt-core/differt_core-0.0.17-cp310-none-win_amd64.whl/differt_core/_lowlevel/geometry/triangle_mesh.pyi
@@ -0,0 +1,11 @@
+import numpy as np
+from jaxtyping import Float, UInt
+
+class TriangleMesh:
+    vertices: Float[np.ndarray, "num_vertices 3"]
+    triangles: UInt[np.ndarray, "num_triangles 3"]
+
+    @classmethod
+    def load_obj(cls, file: str) -> TriangleMesh: ...
+    @classmethod
+    def load_ply(cls, file: str) -> TriangleMesh: ...
