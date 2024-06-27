4de7b409aa2c0b3fcbe1b73ec1d5fd0721cb8360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 06:04:41 +0000

    Add package pywatershed/1.1.0/pywatershed-1.1.0-py3-none-any.whl

---
+++ packages/pywatershed/pywatershed-1.1.0-py3-none-any.whl/pywatershed/hydrology/__init__.py
@@ -0,0 +1,8 @@
+from .prms_canopy import PRMSCanopy
+from .prms_channel import PRMSChannel
+
+# from PRMSEt import PRMSEt
+from .prms_groundwater import PRMSGroundwater
+from .prms_runoff import PRMSRunoff
+from .prms_snow import PRMSSnow
+from .prms_soilzone import PRMSSoilzone
---
+++ packages/pywatershed/pywatershed-1.1.0-py3-none-any.whl/pywatershed/version.py
@@ -0,0 +1,12 @@
+# pywatershed version file automatically created using update_version.py on June 26, 2024 20:25:28
+
+__version__ = "1.1.0"
+__pakname__ = "pywatershed"
+
+author_dict = {
+    "James McCreight": "jmccreight@usgs.gov",
+    "Christian D. Langevin": "langevin@usgs.gov",
+    "Joseph D. Hughes": "jdhughes@usgs.gov",
+}
+__author__ = ", ".join(author_dict.keys())
+__author_email__ = ", ".join(s for _, s in author_dict.items())
