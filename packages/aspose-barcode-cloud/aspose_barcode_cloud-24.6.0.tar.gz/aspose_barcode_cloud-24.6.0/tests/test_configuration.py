bf3d7f675537db8ff5acf738f7f31c1ca439e7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 16:04:26 +0000

    Add package aspose-barcode-cloud/24.6.0/aspose_barcode_cloud-24.6.0.tar.gz

---
+++ packages/aspose-barcode-cloud/aspose_barcode_cloud-24.6.0.tar.gz/aspose_barcode_cloud-24.6.0/tests/test_configuration.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division
+
+import unittest
+
+from aspose_barcode_cloud import Configuration
+
+
+class TestConfiguration(unittest.TestCase):
+    def test_token_url(self):
+        config = Configuration(token_url="token url")
+        self.assertEqual("token url", config.token_url)
