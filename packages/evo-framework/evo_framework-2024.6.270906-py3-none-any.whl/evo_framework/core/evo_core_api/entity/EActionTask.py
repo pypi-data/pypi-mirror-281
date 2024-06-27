63be1eee47dddf88df0419b2949cead760d1caae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 16:04:51 +0000

    Add package evo-framework/2024.6.270906/evo_framework-2024.6.270906-py3-none-any.whl

---
+++ packages/evo-framework/evo_framework-2024.6.270906-py3-none-any.whl/evo_framework/core/evo_core_api/entity/EActionTask.py
@@ -0,0 +1,34 @@
+from evo_framework.entity.EObject import EObject
+from evo_framework.core.evo_core_type.entity.EvoMap import EvoMap
+from evo_framework.core.evo_core_api.entity.EAction import EAction
+
+class EActionTask(EObject):
+    #annotation
+    
+    def __init__(self):
+        super().__init__()
+        self.action:str = ""
+        self.eRequest = None
+        self.eActionInput:EAction = None
+        self.eActionOutput:EAction = None
+        self.evoCallback = None
+        self.task = None
+        self.evoContext:EObject = None
+        self.context = None
+        self.loop = None
+        self.threadBackground = None
+        self.isStreamAction = False
+        
+    def __str__(self) -> str:
+        strReturn = "\n".join([
+                            super().__str__(),
+                            f"action:{self.action}",
+                            f"eApiMediaInput:{self.eActionInput}",
+                            f"eApiMediaOutput:{self.eActionOutput}",
+                            f"evoCallback:{self.evoCallback}",
+                            f"task:{self.task}",
+                            f"evoContext:{self.evoContext}",
+                            f"context:{self.context}",
+                            f"isStreamAction:{self.isStreamAction}",
+                            ]) 
+        return strReturn
