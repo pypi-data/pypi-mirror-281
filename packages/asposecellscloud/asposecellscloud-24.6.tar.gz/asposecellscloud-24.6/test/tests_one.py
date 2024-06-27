c69d3e504bb7c2a978ca8623aa1719c761f62cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 14:04:55 +0000

    Add package asposecellscloud/24.6/asposecellscloud-24.6.tar.gz

---
+++ packages/asposecellscloud/asposecellscloud-24.6.tar.gz/asposecellscloud-24.6/test/tests_one.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+import os
+import sys
+import unittest
+import warnings
+import time
+
+ABSPATH = os.path.abspath(os.path.realpath(os.path.dirname(__file__)) + "/..")
+sys.path.append(ABSPATH)
+
+from asposecellscloud.rest import ApiException
+from asposecellscloud.apis.cells_api import CellsApi
+import AuthUtil
+from asposecellscloud.models import *
+from asposecellscloud.requests import *
+
+global_api = None
+
+class TestOneCase(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter('ignore', ResourceWarning)
+        global global_api
+        if global_api is None:
+           global_api = CellsApi(AuthUtil.GetClientId(),AuthUtil.GetClientSecret(),"v3.0",AuthUtil.GetBaseUrl())
+        self.api = global_api
+    def test_upload_file(self):
+        remote_folder = 'TestData/In'
+
+        local_name = 'Book1.xlsx'
+        remote_name = 'Book1.xlsx'
+
+        mapFiles = { 
+            local_name: os.path.dirname(os.path.realpath(__file__)) + "/../TestData/" +local_name             
+        }
+     
+        request =  UploadFileRequest( mapFiles, remote_folder + '/' + remote_name,storage_name= '')
+        self.api.upload_file(request)
+        time.sleep(1)
+    def tearDown(self):
+        pass
+
+   
+if __name__ == '__main__':
+    unittest.main()
