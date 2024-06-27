9211913fa77930fd9f72dd9a322f53cfa8bc9456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:26 +0000

    Add package rsa-so/0.1.2/rsa_so-0.1.2-py3-none-any.whl

---
+++ packages/rsa-so/rsa_so-0.1.2-py3-none-any.whl/rsa_so/__init__.py
@@ -0,0 +1,3 @@
+from .sign import encrypt_by_rsa_private_key
+
+__all__ = ['encrypt_by_rsa_private_key']
---
+++ packages/rsa-so/rsa_so-0.1.2-py3-none-any.whl/rsa_so/sign.py
@@ -0,0 +1,33 @@
+import ctypes
+import platform
+import os
+
+current_dir = os.path.join(os.path.dirname(__file__), 'bin')
+
+# 加载共享对象文件
+system = platform.system()
+architecture = platform.machine()
+if system == 'Linux':
+    if architecture == 'x86_64':
+        lib_path = os.path.join(current_dir, 'rsa_linux_x86_64.so')
+    else:
+        raise Exception(f"Unsupported architecture: {architecture} on Linux")
+elif system == 'Darwin':
+    if architecture == 'arm64':
+        lib_path = os.path.join(current_dir, 'rsa_darwin_arm64.so')
+    else:
+        raise Exception(f"Unsupported architecture: {architecture} on macOS")
+else:
+    raise Exception(f"Unsupported system: {system}")
+
+sign_lib = ctypes.CDLL(lib_path)
+
+# 设置Sign函数的参数类型和返回类型
+sign_lib.Sign.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_bool, ctypes.c_bool]
+sign_lib.Sign.restype = ctypes.c_char_p
+
+
+def encrypt_by_rsa_private_key(message: str, private_key: str, is_random: bool, hex_format: bool) -> str:
+    # 调用Sign函数
+    result = sign_lib.Sign(message.encode('utf-8'), private_key.encode('utf-8'), is_random, hex_format)
+    return result.decode('utf-8')
