108e1c29d10dad053af0e0e9930981e1ff4f9204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 05:04:51 +0000

    Add package sqlime3/1.0.2/sqlime3-1.0.2.tar.gz

---
+++ packages/sqlime3/sqlime3-1.0.2.tar.gz/sqlime3-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+name = 'sqlime3'
+version = '1.0.2'
+author = 'GooseG4G'
+email = 'danilnegusev@inbox.ru'
+desc = 'The package provides a wrapper for working with SQLite databases.'
+url = 'https://github.com/GooseG4G/sqlime3'
+packages = ['sqlime3']
+requires = []
+
+setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=email,
+    description=desc,
+    url=url,
+    packages=packages,
+    install_requires=requires
+)
