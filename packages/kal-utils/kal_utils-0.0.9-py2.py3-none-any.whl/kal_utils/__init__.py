ce1134d257c95b90ca30c5ce1a64f874b379d958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:27 +0000

    Add package kal-utils/0.0.9/kal_utils-0.0.9-py2.py3-none-any.whl

---
+++ packages/kal-utils/kal_utils-0.0.9-py2.py3-none-any.whl/kal_utils/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for kal-utils."""
+# kal_utils/__init__.py
+from . import requests, logger, handle_response, mongodb
+__author__ = """Bar Lander"""
+__email__ = "barh@kaleidoo.ai"
+__version__ = "0.0.9"
