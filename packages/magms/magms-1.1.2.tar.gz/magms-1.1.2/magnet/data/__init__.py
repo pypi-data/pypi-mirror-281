f8a8858dbf7142b93391515cfcecd7b18492c449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 05:05:23 +0000

    Add package magms/1.1.2/magms-1.1.2.tar.gz

---
+++ packages/magms/magms-1.1.2.tar.gz/magms-1.1.2/magnet/data/__init__.py
@@ -0,0 +1,2 @@
+from monai.data import * # type: ignore
+from .targeting import TargetedDataLoader, TargetedDataset
---
+++ packages/magms/magms-1.1.2.tar.gz/magms-1.1.2/magnet/managers/protocols.py
@@ -0,0 +1,27 @@
+from torchmanager_core import abc
+from torchmanager_core.typing import Optional, Protocol, Union, runtime_checkable
+
+
+@runtime_checkable
+class Targeting(Protocol):
+    """Targeting protocol"""
+
+    @property
+    @abc.abstractmethod
+    def num_targets(self) -> int:
+        return NotImplemented
+
+    @property
+    @abc.abstractmethod
+    def target(self) -> Optional[Union[list[int], int]]:
+        return NotImplemented
+
+    @target.setter
+    @abc.abstractmethod
+    def target(self, t: Optional[Union[list[int], int]]) -> None:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def target_dict(self) -> dict[Optional[int], str]:
+        return NotImplemented
