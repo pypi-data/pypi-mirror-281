f861e5ede4566cd20d12b4f6fc2cebe880a14667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:31 +0000

    Add package llaminate/0.1.2/llaminate-0.1.2.tar.gz

---
+++ packages/llaminate/llaminate-0.1.2.tar.gz/llaminate-0.1.2/.github/README.md
@@ -0,0 +1,27 @@
+# llaminate
+
+Optimized version of [llama3][github-llama3], using [tokun][github-tokun].
+
+This project is a showcase for a new tokenization technique.
+It uses a NN model to compress the whole Unicode space into tokens.
+
+## Resources
+
+### Models
+
+### Notebooks
+
+## TODO
+
+See [TODO](TODO.md).
+
+## Credits
+
+This project winks at [llama3 from Meta][github-llama3], but doesn't actually its weights nor code.
+
+## License
+
+Licensed under the [aGPLv3](LICENSE.md).
+
+[github-llama3]: https://github.com/meta-llama/llama3
+[github-tokun]: https://github.com/apehex/tokun
---
+++ packages/llaminate/llaminate-0.1.2.tar.gz/llaminate-0.1.2/llaminate/pipeline.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+# DISTRIBUTE ##################################################################
+
+_map = lambda __f: (lambda *__t: tuple(map(__f, __t)))
+
+# PREPROCESS ##################################################################
+
+def preprocess(dataset: tf.data.Dataset, token_dim: int, embed_dim: int, sample_dim: int, features: list=[]) -> tf.data.Dataset:
+    __pipeline = [
+        # join the features
+        ((lambda __x: tf.strings.join(inputs=[__x[__f] for __f in features], separator='\x1d') if features else __x), True),
+        # (input, target) where target is the next character for each input
+        ((lambda __x: (tokun.pipeline.offset(data=__x, ticks=16), __x)), True),
+        # encode => (4 * S,) int
+        (_map(functools.partial(tokun.pipeline.encode, token_size=token_dim, sample_size=sample_dim)), True),
+        # reshape => (4 * S,) int
+        (_map(functools.partial(tf.reshape, shape=(4 * sample_dim,))), True),
+        # one-hot encoding for the targets => (4 * S, E) int (bool)
+        ((lambda __x, __y: (__x, tf.one_hot(__y, depth=embed_dim, axis=-1))), True)]
+    # split args
+    __operations, __replace = zip(*__pipeline)
+    # udpate dataset
+    return mlable.data.process(dataset=dataset, pipeline=__operations, replace=__replace)
