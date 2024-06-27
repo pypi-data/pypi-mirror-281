b6669793e3e0e1ab89ac224c3522d39aabba3abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 05:05:04 +0000

    Add package jetset/1.3.0/jetset-1.3.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/jetset/jetset-1.3.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/jetset/jet_tools.py
@@ -0,0 +1,155 @@
+__author__ = "Andrea Tramacere"
+
+import os
+import numpy as np
+import copy
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if on_rtd == True:
+    try:
+        from .jetkernel import jetkernel as BlazarSED
+    except ImportError:
+        from .mock import jetkernel as BlazarSED
+else:
+    from .jetkernel import jetkernel as BlazarSED
+
+from .jetkernel_models_dic import allowed_disk_type
+
+from .jet_paramters import *
+
+
+__all__=[ 'build_emitting_region_dict','build_ExtFields_dic','BLR_constraints','DT_constraints','clean_numba']
+
+
+
+
+
+
+
+
+def build_emitting_region_dict(cosmo, beaming_expr='delta',emitters_type='electrons'):
+    """
+
+    Builds a dictionary to init the :class:`.JetParameter`
+    objects   for the emitting region:
+
+        - **R**, the radius of the emitting region in cm
+
+        - **B**, the magnetic field in G
+
+        - **beaming**, the beaming factor
+
+        - **z**, the redshift
+
+
+    """
+
+    model_dic = {}
+    model_dic['R'] = JetModelDictionaryPar(ptype='region_size', vmin=1E3, vmax=1E30, punit='cm', froz=False, log=False)
+    #    ['region_size',0,30,'cm',False,True]
+    model_dic['R_H'] = JetModelDictionaryPar(ptype='region_position', vmin=0, vmax=None, punit='cm', froz=True)
+    # ['region_position', 0, None, 'cm']
+    model_dic['B'] = JetModelDictionaryPar(ptype='magnetic_field', vmin=0, vmax=None, punit='gauss')
+    if emitters_type=='electrons':
+        model_dic['NH_cold_to_rel_e'] = JetModelDictionaryPar(ptype='cold_p_to_rel_e_ratio', vmin=0, vmax=None, punit='',froz=True, log=False)
+
+    if beaming_expr == 'bulk_theta':
+        model_dic['theta'] = JetModelDictionaryPar(ptype='jet-viewing-angle', vmin=0, vmax=90, punit='deg')
+        # ['jet-viewing-angle',0.0,None,'deg']
+        model_dic['BulkFactor'] = JetModelDictionaryPar(ptype='jet-bulk-factor', vmin=1.0, vmax=1E5,
+                                                        punit='lorentz-factor')
+        # ['jet-bulk-factor',1.0,None,'Lorentz-factor']
+    elif beaming_expr == 'delta' or beaming_expr == '':
+        model_dic['beam_obj'] = JetModelDictionaryPar(ptype='beaming', vmin=1E-4, vmax=None, punit='')
+        # ['beaming', 1, None, '']
+    else:
+        raise RuntimeError('''wrong beaming_expr="%s" value, allowed 'delta' or 'bulk_theta' ''' % (beaming_expr))
+
+    if cosmo._c is not None:
+        model_dic['z_cosm'] = JetModelDictionaryPar(ptype='redshift', vmin=0, vmax=None, punit='')
+    else:
+        model_dic['z_cosm'] = JetModelDictionaryPar(ptype='redshift', vmin=0, vmax=None, val=0, punit='', froz=True)
+
+    return model_dic
+
+
+def BLR_constraints(L_Disk):
+    r1_min = 1E17 * (L_Disk / 1E45) ** 0.5
+    r2_min = r1_min * 1.01
+    r2_max = r1_min * 2
+    print('-> BLR constraints', L_Disk, r1_min, r2_min, r2_max)
+    return r1_min, r2_min, r2_max
+
+
+def DT_constraints(L_Disk):
+    return 1E18 * (L_Disk / 1E45) ** 0.5
+
+
+def build_ExtFields_dic(EC_model_list,disk_type ):
+    """
+
+        """
+
+    model_dic = {}
+
+    for EC_model in EC_model_list:
+
+        # print('EC_model',EC_model)
+        # if EC_model not in allowed_EC_components_list:
+        #   raise RuntimeError("EC model %s not allowed"%EC_model,"please choose among ", allowed_EC_components_list)
+        #print('----->',EC_model_list)
+        if 'Disk' in EC_model:
+            if disk_type is not None:
+                model_dic['disk_type'] = JetModelDictionaryPar(ptype='Disk', vmin=None, vmax=None, punit='', froz=True,
+                                                               allowed_values=allowed_disk_type)
+
+                model_dic['L_Disk'] = JetModelDictionaryPar(ptype='Disk', vmin=0, vmax=None, punit='erg/s')
+
+                if disk_type == 'BB' or disk_type == 'Mono':
+
+                    model_dic['T_Disk'] = JetModelDictionaryPar(ptype='Disk', vmin=0, vmax=None, punit='K')
+
+                if disk_type == 'MultiBB':
+                    model_dic['R_inner_Sw'] = JetModelDictionaryPar(ptype='Disk', vmin=0, vmax=None, punit='Sw. radii')
+
+                    model_dic['R_ext_Sw'] = JetModelDictionaryPar(ptype='Disk', vmin=0, vmax=None, punit='Sw. radii')
+
+                    model_dic['accr_eff'] = JetModelDictionaryPar(ptype='Disk', vmin=0.06, vmax=0.1, punit='')
+
+                    model_dic['M_BH'] = JetModelDictionaryPar(ptype='Disk', vmin=0, vmax=None, punit='M_sun')
+
+        if 'BLR' in EC_model:
+            # r1_BLR_min, r2_BLR_min, r2_BLR_max = BLR_constraints(L_Disk)
+            model_dic['tau_BLR'] = JetModelDictionaryPar(ptype='BLR', vmin=0, vmax=1.0, punit='')
+            # ['BLR',0.0,1.0,'']
+            model_dic['R_BLR_in'] = JetModelDictionaryPar(ptype='BLR', vmin=0, vmax=None, punit='cm', froz=True)
+            # ['BLR',0,None,'cm',True]
+            model_dic['R_BLR_out'] = JetModelDictionaryPar(ptype='BLR', vmin=0, vmax=None, punit='cm', froz=True)
+            # ['BLR',0,None,'cm',True]
+
+        if 'DT' in EC_model:
+            model_dic['T_DT'] = JetModelDictionaryPar(ptype='DT', vmin=0, vmax=None, punit='K')
+            # ['DT',0.0,None,'K']
+            model_dic['R_DT'] = JetModelDictionaryPar(ptype='DT', vmin=0, vmax=None, punit='cm')
+            # ['DT',0,None,'cm',True]
+            model_dic['tau_DT'] = JetModelDictionaryPar(ptype='DT', vmin=0, vmax=1.0, punit='')
+            # ['DT',0.0,1.0,'']
+
+        if 'Star' in EC_model:
+            model_dic['L_Star'] = JetModelDictionaryPar(ptype='Star', vmin=0, vmax=None, punit='erg s-1')
+            # ['DT',0.0,None,'K']
+            model_dic['T_Star'] = JetModelDictionaryPar(ptype='Star', vmin=0, vmax=None, punit='K')
+            model_dic['theta_Star'] = JetModelDictionaryPar(ptype='Star', vmin=0, vmax=180, punit='deg')
+            model_dic['R_H_Star'] = JetModelDictionaryPar(ptype='Star', vmin=0, vmax=None, punit='cm')
+
+            # ['DT',0,None,'cm',True]
+            #model_dic['tau_DT'] = JetModelDictionaryPar(ptype='DT', vmin=0, vmax=1.0, punit='')
+            ## ['DT',0.0,1.0,'']
+    #print('----->', model_dic)
+    return model_dic
+    
+def clean_numba(distr):
+    if hasattr(distr,'_py_distr_func'):
+        setattr(distr,'distr_func',copy.deepcopy(distr._py_distr_func))
+    elif hasattr(distr.distr_func,'py_func'):
+        setattr(distr,'distr_func',copy.deepcopy(distr.distr_func.py_func))
---
+++ packages/jetset/jetset-1.3.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/jetset/tests/test_depending_parameters.py
@@ -0,0 +1,235 @@
+import pytest
+from .base_class import TestBase
+
+class TestDependingParameters(TestBase):
+
+    def integration_suite(self,plot=False):
+        self._all(plot=plot)
+
+    def test_dep_par(self,plot=False):
+        from jetset.jet_emitters import EmittersDistribution
+        import numpy as np
+
+        def distr_func_bkn(gamma_break, gamma, s1, s2):
+            return np.power(gamma, -s1) * (1. + (gamma / gamma_break)) ** (-(s2 - s1))
+
+        n_e_bkn = EmittersDistribution('bkn', spectral_type='bkn')
+        n_e_bkn.add_par('gamma_break', par_type='turn-over-energy', val=1E3, vmin=1., vmax=None, unit='lorentz-factor')
+        n_e_bkn.add_par('s1', par_type='LE_spectral_slope', val=2.5, vmin=-10., vmax=10, unit='')
+        n_e_bkn.add_par('s2', par_type='LE_spectral_slope', val=3.2, vmin=-10., vmax=10, unit='')
+        n_e_bkn.set_distr_func(distr_func_bkn)
+        n_e_bkn.parameters.show_pars()
+        n_e_bkn.parameters.s1.val = 2.0
+        n_e_bkn.parameters.s2.val = 3.5
+        n_e_bkn.update()
+        n_e_bkn.parameters.show_pars()
+
+        from jetset.jet_model import Jet
+
+        j = Jet(emitters_distribution=n_e_bkn)
+
+        # def par_func(s1):
+        #    return s1+1
+        j.make_dependent_par(par='s2', depends_on=['s1'], par_expr='s1+1')
+        print('here')
+        j.parameters.s1.val = 3
+        print('done')
+        np.testing.assert_allclose(j.parameters.s2.val, j.parameters.s1.val + 1)
+        j.save_model('jet.pkl')
+        new_jet = Jet.load_model('jet.pkl')
+        print('here')
+        new_jet.parameters.s1.val = 2
+        print('done')
+
+        np.testing.assert_allclose(new_jet.parameters.s2.val, new_jet.parameters.s1.val + 1)
+        new_jet.eval()
+        new_jet.show_model()
+
+        copied_jet=new_jet.clone()
+
+        copied_jet = Jet.load_model('jet.pkl')
+        print('here')
+        copied_jet.parameters.s1.val = 2
+        print('done')
+
+        np.testing.assert_allclose(copied_jet.parameters.s2.val, copied_jet.parameters.s1.val + 1)
+        copied_jet.eval()
+        copied_jet.show_model()
+
+
+    def test_dep_par_log_values(self,plot=False):
+        from jetset.jet_emitters import EmittersDistribution
+        import numpy as np
+
+        def distr_func_bkn(gamma_break, gamma, s1, s2):
+            return np.power(gamma, -s1) * (1. + (gamma / gamma_break)) ** (-(s2 - s1))
+
+        n_e_bkn = EmittersDistribution('bkn', spectral_type='bkn')
+        n_e_bkn.add_par('gamma_break', par_type='turn-over-energy', val=3, vmin=0.1, vmax=None, unit='lorentz-factor',log=True)
+        n_e_bkn.add_par('s1', par_type='LE_spectral_slope', val=2.5, vmin=-10., vmax=10, unit='')
+        n_e_bkn.add_par('s2', par_type='LE_spectral_slope', val=3.2, vmin=-10., vmax=10, unit='')
+        n_e_bkn.set_distr_func(distr_func_bkn)
+        n_e_bkn.parameters.show_pars()
+        n_e_bkn.parameters.s1.val = 2.0
+        n_e_bkn.parameters.s2.val = 3.5
+        n_e_bkn.update()
+        n_e_bkn.parameters.show_pars()
+
+        from jetset.jet_model import Jet
+
+        j = Jet(emitters_distribution=n_e_bkn)
+
+        # def par_func(s1):
+        #    return s1+1
+
+        j.make_dependent_par(par='gamma_break', depends_on=['gmax'], par_expr='gmax / 10')
+        j.parameters.gmax.val = 1E5
+        print('par',j.parameters.gamma_break.val, j.parameters.gmax.val / 10)
+        np.testing.assert_allclose(j.parameters.gamma_break.val, np.log10(j.parameters.gmax.val/ 10))
+        j.save_model('jet.pkl')
+        new_jet = Jet.load_model('jet.pkl')
+        new_jet.parameters.gmax.val = 1E4
+        np.testing.assert_allclose(new_jet.parameters.gamma_break.val, np.log10(new_jet.parameters.gmax.val/ 10))
+        j.eval()
+        new_jet.show_model()
+
+        j = Jet(emitters_distribution='lppl',emitters_distribution_log_values=True)
+        j.show_pars()
+        j.parameters.gmax.val = 5
+        j.make_dependent_par(par='gamma0_log_parab', depends_on=['gmax'], par_expr='gmax / 10')
+        j.show_pars()  
+        
+        print('par',j.parameters.gamma0_log_parab.val, j.parameters.gmax.val - 1)
+        np.testing.assert_allclose(j.parameters.gamma0_log_parab.val, j.parameters.gmax.val - 1)
+        print(j.parameters.gamma0_log_parab.val, j.parameters.gmax.val -1)
+        j.save_model('jet.pkl')
+        new_jet = Jet.load_model('jet.pkl')
+        new_jet.parameters.gmax.val = 4
+        np.testing.assert_allclose(new_jet.parameters.gamma0_log_parab.val, new_jet.parameters.gmax.val -1)
+        new_jet.eval()
+        new_jet.show_model()
+
+
+    def test_dep_par_jet(self,plot=False):
+        import numpy as np
+
+        from jetset.jet_model import Jet
+        j = Jet(emitters_distribution='plc')
+        j.add_user_par(name='B0',units='G',val=1E-5,val_min=0,val_max=None)
+        j.make_dependent_par(par='B',depends_on=['B0'],par_expr='B0*5')
+        np.testing.assert_allclose(j.parameters.B.val, j.parameters.B0.val*5)
+
+        j.save_model('test.pkl')
+        new_j=Jet.load_model('test.pkl')
+        new_j.parameters.B0.val=1
+        np.testing.assert_allclose(new_j.parameters.B.val, new_j.parameters.B0.val*5)
+
+
+    def test_dep_par_composite_model(self,plot=False):
+        import numpy as np
+        from jetset.model_manager import FitModel
+        from jetset.jet_model import Jet
+
+        jet = Jet(emitters_distribution='plc')
+        fit_model = FitModel(jet=jet, name='SSC-best-fit-lsb', template=None)
+        fit_model.jet_leptonic.parameters.beam_obj.fit_range = [5, 50]
+        fit_model.jet_leptonic.parameters.R_H.val = 5E17
+        fit_model.jet_leptonic.parameters.R_H.frozen = False
+        fit_model.jet_leptonic.parameters.R_H.fit_range = [1E15, 1E19]
+        fit_model.jet_leptonic.parameters.R.fit_range = [10 ** 15.5, 10 ** 17.5]
+        fit_model.jet_leptonic.add_user_par(name='B0', units='G', val=1E3, val_min=0, val_max=None)
+        fit_model.jet_leptonic.add_user_par(name='R0', units='cm', val=5E13, val_min=0, val_max=None)
+        fit_model.jet_leptonic.add_user_par(name='m_B', val=1, val_min=1, val_max=2)
+
+        fit_model.jet_leptonic.parameters.R0.frozen = True
+        fit_model.jet_leptonic.parameters.B0.frozen = True
+
+        def par_func(R0,B0,R_H,m_B): 
+            return B0*np.power((R0/R_H),m_B)
+
+        fit_model.jet_leptonic.make_dependent_par(par='B', depends_on=['B0', 'R0', 'R_H','m_B'], par_expr=par_func)
+
+        B0=fit_model.jet_leptonic.parameters.B0.val
+        R0 = fit_model.jet_leptonic.parameters.R0.val
+        R_H = fit_model.jet_leptonic.parameters.R_H.val
+        m_B= fit_model.jet_leptonic.parameters.m_B.val
+
+        np.testing.assert_allclose(fit_model.jet_leptonic.parameters.B.val, par_func(B0,R0,R_H,m_B))
+
+        fit_model.save_model('test_composite.pkl')
+        fit_model.parameters._build_par_table()
+        t=fit_model.parameters._par_table
+        t.sort(t.colnames)
+        
+        for new_fit_model in [FitModel.load_model('test_composite.pkl'),fit_model.clone()]:
+            
+            new_fit_model.parameters._build_par_table()
+            _t=new_fit_model.parameters._par_table
+            _t.sort(_t.colnames)
+
+            assert(all([_t[i]['frozen']==t[i]['frozen'] for i in range(len(_t))]))
+            assert(all([_t[i]['val']==t[i]['val'] for i in range(len(_t))]))
+
+            new_fit_model.jet_leptonic.parameters.B0.val=1E4
+
+            B0 = new_fit_model.jet_leptonic.parameters.B0.val
+            R0 = new_fit_model.jet_leptonic.parameters.R0.val
+            R_H = new_fit_model.jet_leptonic.parameters.R_H.val
+            
+            np.testing.assert_allclose(new_fit_model.jet_leptonic.parameters.B.val, par_func(B0,R0,R_H,m_B))
+
+    def test_dep_par_fit_model(self,plot=False):
+        import numpy as np
+        from jetset.model_manager import FitModel
+        from jetset.jet_model import Jet
+        j=Jet()
+        
+        j.add_EC_component(['EC_BLR','EC_Disk','EC_DT'],disk_type='MultiBB')
+
+        #kaspi+ 2007:https://iopscience.iop.org/article/10.1086/512094/pdf
+        j.make_dependent_par(par='R_BLR_in', depends_on=['L_Disk'], par_expr='1E17*(L_Disk/1E45)**0.5')
+
+        j.make_dependent_par(par='R_BLR_out', depends_on=['R_BLR_in'], par_expr='R_BLR_in*1.1')
+
+        #Cleary+ 2007:https://iopscience.iop.org/article/10.1086/511969/pdf
+        j.make_dependent_par(par='R_DT', depends_on=['L_Disk'], par_expr='2.5E18*(L_Disk/1E45)**0.5')
+    
+        j.add_user_par(name='theta_open',val=5, units='deg')
+        def f_par(R_H,theta_open):
+            return np.tan( theta_open)*R_H
+
+        j.make_dependent_par(par='R', depends_on=['R_H','theta_open'],
+                              par_expr=f_par)
+
+        j.eval()
+        
+        R_H_val=1E21
+        j.parameters.R_H.val=R_H_val
+        R_val=j.parameters.R.val
+
+        j.save_model('test_jet_EC.pkl')
+
+        fit_model = FitModel(jet=j, name='EC-best-fit-lsb', template=None)
+        fit_model.save_model('test_fit_model_EC.pkl')
+        
+        new_jet=Jet.load_model('test_jet_EC.pkl')
+       
+        new_jet.eval()
+        print("units of theta_open",new_jet.parameters.theta_open.units)
+
+
+        new_jet.parameters.R_H.val=R_H_val
+        
+        assert(new_jet.parameters.R.val==R_val)
+        new_jet.show_model()
+
+        new_fit_model=FitModel.load_model('test_fit_model_EC.pkl')
+
+
+        new_fit_model.eval()
+        print("units of theta_open",new_fit_model.jet_leptonic.parameters.theta_open.units)
+
+        new_fit_model.jet_leptonic.parameters.R_H.val=R_H_val
+        
+        assert(new_fit_model.jet_leptonic.parameters.R.val==R_val)
+        new_fit_model.show_model()
