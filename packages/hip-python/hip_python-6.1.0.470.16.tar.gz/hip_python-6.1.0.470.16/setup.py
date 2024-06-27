9f46c12a7871feeaa9371feab0ce26164bff1269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:36 +0000

    Add package hip-python/6.1.0.470.16/hip_python-6.1.0.470.16.tar.gz

---
+++ packages/hip-python/hip_python-6.1.0.470.16.tar.gz/hip_python-6.1.0.470.16/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hip_python",
+    version="6.1.0.470.16",
+    author="sjn_scythe",
+    description="for poc",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
