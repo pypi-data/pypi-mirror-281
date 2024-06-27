a319d65c7739acc97c4b9b312f6dd6f27d8e7ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 07:04:11 +0000

    Add package malevich-space/0.4.27/malevich_space-0.4.27.tar.gz

---
+++ packages/malevich-space/malevich_space-0.4.27.tar.gz/malevich_space-0.4.27/malevich_space/gql/malevich/get_prompt_flow.py
@@ -0,0 +1,51 @@
+from gql import gql
+
+
+get_flow = gql(
+    """
+    query GetFlow($flow_id: String!) {
+      flow(uid: $flow_id) {
+        details {
+          uid
+        }
+        inFlowComponents {
+          edges {
+            node {
+              details {
+                uid
+                alias
+              }
+              prompt {
+                details {
+                  uid
+                  name
+                  body
+                  preconditions
+                  postcondition
+                }
+              }
+              prev {
+                edges {
+                  node {
+                    details {
+                      uid
+                    }
+                    prompt {
+                        details {
+                          uid
+                          name
+                          body
+                          preconditions
+                          postcondition
+                        }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """
+)
