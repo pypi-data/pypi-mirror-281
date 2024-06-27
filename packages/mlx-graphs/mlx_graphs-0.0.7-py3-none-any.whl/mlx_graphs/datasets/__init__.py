f42c3dd2ded4f6867704a40ee1a6e8cfad367c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 17:04:39 +0000

    Add package mlx-graphs/0.0.7/mlx_graphs-0.0.7-py3-none-any.whl

---
+++ packages/mlx-graphs/mlx_graphs-0.0.7-py3-none-any.whl/mlx_graphs/datasets/__init__.py
@@ -0,0 +1,11 @@
+from .dataset import Dataset  # noqa
+from .karate_club import KarateClubDataset  # noqa
+from .qm7b import QM7bDataset  # noqa
+from .tu_dataset import TUDataset  # noqa
+from .planetoid import PlanetoidDataset  # noqa
+from .superpixel import SuperPixelDataset  # noqa
+from .ogb_dataset import OGBDataset  # noqa
+from .elliptic import EllipticBitcoinDataset  # noqa
+from .movie_lens_100k import MovieLens100K  # noqa
+from .imdb import IMDB  # noqa
+from .dblp import DBLP  # noqa
