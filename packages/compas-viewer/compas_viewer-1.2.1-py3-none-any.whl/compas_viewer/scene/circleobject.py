a197df70b5742392baf0a6b7072b22d8681e1016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:36 +0000

    Add package compas-viewer/1.2.1/compas_viewer-1.2.1-py3-none-any.whl

---
+++ packages/compas-viewer/compas_viewer-1.2.1-py3-none-any.whl/compas_viewer/scene/circleobject.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from compas.geometry import Circle
+from compas.geometry import Line
+from compas.geometry import Point
+from compas.scene import GeometryObject
+
+from .geometryobject import GeometryObject as ViewerGeometryObject
+
+
+class CircleObject(ViewerGeometryObject, GeometryObject):
+    """Viewer scene object for displaying COMPAS Circle geometry."""
+
+    geometry: Circle
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.show_lines = True
+
+    @property
+    def points(self) -> Optional[list[Point]]:
+        return [self.geometry.center]
+
+    @property
+    def lines(self) -> Optional[list[Line]]:
+        return self.geometry.to_polyline(n=self.u).lines
+
+    @property
+    def viewmesh(self):
+        return None
