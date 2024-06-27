c2dfd57d15d0cc9830cfbb858aa5b3d6999d1f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:02 +0000

    Add package quick-stock-sdk/0.1.2/quick_stock_sdk-0.1.2.tar.gz

---
+++ packages/quick-stock-sdk/quick_stock_sdk-0.1.2.tar.gz/quick_stock_sdk-0.1.2/quick_stock/trade_index_client.py
@@ -0,0 +1,32 @@
+import datetime
+from quick_stock.d import getDClient
+from quick_stock.remote.index import TradeIndex
+
+class TradeIndexClient:
+    dClient = None
+
+    def __init__(self):
+        self.dClient = getDClient()
+
+    def get_all_index(self):
+        """
+        :return: list - string
+        """
+        df = self.dClient.select("select * from mc_index_basic")
+        if df.empty or datetime.date.today().day == 1:
+            df = TradeIndex().get_all()
+            self.dClient.save(df, 'mc_index_basic')
+        return df
+
+    def daily(self):
+        pass
+
+    def weekly(self):
+        pass
+
+    def monthly(self):
+        pass
+
+if __name__ == "__main__":
+    TradeIndexClient().get_all_index()
+
