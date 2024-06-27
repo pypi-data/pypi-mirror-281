ccff79316665358d45bc24dd45260041e718358e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 05:04:41 +0000

    Add package edgegap-database/1.10.1/edgegap_database-1.10.1-py3-none-any.whl

---
+++ packages/edgegap-database/edgegap_database-1.10.1-py3-none-any.whl/edgegap_database/_session.py
@@ -0,0 +1,45 @@
+import logging
+from contextlib import contextmanager
+
+from sqlmodel import Session
+
+from ._configuration import DatabaseConfiguration
+from ._engine import DatabaseEngine
+from ._session_factory import SessionFactory
+
+logger = logging.getLogger(__name__)
+
+
+class DatabaseSession:
+    @staticmethod
+    @contextmanager
+    def get_session(configuration: DatabaseConfiguration):
+        engine = DatabaseEngine(configuration).get_write_engine()
+        session = Session(engine)
+
+        try:
+            yield session
+        finally:
+            session.close()
+
+    @staticmethod
+    @contextmanager
+    def get_write_session(configuration: DatabaseConfiguration, session_factory: SessionFactory):
+        engine = DatabaseEngine(configuration).get_write_engine()
+        session = session_factory.create_session(engine)
+
+        try:
+            yield session
+        finally:
+            session.close()
+
+    @staticmethod
+    @contextmanager
+    def get_read_session(configuration: DatabaseConfiguration, session_factory: SessionFactory):
+        engine = DatabaseEngine(configuration).get_read_engine()
+        session = session_factory.create_session(engine)
+
+        try:
+            yield session
+        finally:
+            session.close()
