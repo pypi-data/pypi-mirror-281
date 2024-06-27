61c41396d9dc58b39ba36b724496de4d5f24b8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 07:03:59 +0000

    Add package cheminf-EDU/0.1.0/cheminf-EDU-0.1.0.tar.gz

---
+++ packages/cheminf-EDU/cheminf-EDU-0.1.0.tar.gz/cheminf-EDU-0.1.0/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cheminf-EDU',
+    version='0.1.0',
+    description='A simple educational Python package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/Yakimochinai/che-edu',
+    packages=find_packages(),
+    install_requires=[],  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
