52919962210717552a541008de207f7d4533912e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 17:04:36 +0000

    Add package django-ip-access/2.1.0/django_ip_access-2.1.0.tar.gz

---
+++ packages/django-ip-access/django_ip_access-2.1.0.tar.gz/django_ip_access-2.1.0/django_ip_access/middleware.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import authenticate, login
+from ipware import get_client_ip
+
+
+class IpAccessMiddleware:
+    """Middleware that allows Ip address authentication"""
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.user and request.user.is_authenticated:
+            return self.get_response(request)
+
+        ip, is_routable = get_client_ip(request)
+        user = authenticate(request, ip=ip)
+
+        if user:
+            login(request, user, backend="django_ip_access.backends.IpAccessBackend")
+
+        return self.get_response(request)
