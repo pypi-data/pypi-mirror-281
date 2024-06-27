ca91c59122a63b8d74685e6c7621a4a7800f1165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 17:04:35 +0000

    Add package wiselib/0.0.0/wiselib-0.0.0.tar.gz

---
+++ packages/wiselib/wiselib-0.0.0.tar.gz/wiselib-0.0.0/wise/station/registry.py
@@ -0,0 +1,20 @@
+from wise.station.publish import Publisher
+from wise.station.updater import UpdaterHandler, UpdaterSet
+
+
+class StationRegistry:
+    publishers: list[Publisher] | None = None
+    updater_handlers: list[UpdaterHandler] | None = None
+    periodic_updaters: UpdaterSet | None = None
+
+    def add_publishers(self, publishers: list[Publisher]):
+        self.publishers = publishers
+
+    def add_updater_handlers(self, handlers: list[UpdaterHandler]):
+        self.updater_handlers = handlers
+
+    def add_periodic_updaters(self, updaters: UpdaterSet):
+        self.periodic_updaters = updaters
+
+
+station_registry = StationRegistry()
