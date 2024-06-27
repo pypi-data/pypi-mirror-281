dc8e500479e3cf2cbee409f6bba3c68ef25c9d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:33 +0000

    Add package glidergun/0.5.10/glidergun-0.5.10-py3-none-any.whl

---
+++ packages/glidergun/glidergun-0.5.10-py3-none-any.whl/glidergun/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+import glidergun._ipython
+from glidergun._grid import (
+    Extent,
+    Grid,
+    con,
+    create,
+    grid,
+    interp_linear,
+    interp_nearest,
+    interp_rbf,
+    load_model,
+    maximum,
+    mean,
+    minimum,
+    mosaic,
+    pca,
+    standardize,
+    std,
+)
+from glidergun._stack import Stack, stack
