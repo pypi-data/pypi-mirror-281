ec4050ff8d2ce53304dd2e992e49707183127afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:39 +0000

    Add package syspop/0.1.7/syspop-0.1.7.tar.gz

---
+++ packages/syspop/syspop-0.1.7.tar.gz/syspop-0.1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="syspop",
+    version="0.1.7",
+    setup_requires=["setuptools-git-versioning"],
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "pyarrow",
+        "overpy",
+        "geopy",
+        "scipy",
+        "shapely",
+        "openpyxl",
+        "ray[default]",
+        "xlrd",
+        "OSMPythonTools",
+    ],
+    entry_points={
+        "console_scripts": [
+            "syspop=syspop.syspop:create",
+        ],
+    },
+    # python_requires='==3.9.*',
+)
