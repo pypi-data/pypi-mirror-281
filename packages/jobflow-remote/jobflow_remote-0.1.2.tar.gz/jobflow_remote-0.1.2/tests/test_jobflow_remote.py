1c9bd34d16d2c6aab06308e6e8fc66da372b01b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:31 +0000

    Add package jobflow-remote/0.1.2/jobflow_remote-0.1.2.tar.gz

---
+++ packages/jobflow-remote/jobflow_remote-0.1.2.tar.gz/jobflow_remote-0.1.2/tests/test_jobflow_remote.py
@@ -0,0 +1,12 @@
+def test_version():
+    from jobflow_remote import __version__
+
+    assert __version__.startswith("0.0.1")
+
+
+def test_imports():
+    """This test triggers all the top-level imports by importing
+    the global `SETTINGS`.
+
+    """
+    from jobflow_remote import SETTINGS  # noqa
