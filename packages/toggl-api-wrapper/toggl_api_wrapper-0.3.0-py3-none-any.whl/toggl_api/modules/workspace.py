cc27f6796bdc8c39de059ac601ca7779fe138894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:34 +0000

    Add package toggl-api-wrapper/0.3.0/toggl_api_wrapper-0.3.0-py3-none-any.whl

---
+++ packages/toggl-api-wrapper/toggl_api_wrapper-0.3.0-py3-none-any.whl/toggl_api/modules/workspace.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from .meta import TogglCachedEndpoint
+from .models import TogglWorkspace
+
+
+class WorkspaceEndpoint(TogglCachedEndpoint):
+    def get_workspace(self, *, refresh: bool = False) -> Optional[TogglWorkspace]:
+        """Get the current workspace based on the workspace_id class attribute."""
+
+        if not refresh:
+            tracker = self.cache.find_entry({"id": self.workspace_id})
+            if isinstance(tracker, TogglWorkspace):
+                return tracker
+            refresh = True
+
+        return self.request("", refresh=refresh)  # type: ignore[return-value]
+
+    @property
+    def model(self) -> type[TogglWorkspace]:
+        return TogglWorkspace
+
+    @property
+    def endpoint(self) -> str:
+        return f"workspaces/{self.workspace_id}"
