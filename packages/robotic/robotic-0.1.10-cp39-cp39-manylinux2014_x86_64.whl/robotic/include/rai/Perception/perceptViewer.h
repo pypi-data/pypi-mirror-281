73386e647bcec722aac13adf5917e33771ace5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 06:04:36 +0000

    Add package robotic/0.1.10/robotic-0.1.10-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/robotic/robotic-0.1.10-cp39-cp39-manylinux2014_x86_64.whl/robotic/include/rai/Perception/perceptViewer.h
@@ -0,0 +1,26 @@
+/*  ------------------------------------------------------------------
+    Copyright (c) 2011-2024 Marc Toussaint
+    email: toussaint@tu-berlin.de
+
+    This code is distributed under the MIT License.
+    Please see <root-path>/LICENSE for details.
+    --------------------------------------------------------------  */
+
+#pragma once
+
+#include "percept.h"
+#include "../Core/thread.h"
+
+struct PerceptViewer : Thread {
+  Var<PerceptL> percepts;
+  Var<rai::Configuration> kin;
+  PerceptL copy;
+  MeshA modelCopy;
+  struct OpenGL* gl;
+
+  PerceptViewer(Var<PerceptL>& _percepts, Var<rai::Configuration> _kin);
+  ~PerceptViewer();
+  void open();
+  void step();
+  void close();
+};
