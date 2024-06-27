f8b47033e4e0942ce7a6868d4e9c570f5b2597d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:10 +0000

    Add package djako-plugin-ya-metrics/0.0.3a2/djako_plugin_ya_metrics-0.0.3a2.tar.gz

---
+++ packages/djako-plugin-ya-metrics/djako_plugin_ya_metrics-0.0.3a2.tar.gz/djako_plugin_ya_metrics-0.0.3a2/djako_plugin_ya_metrics/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjakoPluginYandexMetricsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djako_plugin_ya_metrics'
+    verbose_name = "Djako | Яндекс Метрика"
