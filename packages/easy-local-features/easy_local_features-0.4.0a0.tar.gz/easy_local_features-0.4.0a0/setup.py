f887b960ebbad5ab28a84bb647661bc551a452ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 07:03:50 +0000

    Add package easy-local-features/0.4.0a0/easy_local_features-0.4.0a0.tar.gz

---
+++ packages/easy-local-features/easy_local_features-0.4.0a0.tar.gz/easy_local_features-0.4.0a0/setup.py
@@ -0,0 +1,23 @@
+import sys
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+requires = []
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+
+setup(
+    name='easy_local_features',
+    version='0.4.0a',
+    author='eucadar',
+    author_email='python@eucadar.com',
+    packages=find_packages(exclude=('tests', 'docs', 'assets')),
+    include_package_data=True,
+    # include the requirements file
+    package_data={'': ['requirements.txt']},
+    install_requires=requires,
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+)
