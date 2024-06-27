03500dbf25990836dd92c8d82eab4476f0afb332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:25 +0000

    Add package vector-cache/0.1.1/vector_cache-0.1.1.tar.gz

---
+++ packages/vector-cache/vector_cache-0.1.1.tar.gz/vector_cache-0.1.1/README.md
@@ -0,0 +1,44 @@
+# VectorCache: A Python Library for Efficient LLM Query Caching
+
+A streamlined Python library that enhances LLM query performance through semantic caching, making responses faster and more cost-effective.
+
+![Pypi Build](https://github.com/shivendrasoni/vector-cache/actions/workflows/publish.yml/badge.svg)
+
+## What is VectorCache?
+
+As AI applications gain traction, the costs and latency of using large language models (LLMs) can escalate. VectorCache addresses these issues by caching LLM responses based on semantic similarity, thereby reducing both costs and response times.
+
+
+### 🤠 Overview
+
+Semantic Vector Cache leverages the power of LLMs to provide two main advantages:
+
+1. **Faster Responses**: By caching, it significantly reduces latency, offering quicker feedback to user queries.
+2. **Lower Costs**: It minimizes the number of direct LLM requests, thereby saving on usage costs.
+
+### 🤔 Benefits
+
+VectorCache, akin to a more nuanced Redis, enables efficient caching by recognizing not just exact matches but also semantically similar queries. This efficiency is particularly useful in domains where queries within a specific topic or field are frequent.
+
+### 😊 Getting Started
+
+**Prerequisites**:
+- Ensure Python version is **3.9 or higher** (`python --version`).
+- - `pip install vector-cache`
+
+- Install from source
+  - For library installation issues, upgrade pip: `python -m pip install -r requirements.txt`.
+
+
+- Refer to the `examples` folder for sample usage.
+
+VectorCache is designed to work with any LLM provider. It includes modules for:
+- **Embedding Models**: Facilitates similarity searches through various vector_cache.embedding APIs.
+- **Cache Storage**: Stores LLM responses for future retrieval based on semantic matches.
+- **Vector Store**: Identifies similar requests using the input request's embeddings.
+- **Cache Manager**: Manages cache storage and vector store operations, including eviction policies.
+- **Similarity Evaluator**: Determines the similarity between requests to ascertain cache matches.
+
+### 😆 Contributing
+
+Interested in contributing to VectorCache? Check our [contribution guidelines](docs/contributing.md).
---
+++ packages/vector-cache/vector_cache-0.1.1.tar.gz/vector_cache-0.1.1/src/vector_cache/vector_stores/chroma_db.py
@@ -0,0 +1,60 @@
+import uuid
+
+from chromadb import QueryResult
+from chromadb.db.base import UniqueConstraintError
+
+from vector_cache.vector_stores.base import VectorStoreInterface
+from typing import Tuple
+import chromadb
+class ChromeDB(VectorStoreInterface):
+
+    default_collection = 'default_collection'
+
+    def __init__(self, collection: str = default_collection, persistent = False) -> None:
+        if persistent:
+            self.chroma_client = chromadb.PersistentClient()
+        else:
+            self.chroma_client = chromadb.Client()
+
+        try:
+            self.collection = self.chroma_client.create_collection(name=collection)
+        except UniqueConstraintError:
+            print(f"Collection {collection} already exists")
+            self.collection = self.chroma_client.get_collection(name=collection)
+
+    def add(self, embedding: list, **kwargs) -> str:
+        id = str(uuid.uuid4())
+        # chroma db client expects the embeddings themselves to be a list.
+        # So, we convert it to such but only if it's not already a list
+        if isinstance(embedding, list):
+            self.collection.add(ids=[id], embeddings=[embedding])
+        else:
+            embedding_list = embedding.tolist()
+            self.collection.add(ids=[id], embeddings=[embedding_list])
+        return id
+
+    def get(self, id: str) -> Tuple[str, float]:
+        pass
+
+    def search(self, embedding: list, top_k: int, include_distances:bool = True, **kwargs) -> Tuple[list, list]:
+        # chroma db client expects the embeddings themselves to be a list.
+        # So, we convert it to such but only if it's not already a list
+
+        if isinstance(embedding, list):
+            embedding_list = embedding
+        else:
+            embedding_list = embedding.tolist()
+
+        if include_distances:
+            query_result: QueryResult = self.collection.query(query_embeddings=[embedding_list], n_results=top_k, include=["distances"])
+            return query_result['ids'][0], query_result['distances'][0]
+        else:
+            query_result: QueryResult = self.collection.query(query_embeddings=[embedding_list], n_results=top_k, include=["distances"])
+            return query_result['ids'][0]
+
+
+    def __enter__(self) -> "ChromeDB":
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.close()
