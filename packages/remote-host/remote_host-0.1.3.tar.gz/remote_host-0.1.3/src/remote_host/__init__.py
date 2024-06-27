c1b514ecff1e215febad5a520f1e9dc72b0265a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:42 +0000

    Add package remote-host/0.1.3/remote_host-0.1.3.tar.gz

---
+++ packages/remote-host/remote_host-0.1.3.tar.gz/remote_host-0.1.3/src/remote_host/__init__.py
@@ -0,0 +1,10 @@
+from remote_host.linux_remote_host import LinuxRemoteHost
+from remote_host.objects import DirectoryObject, FileObject
+from remote_host.windows_remote_host import WindowsRemoteHost
+
+__version__ = "0.1.3"
+TITLE = "remote-host"
+VERSION = __version__
+AUTHOR = "Julius Koenig"
+AUTHOR_EMAIL = "info@bastelquartier.de"
+LICENSE = "GPL-3.0"
