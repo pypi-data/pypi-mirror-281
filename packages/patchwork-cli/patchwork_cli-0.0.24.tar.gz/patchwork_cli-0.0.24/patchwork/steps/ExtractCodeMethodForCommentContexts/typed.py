d3b56dfe8a29664f2334934ebc7397e2ee7b5ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 17:04:37 +0000

    Add package patchwork-cli/0.0.24/patchwork_cli-0.0.24.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.24.tar.gz/patchwork_cli-0.0.24/patchwork/steps/ExtractCodeMethodForCommentContexts/typed.py
@@ -0,0 +1,17 @@
+from typing_extensions import Iterable, NotRequired, TypedDict
+
+
+class ExtractCodeInputs(TypedDict):
+    base_path: NotRequired[str]
+    context_grouping: NotRequired[str]
+
+
+class ExtractCodeOutputs(TypedDict):
+    files_to_patch: Iterable["ExtractedCode"]
+
+
+class ExtractedCode(TypedDict):
+    uri: str
+    startLine: int
+    endLine: int
+    affectedCode: str
---
+++ packages/patchwork-cli/patchwork_cli-0.0.24.tar.gz/patchwork_cli-0.0.24/patchwork/steps/PreparePrompt/typed.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from typing_extensions import Any, Iterable, NotRequired, TypedDict
+
+
+class PreparePromptInputs(TypedDict):
+    prompt_template_file: str
+    prompt_id: str
+    prompt_value_file: NotRequired[str]
+    prompt_values: NotRequired[Iterable[dict[str, Any]]]
+
+
+class PreparePromptOutputs(TypedDict):
+    prompts: Iterable[dict]
