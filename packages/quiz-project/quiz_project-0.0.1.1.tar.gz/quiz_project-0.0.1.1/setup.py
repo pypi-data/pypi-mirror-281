839308fe7fee9800b18491a877373b15599977d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:45 +0000

    Add package quiz-project/0.0.1.1/quiz_project-0.0.1.1.tar.gz

---
+++ packages/quiz-project/quiz_project-0.0.1.1.tar.gz/quiz_project-0.0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.0.1.1'
+DESCRIPTION = 'Quiz creator and displayer'
+LONG_DESCRIPTION = 'Advanced coding skills project: A game of a quiz that allows you to create it and save it, and play your own and other saved quizzes'
+
+# Setting up
+setup(
+    name="quiz_project",
+    version=VERSION,
+    author="Mariana Aguerrevere",
+    author_email="<aamariana1907@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['time'],
+    keywords=['python', 'quiz', 'creator','display', 'game']
+)
