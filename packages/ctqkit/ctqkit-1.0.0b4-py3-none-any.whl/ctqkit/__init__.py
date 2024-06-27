1706056551c08d8479946d758510c4dc521fa0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:00 +0000

    Add package ctqkit/1.0.0b4/ctqkit-1.0.0b4-py3-none-any.whl

---
+++ packages/ctqkit/ctqkit-1.0.0b4-py3-none-any.whl/ctqkit/__init__.py
@@ -0,0 +1,15 @@
+# This code is part of ctqkit.
+#
+# (C) Copyright China Telecom Quantum Group, QuantumCTek Co., Ltd.,
+# Center for Excellence in Quantum Information and Quantum Physics 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""ctqkit"""
+from .quantum_platform import BasePlatform, TianYanPlatform, GuoDunPlatform
---
+++ packages/ctqkit/ctqkit-1.0.0b4-py3-none-any.whl/ctqkit/visualization/__init__.py
@@ -0,0 +1,7 @@
+"""
+visualization module defines methods to visualize qcis circuits and plotting experiment results.
+"""
+
+from .circuit import draw_circuit
+from .gplot import draw_gplot
+from .result import draw_histogram
