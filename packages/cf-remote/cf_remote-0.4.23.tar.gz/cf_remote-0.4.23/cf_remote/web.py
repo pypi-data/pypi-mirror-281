ec939b87d0d1845d5f4e6c933873de52c14555c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 06:04:39 +0000

    Add package cf-remote/0.4.23/cf_remote-0.4.23.tar.gz

---
+++ packages/cf-remote/cf_remote-0.4.23.tar.gz/cf_remote-0.4.23/cf_remote/web.py
@@ -0,0 +1,47 @@
+import os
+import fcntl
+import urllib.request
+import json
+from collections import OrderedDict
+from cf_remote.utils import write_json, mkdir, parse_json
+from cf_remote import log
+from cf_remote.paths import cf_remote_dir, cf_remote_packages_dir
+
+
+def get_json(url):
+    with urllib.request.urlopen(url) as r:
+        assert r.status >= 200 and r.status < 300
+        data = json.loads(r.read().decode(), object_pairs_hook=OrderedDict)
+
+    filename = os.path.basename(url)
+    dir = cf_remote_dir("json")
+    path = os.path.join(dir, filename)
+    log.debug("Saving '{}' to '{}'".format(url, path))
+    write_json(path, data)
+
+    return data
+
+
+def download_package(url, path=None):
+    if not path:
+        filename = os.path.basename(url)
+        directory = cf_remote_packages_dir()
+        mkdir(directory)
+        path = os.path.join(directory, filename)
+
+    # Use "ab" to prevent truncation of the file in case it is already being
+    # downloaded by a different thread.
+    with open(path, "ab") as f:
+        # Get an exclusive lock. If the file size is != 0 then it's already
+        # downloaded, otherwise we download.
+        fcntl.flock(f.fileno(), fcntl.LOCK_EX)
+        st = os.stat(path)
+        if st.st_size != 0:
+            log.debug("Package '{}' already downloaded".format(path))
+        else:
+            print("Downloading package: '{}'".format(path))
+            f.write(urllib.request.urlopen(url).read())
+            f.flush()
+        fcntl.flock(f.fileno(), fcntl.LOCK_UN)
+
+    return path
