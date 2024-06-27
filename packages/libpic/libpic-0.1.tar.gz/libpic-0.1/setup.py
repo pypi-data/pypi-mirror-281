829b51f442386a0b7d8ea9174bcb63bb5cc8d699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:20 +0000

    Add package libpic/0.1/libpic-0.1.tar.gz

---
+++ packages/libpic/libpic-0.1.tar.gz/libpic-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='libpic',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
