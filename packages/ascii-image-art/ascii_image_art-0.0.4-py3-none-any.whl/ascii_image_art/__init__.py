76ffb39d5313904c9e63a0026661355d5a342030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 14:05:06 +0000

    Add package ascii-image-art/0.0.4/ascii_image_art-0.0.4-py3-none-any.whl

---
+++ packages/ascii-image-art/ascii_image_art-0.0.4-py3-none-any.whl/ascii_image_art/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['ascii_images_and_text','constants']
+
+from ascii_images_and_text import ASCIIArt  # loka
+from constants import PALETTE, CHARS_BY_DENSITY  # loka
+# ###############fuck off############### #
