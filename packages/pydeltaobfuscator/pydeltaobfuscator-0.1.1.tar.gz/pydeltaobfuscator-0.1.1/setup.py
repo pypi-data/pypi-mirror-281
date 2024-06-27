3a3767da32c18cd036f127ce8adb56cac044915d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:30 +0000

    Add package pydeltaobfuscator/0.1.1/pydeltaobfuscator-0.1.1.tar.gz

---
+++ packages/pydeltaobfuscator/pydeltaobfuscator-0.1.1.tar.gz/pydeltaobfuscator-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    desc = f.read()
+
+setup(
+    name='pydeltaobfuscator',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'argparse'
+    ],
+    entry_points={
+        "console_scripts": [
+            'pydelta-obfuscate = pydelta:obfuscate_cli',
+        ],
+    },
+    long_description=desc,
+    long_description_content_type="text/markdown"
+)
