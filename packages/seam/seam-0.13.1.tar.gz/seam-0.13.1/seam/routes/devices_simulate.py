614dfac2d4d118ca09cd9bcc381a03cd85356354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 07:03:51 +0000

    Add package seam/0.13.1/seam-0.13.1.tar.gz

---
+++ packages/seam/seam-0.13.1.tar.gz/seam-0.13.1/seam/routes/devices_simulate.py
@@ -0,0 +1,19 @@
+from typing import Optional, Any, List, Dict, Union
+from ..client import SeamHttpClient
+from .models import AbstractDevicesSimulate
+
+
+class DevicesSimulate(AbstractDevicesSimulate):
+    def __init__(self, client: SeamHttpClient, defaults: Dict[str, Any]):
+        self.client = client
+        self.defaults = defaults
+
+    def remove(self, *, device_id: str) -> None:
+        json_payload = {}
+
+        if device_id is not None:
+            json_payload["device_id"] = device_id
+
+        self.client.post("/devices/simulate/remove", json=json_payload)
+
+        return None
