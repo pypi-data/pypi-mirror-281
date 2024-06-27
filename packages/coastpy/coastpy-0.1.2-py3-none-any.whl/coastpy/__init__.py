fb73e60db6faab70f72e09f1379afcde951f2f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 05:05:38 +0000

    Add package coastpy/0.1.2/coastpy-0.1.2-py3-none-any.whl

---
+++ packages/coastpy/coastpy-0.1.2-py3-none-any.whl/coastpy/__init__.py
@@ -0,0 +1,14 @@
+"""Python tools for cloud-native coastal analytics."""
+
+__author__ = """Floris Calkoen"""
+__email__ = ""
+__version__ = "0.1.2"
+
+from . import io
+from . import geo
+from . import libs
+from . import stac
+from . import stats
+from . import utils
+
+__all__ = ["io", "geo", "libs", "stac", "stats", "utils"]
