36b6e09ab5ec303d1d274bc2c66cf7bb590e93a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:05:00 +0000

    Add package ascii-image-art/0.0.4.1/ascii_image_art-0.0.4.1.tar.gz

---
+++ packages/ascii-image-art/ascii_image_art-0.0.4.1.tar.gz/ascii_image_art-0.0.4.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='ascii_image_art',
+    version='0.0.4.1',
+    description='Python library to convert images into accii chararters text file and ascii characters colored images.',
+    author='Mohammad Asad',
+    license='MIT',
+    packages=find_packages(),
+    keywords=['python','ascii','image to text','image to ascii', 'color ascii image','text image','asad','ascii_art','acii_image']
+)
