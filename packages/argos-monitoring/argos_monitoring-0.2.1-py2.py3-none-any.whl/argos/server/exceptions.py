01512ff04553c0dd94e4b95531e13d4d8723dddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 15:06:26 +0000

    Add package argos-monitoring/0.2.1/argos_monitoring-0.2.1-py2.py3-none-any.whl

---
+++ packages/argos-monitoring/argos_monitoring-0.2.1-py2.py3-none-any.whl/argos/server/exceptions.py
@@ -0,0 +1,13 @@
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+
+
+class NotAuthenticatedException(Exception):
+    pass
+
+
+def auth_exception_handler(request: Request, exc: NotAuthenticatedException):
+    """
+    Redirect the user to the login page if not logged in
+    """
+    return RedirectResponse(url=request.url_for("login_view"))
