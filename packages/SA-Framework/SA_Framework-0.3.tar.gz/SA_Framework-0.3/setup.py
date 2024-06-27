e8d583118f32a217c276d7fcfaff826605c44541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:22 +0000

    Add package SA-Framework/0.3/SA_Framework-0.3.tar.gz

---
+++ packages/SA-Framework/SA_Framework-0.3.tar.gz/SA_Framework-0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SA_Framework',
+    version='0.3',  # Incremented version number
+    packages=find_packages(),
+    install_requires=[
+        'robotframework-seleniumlibrary >=6.1.1',
+        'pycryptodome >=3.19.0',
+        'pymongo >=4.5.0',
+        'robotframework-pabot >=2.16.0',
+        'ratelimit >=2.2.1'
+    ],
+    include_package_data=True,
+    description="A Spotline's Framework",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Sukumar Kutagulla',
+    author_email='your.email@example.com',
+    url='https://github.com/Spotline-Inc/SA_Framework',  # Update with your project's URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
