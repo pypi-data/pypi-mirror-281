635c2ca36152919e313d986482a5b2758ed9c34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:41 +0000

    Add package bobocep/1.2.2/bobocep-1.2.2.tar.gz

---
+++ packages/bobocep/bobocep-1.2.2.tar.gz/bobocep-1.2.2/bobocep/cep/action/common/multi.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2019-2024 r3w0p
+# The following code can be redistributed and/or
+# modified under the terms of the MIT License.
+
+"""
+Multi actions.
+"""
+from abc import ABC
+from typing import Tuple, Any, List
+
+from bobocep.cep.action import BoboAction, BoboActionError
+from bobocep.cep.event import BoboEventComplex
+
+_ACTIONS_MIN: int = 1
+
+
+class BoboActionMulti(BoboAction, ABC):
+    """
+    An abstract multi action.
+    """
+
+    def __init__(self, name: str, *args, **kwargs):
+        """
+        :param name: The action name.
+        :param args: Action arguments.
+        :param kwargs: Action keyword arguments.
+        """
+        super().__init__(name=name, args=args, kwargs=kwargs)
+
+
+class BoboActionMultiSequential(BoboActionMulti):
+    """
+    An abstract sequential multi action.
+    """
+
+    def __init__(self,
+                 name: str,
+                 actions: List[BoboAction],
+                 stop_on_fail: bool,
+                 *args,
+                 **kwargs):
+        """
+        :param name: The action name.
+        :param actions: The list of actions to execute.
+        :param stop_on_fail: If True, the multi-action stops processing its
+            action list if its current action fails. If False, it continues
+            to process its remaining actions. Note: failure of any action in
+            its list will cause the multi-action's success to be False.
+        :param args: Action arguments.
+        :param kwargs: Action keyword arguments.
+        """
+        super().__init__(name=name, args=args, kwargs=kwargs)
+
+        if len(actions) < 1:
+            raise BoboActionError(
+                f"multi sequential action {name} "
+                f"must contain at least {_ACTIONS_MIN} action")
+
+        self._actions: List[BoboAction] = actions
+        self._stop_on_fail: bool = stop_on_fail
+
+    def execute(self, event: BoboEventComplex) \
+            -> Tuple[bool, List[Tuple[bool, Any]]]:
+        """
+        :param event: The complex event that triggered the action.
+
+        :return: A tuple containing:
+                 whether all actions were successful; and
+                 a list of the output from each individual action.
+        """
+        success = True
+        data: List[Tuple[bool, Any]] = []
+
+        for action in self._actions:
+            output: Tuple[bool, Any] = action.execute(event)
+            data.append(output)
+
+            # If action was unsuccessful
+            if not output[0]:
+                success = False
+
+                if self._stop_on_fail:
+                    break
+
+        return success, data
---
+++ packages/bobocep/bobocep-1.2.2.tar.gz/bobocep-1.2.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="bobocep",
+    version="1.2.2",
+    author="r3w0p",
+    author_email="rr33ww00pp@gmail.com",
+    description="A fault-tolerant Complex Event Processing engine "
+                "designed for edge computing in Internet of Things systems.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/r3w0p/bobocep",
+    keywords=[
+        "complex event processing",
+        "distributed systems",
+        "edge computing",
+        "fault tolerance",
+        "internet of things",
+        "web of things"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: System :: Monitoring"
+    ],
+    install_requires=install_requires,
+    packages=find_packages(include=["bobocep", "bobocep.*"]),
+    test_suite="tests",
+    zip_safe=False
+)
