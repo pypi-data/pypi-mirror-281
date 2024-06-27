ba66f81dfaed7aebbe29966407a0098ac9a598b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 16:04:38 +0000

    Add package casualimpact/1.0.1/casualimpact-1.0.1.tar.gz

---
+++ packages/casualimpact/casualimpact-1.0.1.tar.gz/casualimpact-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='casualimpact', #Package_Name
+      version='1.0.1',
+      description='Python Package By Predator_97',
+      author='Predator_97',
+      license='MIT',
+      zip_safe=False
+     )
