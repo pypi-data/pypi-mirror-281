d7d74a29d444a4343d25dc1086e9bb639c600295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 03:04:26 +0000

    Add package funkmodel/0.1.0/funkmodel-0.1.0.tar.gz

---
+++ packages/funkmodel/funkmodel-0.1.0.tar.gz/funkmodel-0.1.0/funkmodel/__init__.py
@@ -0,0 +1,13 @@
+from .funkmodel import (
+    FunkModel, 
+    FatherModel, 
+    TimeModel, 
+    SimpleMathModel, 
+    PrecisionModel,
+    WalletModel,
+    StringModel,
+    
+    package_info,
+    
+    models, fields, transactions, Tortoise
+)
