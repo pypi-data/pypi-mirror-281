e5de74237d17e2e1636cfed2948f34464bbe1f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 17:04:37 +0000

    Add package patchwork-cli/0.0.24/patchwork_cli-0.0.24-py3-none-any.whl

---
+++ packages/patchwork-cli/patchwork_cli-0.0.24-py3-none-any.whl/patchwork/steps/GenerateEmbeddings/typed.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from typing_extensions import Any, Iterable, NotRequired, TypedDict
+
+
+class GenerateEmbeddingsInputs(TypedDict):
+    embedding_name: str
+    documents: Iterable[dict[str, Any]]
+    disable_cache: NotRequired[bool]
+
+
+class GenerateEmbeddingsOutputs(TypedDict):
+    pass
