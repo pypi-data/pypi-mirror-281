86a7e36a30b4e1cbaa20a26b71da6d6e9dc93c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 04:04:04 +0000

    Add package fasttrackpy/0.4.8/fasttrackpy-0.4.8-py3-none-any.whl

---
+++ packages/fasttrackpy/fasttrackpy-0.4.8-py3-none-any.whl/fasttrackpy/__init__.py
@@ -0,0 +1,25 @@
+from fasttrackpy.tracks import OneTrack, CandidateTracks
+from fasttrackpy.processors.smoothers import Smoother
+from fasttrackpy.processors.losses import Loss
+from fasttrackpy.processors.aggs import Agg
+from fasttrackpy.patterns.just_audio import process_audio_file, process_directory
+from fasttrackpy.patterns.audio_textgrid import process_audio_textgrid
+from fasttrackpy.patterns.corpus import process_corpus
+
+from importlib.metadata import version
+
+__version__ = version("fasttrackpy")
+
+__all__ = [
+    "process_audio_file",
+    "process_directory",
+    "OneTrack",
+    "CandidateTracks",
+    "Smoother",
+    "Loss",
+    "Agg",
+    "process_audio_file",
+    "process_audio_textgrid",
+    "process_corpus",
+    "__version__"
+]
