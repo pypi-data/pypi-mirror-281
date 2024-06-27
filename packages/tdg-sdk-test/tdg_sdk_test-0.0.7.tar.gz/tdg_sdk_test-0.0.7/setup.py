0121b62ae292bc3a604ee37761bf79c4cf9ef557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 06:04:39 +0000

    Add package tdg-sdk-test/0.0.7/tdg_sdk_test-0.0.7.tar.gz

---
+++ packages/tdg-sdk-test/tdg_sdk_test-0.0.7.tar.gz/tdg_sdk_test-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.7'
+DESCRIPTION = 'a basic test package'
+
+# Setting up
+setup(
+    name="tdg-sdk-test",
+    version=VERSION,
+    author="ali",
+    author_email="syedali@truthics.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['python']
+)
