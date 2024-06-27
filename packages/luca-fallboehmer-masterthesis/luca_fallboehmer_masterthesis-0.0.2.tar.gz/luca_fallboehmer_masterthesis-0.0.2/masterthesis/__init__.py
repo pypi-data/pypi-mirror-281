f8a71daa476f4002786aee8733b37027a6b4e411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-27 16:05:39 +0000

    Add package luca-fallboehmer-masterthesis/0.0.2/luca_fallboehmer_masterthesis-0.0.2.tar.gz

---
+++ packages/luca-fallboehmer-masterthesis/luca_fallboehmer_masterthesis-0.0.2.tar.gz/luca_fallboehmer_masterthesis-0.0.2/masterthesis/__init__.py
@@ -0,0 +1,4 @@
+"""Data generation and sensitivity analysis framework for the sterile neutrino search using neural networks"""
+
+__version__ = "0.0.2"
+__description__ = "Data generation and sensitivity analysis framework for the sterile neutrino search using neural networks"
