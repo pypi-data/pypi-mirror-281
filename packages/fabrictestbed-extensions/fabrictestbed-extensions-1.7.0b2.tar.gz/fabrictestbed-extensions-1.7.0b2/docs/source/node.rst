f74b575bfd1ead507161a910ec6f0d8ad11b8700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:05:22 +0000

    Add package fabrictestbed-extensions/1.7.0b2/fabrictestbed-extensions-1.7.0b2.tar.gz

---
+++ packages/fabrictestbed-extensions/fabrictestbed-extensions-1.7.0b2.tar.gz/fabrictestbed-extensions-1.7.0b2/docs/source/node.rst
@@ -0,0 +1,9 @@
+node
+====
+
+.. automodule:: fabrictestbed_extensions.fablib.node
+   :members:
+
+.. autoclass:: fabrictestbed_extensions.fablib.node.Node
+   :members:
+   :special-members: __str__
---
+++ packages/fabrictestbed-extensions/fabrictestbed-extensions-1.7.0b2.tar.gz/fabrictestbed-extensions-1.7.0b2/fabrictestbed_extensions/fablib/facility_port.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python3
+# MIT License
+#
+# Copyright (c) 2020 FABRIC Testbed
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Author: Paul Ruth (pruth@renci.org)
+
+"""
+This module contains methods to work with FABRIC `facility ports`_.
+
+.. _`facility ports`: https://learn.fabric-testbed.net/knowledge-base/glossary/#facility_port
+"""
+
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING, List, Union
+
+import jinja2
+from fabrictestbed.slice_editor import Capacities, Labels
+from tabulate import tabulate
+
+from fabrictestbed_extensions.fablib.interface import Interface
+
+if TYPE_CHECKING:
+    from fim.user.node import Node as FimNode
+
+    from fabrictestbed_extensions.fablib.slice import Slice
+
+
+class FacilityPort:
+    fim_interface = None
+    slice = None
+
+    def __init__(self, slice: Slice, fim_interface: FimNode):
+        """
+        :param slice: the fablib slice to have this node on
+        :type slice: Slice
+
+        :param fim_interface:
+        :type fim_interface: FimInterface
+        """
+        super().__init__()
+        self.fim_interface = fim_interface
+        self.slice = slice
+
+    def __str__(self):
+        """
+        Creates a tabulated string describing the properties of the
+        node.  Intended for printing node information.
+
+        :return: Tabulated string of node information
+        :rtype: String
+        """
+        table = [["name", self.get_name()]]
+
+        return tabulate(table)
+
+    def toJson(self):
+        return json.dumps(self.toDict(), indent=4)
+
+    def get_pretty_name_dict(self):
+        return {
+            "name": "Name",
+        }
+
+    def toDict(self, skip=[]):
+        return {"name": str(self.get_name())}
+
+    def get_template_context(self):
+        return self.get_slice().get_template_context(self)
+
+    def render_template(self, input_string):
+        environment = jinja2.Environment()
+        template = environment.from_string(input_string)
+        output_string = template.render(self.get_template_context())
+
+        return output_string
+
+    def show(
+        self, fields=None, output=None, quiet=False, colors=False, pretty_names=True
+    ):
+        data = self.toDict(pretty_names=True)
+
+        # fields = ["Name",
+        #         ]
+
+        if pretty_names:
+            pretty_names_dict = self.get_pretty_name_dict()
+        else:
+            pretty_names_dict = {}
+
+        table = self.get_fablib_manager().show_table(
+            data,
+            fields=fields,
+            title="Facility Port",
+            output=output,
+            quiet=quiet,
+            pretty_names_dict=pretty_names_dict,
+        )
+
+        return table
+
+    def get_fim_interface(self) -> FimNode:
+        return self.fim_interface
+
+    def get_model(self) -> str:
+        return "Facility_Port"
+
+    def get_name(self) -> str:
+        """
+        Gets the name of the FABRIC node.
+        :return: the name of the node
+        :rtype: String
+        """
+        return self.get_fim_interface().name
+
+    def get_site(self) -> str:
+        return self.fim_interface.site
+
+    @staticmethod
+    def new_facility_port(
+        slice: Slice = None,
+        name: str = None,
+        site: str = None,
+        vlan: Union[List, str] = None,
+        bandwidth: int = 10,
+        labels: Labels = None,
+        peer_labels: Labels = None,
+        capacities: Capacities = None,
+    ):
+        if capacities is None:
+            if not bandwidth:
+                bandwidth = 10
+            capacities = Capacities(bw=bandwidth)
+
+        interfaces = None
+
+        if vlan:
+            index = 1
+            interfaces = []
+            if isinstance(vlan, str):
+                vlan = [vlan]
+
+            for v in vlan:
+                iface_tuple = (
+                    f"iface-{index}",
+                    Labels(vlan=v),
+                    capacities,
+                )
+                interfaces.append(iface_tuple)
+
+        fim_facility_port = slice.get_fim_topology().add_facility(
+            name=name,
+            site=site,
+            capacities=capacities,
+            labels=labels,
+            peer_labels=peer_labels,
+            interfaces=interfaces,
+        )
+        return FacilityPort(slice, fim_facility_port)
+
+    @staticmethod
+    def get_facility_port(slice: Slice = None, facility_port: FimNode = None):
+        """
+
+        :param slice:
+        :param facility_port:
+        :return:
+        """
+        return FacilityPort(slice, facility_port)
+
+    def get_slice(self) -> Slice:
+        """
+        Gets the fablib slice associated with this node.
+
+        :return: the fablib slice on this node
+        :rtype: Slice
+        """
+        return self.slice
+
+    def get_interfaces(self) -> List[Interface]:
+        """
+        Gets a particular interface associated with a FABRIC node.
+
+        Accepts either the interface name or a network_name.  If a
+        network name is used this method will return the interface on
+        the node that is connected to the network specified.  If a
+        name and network_name are both used, the interface name will
+        take precedence.
+
+        :param name: interface name to search for
+        :type name: str
+
+        :param network_name: network name to search for
+
+        :type name: str
+
+        :raise Exception: if interface is not found
+
+        :return: an interface on the node
+        :rtype: Interface
+        """
+        ifaces = []
+        for fim_interface in self.get_fim_interface().interface_list:
+            ifaces.append(Interface(node=self, fim_interface=fim_interface))
+
+        return ifaces
