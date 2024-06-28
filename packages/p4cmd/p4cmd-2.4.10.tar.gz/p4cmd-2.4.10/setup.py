a0f2d5f98f0f557f9a7e404a4925718def0f8ad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:37 +0000

    Add package p4cmd/2.4.10/p4cmd-2.4.10.tar.gz

---
+++ packages/p4cmd/p4cmd-2.4.10.tar.gz/p4cmd-2.4.10/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+data_files_to_include = ["*.png"]
+
+setuptools.setup(
+    name="p4cmd",
+    version="2.4.10",
+    author="Niels Vaes",
+    license='MIT',
+    author_email="nielsvaes@gmail.com",
+    description="Simple P4 python module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nielsvaes/p4cmd",
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    package_data={
+        "": data_files_to_include,
+    },
+    classifiers=[
+        "Operating System :: OS Independent",
+    ]
+)
