8569317da486510ef0522997fb8f6e892510b690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 14:05:08 +0000

    Add package formant/1.174.26/formant-1.174.26.tar.gz

---
+++ packages/formant/formant-1.174.26.tar.gz/formant-1.174.26/formant/sdk/cloud/v2/src/resources/commands/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import CommandRequest, Command, CommandDeliveryEventMetadata, CommandDeliveryEventTags, CommandDeliveryEvent, FileInfo, ScopeFilter, CommandTags, CommandResponse, CommandTemplate, CommandTemplateTags, CommandDeliveryEventStreamType, CommandRequestListResponse, ScopeFilterTypesItem, CommandParameterMeta, CommandTemplateParameterMeta, CommandParameter, CommandProgress, CommandResponseTags, CommandTemplateListResponse, CommandResponseStreamType, CommandDeliveryEventType, CommandQuery, CommandStreamType, CommandListResponse
+from formant.sdk.cloud.v2.formant_admin_api_client.api.command import command_controller_post, command_controller_query
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.174.26.tar.gz/formant-1.174.26/formant/sdk/cloud/v2/src/resources/views/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import View, ViewLayoutType, PartialViewTags, LocationViewport, PointCloudViewConfiguration, Location, Filter, PartialViewLayoutType, ImageViewConfiguration, NumericViewConfiguration, Camera, LocationViewConfiguration, ViewConfiguration, LocalizationViewConfiguration, TransformTreeViewConfiguration, ViewTags, LocationViewConfigurationBasemap, PartialView, ViewListResponse, Vector3, ImageViewConfigurationMode, BitsetViewConfiguration, ViewConfigurationType
+from formant.sdk.cloud.v2.formant_admin_api_client.api.view import view_controller_get_one, view_controller_get_all, view_controller_patch
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
