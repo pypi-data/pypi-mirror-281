bd20b832c84c8ddc32e271ec73e79490d4422d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:50 +0000

    Add package constantsimportertest/1.0.0/constantsimportertest-1.0.0.tar.gz

---
+++ packages/constantsimportertest/constantsimportertest-1.0.0.tar.gz/constantsimportertest-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='constantsimportertest',
+
+    version='1.0.0',
+
+    author='J J',
+
+    author_email='jai.jalah@krakenflex.com',
+
+    description='A package for initialising the path to the Constants.py file in the repo, and then importing said constants',
+
+    packages=find_packages(),
+)
