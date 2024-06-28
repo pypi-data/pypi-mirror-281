0fc8ab4f20ed7fbf1237326bfd74045f3e82d6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:06:02 +0000

    Add package confhub/0.1.0.3a0/confhub-0.1.0.3a0-py3-none-any.whl

---
+++ packages/confhub/confhub-0.1.0.3a0-py3-none-any.whl/confhub/__main__.py
@@ -0,0 +1,13 @@
+from confhub.console import main
+from confhub.__meta__ import __version__
+from confhub.setup_logger import SetupLogger, LoggerReg
+
+SetupLogger(
+    name_registration=[
+        LoggerReg(name="confhub", level=LoggerReg.Level.DEBUG),
+    ],
+    developer_mode=True
+)
+
+if __name__ == '__main__':
+    main(prog='confhub', version=__version__)
