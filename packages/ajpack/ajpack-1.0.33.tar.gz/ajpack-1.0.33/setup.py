faf4ac2a77fa31e1a7df65150a3c750e1d7cd9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:06:10 +0000

    Add package ajpack/1.0.33/ajpack-1.0.33.tar.gz

---
+++ packages/ajpack/ajpack-1.0.33.tar.gz/ajpack-1.0.33/setup.py
@@ -0,0 +1,27 @@
+import json
+from setuptools import setup, find_packages # type:ignore
+
+with open("data.json", "r", encoding="utf-8") as f: version: str = json.load(f)["version"]
+
+setup(
+    name='ajpack',
+    version=version,
+    packages=find_packages(),
+    install_requires=[
+        "pyzipper",
+        "opencv-python",
+        "requests",
+        "Pillow",
+        "keyboard",
+        "pywin32",
+        "psutil",
+        "winshell",
+        "plyer",
+        "customtkinter"
+    ],
+    entry_points={
+        'console_scripts': [
+            # command lines
+        ],
+    },
+)
