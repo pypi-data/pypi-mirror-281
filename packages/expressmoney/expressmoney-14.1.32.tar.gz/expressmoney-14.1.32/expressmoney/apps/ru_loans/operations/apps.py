0ceebd36b01d04fb8f8ccf97fd039d4a004b17b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:19 +0000

    Add package expressmoney/14.1.32/expressmoney-14.1.32.tar.gz

---
+++ packages/expressmoney/expressmoney-14.1.32.tar.gz/expressmoney-14.1.32/expressmoney/apps/ru_loans/operations/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OperationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'expressmoney.apps.ru_loans.operations'
+    verbose_name = 'Операции по займам'
