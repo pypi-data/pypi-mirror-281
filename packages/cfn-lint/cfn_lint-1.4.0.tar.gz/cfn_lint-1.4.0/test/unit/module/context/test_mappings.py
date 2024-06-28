6268acc25c03223d935a5caabd6277afd29312c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:26 +0000

    Add package cfn-lint/1.4.0/cfn_lint-1.4.0.tar.gz

---
+++ packages/cfn-lint/cfn_lint-1.4.0.tar.gz/cfn_lint-1.4.0/test/unit/module/context/test_mappings.py
@@ -0,0 +1,36 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+import pytest
+
+from cfnlint.context.context import Map
+
+
+@pytest.mark.parametrize(
+    "name,get_key_1,get_key_2,expected",
+    [
+        ("Valid get", "A", "B", ["C"]),
+        ("Invalid first key", "B", "B", KeyError()),
+        ("Invalid second key", "A", "C", KeyError()),
+    ],
+)
+def test_mapping_value(name, get_key_1, get_key_2, expected):
+    mapping = Map({"A": {"B": "C"}})
+
+    if isinstance(expected, Exception):
+        with pytest.raises(type(expected)):
+            list(mapping.find_in_map(get_key_1, get_key_2))
+    else:
+        assert list(mapping.find_in_map(get_key_1, get_key_2)) == expected
+
+
+def test_transforms():
+    mapping = Map({"A": {"Fn::Transform": "C"}})
+
+    assert mapping.keys.get("A").is_transform is True
+
+    mapping = Map({"Fn::Transform": {"B": "C"}})
+
+    assert mapping.is_transform is True
