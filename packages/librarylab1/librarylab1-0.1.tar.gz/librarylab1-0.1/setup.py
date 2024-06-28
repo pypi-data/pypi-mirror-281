61a31e8b78b26b3e60900cde158b93ab56f576fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:06:15 +0000

    Add package librarylab1/0.1/librarylab1-0.1.tar.gz

---
+++ packages/librarylab1/librarylab1-0.1.tar.gz/librarylab1-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='librarylab1',
+    version='0.1',
+    description='A Python library for loading input values for automatas.',
+    author='Dinca Andrei',
+    packages=find_packages(),
+)
