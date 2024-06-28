85618f2cabb6f866f2f01a16f8cbf94092a0bdbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:06:00 +0000

    Add package dspawpy/1.5.3/dspawpy-1.5.3-py3-none-any.whl

---
+++ packages/dspawpy/dspawpy-1.5.3-py3-none-any.whl/dspawpy/__init__.py
@@ -0,0 +1,31 @@
+import importlib.metadata
+import os
+import sys
+
+from loguru import logger
+
+__version__ = importlib.metadata.version("dspawpy")
+assert sys.version_info >= (3, 8)
+
+logger.remove()
+level = os.getenv("DLL")  # dspawpy log level
+if level:  # default to simulate no log
+    logger.add(
+        sys.stderr,
+        level=level,
+        format="<green>{time:MM-DD HH:mm:ss:SSS}</green> | <level>{message}</level>",
+    )
+else:
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        format="<level>{message}</level>",
+    )
+
+logger.add(
+    ".dspawpy.log",
+    level="DEBUG",
+    rotation="1 day",
+    retention="1 week",
+    compression="zip",
+)
