aa405cbbee22011ceb31a983188964d786dc2d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:06:11 +0000

    Add package forgetnet/0.1.3/forgetnet-0.1.3.tar.gz

---
+++ packages/forgetnet/forgetnet-0.1.3.tar.gz/forgetnet-0.1.3/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='forgetnet',
+    version='0.1.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'torch',
+    ],
+    author='David Zagardo',
+    author_email='dave@greenwillowstudios.com',
+    description='A package for applying differential privacy to model weights',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dzagardo/forgetnet',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
