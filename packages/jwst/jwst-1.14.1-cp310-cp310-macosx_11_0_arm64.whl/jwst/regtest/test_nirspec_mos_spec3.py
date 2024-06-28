d0779a13eac12a2864ba01ed9f29b38b88d58378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:06:14 +0000

    Add package jwst/1.14.1/jwst-1.14.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/jwst/jwst-1.14.1-cp310-cp310-macosx_11_0_arm64.whl/jwst/regtest/test_nirspec_mos_spec3.py
@@ -0,0 +1,38 @@
+import pytest
+from astropy.io.fits.diff import FITSDiff
+
+from jwst.stpipe import Step
+
+
+@pytest.fixture(scope="module")
+def run_pipeline(rtdata_module):
+    """Run calwebb_spec3 on NIRSpec MOS data."""
+    rtdata = rtdata_module
+    rtdata.get_asn("nirspec/mos/jw00626-o030_20191210t193826_spec3_001_asn.json")
+
+    # Run the calwebb_spec3 pipeline on the association
+    args = ["calwebb_spec3", rtdata.input]
+    Step.from_cmdline(args)
+
+    return rtdata
+
+
+@pytest.mark.bigdata
+@pytest.mark.parametrize("suffix", ["cal", "crf", "s2d", "x1d"])
+@pytest.mark.parametrize("source_id", ["s00000", "s00227", "s00279", "s00443",
+                                       "s00482", "s02315"])
+def test_nirspec_mos_spec3(run_pipeline, suffix, source_id, fitsdiff_default_kwargs):
+    """Check results of calwebb_spec3"""
+    rtdata = run_pipeline
+
+    output = f"jw00626-o030_{source_id}_nirspec_f170lp-g235m_{suffix}.fits"
+    rtdata.output = output
+    rtdata.get_truth(f"truth/test_nirspec_mos_spec3/{output}")
+
+    # Adjust tolerance for machine precision with float32 drizzle code
+    if suffix == "s2d":
+        fitsdiff_default_kwargs["rtol"] = 1e-4
+        fitsdiff_default_kwargs["atol"] = 1e-5
+
+    diff = FITSDiff(rtdata.output, rtdata.truth, **fitsdiff_default_kwargs)
+    assert diff.identical, diff.report()
