872936e1a42e36a43e6be72313cb49a985224cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 07:05:01 +0000

    Add package ct-treedir/0.8.6/ct_treedir-0.8.6-py3-none-any.whl

---
+++ packages/ct-treedir/ct_treedir-0.8.6-py3-none-any.whl/cttreedir/printinfo.py
@@ -0,0 +1,21 @@
+import os
+import getpass
+import sys
+sys.path.append('/cttreedir/helpers')
+
+from helpers.info import override_info_tree
+
+def run_printinfo():
+    user_directory = input("Enter the directory path: ")
+
+    absolute_directory = os.path.abspath(user_directory)
+
+    if not getpass.getuser() == "root":
+        print("Administrator required. Please try sudo.")
+        return
+    
+    previous_versions_dir = os.path.join(absolute_directory, '.directory')
+    if not os.path.exists(previous_versions_dir):
+        os.makedirs(previous_versions_dir)
+
+    override_info_tree(main_src=absolute_directory, previous_versions_dir=previous_versions_dir)
