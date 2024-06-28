d359a1385093d8c9f7e88cc50fb0c44c8591190c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:36 +0000

    Add package sync_mailchimp_entra/0.3.4/sync_mailchimp_entra-0.3.4.tar.gz

---
+++ packages/sync_mailchimp_entra/sync_mailchimp_entra-0.3.4.tar.gz/sync_mailchimp_entra-0.3.4/src/sync_mailchimp_entra/utils/errors.py
@@ -0,0 +1,16 @@
+import json
+
+class Error:
+    title: str
+    status: int
+    detail: str
+    instance: str
+
+    def __init__(self, title: str, status: int, detail: str, instance: str):
+        self.title = title
+        self.status = status
+        self.detail = detail
+        self.instance = instance
+
+def deserial_error(error_json):
+    return Error(**json.loads(error_json))
