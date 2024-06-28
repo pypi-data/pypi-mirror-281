6cd5b110f4f90af9d7cb31501537ffc775b225db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 14:05:18 +0000

    Add package bags/0.3.0/bags-0.3.0.tar.gz

---
+++ packages/bags/bags-0.3.0.tar.gz/bags-0.3.0/bags/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.3.0'
+
+from .bags import DatasetWriter
+from .bags import DatasetReader
+from .bags import ShardedWriter
+from .bags import ShardedReader
+from .bags import Writer
+from .bags import Reader
+
+from .formats import encoders
+from .formats import decoders
