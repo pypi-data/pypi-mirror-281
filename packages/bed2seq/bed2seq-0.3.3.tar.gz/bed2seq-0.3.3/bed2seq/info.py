d04720bf567b74e70fdfc6dbcf16b9c7265200b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:05:52 +0000

    Add package bed2seq/0.3.3/bed2seq-0.3.3.tar.gz

---
+++ packages/bed2seq/bed2seq-0.3.3.tar.gz/bed2seq-0.3.3/bed2seq/info.py
@@ -0,0 +1,8 @@
+
+
+APPNAME   = "bed2seq"
+SHORTDESC = "From a BED file, return the sequences according to the genome supplied"
+LICENCE   = "GPL3"
+VERSION   = "0.3.3"
+AUTHOR    = "Benoit Guibert"
+AUTHOR_EMAIL = "benoit.guibert@free.fr"
