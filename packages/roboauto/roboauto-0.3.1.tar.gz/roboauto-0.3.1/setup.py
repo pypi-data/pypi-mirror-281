caad8ead7f225962f2b6f4fb08de2f0d24583087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:05:20 +0000

    Add package roboauto/0.3.1/roboauto-0.3.1.tar.gz

---
+++ packages/roboauto/roboauto-0.3.1.tar.gz/roboauto-0.3.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""setup.py"""
+
+import setuptools
+
+
+setuptools.setup(
+    name="roboauto",
+    version="0.3.1",
+    description="robosats cli",
+    license="ISC",
+    author="jerryfletcher21",
+    author_email="jerryfletcher@cock.email",
+    packages=["roboauto"],
+    scripts=["bin/roboauto"]
+)
