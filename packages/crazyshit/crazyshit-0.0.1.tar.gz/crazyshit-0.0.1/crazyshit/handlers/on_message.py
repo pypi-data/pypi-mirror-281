d61907be9f73b597d6069d32f09e277dfffcc72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:05:35 +0000

    Add package crazyshit/0.0.1/crazyshit-0.0.1.tar.gz

---
+++ packages/crazyshit/crazyshit-0.0.1.tar.gz/crazyshit-0.0.1/crazyshit/handlers/on_message.py
@@ -0,0 +1,13 @@
+from typing import Callable
+import crazyshit
+
+
+class OnMessage:
+    def on_message(self: "crazyshit.Client", func: Callable = None):
+        """An decorator add new messages handler."""
+
+        def decorator(func_: Callable) -> Callable:
+            self.add_message_handler(func_, func)
+            return func_
+
+        return decorator
