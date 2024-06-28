07ebc37dcc838f7749febed0906114afebeaa9e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:05:37 +0000

    Add package iSORTpy/1.0.1/iSORTpy-1.0.1.tar.gz

---
+++ packages/iSORTpy/iSORTpy-1.0.1.tar.gz/iSORTpy-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="iSORTpy",
+    version="1.0.1",
+    description='Transfer learning of multicellular organization via single-cell and spatial transcriptomics',
+    author='Yecheng Tan',
+    author_email='yctan21@m.fudan.edu.cn',
+    url='https://github.com/xiaojierzi/iSORT',
+    packages=['iSORT'],
+    install_requires=[],
+    python_requires='>=3.8',
+    license='GNU General Public License v3.0',
+)
+
+
