a23650d3ae5c932a8367d5e2cebc87611bea8068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:05:34 +0000

    Add package myomappy/0.1.0/myomappy-0.1.0.tar.gz

---
+++ packages/myomappy/myomappy-0.1.0.tar.gz/myomappy-0.1.0/README.md
@@ -0,0 +1,5 @@
+# MyoMapPy
+A python library for diffusion tensor-based tractography of the heart.
+
+# License
+MyoMapPy is licensed under the terms of the GNU AGPLv3 license.
---
+++ packages/myomappy/myomappy-0.1.0.tar.gz/myomappy-0.1.0/myomappy/rungekutta/runge_kutta_4.py
@@ -0,0 +1,9 @@
+"""
+4th order Runge Kutta Tracking
+"""
+
+import runge_kutta_4_calcs
+
+def rk4Track():
+    print("4th order runge kutta track")
+    print(runge_kutta_4_calcs.foo())
