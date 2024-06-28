b06b1d5dd5a9f258368955d024547e8882943b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:05:54 +0000

    Add package convertertools/0.4.0/convertertools-0.4.0-cp311-cp311-win_amd64.whl

---
+++ packages/convertertools/convertertools-0.4.0-cp311-cp311-win_amd64.whl/convertertools/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.4.0"
+
+from .impl import del_dict_set, del_dict_tuple, pop_dict_set, pop_dict_tuple
+
+__all__ = ["del_dict_set", "del_dict_tuple", "pop_dict_set", "pop_dict_tuple"]
