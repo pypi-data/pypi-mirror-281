d387fff81e43adf65b3532f077f716ad830061f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:19 +0000

    Add package NovitecCameraAPIWinPy/0.0.3/NovitecCameraAPIWinPy-0.0.3.tar.gz

---
+++ packages/NovitecCameraAPIWinPy/NovitecCameraAPIWinPy-0.0.3.tar.gz/NovitecCameraAPIWinPy-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='NovitecCameraAPIWinPy',
+    version='0.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Python wrapper for Novitec Camera API (Windows only)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='kkomadev',
+    author_email='kkomadev@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires='>=3.10.11',
+)
