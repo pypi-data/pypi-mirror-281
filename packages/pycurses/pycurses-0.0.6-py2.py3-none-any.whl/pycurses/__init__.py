df3bc456bb9331104f9ee275741ac67f9ec2de19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 14:05:17 +0000

    Add package pycurses/0.0.6/pycurses-0.0.6-py2.py3-none-any.whl

---
+++ packages/pycurses/pycurses-0.0.6-py2.py3-none-any.whl/pycurses/__init__.py
@@ -0,0 +1,15 @@
+""" Python package for making terminal UIs """
+
+from . import colors
+from . import list_view
+from . import windows
+from . import layout
+from . import mainwindow
+from . import popup
+
+
+__version__ = "0.0.6"
+
+
+
+
