0ade58c56a7d1df8c4106441c6147c5f654e6684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:05:17 +0000

    Add package jupyfuncs/1.0.2.dev39/jupyfuncs-1.0.2.dev39.tar.gz

---
+++ packages/jupyfuncs/jupyfuncs-1.0.2.dev39.tar.gz/jupyfuncs-1.0.2.dev39/jupyfuncs/pbar.py
@@ -0,0 +1,36 @@
+import sys
+from IPython.core.display import HTML
+
+__all__ = [
+    'in_jupyter',
+    'tqdm',
+    'trange',
+    'tnrange',
+    'NO_WHITE',
+]
+
+
+NO_WHITE = HTML("""
+    <style>
+    .jp-OutputArea-prompt:empty {
+    padding: 0;
+    border: 0;
+    }
+    </style>
+    """)
+
+
+def in_jupyter():
+
+    which = True if 'ipykernel_launcher.py' in sys.argv[0] else False
+    return which
+
+
+if in_jupyter():
+    from tqdm.notebook import tqdm
+    from tqdm.notebook import trange
+    from tqdm.notebook import tnrange
+else:
+    from tqdm import tqdm
+    from tqdm import trange
+    from tqdm import tnrange
