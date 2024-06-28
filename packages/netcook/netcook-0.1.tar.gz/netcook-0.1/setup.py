0b59f9f1f628c04f37bdea27f224ac976dbcf40d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:41 +0000

    Add package netcook/0.1/netcook-0.1.tar.gz

---
+++ packages/netcook/netcook-0.1.tar.gz/netcook-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='netcook',
+    version='0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='A simple package to store URLs for Netflix',
+    author='M Dicky Alfansyah',
+    author_email='dickyalfansyaah@gmail.com',
+    url='https://github.com/dicky-alfansyah',
+    keywords=['netflix'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
