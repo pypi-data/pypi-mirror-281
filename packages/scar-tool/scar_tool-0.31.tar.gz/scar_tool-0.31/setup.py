698114fa703bf8057130115a2f3879c19eba25ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:41 +0000

    Add package scar-tool/0.31/scar_tool-0.31.tar.gz

---
+++ packages/scar-tool/scar_tool-0.31.tar.gz/scar_tool-0.31/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scar-tool",
+    version="0.31",
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'transformers',
+        'huggingface_hub',
+        'scikit-learn',
+        'tqdm',
+        'nltk',
+        'datasketch'
+    ],
+    author="Zhuang Li",
+    author_email="zhuang.li1@monash.edu",
+    description="SCAR: An AI-powered tool for ranking and filtering instruction-answer pairs based on writing quality and style consistency",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhuang-li/SCAR",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
