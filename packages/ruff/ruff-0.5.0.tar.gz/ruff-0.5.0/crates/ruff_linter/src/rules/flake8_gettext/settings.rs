873c4cd4bad3e87a12f5b98b3cfe8e32feb95266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:05:20 +0000

    Add package ruff/0.5.0/ruff-0.5.0.tar.gz

---
+++ packages/ruff/ruff-0.5.0.tar.gz/ruff-0.5.0/crates/ruff_linter/src/rules/flake8_gettext/settings.rs
@@ -0,0 +1,37 @@
+use crate::display_settings;
+use ruff_macros::CacheKey;
+use std::fmt::{Display, Formatter};
+
+#[derive(Debug, Clone, CacheKey)]
+pub struct Settings {
+    pub functions_names: Vec<String>,
+}
+
+pub fn default_func_names() -> Vec<String> {
+    vec![
+        "_".to_string(),
+        "gettext".to_string(),
+        "ngettext".to_string(),
+    ]
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+            functions_names: default_func_names(),
+        }
+    }
+}
+
+impl Display for Settings {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        display_settings! {
+            formatter = f,
+            namespace = "linter.flake8_gettext",
+            fields = [
+                self.functions_names | array
+            ]
+        }
+        Ok(())
+    }
+}
