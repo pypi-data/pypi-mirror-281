ea75716dc4deeafbef3823b2aac329a3b8dfe5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:28 +0000

    Add package ffmpeg-media-type/1.0.5/ffmpeg_media_type-1.0.5-py3-none-any.whl

---
+++ packages/ffmpeg-media-type/ffmpeg_media_type-1.0.5-py3-none-any.whl/ffmpeg_media_type/utils/shell.py
@@ -0,0 +1,48 @@
+import subprocess
+from shlex import join
+from tempfile import NamedTemporaryFile
+
+from ..exceptions import FFmpegMediaTypeError
+
+
+def call(cmds: list[str]) -> str:
+    """
+    Run a command and return the stdout.
+
+    Args:
+        cmds: List of command and arguments.
+
+    Raises:
+        FFmpegMediaTypeError: If the command fails.
+
+    Returns:
+        stdout of the command.
+    """
+    try:
+        r = subprocess.run(cmds, stdout=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        raise FFmpegMediaTypeError(f"command failed: {join(e.cmd)}")
+
+    return r.stdout.decode("utf-8")
+
+
+def create_temp_filename(suffix: str) -> str:
+    """
+    Create a temporary file path.
+
+    Returns:
+        the path to the temporary file
+    """
+
+    if not suffix.startswith("."):
+        suffix = f".{suffix}"
+
+    # Create a NamedTemporaryFile
+    temp = NamedTemporaryFile(delete=True, suffix=suffix)
+    temp_path = temp.name
+
+    # Close and delete the temporary file (it won't actually create it)
+    temp.close()
+
+    # At this point, temp_path is a path to a non-existent file
+    return temp_path
