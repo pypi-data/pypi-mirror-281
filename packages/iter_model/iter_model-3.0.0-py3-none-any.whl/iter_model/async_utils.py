233ccf6c5cfbd33e3403e00589c78afb91352b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:05:15 +0000

    Add package iter_model/3.0.0/iter_model-3.0.0-py3-none-any.whl

---
+++ packages/iter_model/iter_model-3.0.0-py3-none-any.whl/iter_model/async_utils.py
@@ -0,0 +1,19 @@
+import asyncio
+from functools import wraps
+from typing import TypeVar, ParamSpec, Callable, Awaitable, cast
+
+_T = TypeVar('_T')
+_R = TypeVar('_R')
+_P = ParamSpec('_P')
+
+
+def asyncify(func: Callable[_P, _R | Awaitable[_R]]) -> Callable[_P, Awaitable[_R]]:
+
+    if asyncio.iscoroutinefunction(func):
+        return cast(Callable[_P, Awaitable[_R]], func)
+
+    @wraps(func)
+    async def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _R:
+        return cast(_R, func(*args, **kwargs))
+
+    return cast(Callable[_P, Awaitable[_R]], wrapper)
