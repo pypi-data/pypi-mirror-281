61d2a87e4e4da3278d1f10fa2a8f0991c6d0241c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:06:12 +0000

    Add package sloyka/0.1.7/sloyka-0.1.7.tar.gz

---
+++ packages/sloyka/sloyka-0.1.7.tar.gz/sloyka-0.1.7/sloyka/__init__.py
@@ -0,0 +1,34 @@
+from loguru import logger
+import sys
+
+from .src import (
+    EventDetection,
+    Geocoder,
+    TextClassifiers,
+    GeoDataGetter,
+    Semgraph,
+    VKParser,
+    City_services,
+    AreaMatcher,
+    EmotionRecognizer,
+)
+
+__all__ = [
+    "EventDetection",
+    "TextClassifiers",
+    "Geocoder",
+    "GeoDataGetter",
+    "Semgraph",
+    "VKParser",
+    "City_services",
+    "AreaMatcher",
+    "EmotionRecognizer",
+]
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="<green>{time:MM-DD HH:mm}</green> | <level>{level: <8}</level> | <cyan>{message}</cyan>",
+    level="INFO",
+    colorize=True,
+)
