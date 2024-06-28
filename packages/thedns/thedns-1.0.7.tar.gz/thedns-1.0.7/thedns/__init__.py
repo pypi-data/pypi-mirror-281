a22e43fdda470fdac11271b63549875e6cdc0de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:48 +0000

    Add package thedns/1.0.7/thedns-1.0.7.tar.gz

---
+++ packages/thedns/thedns-1.0.7.tar.gz/thedns-1.0.7/thedns/__init__.py
@@ -0,0 +1,33 @@
+import requests
+from .auth import _token
+from .auth import _credentials
+from .auth import _request_headers
+from .network import _get_private_ip
+from .network import _get_rr_string
+from .wanwang import _list_rrs
+from .wanwang import _build_payload
+from .wanwang import _update_rr
+
+class DDNS(object):
+    access_key = None
+    secret_key = None
+    email = None
+    public_ip = None
+    private_ip = None
+    token = None
+    request_headers = None
+    def __init__(self):
+        self.access_key, self.secret_key, self.email = _credentials()
+        self.public_ip = requests.get('https://devnull.cn/ip').json()['origin']
+        self.private_ip = _get_private_ip()
+        self.token = _token(self.email, self.access_key, self.secret_key)
+        self.request_headers = _request_headers(token=self.token)
+        self.rr_string_public = _get_rr_string() + '.public'
+        self.rr_string_private = _get_rr_string() + '.private'
+    def update(self):
+        _update_rr(request_headers=self.request_headers, rr_record=self.rr_string_private,rr_value=self.private_ip)
+        _update_rr(request_headers=self.request_headers, rr_record=self.rr_string_public,rr_value=self.public_ip)
+        return {
+            self.rr_string_public + '.thedns.cn': self.public_ip,
+            self.rr_string_private + '.thedns.cn': self.private_ip
+        }
