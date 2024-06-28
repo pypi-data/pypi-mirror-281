c3303d4a6ba0d3562e3badfa807bcbc600d11c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:05:32 +0000

    Add package ert/10.2.0b7/ert-10.2.0b7-cp312-cp312-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-10.2.0b7-cp312-cp312-macosx_10_15_x86_64.whl/ert/config/__init__.py
@@ -0,0 +1,94 @@
+from .analysis_config import AnalysisConfig
+from .analysis_module import AnalysisModule, ESSettings, IESSettings
+from .ensemble_config import EnsembleConfig
+from .ert_config import ErtConfig
+from .ert_plugin import CancelPluginException, ErtPlugin
+from .ert_script import ErtScript
+from .ext_param_config import ExtParamConfig
+from .external_ert_script import ExternalErtScript
+from .field import Field, field_transform
+from .forward_model_step import (
+    ForwardModelStep,
+    ForwardModelStepDocumentation,
+    ForwardModelStepJSON,
+    ForwardModelStepPlugin,
+    ForwardModelStepValidationError,
+)
+from .gen_data_config import GenDataConfig
+from .gen_kw_config import GenKwConfig, PriorDict, TransformFunction
+from .lint_file import lint_file
+from .model_config import ModelConfig
+from .parameter_config import ParameterConfig
+from .parsing import (
+    AnalysisMode,
+    ConfigValidationError,
+    ConfigWarning,
+    ErrorInfo,
+    HookRuntime,
+    QueueSystem,
+    WarningInfo,
+)
+from .queue_config import (
+    QueueConfig,
+    queue_bool_options,
+    queue_memory_options,
+    queue_positive_int_options,
+    queue_positive_number_options,
+    queue_string_options,
+)
+from .response_config import ResponseConfig
+from .response_properties import ResponseTypes
+from .summary_config import SummaryConfig
+from .summary_observation import SummaryObservation
+from .surface_config import SurfaceConfig
+from .workflow import Workflow
+from .workflow_job import WorkflowJob
+
+__all__ = [
+    "AnalysisConfig",
+    "AnalysisMode",
+    "AnalysisModule",
+    "CancelPluginException",
+    "ConfigValidationError",
+    "ConfigValidationError",
+    "ConfigWarning",
+    "ErrorInfo",
+    "WarningInfo",
+    "EnsembleConfig",
+    "ErtConfig",
+    "ErtPlugin",
+    "ErtScript",
+    "ExtParamConfig",
+    "ExternalErtScript",
+    "Field",
+    "ForwardModelStep",
+    "ForwardModelStepPlugin",
+    "ForwardModelStepJSON",
+    "ForwardModelStepValidationError",
+    "ForwardModelStepDocumentation",
+    "GenDataConfig",
+    "GenKwConfig",
+    "TransformFunction",
+    "HookRuntime",
+    "lint_file",
+    "ModelConfig",
+    "ParameterConfig",
+    "PriorDict",
+    "QueueConfig",
+    "QueueSystem",
+    "ResponseConfig",
+    "ResponseTypes",
+    "SummaryConfig",
+    "SummaryObservation",
+    "SurfaceConfig",
+    "Workflow",
+    "WorkflowJob",
+    "ESSettings",
+    "IESSettings",
+    "field_transform",
+    "queue_bool_options",
+    "queue_memory_options",
+    "queue_positive_int_options",
+    "queue_positive_number_options",
+    "queue_string_options",
+]
---
+++ packages/ert/ert-10.2.0b7-cp312-cp312-macosx_10_15_x86_64.whl/ert/storage/migration/to4.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING
+
+from ert.storage.local_storage import local_storage_get_ert_config
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+info = "Introducing observations and removing template_file_path"
+
+
+def migrate(path: Path) -> None:
+    ert_config = local_storage_get_ert_config()
+    observations = ert_config.observations.datasets
+
+    for experiment in path.glob("experiments/*"):
+        if observations:
+            output_path = experiment / "observations"
+            output_path.mkdir(parents=True, exist_ok=True)
+
+            for response_type, multi_obs_ds in observations.items():
+                for obs_name, single_obs_ds in multi_obs_ds.groupby(
+                    "obs_name", squeeze=True
+                ):
+                    single_obs_ds = single_obs_ds.dropna("name", how="all")
+                    if response_type == "summary":
+                        single_obs_ds.drop("obs_name").to_netcdf(
+                            output_path / obs_name, engine="scipy"
+                        )
+                    else:
+                        response_name = single_obs_ds["name"].data.tolist()[0]
+                        squeezed = single_obs_ds.drop(["obs_name", "name"]).squeeze(
+                            "name", drop=True
+                        )
+                        squeezed.attrs["response"] = response_name
+                        squeezed.to_netcdf(output_path / obs_name, engine="scipy")
+
+        with open(experiment / "parameter.json", encoding="utf-8") as fin:
+            parameters_json = json.load(fin)
+        with open(experiment / "parameter.json", "w", encoding="utf-8") as fout:
+            fout.write(json.dumps(parameters_json, indent=3))
