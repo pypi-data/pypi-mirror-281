69710cda570c7b830c3d90c03750ef9294cf5aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 14:05:17 +0000

    Add package sec-parsers/0.420/sec_parsers-0.420.tar.gz

---
+++ packages/sec-parsers/sec_parsers-0.420.tar.gz/sec_parsers-0.420/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sec_parsers",
+    author="John Friedman",
+    version="0.420",
+    description = "A package to parse SEC filings",
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'pandas'   
+    ],
+)
