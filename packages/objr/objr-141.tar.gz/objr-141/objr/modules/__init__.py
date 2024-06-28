c02c00de268dcd2f89de155bf1a179aa7e78cae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:28 +0000

    Add package objr/141/objr-141.tar.gz

---
+++ packages/objr/objr-141.tar.gz/objr-141/objr/modules/__init__.py
@@ -0,0 +1,23 @@
+# This file is placed in the Public Domain.
+
+
+"modules"
+
+
+from . import cmd, err, fnd, irc, log, mod, opm, rss, tdo, thr, tmr, upt, ver
+
+
+def __dir__():
+    return (
+        'cmd',
+        'err',
+        'fnd',
+        'irc',
+        'mod',
+        'log',
+        'opm',
+        'rss',
+        'tdo',
+        'thr',
+        'tmr'
+    )
