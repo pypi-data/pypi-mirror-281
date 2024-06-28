fbd74e928c7fd06c61818ccd564eb4fa0dcdd1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:06:11 +0000

    Add package convertertools/0.2.2/convertertools-0.2.2-cp312-cp312-win_amd64.whl

---
+++ packages/convertertools/convertertools-0.2.2-cp312-cp312-win_amd64.whl/convertertools/impl.py
@@ -0,0 +1,31 @@
+"""impl.py."""
+
+from __future__ import annotations
+
+from typing import Any
+
+
+def del_dict_tuple(d: dict[Any, Any], keys: tuple[Any, ...]) -> None:
+    """Delete keys from a dictionary from a tuple."""
+    for key in keys:
+        del d[key]
+
+
+def del_dict_set(d: dict[Any, Any], keys: set[Any]) -> None:
+    """Delete keys from a dictionary from a set."""
+    for key in keys:
+        del d[key]
+
+
+def pop_dict_tuple(d: dict[Any, Any], keys: tuple[Any, ...]) -> None:
+    """Pop keys from a dictionary from a tuple if present."""
+    for key in keys:
+        if key in d:
+            del d[key]
+
+
+def pop_dict_set(d: dict[Any, Any], keys: set[Any]) -> None:
+    """Pop keys from a dictionary from a set if present."""
+    for key in keys:
+        if key in d:
+            del d[key]
