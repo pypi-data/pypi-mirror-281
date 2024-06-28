23d1ee832aab10c2a18f1f90be4bf02f2343b3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 05:05:47 +0000

    Add package singler/0.3.0/singler-0.3.0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/singler/singler-0.3.0-cp311-cp311-macosx_11_0_arm64.whl/singler/annotate_single.py
@@ -0,0 +1,172 @@
+import warnings
+from typing import Any, Optional, Sequence, Union
+
+from biocframe import BiocFrame
+from summarizedexperiment import SummarizedExperiment
+
+from .build_single_reference import build_single_reference
+from .classify_single_reference import classify_single_reference
+
+
+def _resolve_reference(ref_data, ref_labels, ref_features, build_args):
+    if isinstance(ref_data, SummarizedExperiment) or issubclass(type(ref_data), SummarizedExperiment):
+        if ref_features is None:
+            ref_features = ref_data.get_row_names()
+        elif isinstance(ref_features, str):
+            ref_features = ref_data.get_row_data().column(ref_features)
+
+        ref_features = list(ref_features)
+
+        if ref_labels is None:
+            ref_labels = ref_data.get_column_names()
+        elif isinstance(ref_labels, str):
+            ref_labels = ref_data.get_column_data().column(ref_labels)
+
+        ref_labels = list(ref_labels)
+
+        try:
+            _default_asy = "logcounts"
+            if "assay_type" in build_args:
+                _default_asy = build_args["assay_type"]
+
+            ref_data = ref_data.assay(_default_asy)
+        except Exception as _:
+            raise ValueError(f"Reference dataset must contain log-normalized count ('{_default_asy}') assay.")
+
+    if ref_labels is None:
+        raise ValueError("'ref_labels' cannot be `None`.")
+
+    if ref_features is None:
+        raise ValueError("'ref_features' cannot be `None`.")
+
+    return ref_data, ref_labels, ref_features
+
+
+def annotate_single(
+    test_data: Any,
+    ref_data: Any,
+    ref_labels: Optional[Union[Sequence, str]],
+    test_features: Optional[Union[Sequence, str]] = None,
+    ref_features: Optional[Union[Sequence, str]] = None,
+    build_args: dict = {},
+    classify_args: dict = {},
+    num_threads: int = 1,
+) -> BiocFrame:
+    """Annotate a single-cell expression dataset based on the correlation
+    of each cell to profiles in a labelled reference.
+
+    Args:
+        test_data:
+            A matrix-like object representing the test dataset, where rows are
+            features and columns are samples (usually cells). Entries should be expression
+            values; only the ranking within each column will be used.
+
+            Alternatively, a
+            :py:class:`~summarizedexperiment.SummarizedExperiment.SummarizedExperiment`
+            containing such a matrix in one of its assays. Non-default assay
+            types can be specified in ``classify_args``.
+
+        test_features:
+            Sequence of length equal to the number of rows in
+            ``test_data``, containing the feature identifier for each row.
+
+            Alternatively, if ``test_data`` is a ``SummarizedExperiment``, ``test_features``
+            may be a string speciying the column name in `row_data` that contains the
+            features. It can also be set to `None`, to use the `row_names` of
+            the experiment as features.
+
+        ref_data:
+            A matrix-like object representing the reference dataset, where rows
+            are features and columns are samples. Entries should be expression values,
+            usually log-transformed (see comments for the ``ref`` argument in
+            :py:meth:`~singler.build_single_reference.build_single_reference`).
+
+            Alternatively, a
+            :py:class:`~summarizedexperiment.SummarizedExperiment.SummarizedExperiment`
+            containing such a matrix in one of its assays. Non-default assay
+            types can be specified in ``classify_args``.
+
+        ref_labels:
+            If ``ref_data`` is a matrix-like object, ``ref_labels`` should be
+            a sequence of length equal to the number of columns of ``ref_data``,
+            containing the label associated with each column.
+
+            Alternatively, if ``ref_data`` is a ``SummarizedExperiment``, 
+            ``ref_labels`` may be a string specifying the label type to use,
+            e.g., "main", "fine", "ont". It can also be set to `None`, to use 
+            the `row_names` of the experiment as features.
+
+        ref_features:
+            If ``ref_data`` is a matrix-like object, ``ref_features`` should be
+            a sequence of length equal to the number of rows of ``ref_data``,
+            containing the feature identifier associated with each row.
+
+            Alternatively, if ``ref_data`` is a ``SummarizedExperiment``, 
+            ``ref_features`` may be a string speciying the column name in `column_data`
+            that contains the features. It can also be set to `None`, to use the 
+            `row_names` of the experiment as features.
+
+        build_args:
+            Further arguments to pass to
+            :py:meth:`~singler.build_single_reference.build_single_reference`.
+
+        classify_args:
+            Further arguments to pass to
+            :py:meth:`~singler.classify_single_reference.classify_single_reference`.
+
+        num_threads:
+            Number of threads to use for the various steps.
+
+    Returns:
+        A data frame containing the labelling results, see
+        :py:meth:`~singler.classify_single_reference.classify_single_reference`
+        for details. The metadata also contains a ``markers`` dictionary,
+        specifying the markers that were used for each pairwise comparison
+        between labels; and a list of ``unique_markers`` across all labels.
+    """
+
+    if isinstance(test_data, SummarizedExperiment):
+        if test_features is None:
+            test_features = test_data.get_row_names()
+        elif isinstance(test_features, str):
+            test_features = test_data.get_row_data().column(test_features)
+
+    if test_features is None:
+        raise ValueError("'test_features' cannot be `None`.")
+
+    test_features_set = set(test_features)
+    if len(test_features_set) != len(test_features):
+        warnings.warn("'test_features' is not unique, subsetting test matrix...", UserWarning)
+        _idxs = [test_features.index(x) for x in test_features_set]
+        print("modifying test data")
+        test_data = test_data[_idxs,]
+
+    ref_data, ref_labels, ref_features = _resolve_reference(
+        ref_data=ref_data,
+        ref_labels=ref_labels,
+        ref_features=ref_features,
+        build_args=build_args,
+    )
+
+    built = build_single_reference(
+        ref_data=ref_data,
+        ref_labels=ref_labels,
+        ref_features=ref_features,
+        restrict_to=test_features_set,
+        **build_args,
+        num_threads=num_threads,
+    )
+
+    output = classify_single_reference(
+        test_data,
+        test_features=test_features,
+        ref_prebuilt=built,
+        **classify_args,
+        num_threads=num_threads,
+    )
+
+    output.metadata = {
+        "markers": built.markers,
+        "unique_markers": built.marker_subset(),
+    }
+    return output
---
+++ packages/singler/singler-0.3.0-cp311-cp311-macosx_11_0_arm64.whl/singler/build_integrated_references.py
@@ -0,0 +1,171 @@
+from typing import Sequence, Optional, Union
+from numpy import array, ndarray, int32, uintp
+
+import biocutils as ut
+
+from .build_single_reference import SinglePrebuiltReference
+from . import _cpphelpers as lib
+from ._utils import _stable_union, _factorize, _clean_matrix
+
+
+class IntegratedReferences:
+    """Object containing integrated references, typically constructed by
+    :py:meth:`~singler.build_integrated_references.build_integrated_references`."""
+
+    def __init__(self, ptr, ref_names, ref_labels, test_features):
+        self._ptr = ptr
+        self._names = ref_names
+        self._labels = ref_labels
+        self._features = test_features
+
+    def __del__(self):
+        lib.free_integrated_references(self._ptr)
+
+    @property
+    def reference_names(self) -> Union[Sequence[str], None]:
+        """Sequence containing the names of the references. Alternatively
+        None, if no names were supplied."""
+        return self._names
+
+    @property
+    def reference_labels(self) -> list:
+        """List of lists containing the names of the labels for each reference.
+
+        Each entry corresponds to a reference in :py:attr:`~reference_names`,
+        if ``reference_names`` is not None.
+        """
+        return self._labels
+
+    @property
+    def test_features(self) -> list[str]:
+        """Sequence containing the names of the test features."""
+        return self._features
+
+
+def build_integrated_references(
+    test_features: Sequence,
+    ref_data_list: dict,
+    ref_labels_list: list[Sequence],
+    ref_features_list: list[Sequence],
+    ref_prebuilt_list: list[SinglePrebuiltReference],
+    ref_names: Optional[Sequence[str]] = None,
+    assay_type: Union[str, int] = "logcounts",
+    check_missing: bool = True,
+    num_threads: int = 1,
+) -> IntegratedReferences:
+    """Build a set of integrated references for classification of a test dataset.
+
+    Arguments:
+        test_features:
+            Sequence of features for the test dataset.
+
+        ref_data_list:
+            List of reference datasets, where each entry is equivalent to ``ref_data`` in
+            :py:meth:`~singler.build_single_reference.build_single_reference`.
+
+        ref_labels_list:
+            List of reference labels, where each entry is equivalent to ``ref_labels`` in
+            :py:meth:`~singler.build_single_reference.build_single_reference`.
+
+        ref_features_list:
+            List of reference features, where each entry is equivalent to ``ref_features`` in
+            :py:meth:`~singler.build_single_reference.build_single_reference`.
+
+        ref_prebuilt_list:
+            List of prebuilt references, typically created by
+            calling :py:meth:`~singler.build_single_reference.build_single_reference` on the corresponding
+            elements of ``ref_data_list``, ``ref_labels_list`` and ``ref_features_list``.
+
+        ref_names:
+            Sequence of names for the references.
+            If None, these are automatically generated.
+
+        assasy_type:
+            Assay containing the expression matrix for any entry of ``ref_data_list`` that is a
+            :py:class:`~summarizedexperiment.SummarizedExperiment.SummarizedExperiment`.
+
+        check_missing:
+            Whether to check for and remove rows with missing (NaN) values
+            from each entry of ``ref_data_list``.
+
+        num_threads:
+            Number of threads.
+
+    Returns:
+        Integrated references for classification with
+        :py:meth:`~singler.classify_integrated_references.classify_integrated_references`.
+    """
+    nrefs = len(ref_data_list)
+    if nrefs != len(ref_features_list):
+        raise ValueError(
+            "'ref_features_list' and 'ref_data_list' should have the same length"
+        )
+
+    universe = _stable_union(test_features, *ref_features_list)
+    original_test_features = test_features
+    test_features = array(ut.match(test_features, universe), dtype=int32)
+
+    converted_ref_data = []
+    ref_data_ptrs = ndarray(nrefs, dtype=uintp)
+    converted_feature_data = []
+    ref_features_ptrs = ndarray(nrefs, dtype=uintp)
+
+    for i, x in enumerate(ref_data_list):
+        curptr, curfeatures = _clean_matrix(
+            x,
+            ref_features_list[i],
+            assay_type=assay_type,
+            check_missing=check_missing,
+            num_threads=num_threads,
+        )
+        converted_ref_data.append(curptr)
+        ref_data_ptrs[i] = curptr.ptr
+
+        ind = array(ut.match(curfeatures, universe), dtype=int32)
+        converted_feature_data.append(ind)
+        ref_features_ptrs[i] = ind.ctypes.data
+
+    if nrefs != len(ref_labels_list):
+        raise ValueError(
+            "'ref_labels_list' and 'ref_data_list' should have the same length"
+        )
+    converted_label_levels = []
+    converted_label_indices = []
+    ref_labels_ptrs = ndarray(nrefs, dtype=uintp)
+    for i, x in enumerate(ref_labels_list):
+        lev, ind = _factorize(x)
+        converted_label_levels.append(lev)
+        ind = array(ind, dtype=int32)
+        converted_label_indices.append(ind)
+        ref_labels_ptrs[i] = ind.ctypes.data
+
+    if nrefs != len(ref_prebuilt_list):
+        raise ValueError(
+            "'ref_prebuilt_list' and 'ref_data_list' should have the same length"
+        )
+    ref_prebuilt_ptrs = ndarray(nrefs, dtype=uintp)
+    for i, x in enumerate(ref_prebuilt_list):
+        ref_prebuilt_ptrs[i] = x._ptr
+
+    if ref_names is not None:
+        if nrefs != len(ref_names):
+            raise ValueError(
+                "'ref_names' and 'ref_data_list' should have the same length"
+            )
+        elif nrefs != len(set(ref_names)):
+            raise ValueError("'ref_names' should contain unique names")
+
+    output = lib.build_integrated_references(
+        len(test_features),
+        test_features,
+        nrefs,
+        ref_data_ptrs.ctypes.data,
+        ref_labels_ptrs.ctypes.data,
+        ref_features_ptrs.ctypes.data,
+        ref_prebuilt_ptrs.ctypes.data,
+        num_threads,
+    )
+
+    return IntegratedReferences(
+        output, ref_names, converted_label_levels, original_test_features
+    )
