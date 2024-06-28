d34fb79ffaecdcfb339f504d536f20e42aecce33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 07:05:03 +0000

    Add package greeclimate/1.4.6/greeclimate-1.4.6.tar.gz

---
+++ packages/greeclimate/greeclimate-1.4.6.tar.gz/greeclimate-1.4.6/greeclimate/discovery.py
@@ -0,0 +1,220 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+from asyncio import Task
+from asyncio.events import AbstractEventLoop
+from ipaddress import IPv4Address
+from typing import Coroutine, List
+
+from greeclimate.device import DeviceInfo
+from greeclimate.network import BroadcastListenerProtocol, IPAddr
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Listener:
+    """Base class for device discovery events."""
+
+    async def device_found(self, device_info: DeviceInfo) -> None:
+        """Called any time a new (unique) device is found on the network."""
+
+    async def device_update(self, device_info: DeviceInfo) -> None:
+        """Called any time an up address for a device has changed on the network."""
+
+
+class Discovery(BroadcastListenerProtocol, Listener):
+    """Interact with gree devices on the network
+
+    The `GreeClimate` class provides basic services for discovery and
+    interaction with gree device on the network.
+    """
+
+    def __init__(
+        self,
+        timeout: int = 2,
+        allow_loopback: bool = False,
+        loop: AbstractEventLoop = None,
+    ):
+        """Intialized the discovery manager.
+
+        Args:
+            timeout (int): Wait this long for responses to the scan request
+            allow_loopback (bool): Allow scanning the loopback interface, default `False`
+            loop (AbstractEventLoop): Async event loop
+        """
+        super(BroadcastListenerProtocol, self).__init__()
+        self._timeout = timeout
+        self._allow_loopback = allow_loopback
+
+        self._device_infos = []
+        self._listeners = []
+        self._tasks = []
+
+        self._loop = loop or asyncio.get_event_loop()
+        self._transport = None
+
+    # Task management
+    @property
+    def tasks(self) -> List[Coroutine]:
+        """Returns the outstanding tasks waiting completion."""
+        return self._tasks
+
+    @property
+    def devices(self) -> List[DeviceInfo]:
+        """Return the current known list of devices."""
+        return self._device_infos
+
+    def _task_done_callback(self, task):
+        if task.exception():
+            _LOGGER.exception("Uncaught exception", exc_info=task.exception())
+        self._tasks.remove(task)
+
+    def _create_task(self, coro) -> Task:
+        """Create and track tasks that are being created for events."""
+        task = self._loop.create_task(coro)
+        self._tasks.append(task)
+        task.add_done_callback(self._task_done_callback)
+        return task
+
+    # Listener management
+    def add_listener(self, listener: Listener) -> List[Coroutine]:
+        """Add a listener that will receive discovery events.
+
+        Adding a listener will cause all currently known device to trigger a
+        new device added event on the listen object.
+
+        Args:
+            listener (Listener): A listener object which will receive events
+
+        Returns:
+            List[Coro]: List of tasks for device found events.
+        """
+        if not listener in self._listeners:
+            self._listeners.append(listener)
+            return [self._create_task(listener.device_found(x)) for x in self.devices]
+
+    def remove_listener(self, listener: Listener) -> bool:
+        """Remove a listener that has already been registered.
+
+        Args:
+            listener (Listener): A listener object which will receive events
+
+        Returns:
+            bool: True if listener has been remove, false if it didn't exist
+        """
+        if listener in self._listeners:
+            self._listeners.remove(listener)
+            return True
+        return False
+
+    async def device_found(self, device_info: DeviceInfo) -> None:
+        """Device is found.
+
+        Notify all listeners that a device was found. Exceptions raised by
+        listeners are ignored.
+
+        Args:
+            device_info (DeviceInfo): Information about the newly discovered
+            device
+        """
+
+        for index, last_info in enumerate(self._device_infos):
+            if device_info == last_info:
+                if device_info.ip != last_info.ip:
+                    # ip address info may have been updated, so store the new info
+                    # and trigger a `device_update` event.
+                    self._device_infos[index] = device_info
+                    tasks = [l.device_update(device_info) for l in self._listeners]
+                    await asyncio.gather(*tasks, return_exceptions=True)
+                return
+
+        self._device_infos.append(device_info)
+
+        _LOGGER.info("Found gree device %s", str(device_info))
+
+        tasks = [l.device_found(device_info) for l in self._listeners]
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+    def packet_received(self, obj, addr: IPAddr) -> None:
+        """Event called when a packet is received and decoded."""
+        pack = obj.get("pack")
+        if not pack:
+            _LOGGER.error("Received an unexpected response during discovery")
+            return
+
+        device = (
+            addr[0],
+            addr[1],
+            pack.get("mac") or pack.get("cid"),
+            pack.get("name"),
+            pack.get("brand"),
+            pack.get("model"),
+            pack.get("ver"),
+        )
+
+        self._create_task(self.device_found(DeviceInfo(*device)))
+
+    # Discovery
+    async def scan(self, wait_for: int=0, bcast_ifaces: List[IPv4Address] | None=None) -> List[DeviceInfo]:
+        """Sends a discovery broadcast packet on each network interface to
+            locate Gree units on the network
+
+        Args:
+            wait_for (int): Optionally wait this many seconds for discovery
+                            and return the devices found.
+
+        Returns:
+            List[DeviceInfo]: List of devices found during this scan
+        """
+        _LOGGER.info("Scanning for Gree devices ...")
+
+        await self.search_devices(bcast_ifaces)
+        if wait_for:
+            await asyncio.sleep(wait_for)
+            await asyncio.gather(*self.tasks, return_exceptions=True)
+
+        return self._device_infos
+
+    def _get_broadcast_addresses(self) -> List[IPv4Address]:
+        """Return a list of broadcast addresses for each discovered interface"""
+        import netifaces
+
+        bdrAddrs = []
+        for iface in netifaces.interfaces():
+            for addr in netifaces.ifaddresses(iface).get(netifaces.AF_INET, []):
+                ipaddr = addr.get("addr")
+                bdr = addr.get("broadcast")
+                peer = addr.get("peer")
+                if addr:
+                    ip4addr = IPv4Address(ipaddr)
+                    if ip4addr.is_loopback and self._allow_loopback:
+                        if bdr or peer:
+                            bdrAddrs.append(IPv4Address(bdr or peer))
+                    elif not ip4addr.is_loopback:
+                        if bdr:
+                            bdrAddrs.append(IPv4Address(bdr))
+
+        return bdrAddrs
+
+    async def search_on_interface(self, bcast_iface: IPv4Address) -> None:
+        """Search for devices on a specific interface."""
+        _LOGGER.debug(
+            "Listening for devices on %s",
+            bcast_iface,
+        )
+
+        if self._transport is None:
+            self._transport, _ = await self._loop.create_datagram_endpoint(
+                lambda: self, local_addr=("0.0.0.0", 0), allow_broadcast=True
+            )
+
+        await self.send({"t": "scan"}, (str(bcast_iface), 7000))
+
+    async def search_devices(self, broadcastAddrs: list[IPv4Address] | None = None) -> None:
+        """Search for devices with specific broadcast addresses."""
+        if not broadcastAddrs:
+            broadcastAddrs = self._get_broadcast_addresses()
+        await asyncio.gather(
+            *[asyncio.create_task(self.search_on_interface(b)) for b in broadcastAddrs]
+        )
---
+++ packages/greeclimate/greeclimate-1.4.6.tar.gz/greeclimate-1.4.6/tests/test_network.py
@@ -0,0 +1,448 @@
+import asyncio
+import json
+import socket
+from threading import Thread
+from unittest.mock import create_autospec, patch
+
+import pytest
+
+from greeclimate.network import (
+    BroadcastListenerProtocol,
+    DatagramStream,
+    DeviceProtocol,
+    DeviceProtocol2,
+    IPAddr,
+    bind_device,
+    create_datagram_stream,
+    request_state,
+    send_state,
+)
+
+from .common import (
+    DEFAULT_RESPONSE,
+    DEFAULT_TIMEOUT,
+    DISCOVERY_REQUEST,
+    DISCOVERY_RESPONSE,
+    Responder,
+    encrypt_payload,
+    get_mock_device_info,
+)
+
+
+class FakeDiscovery(BroadcastListenerProtocol):
+    """Fake discovery class."""
+
+    def __init__(self):
+        super(BroadcastListenerProtocol, self).__init__()
+        self.packets = []
+
+    def packet_received(self, obj, addr: IPAddr) -> None:
+        self.packets.append(obj)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "addr,bcast,family", [(("127.0.0.1", 7000), "127.255.255.255", socket.AF_INET)]
+)
+async def test_close_connection(addr, bcast, family):
+    """Test closing the connection."""
+    # Run the listener portion now
+    loop = asyncio.get_event_loop()
+
+    bcast = (bcast, 7000)
+    local_addr = (addr[0], 0)
+
+    with patch.object(DeviceProtocol2, "connection_lost") as mock:
+        dp2 = FakeDiscovery()
+        await loop.create_datagram_endpoint(
+            lambda: dp2,
+            local_addr=local_addr,
+        )
+
+        # Send the scan command
+        data = DISCOVERY_REQUEST
+        await dp2.send(data, bcast)
+        dp2.close()
+
+        # Wait on the scan response
+        await asyncio.sleep(DEFAULT_TIMEOUT)
+        response = dp2.packets
+
+        assert not response
+        assert len(response) == 0
+        assert mock.call_count == 1
+
+
+@pytest.mark.asyncio
+async def test_set_get_key():
+    """Test the encryption key property."""
+    key = "faketestkey"
+    dp2 = DeviceProtocol2()
+    dp2.device_key = key
+    assert dp2.device_key == key
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,bcast", [(("127.0.0.1", 7001), "127.255.255.255")])
+async def test_connection_error(addr, bcast):
+    """Test the encryption key property."""
+    dp2 = DeviceProtocol2()
+
+    loop = asyncio.get_event_loop()
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: dp2,
+        local_addr=addr,
+    )
+
+    # Send the scan command
+    data = DISCOVERY_REQUEST
+    await dp2.send(data, bcast)
+    dp2.connection_lost(RuntimeError())
+    assert transport.is_closing()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,bcast", [(("127.0.0.1", 7001), "127.255.255.255")])
+async def test_pause_resume(addr, bcast):
+    """Test the encryption key property."""
+    event = asyncio.Event()
+    dp2 = DeviceProtocol2(drained=event)
+
+    loop = asyncio.get_event_loop()
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: dp2,
+        local_addr=addr,
+    )
+
+    dp2.pause_writing()
+    assert not event.is_set()
+
+    dp2.resume_writing()
+    assert event.is_set()
+
+    dp2.close()
+    assert transport.is_closing()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "addr,bcast,family", [(("127.0.0.1", 7000), "127.255.255.255", socket.AF_INET)]
+)
+async def test_broadcast_recv(addr, bcast, family):
+    """Create a socket broadcast responder, an async broadcast listener, test discovery responses."""
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+            assert p == DISCOVERY_REQUEST
+
+            p = json.dumps(encrypt_payload(DISCOVERY_RESPONSE))
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        loop = asyncio.get_event_loop()
+
+        bcast = (bcast, 7000)
+        local_addr = (addr[0], 0)
+
+        dp2 = FakeDiscovery()
+        await loop.create_datagram_endpoint(
+            lambda: dp2,
+            local_addr=local_addr,
+        )
+
+        # Send the scan command
+        data = DISCOVERY_REQUEST
+        await dp2.send(data, bcast)
+
+        # Wait on the scan response
+        await asyncio.sleep(DEFAULT_TIMEOUT)
+        response = dp2.packets
+
+        assert response
+        assert len(response) == 1
+        assert response[0] == DISCOVERY_RESPONSE
+
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "addr,bcast,family",
+    [
+        (("127.0.0.1", 7000), "127.255.255.255", socket.AF_INET),
+        (("127.0.0.1", 7000), "255.255.255.255", socket.AF_INET),
+    ],
+)
+async def test_broadcast_timeout(addr, bcast, family):
+    """Create an async broadcast listener, test discovery responses."""
+
+    # Run the listener portion now
+    loop = asyncio.get_event_loop()
+
+    bcast = (bcast, 7000)
+    local_addr = (addr[0], 0)
+
+    dp2 = FakeDiscovery()
+    await loop.create_datagram_endpoint(
+        lambda: dp2,
+        local_addr=local_addr,
+    )
+
+    # Send the scan command
+    await dp2.send(DISCOVERY_REQUEST, bcast)
+
+    # Wait on the scan response
+    await asyncio.sleep(DEFAULT_TIMEOUT)
+    response = dp2.packets
+
+    assert response is not None
+    assert len(response) == 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_datagram_connect(addr, family):
+    """Create a socket responder, an async connection, test send and recv."""
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+            assert p == DISCOVERY_REQUEST
+
+            p = json.dumps(DISCOVERY_RESPONSE)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        loop = asyncio.get_event_loop()
+        recvq = asyncio.Queue()
+        excq = asyncio.Queue()
+        drained = asyncio.Event()
+
+        remote_addr = (addr[0], 7000)
+
+        transport, _ = await loop.create_datagram_endpoint(
+            lambda: DeviceProtocol(recvq, excq, drained), remote_addr=remote_addr
+        )
+        stream = DatagramStream(transport, recvq, excq, drained)
+
+        # Send the scan command
+        data = json.dumps(DISCOVERY_REQUEST).encode()
+        await stream.send(data, None)
+
+        # Wait on the scan response
+        task = asyncio.create_task(stream.recv())
+        await asyncio.wait_for(task, timeout=DEFAULT_TIMEOUT)
+        (response, _) = task.result()
+
+        assert response
+        assert len(response) > 0
+        assert json.loads(response) == DISCOVERY_RESPONSE
+
+        sock.close()
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_create_stream(addr, family):
+    """Create a socket responder, a network stream, test send and recv."""
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+            assert p == DISCOVERY_REQUEST
+
+            p = json.dumps(DISCOVERY_RESPONSE)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        stream = await create_datagram_stream(addr)
+
+        # Send the scan command
+        data = json.dumps(DISCOVERY_REQUEST).encode()
+        await stream.send(data)
+
+        # Wait on the scan response
+        task = asyncio.create_task(stream.recv())
+        await asyncio.wait_for(task, timeout=DEFAULT_TIMEOUT)
+        (response, _) = task.result()
+
+        assert response
+        assert len(response) > 0
+        assert json.loads(response) == DISCOVERY_RESPONSE
+
+        sock.close()
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+def test_encrypt_decrypt_payload():
+    test_object = {"fake-key": "fake-value"}
+
+    encrypted = DeviceProtocol2.encrypt_payload(test_object)
+    assert encrypted != test_object
+
+    decrypted = DeviceProtocol2.decrypt_payload(encrypted)
+    assert decrypted == test_object
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_send_receive_device_data(addr, family):
+    """Create a socket responder, a network stream, test send and recv."""
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+            assert p == DISCOVERY_REQUEST
+
+            # Echoing because part of the request is encrypted
+            p = json.dumps(DISCOVERY_REQUEST)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        stream = await create_datagram_stream(addr)
+
+        # Send the scan command
+        await stream.send_device_data(DISCOVERY_REQUEST)
+
+        # Wait on the scan response
+        task = asyncio.create_task(stream.recv_device_data())
+        await asyncio.wait_for(task, timeout=DEFAULT_TIMEOUT)
+        (response, _) = task.result()
+
+        assert response
+        assert response == DISCOVERY_REQUEST
+
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_bind_device(addr, family):
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+
+            r = DEFAULT_RESPONSE
+            r["pack"] = DeviceProtocol2.encrypt_payload(
+                {"t": "bindok", "key": "acbd1234"}
+            )
+            p = json.dumps(r)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        response = await bind_device(get_mock_device_info())
+
+        assert response
+        assert response == "acbd1234"
+
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_send_and_update_device_status_using_val(addr, family):
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+
+            r = DEFAULT_RESPONSE
+            r["pack"] = DeviceProtocol2.encrypt_payload(
+                {"opt": ["prop-a", "prop-b"], "val": ["val-a", "val-b"]}
+            )
+            p = json.dumps(r)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        properties = {"prop-a": "val-a", "prop-b": "val-b"}
+        response = await send_state(properties, get_mock_device_info())
+
+        assert response
+        assert response == properties
+
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_send_and_update_device_status_using_p(addr, family):
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+
+            r = DEFAULT_RESPONSE
+            r["pack"] = DeviceProtocol2.encrypt_payload(
+                {"opt": ["prop-a", "prop-b"], "p": ["val-a", "val-b"]}
+            )
+            p = json.dumps(r)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        properties = {"prop-a": "val-a", "prop-b": "val-b"}
+        response = await send_state(properties, get_mock_device_info())
+
+        assert response
+        assert response == properties
+
+        serv.join(timeout=DEFAULT_TIMEOUT)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("addr,family", [(("127.0.0.1", 7000), socket.AF_INET)])
+async def test_request_device_status(addr, family):
+    with Responder(family, addr[1]) as sock:
+
+        def responder(s):
+            (d, addr) = s.recvfrom(2048)
+            p = json.loads(d)
+
+            r = DEFAULT_RESPONSE
+            r["pack"] = DeviceProtocol2.encrypt_payload(
+                {"cols": ["prop-a", "prop-b"], "dat": ["val-a", "val-b"]}
+            )
+            p = json.dumps(r)
+            s.sendto(p.encode(), addr)
+
+        serv = Thread(target=responder, args=(sock,))
+        serv.start()
+
+        # Run the listener portion now
+        properties = {"prop-a": "val-a", "prop-b": "val-b"}
+        response = await request_state(properties, get_mock_device_info())
+
+        assert response
+        assert response == properties
+
+        serv.join(timeout=DEFAULT_TIMEOUT)
