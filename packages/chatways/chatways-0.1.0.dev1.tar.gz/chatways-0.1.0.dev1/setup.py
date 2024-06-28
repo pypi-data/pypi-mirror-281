57b412b1adf1de0913543abf1eb2ec14bc4840bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:06:11 +0000

    Add package chatways/0.1.0.dev1/chatways-0.1.0.dev1.tar.gz

---
+++ packages/chatways/chatways-0.1.0.dev1.tar.gz/chatways-0.1.0.dev1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chatways",
+    version="0.1.0-dev1",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=[
+        "gradio",
+        "transformers",
+        "openai",
+        "sentencepiece",
+        "protobuf",
+        "accelerate",
+    ],
+    entry_points={
+        "console_scripts": [
+            "chatways=chatways.cli:main",
+        ],
+    },
+    author="Sibo Zhang",
+    author_email="zhangsibo1129@gmail.com",
+    description="A command line tool for chatways",
+    url="https://github.com/zhangsibo1129/chatways",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
