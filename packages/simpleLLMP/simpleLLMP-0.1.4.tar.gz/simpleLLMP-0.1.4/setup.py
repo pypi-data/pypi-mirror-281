3a396bb49853995f90b4c67c6a409f8781b12096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:38 +0000

    Add package simpleLLMP/0.1.4/simpleLLMP-0.1.4.tar.gz

---
+++ packages/simpleLLMP/simpleLLMP-0.1.4.tar.gz/simpleLLMP-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpleLLMP',
+    version='0.1.4',
+    packages=find_packages(exclude=['tests*']),
+    install_requires=[
+        'openai',
+        'pytesseract',
+        'pdf2image',
+        'pillow',
+        'PyPDF2',
+        'requests'
+    ],
+    url='https://github.com/ykim336/simpleML',  
+    author='Yvon Kim',
+    author_email='kmyn7up@gmail.com',
+    description='A simple library for interacting with OpenAI models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
