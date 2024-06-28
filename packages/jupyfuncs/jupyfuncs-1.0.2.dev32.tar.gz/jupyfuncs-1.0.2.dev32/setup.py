d4fde89b107e2d3ba24054d47f5a261e5316ec21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:05:11 +0000

    Add package jupyfuncs/1.0.2.dev32/jupyfuncs-1.0.2.dev32.tar.gz

---
+++ packages/jupyfuncs/jupyfuncs-1.0.2.dev32.tar.gz/jupyfuncs-1.0.2.dev32/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import setuptools_scm
+
+def custom_version_scheme(version):
+    """自定义版本号方案，确保没有 .dev 后缀"""
+    base_version = version.tag.base_version if version.tag else '0.0.0'
+    if version.distance is not None:
+        return f"{base_version}.post{version.distance}"
+    return base_version
+
+setup(
+    name="jupyfuncs",
+    use_scm_version={
+        "local_scheme": "no-local-version",
+        "version_scheme": custom_version_scheme,
+        "write_to": "jupyfuncs/_version.py"
+    },
+    author="Jianxing Hu",
+    author_email="hu.jx@outlook.com",
+    description="A collection of functions for Jupyter notebooks",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/huluxiaohuowa/jupyfuncs",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    setup_requires=['setuptools_scm']
+)
