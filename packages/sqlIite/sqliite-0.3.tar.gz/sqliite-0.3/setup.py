8caec9ce6e30a12ce479bc5b669c986aa4c93f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 07:04:56 +0000

    Add package sqlIite/0.3/sqliite-0.3.tar.gz

---
+++ packages/sqlIite/sqliite-0.3.tar.gz/sqliite-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sqlIite',
+      version='0.3',
+      description='Gaussian and Binomial distributions',
+      packages=['sqlIite'],
+      author_email='fw3fsw@yandex.ru',
+      zip_safe=False)
