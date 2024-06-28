c0180719f8f50d67f728ba0a635f3fcb2b03c9a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:05:09 +0000

    Add package pyedt/0.1.4/pyedt-0.1.4.tar.gz

---
+++ packages/pyedt/pyedt-0.1.4.tar.gz/pyedt-0.1.4/pyedt/__init__.py
@@ -0,0 +1,6 @@
+from .interface import edt_gpu
+from .interface import edt_cpu
+from .interface import edt_gpu_split
+from .interface import edt
+from .interface import jit_edt_cpu
+from .interface import run_benchmark
---
+++ packages/pyedt/pyedt-0.1.4.tar.gz/pyedt-0.1.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="pyedt",
+    version="0.1.4",
+    author="LTrace technologies",
+    description="Euclidian Distance Transform functions for GPU and parallel CPU",
+    packages=["pyedt"],
+    url='https://pypi.org/project/pyedt/',
+    install_requires=[
+        'matplotlib>=3.5.1',
+        'numba>=0.56.2',
+        'numpy>=1.23.1',
+        'scipy>=1.8.1',
+        'pytest>=7.4.1'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    )
