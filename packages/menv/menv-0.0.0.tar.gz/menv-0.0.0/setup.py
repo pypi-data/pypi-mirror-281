3782560d35550af49aae5ed0e63fe8cce3b33b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:05:52 +0000

    Add package menv/0.0.0/menv-0.0.0.tar.gz

---
+++ packages/menv/menv-0.0.0.tar.gz/menv-0.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    # ...
+    packages=find_packages(
+        where='src',
+        include=['main']
+    ),
+    package_dir={"": "src"},
+    # ...
+    install_requires=[
+        'virtualenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'menv = main:main'
+        ],
+    },
+    author="JMMMMMMMMMM",
+    description="virtualenv manager",
+    name="menv",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    platforms=['win32'],
+)
