c802418a19fb40151f61bc1f0131c9cf73557b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:06:10 +0000

    Add package lycoris-lora/3.0.0.dev15/lycoris_lora-3.0.0.dev15.tar.gz

---
+++ packages/lycoris-lora/lycoris_lora-3.0.0.dev15.tar.gz/lycoris_lora-3.0.0.dev15/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="lycoris_lora",
+    packages=find_packages(),
+    version="3.0.0.dev15",
+    url="https://github.com/KohakuBlueleaf/LyCORIS",
+    description="Lora beYond Conventional methods, Other Rank adaptation Implementations for Stable diffusion",
+    author="Shih-Ying Yeh(KohakuBlueLeaf), Yu-Guan Hsieh, Zhidong Gao",
+    author_email="apolloyeh0123@gmail.com",
+    zip_safe=False,
+    install_requires=["torch", "einops", "toml", "tqdm"],
+    python_requires=">=3.10",
+)
