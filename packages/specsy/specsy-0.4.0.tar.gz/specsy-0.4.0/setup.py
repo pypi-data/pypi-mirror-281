73b8ef61194de711b4bc75f5397b6450339e4284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:39 +0000

    Add package specsy/0.4.0/specsy-0.4.0.tar.gz

---
+++ packages/specsy/specsy-0.4.0.tar.gz/specsy-0.4.0/setup.py
@@ -0,0 +1,42 @@
+import os
+import pathlib
+import configparser
+from setuptools import setup
+from setuptools import find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# README
+README = (HERE / "README.rst").read_text()
+
+# Read lime configuration
+_dir_path = os.path.dirname(os.path.realpath(__file__))
+_setup_cfg = configparser.ConfigParser()
+_setup_cfg.optionxform = str
+_setup_cfg.read(HERE / 'setup.cfg')
+
+# Setup
+setup(name=_setup_cfg['metadata']['name'],
+      version=_setup_cfg['metadata']['version'],
+      author=_setup_cfg['metadata']['author'],
+      author_email=_setup_cfg['metadata']['author_email'],
+      description=_setup_cfg['metadata']['description'],
+      long_description=README,
+      long_description_content_type=_setup_cfg['metadata']['long_description_content_type'],
+      url=_setup_cfg['metadata']['url'],
+      license=_setup_cfg['metadata']['licence'],
+      classifiers=[
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.9",
+      ],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      package_data={'': ['config.toml', 'inference/*', 'innate/*', 'models/*', 'operations/*', 'resources/*', 'workflow/*']},
+      include_package_data=True,
+      install_requires=["arviz", "astropy", "h5netcdf", "jax", "jaxlib", "lime-stable", "lmfit", "matplotlib", "numpy",
+                        "pandas", "pymc", "pytensor", "scipy", "toml", "xarray"],
+      )
+
+
