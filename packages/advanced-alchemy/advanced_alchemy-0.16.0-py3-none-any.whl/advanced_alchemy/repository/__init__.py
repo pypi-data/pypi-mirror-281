316d63fd41f5d4c65ad626344c3d158b1b94e75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:52 +0000

    Add package advanced-alchemy/0.16.0/advanced_alchemy-0.16.0-py3-none-any.whl

---
+++ packages/advanced-alchemy/advanced_alchemy-0.16.0-py3-none-any.whl/advanced_alchemy/repository/__init__.py
@@ -0,0 +1,40 @@
+from advanced_alchemy.repository._async import (
+    SQLAlchemyAsyncQueryRepository,
+    SQLAlchemyAsyncRepository,
+    SQLAlchemyAsyncRepositoryProtocol,
+    SQLAlchemyAsyncSlugRepository,
+    SQLAlchemyAsyncSlugRepositoryProtocol,
+)
+from advanced_alchemy.repository._sync import (
+    SQLAlchemySyncQueryRepository,
+    SQLAlchemySyncRepository,
+    SQLAlchemySyncRepositoryProtocol,
+    SQLAlchemySyncSlugRepository,
+    SQLAlchemySyncSlugRepositoryProtocol,
+)
+from advanced_alchemy.repository._util import (
+    FilterableRepositoryProtocol,
+    LoadSpec,
+    get_instrumented_attr,
+    model_from_dict,
+)
+from advanced_alchemy.repository.typing import ModelOrRowMappingT, ModelT
+
+__all__ = (
+    "SQLAlchemyAsyncRepository",
+    "SQLAlchemyAsyncRepositoryProtocol",
+    "SQLAlchemyAsyncSlugRepositoryProtocol",
+    "FilterableRepositoryProtocol",
+    "SQLAlchemySyncRepositoryProtocol",
+    "SQLAlchemySyncSlugRepositoryProtocol",
+    "SQLAlchemyAsyncQueryRepository",
+    "SQLAlchemyAsyncSlugRepository",
+    "SQLAlchemySyncSlugRepository",
+    "SQLAlchemySyncRepository",
+    "SQLAlchemySyncQueryRepository",
+    "get_instrumented_attr",
+    "model_from_dict",
+    "LoadSpec",
+    "ModelT",
+    "ModelOrRowMappingT",
+)
