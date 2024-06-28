0ea49bd7fcfaf78c4199b450256008b28b50103a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:29 +0000

    Add package grr-api-client/3.4.7.post5/grr-api-client-3.4.7.post5.zip

---
+++ packages/grr-api-client/grr-api-client-3.4.7.post5.zip/grr-api-client-3.4.7.post5/grr_api_client/artifact.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""Functions and objects to access config-related GRR API methods."""
+
+from grr_api_client import context as api_context
+from grr_api_client import utils
+from grr_response_proto import artifact_pb2
+from grr_response_proto.api import artifact_pb2 as api_artifact_pb2
+
+
+class Artifact(object):
+  """GRR artifact object with fetched data."""
+
+  def __init__(
+      self,
+      data: artifact_pb2.ArtifactDescriptor,
+      context: api_context.GrrApiContext,
+  ):
+    self.data = data
+    self._context = context
+
+
+def ListArtifacts(
+    context: api_context.GrrApiContext,
+) -> utils.ItemsIterator[Artifact]:
+  """Lists all registered Grr artifacts."""
+  args = api_artifact_pb2.ApiListArtifactsArgs()
+
+  items = context.SendIteratorRequest("ListArtifacts", args)
+  return utils.MapItemsIterator(
+      lambda data: Artifact(data=data, context=context), items
+  )
