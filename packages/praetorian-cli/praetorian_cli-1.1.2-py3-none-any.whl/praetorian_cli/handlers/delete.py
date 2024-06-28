91b2f4c0cb67c0f3c1887c6a6f652edf324fd34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 07:05:08 +0000

    Add package praetorian-cli/1.1.2/praetorian_cli-1.1.2-py3-none-any.whl

---
+++ packages/praetorian-cli/praetorian_cli-1.1.2-py3-none-any.whl/praetorian_cli/handlers/delete.py
@@ -0,0 +1,29 @@
+import click
+
+from praetorian_cli.handlers.chariot import chariot
+from praetorian_cli.handlers.cli_decorators import cli_handler
+
+
+@chariot.group()
+@cli_handler
+def delete(ctx):
+    """Delete a resource from Chariot"""
+    pass
+
+
+def delete_command(item):
+    @delete.command(item, help=f"Delete {item}")
+    @click.argument('key', required=True)
+    @cli_handler
+    def command(controller, key):
+        if item == 'attribute':
+            resp = controller.delete('asset/attribute', key)
+        elif item == 'reference':
+            resp = controller.delete('risk/reference', key)
+        else:
+            resp = controller.delete(item, key)
+        print(f"Key: {key} \nDeleted successfully")
+
+
+for item in ['seed', 'attribute', 'reference']:
+    delete_command(item)
