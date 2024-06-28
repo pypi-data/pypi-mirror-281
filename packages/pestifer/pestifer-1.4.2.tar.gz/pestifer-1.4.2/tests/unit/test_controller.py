d4aa80e4159385b4dcdfc6f3eba4b26b4083b6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 14:05:16 +0000

    Add package pestifer/1.4.2/pestifer-1.4.2.tar.gz

---
+++ packages/pestifer/pestifer-1.4.2.tar.gz/pestifer-1.4.2/tests/unit/test_controller.py
@@ -0,0 +1,15 @@
+import unittest
+from pestifer.controller import Controller
+from pestifer.mods import *
+from pestifer.config import ResourceManager
+class TestController(unittest.TestCase):
+    def test_controller_base(self):
+        rm=ResourceManager()
+        configfile=rm['examples']+'/01-bpti.yaml'
+        C=Controller(configfile)
+        self.assertEqual(C.config['user']['title'],'BPTI')
+        self.assertEqual(len(C.tasks),7)
+        configfile=rm['examples']+'/06-hiv-env-8fad.yaml'
+        C=Controller(configfile)
+        self.assertEqual(C.config['user']['title'],'HIV-1 Env Trimer 8fad, drug molecule removed')
+        self.assertEqual(len(C.tasks),10)
