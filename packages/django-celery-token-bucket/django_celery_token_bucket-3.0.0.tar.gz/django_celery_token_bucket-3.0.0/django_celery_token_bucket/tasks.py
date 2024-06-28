16a4c0ed747d000c6266a8dd177710266d27577f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:05:37 +0000

    Add package django-celery-token-bucket/3.0.0/django_celery_token_bucket-3.0.0.tar.gz

---
+++ packages/django-celery-token-bucket/django_celery_token_bucket-3.0.0.tar.gz/django_celery_token_bucket-3.0.0/django_celery_token_bucket/tasks.py
@@ -0,0 +1,27 @@
+from celery import shared_task
+from django.conf import settings
+from kombu.entity import Queue
+
+from django_celery_token_bucket.bucket import TokenBucket
+
+
+@shared_task(ignore_result=True)
+def token_bucket_token():
+    return 1
+
+
+@shared_task
+def token_bucket_refill(name: str, *args, **kwargs):
+    token_bucket: TokenBucket
+
+    # get queue
+    try:
+        token_bucket = settings.CELERY_TOKEN_BUCKETS[name]
+    except KeyError:
+        raise Exception(f"bucket '{name}' is not registered")
+    queue = token_bucket.get_queue()
+
+    # fill it up
+    for _ in range(token_bucket.amount):
+        token_bucket_token.apply_async(queue=queue)
+    return
