d4dff39fcedf82108fe9dd0680ecf3136c4b7fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:31 +0000

    Add package quantminer/1.0.1/quantminer-1.0.1.tar.gz

---
+++ packages/quantminer/quantminer-1.0.1.tar.gz/quantminer-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+# Function to read the requirements from the requirements.txt file
+def read_requirements():
+    with open("requirements.txt", "r") as f:
+        return [line.strip() for line in f if line.strip()]
+
+
+# Read the README file
+def read_readme():
+    with open("README.rst", "r") as f:
+        return f.read()
+
+
+setup(
+    name="quantminer",
+    version="1.0.1",
+    description="Data/Pattern Mining Algorithms for Financial Data",
+    long_description=read_readme(),
+    long_description_content_type="text/x-rst",
+    author="Jerry Inyang",
+    author_email="jerprog0@gmail.com",
+    packages=find_packages(),  # Automatically finds your package
+    install_requires=read_requirements(),  # Reads requirements dynamically from requirements.txt
+)
