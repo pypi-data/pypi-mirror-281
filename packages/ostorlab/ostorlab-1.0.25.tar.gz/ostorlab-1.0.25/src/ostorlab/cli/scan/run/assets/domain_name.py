408d842b26a1e001cdc5b60adee79c66622d7ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:05:58 +0000

    Add package ostorlab/1.0.25/ostorlab-1.0.25.tar.gz

---
+++ packages/ostorlab/ostorlab-1.0.25.tar.gz/ostorlab-1.0.25/src/ostorlab/cli/scan/run/assets/domain_name.py
@@ -0,0 +1,34 @@
+"""Asset of type Domain Name."""
+
+import logging
+from typing import List
+
+import click
+
+from ostorlab.assets import domain_name
+from ostorlab.cli.scan.run import run
+from ostorlab import exceptions
+from ostorlab.cli import console as cli_console
+
+console = cli_console.Console()
+logger = logging.getLogger(__name__)
+
+
+@run.run.command(name="domain-name")
+@click.argument("names", required=True, nargs=-1)
+@click.pass_context
+def domain_name_cli(ctx: click.core.Context, names: List[str]) -> None:
+    """Run scan for Domain Name asset."""
+    runtime = ctx.obj["runtime"]
+    assets = []
+    for d in names:
+        assets.append(domain_name.DomainName(name=d))
+    logger.debug("scanning assets %s", [str(asset) for asset in assets])
+    try:
+        runtime.scan(
+            title=ctx.obj["title"],
+            agent_group_definition=ctx.obj["agent_group_definition"],
+            assets=assets,
+        )
+    except exceptions.OstorlabError as e:
+        console.error(f"An error was encountered while running the scan: {e}")
