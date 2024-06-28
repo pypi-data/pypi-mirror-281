0b7ba27738bcc1fad731da7ec32f0c331f57e0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 04:05:45 +0000

    Add package SIMNet/0.1.22/simnet-0.1.22-py3-none-any.whl

---
+++ packages/SIMNet/simnet-0.1.22-py3-none-any.whl/simnet/__init__.py
@@ -0,0 +1,5 @@
+from simnet.client.genshin import GenshinClient
+from simnet.client.starrail import StarRailClient
+from simnet.utils.enums import Game, Region
+
+__all__ = ("StarRailClient", "GenshinClient", "Game", "Region")
