ce90fa68fec2d67e961af298006510cf8c612bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:53 +0000

    Add package example-pkg-mq/0.0.1/example_pkg_mq-0.0.1.tar.gz

---
+++ packages/example-pkg-mq/example_pkg_mq-0.0.1.tar.gz/example_pkg_mq-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="example-pkg-mq",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
