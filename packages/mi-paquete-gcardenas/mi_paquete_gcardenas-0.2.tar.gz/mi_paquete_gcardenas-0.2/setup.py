52c33c255688e1a709f70ac99d99853b2b8752bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:44 +0000

    Add package mi-paquete-gcardenas/0.2/mi_paquete_gcardenas-0.2.tar.gz

---
+++ packages/mi-paquete-gcardenas/mi_paquete_gcardenas-0.2.tar.gz/mi_paquete_gcardenas-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mi_paquete_gcardenas',
+    version='0.2',
+    packages=find_packages(),
+    description='Una breve descripción de tu paquete',
+    author='Tu Nombre',
+    author_email='tu_email@example.com',
+    url='https://github.com/tu_usuario/mi_paquete',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
