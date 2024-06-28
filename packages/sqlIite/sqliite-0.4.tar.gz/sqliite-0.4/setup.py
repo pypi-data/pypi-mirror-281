ab3e35d0ec285e72a2e05925cf11d7247c022709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 07:04:48 +0000

    Add package sqlIite/0.4/sqliite-0.4.tar.gz

---
+++ packages/sqlIite/sqliite-0.4.tar.gz/sqliite-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sqlIite',
+      version='0.4',
+      description='Sql helper',
+      packages=['sqlIite'],
+      author_email='fw3fsw@yandex.ru',
+      zip_safe=False)
