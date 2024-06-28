09283a9f0dc9589f25e67222acf92e43a14217ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:06:05 +0000

    Add package webknossos/0.14.23/webknossos-0.14.23-py3-none-any.whl

---
+++ packages/webknossos/webknossos-0.14.23-py3-none-any.whl/webknossos/dataset/_upsampling_utils.py
@@ -0,0 +1,98 @@
+import logging
+import math
+from itertools import product
+from typing import List, Tuple
+
+import numpy as np
+
+from ..geometry import BoundingBox, Vec3Int
+from .view import View
+
+
+def _vec3int_mulf(vec: Vec3Int, factors: List[float]) -> Vec3Int:
+    return Vec3Int(
+        int(vec.x * factors[0]), int(vec.y * factors[1]), int(vec.z * factors[2])
+    )
+
+
+def upsample_cube(cube_buffer: np.ndarray, factors: List[int]) -> np.ndarray:
+    ds = cube_buffer.shape
+    out_buf = np.zeros(tuple(s * f for s, f in zip(ds, factors)), cube_buffer.dtype)
+    for dx in (0, factors[0] - 1):
+        for dy in (0, factors[1] - 1):
+            for dz in (0, factors[2] - 1):
+                out_buf[
+                    dx : out_buf.shape[0] : factors[0],
+                    dy : out_buf.shape[1] : factors[1],
+                    dz : out_buf.shape[2] : factors[2],
+                ] = cube_buffer
+    return out_buf
+
+
+def upsample_cube_job(
+    args: Tuple[View, View, int],
+    mag_factors: List[float],
+    buffer_shape: Vec3Int,
+) -> None:
+    (source_view, target_view, _i) = args
+
+    assert all(
+        1 >= f for f in mag_factors
+    ), f"mag_factors ({mag_factors}) for upsampling must be smaller than 1"
+
+    try:
+        num_channels = target_view.info.num_channels
+        target_size = target_view.bounding_box.in_mag(target_view.mag).size
+        shape = (num_channels,) + target_size.to_tuple()
+        file_buffer = np.zeros(shape, target_view.get_dtype())
+
+        tiles = product(
+            *list(
+                [
+                    list(range(0, math.ceil(length)))
+                    for length in target_size.to_np() / buffer_shape.to_np()
+                ]
+            )
+        )
+
+        for tile in tiles:
+            target_offset = Vec3Int(tile) * buffer_shape
+            source_offset = _vec3int_mulf(target_offset, mag_factors)
+            source_size = source_view.bounding_box.in_mag(source_view.mag).size
+            source_size = _vec3int_mulf(buffer_shape, mag_factors).pairmin(
+                source_size - source_offset
+            )
+
+            bbox = BoundingBox(source_offset, source_size)
+            cube_buffer_channels = source_view.read(
+                relative_bounding_box=bbox.from_mag_to_mag1(source_view.mag),
+            )
+
+            for channel_index in range(num_channels):
+                cube_buffer = cube_buffer_channels[channel_index]
+
+                if not np.all(cube_buffer == 0):
+                    # Upsample the buffer
+                    inverse_factors = [int(1 / f) for f in mag_factors]
+                    data_cube = upsample_cube(cube_buffer, inverse_factors)
+
+                    buffer_offset = target_offset
+                    buffer_end = buffer_offset + data_cube.shape
+
+                    file_buffer[
+                        channel_index,
+                        buffer_offset[0] : buffer_end[0],
+                        buffer_offset[1] : buffer_end[1],
+                        buffer_offset[2] : buffer_end[2],
+                    ] = data_cube
+
+        # Write the upsampled buffer to target
+        if source_view.info.num_channels == 1:
+            file_buffer = file_buffer[0]  # remove channel dimension
+        target_view.write(file_buffer)
+
+    except Exception as exc:
+        logging.error(
+            f"Upsampling of target {target_view.bounding_box} failed with {exc}"
+        )
+        raise exc
---
+++ packages/webknossos/webknossos-0.14.23-py3-none-any.whl/webknossos/skeleton/skeleton.py
@@ -0,0 +1,122 @@
+import itertools
+from os import PathLike
+from pathlib import Path
+from typing import Iterator, Optional, Tuple, Union
+
+import attr
+
+from ..utils import warn_deprecated
+from .group import Group
+
+Vector3 = Tuple[float, float, float]
+
+
+@attr.define()
+class Skeleton(Group):
+    """
+    Representation of the [skeleton](/webknossos/skeleton_annotation.html) of an `Annotation`.
+    It contains metadata to identify the related dataset and is the root-group of sub-groups and trees.
+    See the parent class `Group` for methods about group and tree handling.
+    To upload a skeleton to webknossos, please create an `Annotation()` with it.
+
+    A small usage example:
+
+    ```python
+    annotation = Annotation(
+        name="my_annotation", dataset_name="my_dataset", voxel_size=(11, 11, 24)
+    )
+    group = annotation.skeleton.add_group("a group")
+    tree = group.add_tree("a tree")
+    node_1 = tree.add_node(position=(0, 0, 0), comment="node 1")
+    node_2 = tree.add_node(position=(100, 100, 100), comment="node 2")
+
+    tree.add_edge(node_1, node_2)
+    ```
+
+    Also see [this example](/webknossos-py/examples/skeleton_synapse_candidates.html) for a more
+    complex interaction.
+    """
+
+    voxel_size: Vector3
+    dataset_name: str
+    organization_id: Optional[str] = None
+    description: Optional[str] = None
+    # from Group parent to support mypy:
+    _enforced_id: Optional[int] = attr.field(default=None, eq=False, repr=False)
+
+    name: str = attr.field(default="Root", init=False, eq=False, repr=False)
+    """Should not be used with `Skeleton`, this attribute is only useful for sub-groups. Set to `Root`."""
+
+    # initialized in post_init:
+    _id: int = attr.field(init=False, repr=False)
+    _element_id_generator: Iterator[int] = attr.field(init=False, eq=False, repr=False)
+    _skeleton: "Skeleton" = attr.field(init=False, eq=False, repr=False)
+
+    @classmethod
+    def _set_init_docstring(cls) -> None:
+        Skeleton.__init__.__doc__ = """
+        To initialize a skeleton, setting the following parameters is required (or recommended):
+        - voxel_size
+        - dataset_name
+        - organization_id
+        - description
+        """
+
+    def __attrs_post_init__(self) -> None:
+        self._element_id_generator = itertools.count()
+        self._skeleton = self
+        super().__attrs_post_init__()  # sets self._id
+
+    @property
+    def scale(self) -> Tuple[float, float, float]:
+        """Deprecated, please use `voxel_size`."""
+        warn_deprecated("scale", "voxel_size")
+        return self.voxel_size
+
+    @scale.setter
+    def scale(self, scale: Tuple[float, float, float]) -> None:
+        """Deprecated, please use `voxel_size`."""
+        warn_deprecated("scale", "voxel_size")
+        self.voxel_size = scale
+
+    @staticmethod
+    def load(file_path: Union[PathLike, str]) -> "Skeleton":
+        """Loads a `.nml` file or a `.zip` file containing an NML (and possibly also volume
+        layers). Returns the `Skeleton` object. Also see `Annotation.load` if you want to
+        have the annotation which wraps the skeleton."""
+
+        from ..annotation import Annotation
+
+        return Annotation.load(file_path).skeleton
+
+    def save(self, out_path: Union[str, PathLike]) -> None:
+        """
+        Stores the skeleton as a zip or nml at the given path.
+        """
+
+        from ..annotation import Annotation
+
+        out_path = Path(out_path)
+        assert out_path.suffix in [
+            ".nml",
+            ".zip",
+        ], f"The suffix if the file must be .nml or .zip, not {out_path.suffix}"
+        annotation = Annotation(name=out_path.stem, skeleton=self, time=None)
+        annotation.save(out_path)
+
+    @staticmethod
+    def from_path(file_path: Union[PathLike, str]) -> "Skeleton":
+        """Deprecated. Use Skeleton.load instead."""
+        warn_deprecated("Skeleton.from_path", "Skeleton.load")
+        return Skeleton.load(file_path)
+
+    def write(self, out_path: PathLike) -> None:
+        """Deprecated. Use Skeleton.save instead."""
+        warn_deprecated("Skeleton.write", "skeleton.save")
+        self.save(out_path)
+
+    def __hash__(self) -> int:
+        return id(self)
+
+
+Skeleton._set_init_docstring()
