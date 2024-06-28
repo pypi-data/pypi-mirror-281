62e8592a4e88a7bb6333a5e63b0d4dcbd678afe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 06:05:35 +0000

    Add package currency-quote/2.0.0/currency_quote-2.0.0.tar.gz

---
+++ packages/currency-quote/currency_quote-2.0.0.tar.gz/currency_quote-2.0.0/src/currency_quote/application/use_cases/test_get_history_currency_quote.py
@@ -0,0 +1,8 @@
+from currency_quote.application.use_cases.get_history_currency_quote import GetHistCurrencyQuoteUseCase
+
+
+def test_valid_date():
+    currency_list = ["USD-BRL", "EUR-BRL"]
+    reference_date = 20240625
+    GetHistCurrencyQuoteUseCase.execute(currency_list=currency_list, reference_date=reference_date)
+    assert True
