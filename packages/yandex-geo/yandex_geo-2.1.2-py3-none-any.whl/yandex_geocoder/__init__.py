3606696f68f58d7949b788bba563a6e145818135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 03:05:31 +0000

    Add package yandex-geo/2.1.2/yandex_geo-2.1.2-py3-none-any.whl

---
+++ packages/yandex-geo/yandex_geo-2.1.2-py3-none-any.whl/yandex_geocoder/__init__.py
@@ -0,0 +1,17 @@
+from yandex_geocoder.client import (
+    Client,
+)
+from yandex_geocoder.exceptions import (
+    InvalidKey,
+    NothingFound,
+    UnexpectedResponse,
+    YandexGeocoderException,
+)
+
+__all__ = (
+    "Client",
+    "InvalidKey",
+    "NothingFound",
+    "UnexpectedResponse",
+    "YandexGeocoderException",
+)
