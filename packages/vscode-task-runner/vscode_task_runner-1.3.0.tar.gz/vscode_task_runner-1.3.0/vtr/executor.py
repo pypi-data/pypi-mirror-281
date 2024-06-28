604710aafcf8642205b9677097939cbd7c7135ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 14:05:12 +0000

    Add package vscode-task-runner/1.3.0/vscode_task_runner-1.3.0.tar.gz

---
+++ packages/vscode-task-runner/vscode_task_runner-1.3.0.tar.gz/vscode_task_runner-1.3.0/vtr/executor.py
@@ -0,0 +1,64 @@
+import subprocess
+import sys
+from typing import Dict, List, Optional
+
+import vtr.helpers
+import vtr.printer
+import vtr.variables
+from vtr.task import Task
+
+
+def execute_task(
+    task: Task,
+    index: int,
+    total: int,
+    input_vars_values: Dict[str, str],
+    default_build_task: Optional[str] = None,
+    extra_args: Optional[List[str]] = None,
+) -> None:
+    """
+    Execute a given task. A 1-based index and total number of tasks must be provided
+    for printing.
+    """
+    if task.is_virtual:
+        vtr.printer.info(
+            f'[{index}/{total}] Task "{task.label}" has no direct command to execute'
+        )
+        return
+
+    cmd = vtr.variables.replace_runtime_variables(
+        task.subprocess_command(extra_args), input_vars_values, default_build_task
+    )
+
+    with vtr.printer.group(f"Task {task.label}"):
+        vtr.printer.info(
+            f"[{index}/{total}] Executing task {vtr.printer.yellow(task.label)}: {vtr.printer.blue(vtr.helpers.combine_string(cmd))}"
+        )
+        proc = subprocess.run(
+            cmd,
+            shell=False,
+            cwd=task.cwd,
+            env=task.env,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+        )
+
+        if proc.returncode != 0:
+            vtr.printer.error(
+                f"Task {vtr.printer.yellow(task.label)} returned with exit code {proc.returncode}"
+            )
+            sys.exit(proc.returncode)
+
+
+def collect_task(task: Task, all_tasks: Optional[List[Task]] = None) -> List[Task]:
+    """
+    Given a task, return the given task and all child tasks, recursively.
+    """
+    if all_tasks is None:
+        all_tasks = []
+
+    for child_task in task.depends_on:
+        collect_task(child_task, all_tasks)
+
+    all_tasks.append(task)
+    return all_tasks
