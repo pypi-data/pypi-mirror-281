dc8d83bb5fecc054bcfafe071ac571616cd86dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 15:05:49 +0000

    Add package paddlenlp/3.0.0b0/paddlenlp-3.0.0b0-py3-none-any.whl

---
+++ packages/paddlenlp/paddlenlp-3.0.0b0-py3-none-any.whl/paddlenlp/version/__init__.py
@@ -0,0 +1,25 @@
+# THIS FILE IS GENERATED FROM PADDLENLP SETUP.PY
+commit           = 'a2b8a7835cc8cabe8ed7844de34163448699b3ee.dirty'
+
+__all__ = ['show']
+
+def show():
+    """Get the corresponding commit id of paddlenlp.
+
+    Returns:
+        The commit-id of paddlenlp will be output.
+
+        full_version: version of paddlenlp
+
+
+    Examples:
+        .. code-block:: python
+
+            import paddlenlp
+
+            paddlenlp.version.show()
+            # commit: 1ef5b94a18773bb0b1bba1651526e5f5fc5b16fa
+
+    """
+    print("commit:", commit)
+
