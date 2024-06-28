82cad1b7899c298bc515fd3870c6b533e9194796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-28 16:05:33 +0000

    Add package bffl/0.3.2/bffl-0.3.2.tar.gz

---
+++ packages/bffl/bffl-0.3.2.tar.gz/bffl-0.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bffl',
+    version='0.3.2',
+    author='Ken Seehart',
+    author_email='ken@agi.green',
+    description='A framework for structured bitfield processing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kenseehart/bffl',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "libcst",
+    ],
+)
