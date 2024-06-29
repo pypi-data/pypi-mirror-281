2310d59ece2853513e9c7c8bb40f61a1f232d845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:06:20 +0000

    Add package bclib/3.30.0/bclib-3.30.0.tar.gz

---
+++ packages/bclib/bclib-3.30.0.tar.gz/bclib-3.30.0/bclib/dispatcher/dev_server_dispatcher.py
@@ -0,0 +1,17 @@
+from ..dispatcher.socket_dispatcher import RoutingDispatcher
+from bclib.listener import Endpoint,  HttpListener
+
+
+class DevServerDispatcher(RoutingDispatcher):
+    def __init__(self, options: dict):
+        super().__init__(options)
+        self.__listener = HttpListener(
+            Endpoint(self.options.server),
+            self._on_message_receive_async,
+            self.options.ssl,
+            self.options.configuration
+            )
+
+    def initialize_task(self):
+        super().initialize_task()
+        self.__listener.initialize_task(self.event_loop)
