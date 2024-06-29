789049338eae1bcffc300021486850a103697852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:06:05 +0000

    Add package chemistry-tools/1.1.1/chemistry_tools-1.1.1-py3-none-any.whl

---
+++ packages/chemistry-tools/chemistry_tools-1.1.1-py3-none-any.whl/chemistry_tools/pubchem/__init__.py
@@ -0,0 +1,80 @@
+#  !/usr/bin/env python
+#
+#  __init__.py
+r"""
+This module provides a wrapper around the PubChem PUG_REST API.
+
+Data for compounds can be accessed using the
+:func:`pubchem.lookup.get_compounds <chemistry_tools.pubchem.lookup.get_compounds>` function.
+
+.. _properties table:
+
+The following table lists the various properties that can be obtained from the PubChem API:
+
+.. tabularcolumns:: |l|>{\raggedright\arraybackslash}p{10cm}|
+
+.. collapse:: Click to expand
+
+	.. properties-table::
+
+"""
+#
+#  Copyright (c) 2019-2021 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as
+#  published by the Free Software Foundation; either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#  Based on PubChemPy https://github.com/mcs07/PubChemPy/blob/master/LICENSE
+#  |  Copyright 2014 Matt Swain <m.swain@me.com>
+#  |  Licensed under the MIT License
+#  |
+#  |  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  |  of this software and associated documentation files (the "Software"), to deal
+#  |  in the Software without restriction, including without limitation the rights
+#  |  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  |  copies of the Software, and to permit persons to whom the Software is
+#  |  furnished to do so, subject to the following conditions:
+#  |
+#  |  The above copyright notice and this permission notice shall be included in
+#  |  all copies or substantial portions of the Software.
+#  |
+#  |  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  |  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  |  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  |  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  |  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  |  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  |  THE SOFTWARE.
+#
+
+# 3rd party
+from apeye.requests_url import RequestsURL
+
+# this package
+from chemistry_tools.cache import cached_requests
+
+# this package
+from . import atom, bond, enums, errors
+
+API_BASE = RequestsURL("https://pubchem.ncbi.nlm.nih.gov/rest/pug")
+API_BASE.session = cached_requests
+
+__all__ = [
+		"API_BASE",
+		"atom",
+		"bond",
+		"enums",
+		"errors",
+		]
