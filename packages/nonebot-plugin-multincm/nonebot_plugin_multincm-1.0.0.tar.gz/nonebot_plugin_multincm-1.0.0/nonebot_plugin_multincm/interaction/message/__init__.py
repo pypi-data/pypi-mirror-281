8ed013b489d06b2f4c57a255fcc1653c34bbdd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:05:05 +0000

    Add package nonebot-plugin-multincm/1.0.0/nonebot_plugin_multincm-1.0.0.tar.gz

---
+++ packages/nonebot-plugin-multincm/nonebot_plugin_multincm-1.0.0.tar.gz/nonebot_plugin_multincm-1.0.0/nonebot_plugin_multincm/interaction/message/__init__.py
@@ -0,0 +1,13 @@
+from .common import (
+    construct_info_msg as construct_info_msg,
+    send_song as send_song,
+)
+from .song_card import (
+    get_card_sendable_adapter_type as get_card_sendable_adapter_type,
+    get_song_card_msg as get_song_card_msg,
+    is_card_sendable_adapter as is_card_sendable_adapter,
+)
+from .song_file import (
+    download_song as download_song,
+    send_song_media as send_song_media,
+)
