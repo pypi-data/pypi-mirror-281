394e9ebc204576a7bd2bb3c045aaa9cdb5b1017a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:05:46 +0000

    Add package uugai-python-color-prediction/3.0.0/uugai_python_color_prediction-3.0.0.tar.gz

---
+++ packages/uugai-python-color-prediction/uugai_python_color_prediction-3.0.0.tar.gz/uugai_python_color_prediction-3.0.0/README.md
@@ -0,0 +1,89 @@
+# Python Color Prediction
+This project aims to predict the color of an image using Python. It utilizes machine learning algorithms, specifically k-means clustering, to analyze the pixel values of an image and predict the dominant color.
+
+K-means clustering is a popular unsupervised learning algorithm used for clustering data points into groups. In the context of color prediction, k-means clustering is used to group similar pixel values together, allowing us to identify the dominant color in an image. The algorithm works by iteratively assigning each pixel to the cluster with the closest centroid, and then updating the centroids based on the new assignments. This process continues until the centroids no longer change significantly. By applying k-means clustering to the pixel values of an image, we can identify the cluster centroids, which represent the dominant colors. To use this project, simply follow the installation instructions below. 
+
+## Installation
+
+To get started with the Python color prediction function, simply install the package using pip and follow the provided documentation for configuration and usage instructions.
+Thereafter, import the function into your own python file. The package can be found on [PyPi](https://pypi.org/project/uugai-python-color-prediction/).
+
+* installation using pip: `pip install uugai_python_color_prediction`
+* python import: `from uugai_python_color_prediction.ColorPrediction import ColorPrediction`
+
+## Usage
+The find_main_colors function is used to predict the dominant colors of an image. It takes several parameters:
+
+* image: The input image for color prediction, using `cv2.imread(IMAGE_PATH)`.
+* coding: The colorsystem used, supported is BGR (standard), RGB, BGRA and RGBA.
+* min_clusters and max_clusters: The minimum and maximum number of clusters to use for k-means clustering.
+* downsample_factor: A factor used to downsample the image before color prediction. Default value is 0.
+* increase_elbow: An optional parameter to increase/decrease the elbow point in the k-means clustering algorithm. Default value is 0.
+* verbose: A boolean value indicating whether to print verbose output during color prediction. Default value is False.
+* plot: A boolean value indicating whether to plot the color prediction results. Default value is False.
+
+To use the find_main_colors function, simply pass the required parameters and call the function. The function will return all the kmeans cluster data and optimal k-value.
+The kmeans-data looks something like this:
+```
+{
+1: 
+    {'inertias': 18358588.71525097, 
+    'centroids': array([[128, 108, 100]]), 
+    'percentages': array([100.])}, 
+2: 
+    {'inertias': 4514844.78507838, 
+    'centroids': array([[ 85,  93,  71], [227, 143, 167]]), 
+    'percentages': array([69.8, 30.2])}
+...
+}
+```
+
+### Alpha channel additonal details
+If RGBA or BGRA is used, pixels with full transparancy are removed for clustering. This allows the color detection to work with for example masked images from segmentation models. For pixels with alpha values different from 0, the color channels are used for clustering in 3D space. This means pixels with the same color but altering alpha values are classified as identical.
+
+### No elbow point found
+If no elbow point can be found, the function will return an optimal cluster value of None.
+
+## Usage example
+```Python
+from uugai_python_color_prediction.ColorPrediction import ColorPrediction
+
+IMAGE_PATH = 'python_color_prediction/data/flowers.jpeg'
+
+# Read the image
+img = cv2.imread(IMAGE_PATH, cv2.IMREAD_UNCHANGED)
+
+# Call the elbow_method function
+optimal_k, kmeans_data = ColorPrediction.find_main_colors(
+    image = img,
+    coding = 'RGB'
+    min_clusters=1,
+    max_clusters=8, 
+    downsample_factor=0.98, 
+    increase_elbow=0, 
+    verbose=True, 
+    plot=True)
+```
+
+This would result in the following output, with `verbose = True`:
+
+```
+Calculating KMeans for k=1...
+Calculating KMeans for k=2...
+Calculating KMeans for k=3...
+Calculating KMeans for k=4...
+Calculating KMeans for k=5...
+Calculating KMeans for k=6...
+Calculating KMeans for k=7...
+Calculating KMeans for k=8...
+
+The optimal number of clusters is: 2
+Color: [227 143 167], Percentage: 30.2%
+Color: [85 93 71], Percentage: 69.8%
+```
+
+The plotting parameter enables the creation of a KMeans-clustering inertia plot, which is used for the elbow method. Additionally, it generates a 3D scatter plot of the pixel values, displaying the cluster centroids in their corresponding colors. Finally, it includes a small plot showing the centroid colors along with their respective percentages.
+
+<div style="text-align:center;">
+    <img src="./uugai_python_color_prediction/data/plot_flowers.png" alt="Flowers plot" width=1000px>
+</div>
---
+++ packages/uugai-python-color-prediction/uugai_python_color_prediction-3.0.0.tar.gz/uugai_python_color_prediction-3.0.0/uugai_python_color_prediction/ColorPrediction.py
@@ -0,0 +1,344 @@
+import cv2
+import numpy as np
+from kneed import KneeLocator
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import time
+
+
+
+class ColorPrediction:
+    def __init__(self):
+        pass
+
+
+    def find_main_colors(image, coding : str = 'BGR', min_clusters : int = 1, max_clusters : int = 8, downsample_factor : float = 0, increase_elbow : int = 0, verbose : bool = False, plot : bool = False):
+        """Function to find the optimal number of clusters using the elbow method.
+        
+        Parameters:
+        ----------
+        data: numpy array
+            The data points to cluster.
+        min_clusters: int
+            The minimum number of clusters to consider.
+        max_clusters: int
+            The maximum number of clusters to consider.
+        downsample_factor: float
+            The factor to downsample the image by.
+        increase_elbow: int
+            The number of clusters to increase the elbow point by.
+        verbose: bool
+            Whether to print the optimal values.
+        plot: bool
+            Whether to plot the elbow plot and the optimal centroids.
+
+        """
+
+        # Downsample the image if the downsample factor is not 0
+        start_time_downsample = time.time()
+        if downsample_factor != 0:
+            image = ColorPrediction.downsample_image(image=image, scale_factor=downsample_factor)
+        else:
+            image = image
+        end_time_downsample = time.time()
+
+
+        # Depending on the coding, create the data and image arrays.
+        start_time_preprocessing = time.time()
+        image, data = ColorPrediction.create_data_and_image(
+            in_image=image,
+            coding=coding)
+        end_time_preprocessing = time.time()
+
+
+        # Calculate the inertia values, percentages, and centroids with the KMeans algorithm
+        start_time_kmeans = time.time()
+        kmeans_data = ColorPrediction.kmeans_inertias(
+            data=data,
+            min_clusters=min_clusters,
+            max_clusters=max_clusters,
+            verbose = verbose)
+        end_time_kmeans = time.time()
+
+
+        # Find the optimal k-clusters value using the elbow method
+        start_time_elbow = time.time()
+        if min_clusters == max_clusters:
+            optimal_k = min_clusters
+        else:
+            optimal_k = ColorPrediction.find_elbow_point(
+                inertias=[kmeans_data[k]['inertias'] for k in kmeans_data.keys()],
+                min_clusters=min_clusters,
+                max_clusters=max_clusters)
+        end_time_elbow = time.time()
+
+        # Print the optimal values
+        if verbose:
+            if optimal_k is not None:
+                print(f"\nThe optimal number of clusters is: {optimal_k}")
+                for color, percentage in zip(kmeans_data[optimal_k]['centroids'], kmeans_data[optimal_k]['percentages']):
+                    print(f"Color: {color}, Percentage: {percentage}%")
+
+            
+                if increase_elbow != 0:
+                    print(f"\nIncreasing the elbow point by {increase_elbow} cluster(s):")
+                    print(f"New number of clusters is: {optimal_k + increase_elbow}")
+                    for color, percentage in zip(kmeans_data[optimal_k + increase_elbow]['centroids'], kmeans_data[optimal_k + increase_elbow]['percentages']):
+                        print(f"Color: {color}, Percentage: {percentage}%")
+
+                print(f"\nTime taken to downsample the image: {end_time_downsample - start_time_downsample:.2f} seconds")
+                print(f"Time taken to preprocess the image: {end_time_preprocessing - start_time_preprocessing:.2f} seconds")
+                print(f"Time taken to calculate the KMeans-inertias: {end_time_kmeans - start_time_kmeans:.2f} seconds")
+                print(f"Time taken to find the elbow point: {end_time_elbow - start_time_elbow:.2f} seconds")
+
+            else:
+                print("No elbow point found. Please try again with different parameters.")
+        
+
+        # Plot the inertia values, RGB squares, and RGB scatterplot
+        if plot and optimal_k is not None:
+            ColorPrediction.plot_kmeans_inertias(kmeans_data)
+            ColorPrediction.plot_bgr_squares(kmeans_data, optimal_k + increase_elbow)
+            ColorPrediction.plot_bgr_scatter(data, kmeans_data, optimal_k + increase_elbow)
+            plt.show()
+            
+        return optimal_k, kmeans_data
+    
+
+    def downsample_image(image, scale_factor):
+        """ Downsample the given image by the specified scale factor.
+
+        Parameters:
+        ----------
+        image: numpy array
+            The image to downsample.
+        scale_factor: float
+            The scale factor to downsample the image by.
+
+        """
+
+        # Calculate the new dimensions
+        new_width = int(image.shape[1] * (1 - scale_factor))
+        new_height = int(image.shape[0] * (1 - scale_factor))
+        new_dimensions = (new_width, new_height)
+
+        # Downsample the image
+        downsampled_image = cv2.resize(image, new_dimensions, interpolation=cv2.INTER_AREA)
+        return downsampled_image
+
+
+    def create_data_and_image(in_image, coding):
+        """ Function to create the data and image arrays based on the input image and coding.
+
+        Parameters:
+        ----------
+        in_image: numpy array
+            The input image.
+        coding: str
+            The coding of the image, either 'BGR', 'RGB', 'BGRA', or 'RGBA'.
+
+        """
+
+        # If the image is in BGR format, just return the image and data back
+        if coding == 'BGR':
+            image = in_image
+            data = image.reshape(-1, 3)
+            return image, data
+        
+        # If the image is in RGB format, convert it to BGR and return the image and data back
+        elif coding == 'RGB':
+            image = cv2.cvtColor(in_image, cv2.COLOR_RGB2BGR)
+            data = image.reshape(-1, 3)
+            return image, data
+        
+        # If the image is in BGRA format, convert it to BGR and return the image.
+        # Also, remove the transparant data points from the data array.
+        elif coding == 'BGRA':
+            image = cv2.cvtColor(in_image, cv2.COLOR_BGRA2BGR)
+
+            data = in_image.reshape(-1, 4)
+            non_transparant_data = data[data[:, 3] != 0]
+            bgr_data = non_transparant_data[:, :3]
+
+            return image, bgr_data
+        
+        # If the image is in RGBA format, convert it to BGR and return the image.
+        # Also, remove the transparant data points from the data array, and convert the RGB data to BGR.
+        elif coding == 'RGBA':
+            image = cv2.cvtColor(in_image, cv2.COLOR_RGBA2BGR)
+
+            data = in_image.reshape(-1, 4)
+            non_transparant_data = data[data[:, 3] != 0]
+            rgb_data = non_transparant_data[:, :3]
+            bgr_data = rgb_data[:, [2, 1, 0]]
+
+            return image, bgr_data
+
+        # If the coding is invalid, raise a ValueError
+        else:
+            return ValueError("Invalid coding. Please choose one of the following: 'BGR', 'RGB', 'BGRA', 'RGBA'")        
+        
+
+    def kmeans_inertias(data, min_clusters, max_clusters, verbose = False):
+        """ Function to calculate the KMeans-inertia values for different k values.
+        
+        Parameters:
+        ----------
+        data: numpy array
+            The data points to cluster.
+        min_clusters: int
+            The minimum number of clusters to consider.
+        max_clusters: int
+            The maximum number of clusters to consider.
+        
+        """
+
+        # Initialize lists to store the inertias, percentages, and centroids
+
+        kmeans_data = {}
+
+        # Iterate over the range of k values, +1 to include max_clusters
+        for k in range(min_clusters, max_clusters + 1):
+
+            if verbose:
+                print(f"Calculating KMeans for k={k}...")
+
+            kclusters_data = {
+            "inertias": [],
+            "centroids": [],
+            "percentages": []
+            }
+
+            # Initialise the KMeans algorithm and fit the data
+            kmeans = KMeans(n_clusters=k, algorithm='lloyd')
+            kmeans.fit(data)
+
+            # Store the inertia, centroids, and percentages in the lists for each k value
+            kclusters_data['inertias'] = kmeans.inertia_
+            kclusters_data['centroids'] = kmeans.cluster_centers_.astype(int)
+            _, counts = np.unique(kmeans.labels_, return_counts=True)
+            kclusters_data['percentages'] = (counts / len(data) * 100).round(1)
+
+            # Store the data in the kmeans_data dictionary
+            kmeans_data[k] = kclusters_data
+
+        return kmeans_data
+
+
+    def find_elbow_point(inertias, min_clusters, max_clusters):
+        """ Function to find the elbow point in the inertia plot.
+
+        Parameters:
+        ----------
+        inertias: list
+            The list of inertia values for different k values.
+        min_k: int  
+            The minimum number of clusters to consider.
+        max_k: int
+            The maximum number of clusters to consider.
+        
+        """
+
+        # Use the KneeLocator to find the elbow point
+        kn = KneeLocator(
+            x = range(min_clusters, max_clusters + 1),
+            y = inertias,
+            S=2.0,
+            curve='convex', 
+            direction='decreasing')
+        
+        # Return the elbow point, if no elbow point is found, return None
+        return kn.elbow
+    
+
+    def plot_kmeans_inertias(kmeans_data):
+        """ Function to plot the KMeans inertia values for different k values.
+
+        Parameters:
+        ----------
+        kmeans_data: dict
+            The dictionary containing the KMeans data.
+        
+        """
+
+        plt.figure(figsize=(8, 4))
+        plt.plot(
+            kmeans_data.keys(),
+            [kmeans_data[k]["inertias"] for k in kmeans_data.keys()],
+            'bx-')
+        
+        plt.xlabel('Number of clusters')
+        plt.ylabel('Inertia')
+        plt.title('Inertias from KMeans-clustering technique used in the elbow method to find the optimal k-value')
+
+
+    def plot_bgr_squares(kmeans_data, k, square_size=20):
+        """ Function to plot RGB color squares with text labels (percentages).
+
+        Parameters:
+        ----------
+        kmeans_data: dict
+            The dictionary containing the KMeans data.
+        k: int
+            The number of clusters to plot.
+        square_size: int
+            The size of the square to plot.
+
+        """
+        
+        # Create an image array with the BGR colors
+        image_array = np.zeros((square_size, square_size * k, 3), dtype=np.uint8)
+        
+        for i, color in enumerate(kmeans_data[k]['centroids']):
+            image_array[:, i*square_size:(i+1)*square_size, :] = color
+        
+        # Plot the image array
+        fig, ax = plt.subplots(figsize=(k, 2))
+        ax.imshow(image_array)
+        ax.axis('off')
+        
+        # Add text labels
+        for i, text in enumerate(kmeans_data[k]['percentages']):
+            ax.text(
+                (i + 0.5) * square_size,                                            # x position
+                square_size / 2,                                                    # y position
+                str(text)+'%',                                                      # text
+                color='black',                                                      # text color
+                fontsize=12,                                                        # text size
+                ha='center',                                                        # horizontal alignment
+                va='center',                                                        # vertical alignment
+                bbox=dict(facecolor='white', alpha=0.5, boxstyle='round,pad=0.3'))  # background
+                
+
+    # Function to create a 3D scatter plot of the RGB color space with centroids 
+    def plot_bgr_scatter(image_data, kmeans_data, optimal_k):
+        """ Function to create a 3D scatter plot of the RGB color space, optionally add centroids.
+
+        Parameters:
+        ----------
+        image_data: numpy array
+            The image data to plot.
+        kmeans_data: dict
+            The dictionary containing the KMeans data.
+        optimal_k: int
+            The optimal number of clusters to plot.
+        
+        """
+
+        # Create a matplotlib figure and axis
+        fig = plt.figure(figsize=(10, 8))
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Normalise the image data and plot the scatter plot, these are the pixels in the image
+        normalised_image_data = image_data / 255.0
+        ax.scatter(normalised_image_data[:, 0], normalised_image_data[:, 1], normalised_image_data[:, 2], c=normalised_image_data, marker='o')
+
+        # Normalise the centroids and plot the centroids, these are the main colors in the image
+        normalised_centroids = kmeans_data[optimal_k]['centroids']/255
+        ax.scatter(normalised_centroids[:, 0], normalised_centroids[:, 1], normalised_centroids[:, 2], color=normalised_centroids, s=2000, alpha=0.5, edgecolors='w', linewidths=2)
+                
+        # Set the axis labels and title
+        ax.set_xlabel('Blue')
+        ax.set_ylabel('Green')
+        ax.set_zlabel('Red')
+        ax.set_title('3D BGR color-space with centroids')
