2206413c627fc8dfcbac1eb4eedea002b774bd14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:06:01 +0000

    Add package THISDLChatBot/2.0.13/THISDLChatBot-2.0.13-py3-none-any.whl

---
+++ packages/THISDLChatBot/THISDLChatBot-2.0.13-py3-none-any.whl/THISDLChatBot/Friend.py
@@ -0,0 +1,21 @@
+class Friend:
+    def __init__(self, profile: dict) -> None:
+        self.profile = profile
+        pass
+
+    def __str__(self):
+        return str(self.profile)
+
+    def get_user_id(self) -> str:
+        return self.profile['userId']
+
+    def get_login_name(self) -> str:
+        return self.profile['loginName']
+
+    def get_nick_name(self) -> str:
+        return self.profile['nickname']
+
+    def get_avatar_url(self) -> str:
+        return ('http://chat.thisit.cc/index.php?action=http.file.downloadFile&fileId='
+                + self.profile["avatar"]
+                + '&returnBase64=0&lang=1')
---
+++ packages/THISDLChatBot/THISDLChatBot-2.0.13-py3-none-any.whl/THISDLChatBot/Plugin.py
@@ -0,0 +1,40 @@
+from .CommandProcessor import CommandProcessor
+from .MessageProcessor import MessageProcessor
+
+
+class Plugin:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.CommandProcessors = []
+        self.MessageProcessors = []
+        self.bot = None
+
+    async def onload(self):
+        pass
+
+    def on_command(self, command: str):
+        def command_processor(func):
+            def wrapper():
+                self.CommandProcessors.append(CommandProcessor(command, func))
+
+            return wrapper()
+
+        return command_processor
+
+    def on_message(self):
+        def message_processor(func):
+            def wrapper():
+                self.MessageProcessors.append(MessageProcessor(func))
+
+            return wrapper()
+
+        return message_processor
+
+
+def load_plugin(plugin: str) -> Plugin:
+    pl = __import__(plugin)
+    if '.' not in plugin:
+        return pl.plugin
+    for package in plugin.split('.')[1:]:
+        pl = getattr(pl, package)
+    return pl.plugin
