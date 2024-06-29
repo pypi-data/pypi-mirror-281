1c560fc7bdc453ea092927113d3726c545c7bc03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:05:04 +0000

    Add package galaxy-app/24.0.3/galaxy_app-24.0.3-py3-none-any.whl

---
+++ packages/galaxy-app/galaxy_app-24.0.3-py3-none-any.whl/galaxy/tools/expressions/evaluation.py
@@ -0,0 +1,51 @@
+import json
+import os
+import subprocess
+from typing import MutableMapping
+
+from cwl_utils.expression import do_eval as _do_eval
+
+from .util import find_engine
+
+FILE_DIRECTORY = os.path.normpath(os.path.dirname(os.path.join(__file__)))
+NODE_ENGINE = os.path.join(FILE_DIRECTORY, "cwlNodeEngine.js")
+
+
+def do_eval(expression: str, context: MutableMapping):
+    return _do_eval(
+        expression,
+        context,
+        [{"class": "InlineJavascriptRequirement"}],
+        None,
+        None,
+        {},
+        cwlVersion="v1.2.1",
+    )
+
+
+def evaluate(config, input):
+    application = find_engine(config)
+
+    default_context = {
+        "engineConfig": [],
+        "job": {},
+        "context": None,
+        "outdir": None,
+        "tmpdir": None,
+    }
+
+    new_input = default_context
+    new_input.update(input)
+
+    sp = subprocess.Popen(
+        [application, NODE_ENGINE], shell=False, close_fds=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE
+    )
+    input_str = f"{json.dumps(new_input)}\n\n"
+    input_bytes = input_str.encode("utf-8")
+    (stdoutdata, stderrdata) = sp.communicate(input_bytes)
+    if sp.returncode != 0:
+        message = f"Expression engine returned non-zero exit code on evaluation of\n{json.dumps(new_input, indent=4)}{stdoutdata}{stderrdata}"
+        raise Exception(message)
+
+    rval_raw = stdoutdata.decode("utf-8")
+    return json.loads(rval_raw)
