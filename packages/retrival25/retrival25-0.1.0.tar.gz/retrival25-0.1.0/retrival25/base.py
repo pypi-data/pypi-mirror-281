3d0f9bf024cb83fe56fc9af963cb276d3f500aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-06-29 03:05:36 +0000

    Add package retrival25/0.1.0/retrival25-0.1.0.tar.gz

---
+++ packages/retrival25/retrival25-0.1.0.tar.gz/retrival25-0.1.0/retrival25/base.py
@@ -0,0 +1,57 @@
+from abc import abstractmethod
+from tok import word_tokenize
+from collections import defaultdict
+import copy
+import math
+
+
+class bm25:
+
+    def __init__(self, document_corpus: list) -> None:
+        self.corpus = dict(enumerate(document_corpus))
+        self.corpus_copy = copy.deepcopy(self.corpus)
+        self.corpus = {i: word_tokenize(doc) for i, doc in self.corpus.items()}
+        self.__initalise_variables__()
+
+    def __initalise_variables__(self):
+        self.number_document = max(self.corpus.keys()) + 1
+
+        len_of_doc = [len(doc) for doc in self.corpus.values()]
+        self.avg_tok_doc = sum(len_of_doc) / self.number_document
+
+        self.term_doc_freq = defaultdict(int)
+        for doc in self.corpus.values():
+            # term_freq = Counter(doc)
+            for term in set(doc):
+                self.term_doc_freq[term] += 1
+        # self.term_doc_freq = {
+        #     term: math.log(self.number_document / freq)
+        #     for term, freq in self.term_doc_freq.items()
+        # }
+        # self.term_doc_freq = defaultdict(lambda: 1, self.term_doc_freq)
+
+    @abstractmethod
+    def idf(self):
+        pass
+
+    @abstractmethod
+    def tf(self):
+        pass
+
+    def score(self, query: list, doc: list) -> float:
+        return sum([self.idf(term) * self.tf(term, doc) for term in query])
+
+    def get_top_n(self, query: str, n=5) -> dict:
+        """Retrive top n document from corpus"""
+        toknised_query = word_tokenize(query)
+        scores = {
+            id: [doc, self.score(toknised_query, doc)]
+            for id, doc in self.corpus.items()
+        }
+        sorted_scores = [
+            [self.corpus_copy[id], v[1]]
+            for id, v in sorted(
+                scores.items(), key=lambda item: item[1][1], reverse=True
+            )[:n]
+        ]
+        return sorted_scores
---
+++ packages/retrival25/retrival25-0.1.0.tar.gz/retrival25-0.1.0/retrival25/rank.py
@@ -0,0 +1,30 @@
+from retrival25.robertson import robertson
+from retrival25.atire_bm25 import atire_bm25
+from retrival25.bm25L import bm25L
+from retrival25.bm25_plus import bm25_plus
+from retrival25.bm25_adpt import bm25_adbt
+
+VARIANTS = {
+    "robertson": robertson,
+    "atire_bm25": atire_bm25,
+    "bm25_L": bm25L,
+    "bm25_plus": bm25_plus,
+    "bm25_adbt": bm25_adbt,
+}
+
+
+class ranker:
+    def __init__(
+        self,
+        corpus: list,
+        k: float = 1.2,
+        b: float = 0.75,
+        epsilon: float = 1,
+        type: str = "robertson",
+    ) -> None:
+        self.bm25_obj = VARIANTS[type](
+            document_corpus=corpus, b=b, k=k, epsilon=epsilon
+        )
+
+    def get_top_n(self, query: str, n=5):
+        return self.bm25_obj.get_top_n(query, n)
